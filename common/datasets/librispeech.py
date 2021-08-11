cbaf51dc6aefed63da13f8500b80f016b1b592ca
Wilfried Michel <michel@i6.informatik.rwth-aachen.de>
2021-08-11 15:05:39 +0200

    allow for g2p augmented Librispeech lexicon with stress markers

--- common/datasets/librispeech.py
+++ common/datasets/librispeech.py
@@ -393,7 +393,7 @@ def get_bliss_lexicon(use_stress_marker=False, subdir_prefix=""):
     return merge_lexicon_job.out_bliss_lexicon
 
 
-def get_g2p_augmented_bliss_lexicon_dict(subdir_prefix=""):
+def get_g2p_augmented_bliss_lexicon_dict(use_stress_marker=False, subdir_prefix=""):
     """
     Given the original LibriSpeech bliss lexicon, it is possible to estimate the pronunciation for
     out of vocabulary (OOV) words for each of the LibriSpeech training corpora. Here, we create a dictionary
@@ -406,7 +406,9 @@ def get_g2p_augmented_bliss_lexicon_dict(subdir_prefix=""):
     alias_path = os.path.join(subdir_prefix, "LibriSpeech", "lexicon")
     augmented_bliss_lexica = {}
 
-    original_bliss_lexicon = get_bliss_lexicon(subdir_prefix=subdir_prefix)
+    original_bliss_lexicon = get_bliss_lexicon(
+        use_stress_marker=use_stress_marker, subdir_prefix=subdir_prefix
+    )
     g2p_augmenter = G2PBasedOovAugmenter(original_bliss_lexicon=original_bliss_lexicon)
 
     bliss_corpus_dict = get_bliss_corpus_dict(subdir_prefix=subdir_prefix)
