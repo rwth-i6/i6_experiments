8525f1b3726ce941af6060c8cc9a33aa58f071e7
JanEnglerRWTH <63725985+JanEnglerRWTH@users.noreply.github.com>
2021-09-17 11:29:43 +0200

    Fairseq hydra training job (#21)
    * Fairseq hydra training job
    
    * Changing all the requested parts from the pull request. Most notably removing the dict to yaml job and replacing it with the class FairseqHydraConfig
    
    * Update users/engler/fairseq/training.py
    
    Co-authored-by: vieting <45091115+vieting@users.noreply.github.com>
    
    * Second round of fixes
    
    * Added functionalty to store a checkpoint every n-epochs. Furthermore, added self.out_models and keep_epochs similar to ReturnnTrainingJob
    
    Co-authored-by: vieting <45091115+vieting@users.noreply.github.com>

---
+++ users/engler/fairseq/training.py
@@ -0,0 +1,128 @@
+import os
+import subprocess as sp
+import yaml
+
+from sisyphus import *
+
+import recipe.i6_core.util as util
+
+
+class FairseqHydraConfig:
+    """
+    An object that manages a Fairseq hydra config (inspired by the ReturnnConfig).
+    """
+
+    def __init__(self, config_dict, *, yaml_prefix=""):
+        """
+        :param dict config_dict: Contains the information which is needed for fairseq-hydra-train. Will be converted and dumped into a .yaml
+        :param str yaml_prefix: Prefix which should be written to the beginning of the config, for example "# @package _group_"
+        """
+        assert isinstance(config_dict, dict)
+        self.config_dict = config_dict
+        self.yaml_prefix = yaml_prefix
+
+    def write(self, path):
+        config_yaml = yaml.dump(self.config_dict)
+        # "# @package _group_" was written at the beginning in the example .yaml from fairseq:
+        if self.yaml_prefix != "":
+            config_yaml = self.yaml_prefix + "\n" + config_yaml
+        with open(path, "w") as file:
+            file.write(config_yaml)
+
+
+class FairseqHydraTrainingJob(Job):
+    """
+    Train a Fairseq model using fairseq-hydra-train
+    """
+
+    def __init__(
+        self,
+        fairseq_hydra_config,
+        *,  # args below are keyword only
+        command_line_args=None,
+        max_epoch=1,
+        save_interval=1,
+        keep_epochs=None,
+        time_rqmt=4,
+        mem_rqmt=4,
+        cpu_rqmt=2,
+        gpu_rqmt=1,
+        fairseq_python_exe=None,
+        fairseq_hydra_exe=None,
+    ):
+        """
+        :param FairseqHydraConfig fairseq_hydra_config:
+        :param list command_line_args: Additional command line arguments (starting with "--*") to configure the Fairseq-hydra task
+        :param int max_epoch: maximum number of epochs to run. Note that this value IS currently HASHED.
+        :param int save_interval: save a checkpoint each n-th epoch
+        :param list[int]|set[int]|None keep_epochs: specify which checkpoints are kept in self.out_models, use None for each save_interval-th epoch
+        :param int|float time_rqmt: Overall time requirements
+        :param int|float mem_rqmt: Memory requirements (per GPU)
+        :param int cpu_rqmt: Required number of CPUs (per GPU)
+        :param int gpu_rqmt: Number of required GPUs
+        :param Path|str python_exe: File path to the executable for running python
+        :param Path|str fairseq_hydra_exe: File path to the Fairseq executable: fairseq-hydra-train (usually in the same folder as python exe '.../bin/')
+
+        """
+        self.fairseq_hydra_config = fairseq_hydra_config
+        self.command_line_args = command_line_args or []
+
+        stored_epochs = list(range(save_interval, max_epoch, save_interval)) + [
+            max_epoch
+        ]
+        if keep_epochs is None:
+            self.keep_epochs = set(stored_epochs)
+        else:
+            self.keep_epochs = set(keep_epochs)
+        self.max_epoch = max_epoch
+        self.save_interval = save_interval
+        self.out_fairseq_hydra_yaml = self.output_path("fairseq_hydra_config.yaml")
+        self.out_checkpoint_dir = self.output_path("checkpoints", directory=True)
+        self.out_models = {
+            k: str(self.out_checkpoint_dir) + "/checkpoint{}.pt".format(k)
+            for k in stored_epochs
+            if k in self.keep_epochs
+        }
+
+        self.gpu_rqmt = gpu_rqmt
+        self.fairseq_python_exe = fairseq_python_exe
+        self.fairseq_hydra_exe = (
+            fairseq_hydra_exe if fairseq_hydra_exe is not None else gs.FAIRSEQ_HYDRA_EXE
+        )
+        self.rqmt = {
+            "gpu": gpu_rqmt,
+            "cpu": cpu_rqmt,
+            "mem": mem_rqmt,
+            "time": time_rqmt,
+        }
+
+        if self.gpu_rqmt > 1:
+            self.rqmt["cpu"] *= self.gpu_rqmt
+            self.rqmt["mem"] *= self.gpu_rqmt
+
+    def tasks(self):
+        yield Task("create_files", mini_task=True)
+        yield Task("run", resume="run", rqmt=self.rqmt)
+
+    def create_files(self):
+        self.fairseq_hydra_config.write(self.out_fairseq_hydra_yaml.get_path())
+        util.create_executable("fairseq.sh", self._get_run_cmd())
+
+    def run(self):
+        sp.check_call(self._get_run_cmd())
+
+    def _get_run_cmd(self):
+        run_cmd = [
+            tk.uncached_path(self.fairseq_hydra_exe),
+            "--config-dir",
+            os.path.dirname(self.out_fairseq_hydra_yaml.get_path()),
+            "--config-name",
+            os.path.basename(self.out_fairseq_hydra_yaml.get_path()),
+        ]
+        run_cmd += self.command_line_args
+        run_cmd += ["checkpoint.save_dir=" + str(self.out_checkpoint_dir)]
+        run_cmd += ["checkpoint.save_interval=" + str(self.save_interval)]
+        run_cmd += ["optimization.max_epoch=" + str(self.max_epoch)]
+        if self.fairseq_python_exe is not None:
+            run_cmd.insert(0, tk.uncached_path(self.fairseq_python_exe))
+        return run_cmd
