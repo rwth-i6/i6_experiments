c65f7fa0b7b14bd07729c39ff9512b5b4d8d7f35
JanEnglerRWTH <jan.engler@rwth-aachen.de>
2021-10-15 12:14:51 +0200

    Fixed the things that were mentioned in the PR#2, will rebase in the next step

--- users/engler/fairseq/training.py
+++ users/engler/fairseq/training.py
@@ -67,26 +67,31 @@ class FairseqHydraTrainingJob(Job):
         gpu_rqmt=1,
         fairseq_python_exe=None,
         fairseq_hydra_exe=None,
-        fairseq_git=None,
+        fairseq_root=None,
     ):
         """
         :param FairseqHydraConfig fairseq_hydra_config:
-        :param list command_line_args: Additional command line arguments (starting with "--*") to configure the Fairseq-hydra task
+        :param list command_line_args: Additional command line arguments (starting with "--*"),
+            to configure the Fairseq-hydra task
         :param int max_epoch: maximum number of epochs to run. Note that this value IS currently HASHED.
         :param int save_interval: save a checkpoint each n-th epoch
-        :param list[int]|set[int]|None keep_epochs: specify which checkpoints are kept in self.out_models, use None for each save_interval-th epoch
+        :param list[int]|set[int]|None keep_epochs: specify which checkpoints are kept in self.out_models.
+            Use None for each save_interval-th epoch
         :param int|float time_rqmt: Overall time requirements
         :param int|float mem_rqmt: Memory requirements (per GPU)
         :param int cpu_rqmt: Required number of CPUs (per GPU)
         :param int gpu_rqmt: Number of required GPUs
         :param Path|str python_exe: File path to the executable for running python
-        :param Path|str fairseq_hydra_exe: File path to the Fairseq executable: fairseq-hydra-train (usually in the same folder as python exe '.../bin/')
-        :param Path|str fairseq_git: File path to the fairseq git for alternative call of fairseq-hydra-train (no need to install fairseq here)
+        :param Path|str fairseq_hydra_exe: File path to the python executable for running fairseq-hydra-train.
+            (usually in the same folder as python exe '.../bin/')
+        :param Path|str fairseq_root: File path to the fairseq git for alternative call of fairseq-hydra-train
+            (no need to install fairseq here)
 
         """
+
+        # Inputs:
         self.fairseq_hydra_config = fairseq_hydra_config
         self.command_line_args = command_line_args or []
-
         stored_epochs = list(range(save_interval, max_epoch, save_interval)) + [
             max_epoch
         ]
@@ -96,6 +101,17 @@ class FairseqHydraTrainingJob(Job):
             self.keep_epochs = set(keep_epochs)
         self.max_epoch = max_epoch
         self.save_interval = save_interval
+        self.fairseq_python_exe = (
+            fairseq_python_exe
+            if fairseq_python_exe is not None
+            else gs.FAIRSEQ_PYTHON_EXE
+        )
+        self.fairseq_hydra_exe = (
+            fairseq_hydra_exe if fairseq_hydra_exe is not None else gs.FAIRSEQ_HYDRA_EXE
+        )
+        self.fairseq_root = fairseq_root
+
+        # Outputs:
         self.out_fairseq_hydra_yaml = self.output_path("fairseq_hydra_config.yaml")
         self.out_checkpoint_dir = self.output_path("checkpoints", directory=True)
         self.out_models = {
@@ -107,20 +123,15 @@ class FairseqHydraTrainingJob(Job):
             for k in stored_epochs
             if k in self.keep_epochs
         }
-        self.gpu_rqmt = gpu_rqmt
-        self.fairseq_python_exe = fairseq_python_exe
-        self.fairseq_hydra_exe = (
-            fairseq_hydra_exe if fairseq_hydra_exe is not None else gs.FAIRSEQ_HYDRA_EXE
-        )
-        self.fairseq_git = fairseq_git
 
+        # Requirements:
+        self.gpu_rqmt = gpu_rqmt
         self.rqmt = {
             "gpu": gpu_rqmt,
             "cpu": cpu_rqmt,
             "mem": mem_rqmt,
             "time": time_rqmt,
         }
-
         if self.gpu_rqmt > 1:
             self.rqmt["cpu"] *= self.gpu_rqmt
             self.rqmt["mem"] *= self.gpu_rqmt
@@ -135,8 +146,8 @@ class FairseqHydraTrainingJob(Job):
 
     def run(self):
         my_env = os.environ
-        if self.fairseq_git is not None:
-            my_env["PYTHONPATH"] = self.fairseq_git
+        if self.fairseq_root is not None:
+            my_env["PYTHONPATH"] = self.fairseq_root
         sp.check_call(self._get_run_cmd(), env=my_env)
 
     def _get_run_cmd(self):
@@ -147,16 +158,15 @@ class FairseqHydraTrainingJob(Job):
             os.path.basename(self.out_fairseq_hydra_yaml.get_path()),
         ]
         run_cmd += self.command_line_args
-        run_cmd += ["checkpoint.save_dir=" + str(self.out_checkpoint_dir)]
+        run_cmd += ["checkpoint.save_dir=" + self.out_checkpoint_dir.get_path()]
         run_cmd += ["checkpoint.save_interval=" + str(self.save_interval)]
         run_cmd += ["optimization.max_epoch=" + str(self.max_epoch)]
-        if self.fairseq_git is not None:
-            sys.path.insert(0, self.fairseq_git)
-            hydra_train_entry = self.fairseq_git + "fairseq_cli/hydra_train.py"
+        if self.fairseq_root is not None:
+            sys.path.insert(0, self.fairseq_root)
+            hydra_train_entry = self.fairseq_root + "fairseq_cli/hydra_train.py"
             run_cmd.insert(0, tk.uncached_path(self.fairseq_python_exe))
             run_cmd.insert(1, tk.uncached_path(hydra_train_entry))
         else:
-            run_cmd.insert(0, tk.uncached_path(self.fairseq_hydra_exe))
-            if self.fairseq_python_exe is not None:
-                run_cmd.insert(0, tk.uncached_path(self.fairseq_python_exe))
+            run_cmd.insert(0, tk.uncached_path(self.fairseq_python_exe))
+            run_cmd.insert(1, tk.uncached_path(self.fairseq_hydra_exe))
         return run_cmd
