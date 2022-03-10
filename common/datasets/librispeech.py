51dd9889925d2add6a67762e196d5772cc2d7c98
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2022-03-10 11:51:51 +0100

    add Librispeech num_segments

--- common/datasets/librispeech.py
+++ common/datasets/librispeech.py
@@ -65,11 +65,28 @@ durations = {
     "test-other": 5.1,
 }
 
+num_segments = {
+    "train-clean-100": 28539,
+    "train-clean-360": 104014,
+    "train-other-500": 148688,
+    "dev-clean": 2703,
+    "dev-other": 2864,
+    "test-clean": 2620,
+    "test-other": 2939,
+}
+
 durations["train-clean-460"] = (
     durations["train-clean-100"] + durations["train-clean-360"]
 )
 durations["train-other-960"] = (
     durations["train-clean-460"] + durations["train-other-500"]
+)
+
+num_segments["train-clean-460"] = (
+        num_segments["train-clean-100"] + num_segments["train-clean-360"]
+)
+num_segments["train-other-960"] = (
+        num_segments["train-clean-460"] + num_segments["train-other-500"]
 )
 
 
