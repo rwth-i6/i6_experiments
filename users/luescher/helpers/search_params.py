8ba2b0d1dda2233dc56e977dd28ffa16daa7b8d5
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-04-06 19:56:39 +0200

    restructure lookahead option func

--- users/luescher/helpers/search_params.py
+++ users/luescher/helpers/search_params.py
@@ -17,23 +17,30 @@ def get_search_parameters(bp=15.0, bpl=50000, wep=0.5, wepl=10000, lsp=None):
 
 
 def get_lookahead_options(
-    scale=None,
-    hlimit=-1,
-    laziness=15,
-    treecf=30,
-    clow=2000,
-    chigh=3000,
-    minrepr=1,
+    *,
+    scale=None,  # 0.4
+    hlimit=None,  # bigram == 1
+    laziness=None,  # 15
+    treecf=None,  # 30
+    clow=None,  # 2000
+    chigh=None,  # 3000
+    minrepr=None,  # 1
 ):
-    lmla_options = {
-        "history_limit": hlimit,
-        "minimum_representation": minrepr,
-        "laziness": laziness,
-        "tree_cutoff": treecf,
-        "cache_low": clow,
-        "cache_high": chigh,
-    }
+    lmla_options = {}
+
     if scale is not None:
         lmla_options["lm_lookahead_scale"] = scale
+    if hlimit is not None:
+        lmla_options["history_limit"] = hlimit
+    if laziness is not None:
+        lmla_options["laziness"] = laziness
+    if treecf is not None:
+        lmla_options["tree_cutoff"] = treecf
+    if clow is not None:
+        lmla_options["cache_low"] = clow
+    if chigh is not None:
+        lmla_options["cache_high"] = chigh
+    if minrepr is not None:
+        lmla_options["minimum_representation"] = minrepr
 
     return lmla_options
