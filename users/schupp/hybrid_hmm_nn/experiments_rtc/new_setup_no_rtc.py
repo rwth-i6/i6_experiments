654f8a029373bd8da1e32c1e0c7091bcded8d1aa
Tim <tim@timschupp.de>
2022-04-25 20:10:31 +0200

    minor config adjustsment, notes on *all* usable arguments

--- users/schupp/hybrid_hmm_nn/experiments_rtc/new_setup_no_rtc.py
+++ users/schupp/hybrid_hmm_nn/experiments_rtc/new_setup_no_rtc.py
@@ -15,6 +15,21 @@ OUTPUT_PATH = "conformer/test_new_nortc/"
 gs.ALIAS_AND_OUTPUT_SUBDIR = OUTPUT_PATH
 NAME = "testrun"
 
+# All possible inputs that 'diversify' a config
+# - NAME
+# - OUTPUT_PATH 
+# - config_base_args
+# - network_args:
+#   - sampling_func_args
+#   - ff1_func_args
+#   - ff2_func_args
+#   - sa_func_args
+#   - conv_func_args
+#   - conformer_default_args_00
+# - train:
+#   - returnn_train_post_config_00
+#   - returnn_rasr_args_defaults_00
+
 # Start system:
 # - register alignments and features ...
 system = system.LibrispeechHybridSystemTim()
@@ -63,7 +78,8 @@ returnn_train_config : ReturnnConfig = job_dispatcher.make_returnn_train_config_
 
 returnn_rasr_config_args : dict = rasr_config_args_maker.get_returnn_rasr_args(
   system, 
-  train_corpus_key=train_corpus_key
+  train_corpus_key=train_corpus_key,
+  **experiment_config_args.returnn_rasr_args_defaults_00
 )
 
 train_job : ReturnnRasrTrainingJob = job_dispatcher.make_and_register_returnn_rasr_train(
@@ -87,26 +103,3 @@ job_dispatcher.make_and_register_returnn_rasr_search(
   limit_eps=experiment_config_args.returnn_train_post_config_00["cleanup_old_models"]["keep"],
   exp_name=NAME
 )
-
-
-if False:
-
-  # Test network contstuction and training on cpu ( saves qsub if this failes )
-  # TODO: ...
-
-
-  # Create ReturnnRasrTrainJob, register outputs -> submit train
-  train_job : ReturnnRasrTrainingJob = job_dispatcher.make_and_register_returnn_rasr_train(
-      returnn_train_config,
-      returnn_rasr_config_args,
-      output_path=OUTPUT_PATH
-  )
-
-
-# Create Search Jobs for given epochs, *but* will also always make recog for epochs in 'keep_best'
-  search_jobs = job_dispatcher.make_and_register_returnn_rasr_search(
-    recog_for_epochs=experiment_config_args.search_job_dispatcher_defaults["epochs"],
-    recog_corpus=train_corpus_key,
-    train_job=train_job,
-    output_path=OUTPUT_PATH
-  )
