190a9e03bd34be2dcda97b71645f998a458b3412
Tim <tim@timschupp.de>
2022-05-02 14:00:21 +0200

    updated summary tool, only look best-wer for dev-other, then same ep for others

--- users/schupp/hybrid_hmm_nn/tools_sis/scan_alias_generate_overviews.py
+++ users/schupp/hybrid_hmm_nn/tools_sis/scan_alias_generate_overviews.py
@@ -18,8 +18,10 @@ templates = glob.glob('*.j2')
 # 2 - Subtag name
 
 consider_out_dirs = ["conformer/best/"] # TODO: use should be asked which of these he wants updated
+consider_out_dirs = ["conformer/se_block/"]
 
 # For now consider all!
+# TODO; uncomment
 consider_out_dirs = glob.glob("alias/conformer/*")
 consider_out_dirs = [x.replace("alias/", "") + "/" for x in consider_out_dirs]
 
@@ -79,35 +81,54 @@ for drr in consider_out_dirs:
 
 _dir_files_short_names = { name : sorted([ d.split("/")[-1] for d in _dir_files[name] ]) for name in _dir_files.keys()} # Short name descriptions
 
-import re # Stolen: https://stackoverflow.com/questions/45001775/find-all-floats-or-ints-in-a-given-string
-re_float = re.compile("""(?x)
-   ^
-      [+-]?\ *      # first, match an optional sign *and space*
-      (             # then match integers or f.p. mantissas:
-          \d+       # start out with a ...
-          (
-              \.\d* # mantissa of the form a.b or a.
-          )?        # ? takes care of integers of the form a
-         |\.\d+     # mantissa of the form .b
-      )
-      ([eE][+-]?\d+)?  # finally, optionally match an exponent
-   $""")
+import re
 
+# I know this whole thing is computation hell, but only used once in a while anyways
 def content_map_filter_data(map):
     map_list = map.split("\n")
     # filters all epochs and epoch data of the content map
-    wers_by_dataset_epoch = []
+    ex_float_or_int = r'[\d\.\d]+' #r"[-+]?(?:\d*\.\d+|\d+)" <- might be a bit more precise
+    wers_by_dataset_epoch = {}
+    best_wer_for_set = {}
+    best_ep_dev_other = -1
     for tag in ["dev-other", "dev-clean", "test-other", "test-clean"]:
-        wers_by_dataset_epoch[tag] = []
+        wers_by_dataset_epoch[tag] = {}
         data = [k for k in map_list if ("WER" in k) and (tag in k)]
-        epoch_data = [k for k in map_list if ("epoch" in k)]
+        epoch_data = [k for k in map_list if ("epoch" in k and "~" in k)]
+
+        cur_best = 999.0
+        best_ep = -1
+        if len(data) == 0:
+            best_wer_for_set[tag] = f"NONE"
+            continue
 
         for i in range(len(data)):
-            WERS = re_float.findall(data[i]) # First is non tuned LM
-            EP = re_float.findall(epoch_data[i]) # First is epoch number, second is time ( TODO store also time? )
-            wers_by_dataset_epoch[tag][int(EP[0])] = [WERS[0], WERS[1]]
+            #print("data", data[i])
+            #print("ep", epoch_data[i])
+            WERS = re.findall(ex_float_or_int, data[i]) # First is non tuned LM
+            EP = re.findall(ex_float_or_int, epoch_data[i]) # First is epoch number, second is time ( TODO store also time? )
+            #print("EPS", EP)
+            #print("WER", WERS)
+            if float(WERS[-1]) < cur_best:
+                cur_best = float(WERS[-1]) # Doing the convertion over and over :P
+                best_ep = int(EP[0])
+                if tag == "dev-other":
+                    best_ep_dev_other = int(EP[0])
+            wers_by_dataset_epoch[tag][int(EP[0])] = [WERS[0], WERS[-1]] # The one in the middle is '4'gramLM
 
-    print(wers_by_dataset_epoch)
+        best_wer_for_set[tag] = f"ep: {best_ep}, WER: {cur_best}"
+
+    #print(wers_by_dataset_epoch)
+    #print(best_wer_for_set)
+    # Now we got the best wer for evry tag but acutally we always only want to display the best for dev-other, and then same epoch for the other datasets
+
+    for tag in ["dev-clean", "test-other", "test-clean"]:
+        if best_ep_dev_other in wers_by_dataset_epoch[tag]:
+            best_wer_for_set[tag] = f"ep: {best_ep_dev_other}, WER: {wers_by_dataset_epoch[tag][best_ep_dev_other]}"
+        else:
+            best_wer_for_set[tag] = f"no data for ep {best_ep_dev_other}"
+
+    return best_wer_for_set
 
 #print(_dir_files_short_names)
 
@@ -116,6 +137,19 @@ second_names = _dir_files_short_names
 content_map = _file_data_map
 
 summary_file = True
+generate_csv = True # Csv files with only the best WER
+
+csv_columns = {
+    "NAME" : [],
+
+    "BEST WER (dev-other)" : [],
+    "WER (dev-clean)" : [],
+
+    "WER (test-other)": [],
+    "WER (test-clean)": [],
+
+    "FULL CONFIG PATH" : []
+}
 
 if summary_file:
     # Also generate a summary file for all results
@@ -127,14 +161,28 @@ if summary_file:
         # Short summary
         short_sum = "" # Every sub setup also generates a 'short summary'
 
+
+        if generate_csv:
+            l = [x,"-","-","-", "-", "-"]
+            keys = list(csv_columns.keys())
+            for i in range(len(keys)):
+                csv_columns[keys[i]].append(l[i])
+
         # This generates all individual summary files
         for exp in _dir_files_short_names[x]:
             file_name = sub_dir_name + exp
             outp = open(file_name, "w")
             n = outp.write(content_map[exp])
 
-            if True: # Make this if generate_csv
-                content_map_filter_data(content_map[exp])
+            if generate_csv: # Make this if generate_csv
+                best_wer_by_set = content_map_filter_data(content_map[exp])
+                full_config_path = os.getcwd() + "/output/" + x + exp + "/returnn.config"
+                l = [exp] + [best_wer_by_set[data] for data in ["dev-other", "dev-clean", "test-other", "test-clean"]] + [full_config_path]
+                keys = list(csv_columns.keys())
+                for i in range(len(keys)):
+                    csv_columns[keys[i]].append(l[i])
+                    
+
             # The short summary only accounts for dev-other and the most recent epoch
             data = [k for k in content_map[exp].split("\n") if ("WER" in k) and ("dev-other" in k)]
             epoch_data = [k for k in content_map[exp].split("\n") if ("epoch" in k)]
@@ -149,6 +197,25 @@ if summary_file:
         outp = open(sub_dir_name + "summary.txt", "w")
         n = outp.write(short_sum)
         outp.close()
+
+    if generate_csv:
+        print(csv_columns)
+        import csv
+        f = open('summary.csv', 'w')
+
+        writer = csv.writer(f)
+        keys = list(csv_columns.keys())
+        writer.writerow(keys)
+
+        for x in range(len(csv_columns[keys[0]])):
+            print(x)
+            writer.writerow([csv_columns[k][x] for k in keys])
+
+        f.close()
+
+    
+
+
 
         
             
