14b9f7c92f2e0ce714e2f7e4c5ce762f22690343
Tim <tim@timschupp.de>
2022-05-05 22:34:18 +0200

    new arguments for extraction

--- users/schupp/hybrid_hmm_nn/tools_sis/make_py_summary.py
+++ users/schupp/hybrid_hmm_nn/tools_sis/make_py_summary.py
@@ -7,7 +7,30 @@ import glob
 import json
 import logging as log
 import os
+import argparse
+
+from black import filter_cached
+
 log.basicConfig(level=log.DEBUG)
+
+# Allowed arguments: (WIP)
+# --basepath conformer      -> the root path alias/*/
+# --base-experiment         -> the path alias/--basepath/*
+# --only dev-other          -> only one dataset
+# --update-data-filter   "dev-other:filter-new"
+# --filter-ex-name "dummy_job"         -> filters all these names out
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--basepath', default='conformer')
+parser.add_argument('-bx', '--base-experiment', default=None)
+parser.add_argument('-o', '--only', default=None)
+parser.add_argument('-fex', '--filter-ex-name', default=None)
+parser.add_argument('-udf', '--update-data-filter', default=None)
+args = parser.parse_args()
+
+BASE = args.basepath
+
+print(args)
 
 
 data_set_prefixes = {
@@ -17,13 +40,32 @@ data_set_prefixes = {
     "train-clean" : "_tain-clean"
 }
 
+if args.only:
+    data_set_prefixes = { args.only : data_set_prefixes[args.only]}
+
+if args.update_data_filter:
+    data, new_filt = args.update_data_filter.split(":")
+    if data in data_set_prefixes:
+        data_set_prefixes[data] = new_filt
+
 datasets = list(data_set_prefixes.keys())
 
-all_existing_experiments = [s.replace("alias/", "") for s in glob.glob("alias/conformer/*") ]
-all_existing_experiments = ["conformer/baseline"] # With this you can filter for a specific setup
+log.debug(f"Datsets to consider: {datasets}")
+
+all_existing_experiments = [s.replace("alias/", "") for s in glob.glob(f"alias/{BASE}/*") ]
+if args.base_experiment:
+    all_existing_experiments = [f"{BASE}/{args.base_experiment}"] # With this you can filter for a specific setup
 sub_experiments = {k : [] for k in all_existing_experiments}
+
+filter_check = lambda s : (not "recog_" in s)
+if args.filter_ex_name:
+    filters = ["recog_", *args.filter_ex_name.split(",") ]
+    log.debug(f"Using updated filters: {filters}")
+    filter_check = lambda s : all([x not in s for x in filters])
+
 for i, k in enumerate(all_existing_experiments):
-    sub_experiments[k] = [s.split("/")[-1] for s in glob.glob(f"alias/{all_existing_experiments[i]}/*") if not "recog_" in s ]
+    sub_experiments[k] = [s.split("/")[-1] for s in glob.glob(f"alias/{all_existing_experiments[i]}/*") if filter_check(s) ]
+
 log.debug(all_existing_experiments)
 
 
@@ -53,13 +95,20 @@ def parse_experiment_out_string(data_string):
 
     float_or_int = r'[\d\.\d]+'
 
+    def try_or_default(run, default): # Againt all rules but shoretest solution here
+        try:
+            return run()
+        except Exception as e:
+            log.debug(e)
+            return default
+
     for line in data_string.split("\n"):
         if "num_params:" in line:
-            num_params = float(re.findall(float_or_int, line)[0])
+            num_params = try_or_default(lambda : float(re.findall(float_or_int, line)[0]), "not found")
         elif "average steps per subepoch" in line:
-            avg_s_per_sep = float(re.findall(float_or_int, line)[0])
+            avg_s_per_sep = try_or_default(lambda : float(re.findall(float_or_int, line)[0]), "not found")
         elif "training time per subepoch" in line and not time_p_sep:
-            time_p_sep = float(re.findall(float_or_int, line)[0])
+            time_p_sep = try_or_default(lambda : float(re.findall(float_or_int, line)[0]), "not found")
         elif "recog:" in line:
             log.debug(line)
             try:
@@ -72,7 +121,7 @@ def parse_experiment_out_string(data_string):
             except SyntaxError as e:
                 optim_wer_by_ep = "No optim wers found"
         elif "best epoch:" in line:
-            best_ep_by_score = int(re.findall(float_or_int, line)[0])
+            best_ep_by_score = try_or_default(lambda : int(re.findall(float_or_int, line)[0]), "not found")
         elif "errors:" in line:
             try:
                 errors_per_ep = eval(line.replace("errors:", ""))
@@ -103,7 +152,7 @@ def get_all_data_experiment(experiment_path, name):
 
 for experiment in all_existing_experiments:
     for sub_experiment in sub_experiments[experiment]:
-        ex_path = experiment.replace("conformer/","")
+        ex_path = experiment.replace(f"{BASE}/","")
 
         if not os.path.exists(f"{RESULTS_FOLDER}{ex_path}"):
             os.mkdir(f"{RESULTS_FOLDER}{ex_path}")
