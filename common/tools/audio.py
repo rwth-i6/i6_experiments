b1a9ef5b66675d7f3baab0e5a2a282c2f4ed476a
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2022-06-30 17:34:40 +0200

    add FFmpeg and RASR compile helper (#72)

---
+++ common/tools/audio.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+from sisyphus import tk
+
+from i6_core.tools.git import CloneGitRepositoryJob
+from i6_core.tools.compile import MakeJob
+
+
+def compile_ffmpeg_binary(
+    branch: Optional[str] = None, commit: Optional[str] = None
+) -> tk.Path:
+    """
+    Compiles FFmpeg using the official Github master.
+
+    For requirements see:
+    https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
+
+    The current configure options are set in a way that the resulting FFmpeg binary lacks many features.
+    This is done because we only need limited codec capabilities.
+
+    :param branch: specify a specific branch or tag, e.g. "n4.1.4"
+    :param commit: specify a specific commit
+    :return: Path object pointing to the "ffmpeg" binary executable
+    """
+    ffmpeg_repo = CloneGitRepositoryJob(
+        "https://github.com/FFmpeg/FFmpeg",
+        branch=branch,
+        commit=commit,
+    )
+
+    ffmpeg_make_binary = MakeJob(
+        folder=ffmpeg_repo.out_repository,
+        make_sequence=["all"],
+        num_processes=4,
+        configure_opts=["--disable-x86asm", "--enable-libvorbis"],
+        link_outputs={"ffmpeg": "ffmpeg"},
+        output_folder_name=None,
+    )
+    # optionally add additional configuration options without changing the hash:
+    # ffmpeg_make_binary.configure_opts += ["libmp3lame-dev"]
+
+    return ffmpeg_make_binary.out_links["ffmpeg"]
---
+++ common/tools/rasr.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from sisyphus import tk
+
+from i6_core.tools.compile import MakeJob
+from i6_core.tools.git import CloneGitRepositoryJob
+
+
+def compile_rasr_binaries_i6mode(
+    branch: Optional[str] = None,
+    commit: Optional[str] = None,
+    rasr_git_repository: str = "https://github.com/rwth-i6/rasr",
+    rasr_arch: str = "linux-x86_64-standard",
+) -> tk.Path:
+    """
+    Compile RASR for i6-internal usage
+
+    :param branch: specify a specific branch
+    :param commit: specify a specific commit
+    :param rasr_git_repository: where to clone RASR from, usually does not need to be altered
+    :param rasr_arch: RASR compile architecture string
+    :return: path to the binary folder
+    """
+    rasr_repo = CloneGitRepositoryJob(
+        rasr_git_repository, branch=branch, commit=commit
+    ).out_repository
+    make_job = MakeJob(
+        folder=rasr_repo,
+        make_sequence=["build", "install"],
+        configure_opts=["--i6"],
+        num_processes=8,
+        link_outputs={"binaries": f"arch/{rasr_arch}/"},
+    )
+    make_job.rqmt["mem"] = 8
+    return make_job.out_links["binaries"]
