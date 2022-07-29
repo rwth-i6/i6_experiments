9b7ca48e885e5e27125f1fe6aa8bff51227a9ec9
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-07-29 11:04:10 +0200

    base and hybrid decoder class and utils [wip+draft]

---
+++ common/setups/rasr/hybrid_decoder.py
@@ -0,0 +1,46 @@
+__all__ = ["HybridDecoder"]
+
+from typing import Dict, Optional
+
+from sisyphus import tk
+
+from .base_decoder import BaseDecoder
+from .util import RasrInitArgs, ReturnnRasrDataInput, NnRecogArgs
+
+
+class HybridDecoder(BaseDecoder):
+    def __init__(
+        self, rasr_binary_path: tk.Path, rasr_arch: str = "linux-x86_64-standard"
+    ):
+        """
+        Class to perform Hybrid Decoding with a RETURNN model
+
+        :param rasr_binary_path: path to the rasr binary folder
+        :param rasr_arch: RASR compile architecture suffix
+        """
+        super().__init__(rasr_binary_path=rasr_binary_path, rasr_arch=rasr_arch)
+
+        self.eval_corpora = []
+
+        self.eval_input_data = None
+
+    def init_system(
+        self,
+        hybrid_init_args: RasrInitArgs,
+        eval_data: Dict[str, ReturnnRasrDataInput],
+        nn_recog_args: NnRecogArgs,
+    ):
+        self.hybrid_init_args = hybrid_init_args
+
+        self._init_am(**self.hybrid_init_args.am_args)
+
+        self.eval_input_data = eval_data
+
+        self.eval_corpora.extend(list(eval_data.keys()))
+
+        self._set_e
+
+        self.nn_recog_args = nn_recog_args
+
+    def recognition(self):
+        pass
