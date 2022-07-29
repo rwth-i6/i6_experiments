974f6a9844f48b26a8390211c30d6bfa76428b76
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-07-29 20:09:50 +0200

    improvements and more stuff

--- common/setups/rasr/base_decoder.py
+++ common/setups/rasr/base_decoder.py
@@ -17,6 +17,7 @@ from .util.decode import (
     SearchJobArgs,
     Lattice2CtmArgs,
     ScorerArgs,
+    OptimizeJobArgs,
 )
 
 
@@ -31,6 +32,7 @@ class BaseDecoder:
         compress_after_run: bool = True,
         search_job_class: tk.Job = recog.AdvancedTreeSearchJob,
         scorer_job_class: tk.Job = recog.ScliteJob,
+        alias_output_prefix: str = "",
     ):
         self.rasr_binary_path = rasr_binary_path
         self.rasr_arch = rasr_arch
@@ -45,6 +47,8 @@ class BaseDecoder:
         self.crp["base"].set_executables(rasr_binary_path, rasr_arch)
 
         self.eval_corpora = []
+
+        self.alias_output_prefix = alias_output_prefix
 
     def init_decoder(
         self,
@@ -81,14 +85,32 @@ class BaseDecoder:
         scorer_args: ScorerArgs,
     ):
         adv_tree_search_job = self.search_job_class(**search_job_args)
+        adv_tree_search_job.set_vis_name(
+            f"Recog: {self.alias_output_prefix}{name}. Corpus: {corpus_key}"
+        )
+        adv_tree_search_job.add_alias(
+            f"{self.alias_output_prefix}recog_{corpus_key}/{name}"
+        )
 
         lat_2_ctm_job = recog.LatticeToCtmJob(**lat_2_ctm_args)
+        lat_2_ctm_job.add_alias(
+            f"{self.alias_output_prefix}lattice2ctm_{corpus_key}/{name}"
+        )
 
         scorer_job = self.scorer_job_class(**scorer_args)
+        scorer_job.add_alias(f"{self.alias_output_prefix}scoring_{corpus_key}/{name}")
+        tk.register_output(
+            f"{self.alias_output_prefix}recog_{corpus_key}/{name}.reports",
+            scorer_job.out_report_dir,
+        )
 
-    def _optimize_scales(self):
-        opt_job = recog.OptimizeAMandLMScaleJob()
-        return 0.0, 0.0
+    def _optimize_scales(
+        self, name: str, corpus_key: str, optimize_args: OptimizeJobArgs
+    ) -> (float, float):
+        opt_job = recog.OptimizeAMandLMScaleJob(**optimize_args)
+        opt_job.add_alias(f"{self.alias_output_prefix}optimize_{corpus_key}/{name}")
+
+        return opt_job.out_best_am_score, opt_job.out_best_lm_score
 
     def decode(
         self,
@@ -106,7 +128,11 @@ class BaseDecoder:
                 scorer_args={},
             )
             if optimize_am_lm_scales:
-                best_am_scale, best_lm_scale = self._optimize_scales()
+                best_am_scale, best_lm_scale = self._optimize_scales(
+                    scorer_cls=self.scorer_job_class,
+                    scorer_kwargs={},
+                    opt_only_lm_scale=True if best_am_scale == 1.0 else False,
+                )
                 search_job_args = {}
                 self._recog(
                     name=f"{name}-opt",
