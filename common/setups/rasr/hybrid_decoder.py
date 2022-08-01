d97d203a451fc45e5429086006280da59f3a25bd
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-08-01 11:47:14 +0200

    method stubs

--- common/setups/rasr/hybrid_decoder.py
+++ common/setups/rasr/hybrid_decoder.py
@@ -1,6 +1,6 @@
 __all__ = ["HybridDecoder"]
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Set, Tuple
 
 from sisyphus import tk
 
@@ -10,7 +10,13 @@ from .util import RasrInitArgs, ReturnnRasrDataInput, NnRecogArgs
 
 class HybridDecoder(BaseDecoder):
     def __init__(
-        self, rasr_binary_path: tk.Path, rasr_arch: str = "linux-x86_64-standard"
+        self,
+        rasr_binary_path: tk.Path,
+        rasr_arch: str = "linux-x86_64-standard",
+        returnn_root: Optional[tk.Path] = None,
+        returnn_python_home: Optional[tk.Path] = None,
+        returnn_python_exe: Optional[tk.Path] = None,
+        blas_lib: Optional[tk.Path] = None,
     ):
         """
         Class to perform Hybrid Decoding with a RETURNN model
@@ -19,6 +25,11 @@ class HybridDecoder(BaseDecoder):
         :param rasr_arch: RASR compile architecture suffix
         """
         super().__init__(rasr_binary_path=rasr_binary_path, rasr_arch=rasr_arch)
+
+        self.returnn_root = returnn_root
+        self.returnn_python_home = returnn_python_home
+        self.returnn_python_exe = returnn_python_exe
+        self.blas_lib = blas_lib
 
         self.rasr_init_args = None
         self.eval_corpora = []
@@ -30,11 +41,43 @@ class HybridDecoder(BaseDecoder):
         rasr_init_args: RasrInitArgs,
         eval_data: Dict[str, ReturnnRasrDataInput],
         nn_recog_args: NnRecogArgs,
+        dev_test_mapping: Set[Tuple[str, str]],
     ):
         self.rasr_init_args = rasr_init_args
         self.eval_input_data = eval_data
         self.eval_corpora.extend(list(eval_data.keys()))
         self.nn_recog_args = nn_recog_args
+        self.dev_test_mapping = dev_test_mapping
+
+    def _compile_necessary_native_ops(self):
+        pass
+
+    def _compile_tf_graphs(self):
+        pass
+
+    def _get_base_flow(self):
+        pass
+
+    def _get_tf_flow(self):
+        pass
+
+    def _get_rasr_flow(self):
+        pass
+
+    def _get_lm_image_and_global_cache(self):
+        pass
+
+    def _get_prior_file(self):
+        pass
+
+    def _build_recog_loop(self):
+        pass
 
     def recognition(self):
+        # get necessary ops
+        # get base flow
+        # get tf
+        # get flow for decoding
+        # build dev-test map
+        # construct crp
         pass
