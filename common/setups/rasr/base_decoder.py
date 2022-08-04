d3a38e4238ec264c059d0a71605dd784428b18a7
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-08-04 19:26:40 +0200

    base decoder, first draft, untested

--- common/setups/rasr/base_decoder.py
+++ common/setups/rasr/base_decoder.py
@@ -2,7 +2,7 @@ __all__ = ["BaseDecoder"]
 
 import copy
 import logging
-from typing import Dict, Optional, Type, Union
+from typing import Dict, List, Optional, Type, Union
 
 from sisyphus import tk
 
@@ -12,10 +12,9 @@ import i6_core.recognition as recog
 from i6_core.corpus import CorpusToStmJob
 from i6_core.meta import CorpusObject
 
-
+from .config.am_config import Tdp
 from .util.decode import (
     BaseRecognitionParameters,
-    PriorArgs,
     SearchJobArgs,
     Lattice2CtmArgs,
     ScliteScorerArgs,
@@ -93,59 +92,97 @@ class BaseDecoder:
 
     @staticmethod
     def _get_scales_string(
-        am_scale, lm_scale, pronunciation_scale, tdp_scale, prior_scale
+        am_scale: float,
+        lm_scale: float,
+        prior_scale: float,
+        tdp_scale: float,
+        tdp_speech: Optional[Tdp],
+        tdp_silence: Optional[Tdp],
+        tdp_nonspeech: Optional[Tdp] = None,
+        pronunciation_scale: Optional[float] = None,
     ):
         out_str = ""
         out_str += f"am{am_scale:2f.2f}"
         out_str += f"_lm{lm_scale:2f.2f}"
-        out_str += f"_pron{pronunciation_scale:2f.2f}"
-        out_str += f"_tdp{tdp_scale:2f.2f}"
         out_str += f"_prior{prior_scale:2f.2f}"
+
+        out_str += f"_tdp{tdp_scale}"
+        out_str += f"_tdpspeech{tdp_speech}"
+        out_str += f"_tdpsilence{tdp_silence}"
+
+        if tdp_nonspeech is not None:
+            out_str += f"_tdpnonspeech{tdp_nonspeech}"
+
+        if pronunciation_scale is not None:
+            out_str += f"_ps{pronunciation_scale:2f.2f}"
+
         return out_str
 
     def _set_scales_and_tdps(
         self,
-        corpus_key,
-        am_scale,
-        lm_scale,
-        prior_scale,
-        tdp_scale,
-        tdp_transition,
-        tdp_silence,
-    ):
+        corpus_key: str,
+        am_scale: float,
+        lm_scale: float,
+        prior_scale: float,
+        tdp_scale: float,
+        tdp_speech: Tdp,
+        tdp_silence: Tdp,
+        tdp_nonspeech: Optional[Tdp],
+        pronunciation_scale: Optional[float],
+    ) -> str:
+        base_corpus_key = corpus_key
+        corpus_key += "_"
+        corpus_key += self._get_scales_string(
+            am_scale=am_scale,
+            lm_scale=lm_scale,
+            prior_scale=prior_scale,
+            tdp_scale=tdp_scale,
+            tdp_speech=tdp_speech,
+            tdp_silence=tdp_silence,
+            tdp_nonspeech=tdp_nonspeech,
+            pronunciation_scale=pronunciation_scale,
+        )
+
+        self.crp[corpus_key] = rasr.CommonRasrParameters(base=self.crp[base_corpus_key])
+
         self.crp[corpus_key].acoustic_model_config.scale = am_scale
         self.crp[corpus_key].language_model_config.scale = lm_scale
 
         self.crp[corpus_key].mixture_set.prior_scale = prior_scale
 
         self.crp[corpus_key].acoustic_model_config.tdp.scale = tdp_scale
-        self.crp[corpus_key].acoustic_model_config.tdp["*"].loop = tdp_transition.loop
-        self.crp[corpus_key].acoustic_model_config.tdp[
-            "*"
-        ].forward = tdp_transition.forward
-        self.crp[corpus_key].acoustic_model_config.tdp["*"].skip = tdp_transition.skip
-        self.crp[corpus_key].acoustic_model_config.tdp[
-            "*"
-        ].exit = tdp_transition.transition
+
+        self.crp[corpus_key].acoustic_model_config.tdp["*"].loop = tdp_speech.loop
+        self.crp[corpus_key].acoustic_model_config.tdp["*"].forward = tdp_speech.forward
+        self.crp[corpus_key].acoustic_model_config.tdp["*"].skip = tdp_speech.skip
+        self.crp[corpus_key].acoustic_model_config.tdp["*"].exit = tdp_speech.exit
+
         self.crp[corpus_key].acoustic_model_config.tdp.silence.loop = tdp_silence.loop
         self.crp[
             corpus_key
         ].acoustic_model_config.tdp.silence.forward = tdp_silence.forward
         self.crp[corpus_key].acoustic_model_config.tdp.silence.skip = tdp_silence.skip
-        self.crp[
-            corpus_key
-        ].acoustic_model_config.tdp.silence.exit = tdp_silence.transition
+        self.crp[corpus_key].acoustic_model_config.tdp.silence.exit = tdp_silence.exit
+
+        return corpus_key
 
     def _recog(
         self,
         name: str,
         corpus_key: str,
+        feature_scorer: rasr.FeatureScorer,
+        feature_flow: rasr.FlowNetwork,
         search_job_args: Union[SearchJobArgs, Dict],
         lat_2_ctm_args: Union[Lattice2CtmArgs, Dict],
         scorer_args: Union[ScliteScorerArgs, Dict],
         scorer_hyp_param_name: str,
-    ) -> (Type[tk.Job], recog.LatticeToCtmJob, Type[tk.Job]):
-        search_job = self.search_job_class(**search_job_args)
+    ) -> (tk.Job, recog.LatticeToCtmJob, tk.Job):
+        search_job = self.search_job_class(
+            crp=self.crp[corpus_key],
+            feature_scorer=feature_scorer,
+            feature_flow=feature_flow,
+            **search_job_args,
+        )
         search_job.set_vis_name(
             f"Recog: {self.alias_output_prefix}{name}. Corpus: {corpus_key}"
         )
@@ -179,20 +216,33 @@ class BaseDecoder:
         lattice_cache,
         initial_am_scale,
         initial_lm_scale,
-        scorer_class,
         scorer_kwargs,
+        scorer_hyp_param_name: str,
         optimize_args: OptimizeJobArgs,
     ) -> (float, float):
-        opt_job = recog.OptimizeAMandLMScaleJob(**optimize_args)
+        opt_job = recog.OptimizeAMandLMScaleJob(
+            crp=self.crp[corpus_key],
+            lattice_cache=lattice_cache,
+            initial_am_scale=initial_am_scale,
+            initial_lm_scale=initial_lm_scale,
+            scorer_cls=self.scorer_job_class,
+            scorer_kwargs=scorer_kwargs,
+            scorer_hyp_param_name=scorer_hyp_param_name,
+            **optimize_args,
+        )
         opt_job.add_alias(f"{self.alias_output_prefix}optimize_{corpus_key}/{name}")
 
-        return opt_job.out_best_am_score, opt_job.out_best_lm_score
+        best_am_scale = opt_job.out_best_am_score
+        best_lm_scale = opt_job.out_best_lm_score
+
+        return best_am_scale, best_lm_scale
 
     def decode(
         self,
         name,
+        feature_scorer: List[Optional[rasr.FeatureScorer]],
+        feature_flow: rasr.FlowNetwork,
         recognition_parameters: BaseRecognitionParameters,
-        prior: PriorArgs,
         *,
         search_job_args: Union[SearchJobArgs, Dict],
         lat_2_ctm_args: Union[Lattice2CtmArgs, Dict],
@@ -203,14 +253,16 @@ class BaseDecoder:
     ):
         for (
             corpus_key,
+            scorer,
             am_sc,
             lm_sc,
             prior_sc,
             pron_sc,
             tdp_sc,
-            tdp_trans,
-            tdp_sil,
-        ) in zip(self.eval_corpora, recognition_parameters):
+            tdp_speech,
+            tdp_silence,
+            tdp_nonspeech,
+        ) in zip(self.eval_corpora, feature_scorer, recognition_parameters):
             if scorer_args["ref"] is None:
                 logging.warning("Using no cleanup during STM creation.")
                 scorer_args["ref"] = CorpusToStmJob(
@@ -220,26 +272,28 @@ class BaseDecoder:
                     fix_whitespace=False,
                 ).out_stm_path
 
-            self._set_scales_and_tdps(
+            derived_corpus_key = self._set_scales_and_tdps(
                 corpus_key=corpus_key,
                 am_scale=am_sc,
                 lm_scale=lm_sc,
                 prior_scale=prior_sc,
                 tdp_scale=tdp_sc,
-                tdp_transition=tdp_trans,
-                tdp_silence=tdp_sil,
+                tdp_speech=tdp_speech,
+                tdp_silence=tdp_silence,
+                tdp_nonspeech=tdp_nonspeech,
+                pronunciation_scale=pron_sc,
             )
 
             if pron_sc is not None:
                 model_combination_config = rasr.RasrConfig()
                 model_combination_config.pronunciation_scale = pron_sc
-            else:
-                model_combination_config = None
-            search_job_args["model_combination_config"] = model_combination_config
+                search_job_args["model_combination_config"] = model_combination_config
 
             search_job, lat_2_ctm_job, scorer_job = self._recog(
                 name=name,
-                corpus_key=corpus_key,
+                corpus_key=derived_corpus_key,
+                feature_scorer=scorer,
+                feature_flow=feature_flow,
                 search_job_args=search_job_args,
                 lat_2_ctm_args=lat_2_ctm_args,
                 scorer_args=scorer_args,
@@ -248,17 +302,32 @@ class BaseDecoder:
             if optimize_am_lm_scales:
                 best_am_scale, best_lm_scale = self._optimize_scales(
                     name=name,
-                    corpus_key=corpus_key,
+                    corpus_key=derived_corpus_key,
                     lattice_cache=search_job.out_lattice_bundle,
                     initial_am_scale=am_sc,
                     initial_lm_scale=lm_sc,
-                    scorer_class=self.scorer_job_class,
                     scorer_kwargs={},
+                    scorer_hyp_param_name=scorer_hyp_param_name,
                     optimize_args=optimize_parameters,
                 )
+
+                optimized_corpus_key = self._set_scales_and_tdps(
+                    corpus_key=corpus_key,
+                    am_scale=best_am_scale,
+                    lm_scale=best_lm_scale,
+                    prior_scale=prior_sc,
+                    tdp_scale=tdp_sc,
+                    tdp_speech=tdp_speech,
+                    tdp_silence=tdp_silence,
+                    tdp_nonspeech=tdp_nonspeech,
+                    pronunciation_scale=pron_sc,
+                )
+
                 self._recog(
                     name=f"{name}-opt",
-                    corpus_key=corpus_key,
+                    corpus_key=optimized_corpus_key,
+                    feature_scorer=scorer,
+                    feature_flow=feature_flow,
                     search_job_args=search_job_args,
                     lat_2_ctm_args=lat_2_ctm_args,
                     scorer_args=scorer_args,
