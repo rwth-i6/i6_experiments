092959d8219a53f7388f9c42b845245c9490e703
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2022-08-05 08:23:18 +0200

    nvm willi was right

--- common/setups/rasr/gmm_system.py
+++ common/setups/rasr/gmm_system.py
@@ -1450,7 +1450,7 @@ class GmmSystem(RasrSystem):
 
             # ---------- Forced Alignment ----------
             if step_name.startswith("forced_align"):
-                corpus_keys = step_args.pop("corpus_keys", [self.train_corpora])
+                corpus_keys = step_args.pop("corpus_keys", self.train_corpora)
                 for corpus in corpus_keys:
                     self.forced_align(
                         feature_scorer_corpus_key=corpus,
