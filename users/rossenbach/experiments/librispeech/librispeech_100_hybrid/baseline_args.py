d428dfbd7e810c6987b78eb6d461bc34aa52013b
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2022-08-23 09:06:59 +0200

    updated ls100 hybrid

--- users/rossenbach/experiments/librispeech/librispeech_100_hybrid/baseline_args.py
+++ users/rossenbach/experiments/librispeech/librispeech_100_hybrid/baseline_args.py
@@ -107,6 +107,38 @@ def get_nn_args(num_outputs: int = 12001, num_epochs: int = 250):
     return nn_args
 
 
+def get_feature_extraction_args():
+    dc_detection = False
+    samples_options = {
+        'audio_format': "wav",
+        'dc_detection': dc_detection,
+    }
+
+    return {
+        "gt": {
+            "gt_options": {
+                "minfreq": 100,
+                "maxfreq": 7500,
+                "channels": 50,
+                # "warp_freqbreak": 7400,
+                "tempint_type": "hanning",
+                "tempint_shift": 0.01,
+                "tempint_length": 0.025,
+                "flush_before_gap": True,
+                "do_specint": False,
+                "specint_type": "hanning",
+                "specint_shift": 4,
+                "specint_length": 9,
+                "normalize": True,
+                "preemphasis": True,
+                "legacy_scaling": False,
+                "without_samples": False,
+                "samples_options": samples_options,
+                "normalization_options": {},
+            }
+        }
+    }
+
 def get_returnn_configs(
         num_inputs: int, num_outputs: int, batch_size: int, evaluation_epochs: List[int],
         recognition=False,
