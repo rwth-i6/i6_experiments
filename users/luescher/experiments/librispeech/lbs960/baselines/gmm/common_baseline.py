e2ab167c9695614b3f14f591eca28d3f37360dec
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-08-26 20:02:08 +0200

    revert g2p params

--- users/luescher/experiments/librispeech/lbs960/baselines/gmm/common_baseline.py
+++ users/luescher/experiments/librispeech/lbs960/baselines/gmm/common_baseline.py
@@ -17,9 +17,7 @@ import i6_experiments.users.luescher.setups.librispeech.pipeline_base_args as lb
 from i6_experiments.common.tools.rasr import compile_rasr_binaries_i6mode
 
 
-def run_librispeech_960_common_baseline(
-    g2p_python: Optional[str] = None, g2p_path: Optional[str] = None
-):
+def run_librispeech_960_common_baseline():
     # ******************** Settings ********************
 
     alias_and_output_dir_tmp = gs.ALIAS_AND_OUTPUT_SUBDIR
@@ -37,10 +35,8 @@ def run_librispeech_960_common_baseline(
         test_data_inputs,
     ) = lbs_gmm_setups.get_data_inputs(
         use_eval_data_subset=True,
-        g2p_python=g2p_python,
-        g2p_path=g2p_path,
     )
-    hybrid_init_args = lbs_gmm_setups.get_init_args()
+    rasr_init_args = lbs_gmm_setups.get_init_args()
     mono_args = lbs_gmm_setups.get_monophone_args()
     cart_args = lbs_gmm_setups.get_cart_args(max_leaves=12001)
     tri_args = lbs_gmm_setups.get_triphone_args()
@@ -50,7 +46,7 @@ def run_librispeech_960_common_baseline(
     final_output_args = lbs_gmm_setups.get_final_output()
 
     steps = rasr_util.RasrSteps()
-    steps.add_step("extract", hybrid_init_args.feature_extraction_args)
+    steps.add_step("extract", rasr_init_args.feature_extraction_args)
     steps.add_step("mono", mono_args)
     steps.add_step("cart", cart_args)
     steps.add_step("tri", tri_args)
@@ -63,7 +59,7 @@ def run_librispeech_960_common_baseline(
 
     lbs_gmm_system = gmm_system.GmmSystem(rasr_binary_path=rasr_binary_path)
     lbs_gmm_system.init_system(
-        hybrid_init_args=hybrid_init_args,
+        rasr_init_args=rasr_init_args,
         train_data=train_data_inputs,
         dev_data=dev_data_inputs,
         test_data=test_data_inputs,
