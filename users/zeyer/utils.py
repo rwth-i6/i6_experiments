9274a4f00ba15330f58b316779905ca3f0efaa04
Albert Zeyer <albzey@gmail.com>
2022-09-11 01:33:10 +0200

    GroupJob

--- users/zeyer/utils.py
+++ users/zeyer/utils.py
@@ -1,6 +1,8 @@
 """generic utils"""
 
+from __future__ import annotations
 from typing import Union, TextIO, BinaryIO
+from sisyphus import Job, Task
 
 
 def generic_open(filename: str, mode: str = "r") -> Union[TextIO, BinaryIO]:
@@ -18,3 +20,25 @@ def generic_open(filename: str, mode: str = "r") -> Union[TextIO, BinaryIO]:
       mode += "t"
     return gzip.open(filename, mode)
   return open(filename, mode)
+
+
+class GroupJob(Job):
+  """
+  Like tf.group. Depends on a number of inputs.
+  It is itself a no-op.
+  Because Sisyphus needs an output, it creates a dummy output.
+  """
+
+  def __init__(self, inputs):
+    super(GroupJob, self).__init__()
+    self.inputs = inputs
+    self.output = self.output_path("dummy.txt")
+
+  def tasks(self):
+    """tasks"""
+    yield Task("run", resume="run", mini_task=True)
+
+  def run(self):
+    """run"""
+    with open(self.output.get_path(), "wt"):
+      pass
