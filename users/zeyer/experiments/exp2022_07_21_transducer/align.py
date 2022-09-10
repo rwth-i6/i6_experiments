956216f8fad80cad0b15737d7d9da8d66e8b3a7b
Albert Zeyer <albzey@gmail.com>
2022-09-11 01:51:36 +0200

    more

--- users/zeyer/experiments/exp2022_07_21_transducer/align.py
+++ users/zeyer/experiments/exp2022_07_21_transducer/align.py
@@ -12,5 +12,5 @@ def align(*, task: Task, model: ModelWithCheckpoint) -> AlignmentCollection:
     # really just a dummy...
     return AlignmentCollection(alignments={
         name: Alignment(hdf_files=[model.checkpoint.index_path])
-        for name, dataset in task.eval_datasets.items()
+        for name, dataset in (task.eval_datasets | {"train": task.train_dataset, "dev": task.dev_dataset}).items()
     })
