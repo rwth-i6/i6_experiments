7f54811d7bfc694801c94e4c1c6d9f9ee9eedb75
Albert Zeyer <albzey@gmail.com>
2022-09-21 21:09:47 +0200

    dummy

--- users/zeyer/experiments/exp2022_07_21_transducer/test_recog.py
+++ users/zeyer/experiments/exp2022_07_21_transducer/test_recog.py
@@ -11,13 +11,18 @@ from .recog import ModelWithCheckpoint
 from .model import Checkpoint
 
 
+def _dummy_path_available(_) -> bool:
+    return True
+
+
 def sis_config_main():
     """sis config function"""
     task = get_nltk_timit_task()
 
     model = ModelWithCheckpoint(
         definition=from_scratch_model_def,
-        checkpoint=Checkpoint(index_path=tk.Path("dummy-non-existing-model.ckpt-100000.index")))
+        checkpoint=Checkpoint(
+            index_path=tk.Path("dummy-non-existing-model.ckpt-100000.index", available=_dummy_path_available)))
 
     tk.register_output('test_recog', recog(task, model, recog_def=model_recog).main_measure_value)
 
