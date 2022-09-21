0c25c40bccdb24d05ebebd289d97138e1503be50
Albert Zeyer <albzey@gmail.com>
2022-09-21 17:28:19 +0200

    test recog

---
+++ users/zeyer/experiments/exp2022_07_21_transducer/test_recog.py
@@ -0,0 +1,25 @@
+"""
+Demo/test for recog
+"""
+
+
+from __future__ import annotations
+from sisyphus import tk
+from .task import get_nltk_timit_task
+from .pipeline_swb_2020 import recog, model_recog, from_scratch_model_def
+from .recog import ModelWithCheckpoint
+from .model import Checkpoint
+
+
+def sis_config_main():
+    """sis config function"""
+    task = get_nltk_timit_task()
+
+    model = ModelWithCheckpoint(
+        definition=from_scratch_model_def,
+        checkpoint=Checkpoint(index_path=tk.Path("dummy-non-existing-model.ckpt-100000.index")))
+
+    tk.register_output('test_recog', recog(task, model, recog_def=model_recog).main_measure_value)
+
+
+py = sis_config_main  # `py` is the default sis config function name
