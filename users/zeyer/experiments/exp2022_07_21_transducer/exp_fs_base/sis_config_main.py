34d0f5b68ab2de6d83f4e39d4d9cacb5d3b33363
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-10-21 09:23:12 +0000

    better sis main

--- users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/sis_config_main.py
+++ users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/sis_config_main.py
@@ -17,11 +17,28 @@ def sis_config_main():
             continue
         if filename.startswith("_"):
             continue
+        # Read the file and search for expected content.
+        # This is much faster than importing the file,
+        # so good to exclude files which are not relevant.
+        file_content = open(filename).read().splitlines()
+        found_entry_function = False
+        found_exclude = False
+        for line in file_content[:30]:
+            if not found_entry_function and line.startswith("def sis_run_with_prefix("):
+                found_entry_function = True
+                continue
+            if line.startswith("_exclude_me = True"):
+                found_exclude = True
+                break
+        if found_exclude:
+            continue
+        if not found_entry_function:
+            continue
+        # Ok import it.
         basename = filename[:-len('.py')]
         name = f"{__package__}.{basename}"
         mod = importlib.import_module(name)
-        if not hasattr(mod, "sis_run_with_prefix"):
-            continue
+        assert hasattr(mod, "sis_run_with_prefix")
         mod.sis_run_with_prefix(prefix + basename)
 
 
