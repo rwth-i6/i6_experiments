30822c3d2550f25f25991ed5e69e1cfeff4a4822
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-11-17 09:44:26 +0000

    exclude

--- users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/conformer_pre10_d384_h6_blstmf2_specaug_attdrop01_posdrop01_aux48ff_ls01.py
+++ users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/conformer_pre10_d384_h6_blstmf2_specaug_attdrop01_posdrop01_aux48ff_ls01.py
@@ -16,7 +16,8 @@ from i6_experiments.users.zeyer.recog import recog_training_exp
 from ..train import train
 
 
-_exclude_me = False
+# Unstable, diverges in the end, needs less label smoothing, or more warmup, slower pretrain.
+_exclude_me = True
 
 
 def sis_run_with_prefix(prefix_name: str):
