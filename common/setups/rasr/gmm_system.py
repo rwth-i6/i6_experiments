862f62a808ea18e5decd5ce3c0b8ca220462b252
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2022-11-17 10:05:09 +0100

    remove wrong #

--- common/setups/rasr/gmm_system.py
+++ common/setups/rasr/gmm_system.py
@@ -1478,13 +1478,13 @@ class GmmSystem(RasrSystem):
             self.create_stm_from_corpus(eval_c, **stm_args)
             self._set_scorer_for_corpus(eval_c)
 
-            # ppl_job = lm.ComputePerplexityJob(
-            #    self.crp[eval_c],
-            #    corpus_recipes.CorpusToTxtJob(
-            #        self.crp[eval_c].corpus_config.file
-            #    ).out_txt,
-            # )
-            # tk.register_output(f"lms/{eval_c}.ppl", ppl_job.perplexity)
+            ppl_job = lm.ComputePerplexityJob(
+                self.crp[eval_c],
+                corpus_recipes.CorpusToTxtJob(
+                    self.crp[eval_c].corpus_config.file
+                ).out_txt,
+            )
+            tk.register_output(f"lms/{eval_c}.ppl", ppl_job.perplexity)
 
         for step_idx, (step_name, step_args) in enumerate(steps.get_step_iter()):
             # ---------- Feature Extraction ----------
