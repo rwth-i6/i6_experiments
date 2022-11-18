e6e61c6e67ec058dcefeee141997ea7908562f69
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-11-18 14:29:04 +0000

    datasets conversion

--- users/zeyer/datasets/switchboard_2020/task.py
+++ users/zeyer/datasets/switchboard_2020/task.py
@@ -48,3 +48,33 @@ def _bpe_to_words(bpe: RecogOutput) -> RecogOutput:
     """BPE to words"""
     words = SearchBPEtoWordsJob(bpe.output).out_word_search_results
     return RecogOutput(output=words)
+
+
+def _compare_old_new():
+    # Now that we have the new datasets interface, check that we get the same hash.
+    from sisyphus.hash import short_hash
+    task = get_switchboard_task_bpe1k()
+    # dataset in training
+    config = dict(
+        default_input=task.train_dataset.get_default_input(),
+        target=task.train_dataset.get_default_target(),
+        train=task.train_dataset.get_train_dataset(),
+        eval_datasets=task.train_dataset.get_eval_datasets(),
+    )
+    print("train dataset hash:", short_hash(config))
+    # datasets in recognition
+    for name, dataset in task.eval_datasets.items():
+        # dataset
+        config = dict(
+            default_input=dataset.get_default_input(),
+            target=dataset.get_default_target(),
+            dev=dataset.get_main_dataset(),
+        )
+        print(f"eval {name} dataset hash:", short_hash(config))
+    # train dataset hash: jqGQaAMPnEm6
+    # eval hub5e_00 dataset hash: RzkAWyRoWjqa
+    # eval hub5e_01 dataset hash: Y76oLHItr2Pt
+    # eval rt03s dataset hash: qgqYRU0DGiw1
+
+
+py = _compare_old_new
