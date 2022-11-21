c0bc6c6bcc6d00fec602ee7eda2c08203eb20069
Albert Zeyer <albzey@gmail.com>
2022-11-22 00:34:58 +0100

    inc behavior version

--- users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/conformer_ln_pre10_d384_h6_cnnblstmf2_chunk50_25_fix245_wdro_specaugweia_attdrop01_posdrop01_mhsapinit05_lsxx01.py
+++ users/zeyer/experiments/exp2022_07_21_transducer/exp_fs_base/conformer_ln_pre10_d384_h6_cnnblstmf2_chunk50_25_fix245_wdro_specaugweia_attdrop01_posdrop01_mhsapinit05_lsxx01.py
@@ -376,7 +376,7 @@ def from_scratch_model_def(*, epoch: int, in_dim: nn.Dim, target_dim: nn.Dim) ->
 
 
 from_scratch_model_def: ModelDef[Model]
-from_scratch_model_def.behavior_version = 14
+from_scratch_model_def.behavior_version = 16
 
 
 def from_scratch_training(*,
