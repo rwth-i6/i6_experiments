9a23ac4cae261503a494609fc40881bef5511d57
Christoph M. Lüscher <chris.luscher@gmx.de>
2023-01-04 15:21:07 +0100

    add dataclass

--- common/datasets/tedlium2/download.py
+++ common/datasets/tedlium2/download.py
@@ -1,6 +1,9 @@
 import os
+from dataclasses import dataclass
 from functools import lru_cache
 from typing import Any, Dict
+
+from sisyphus import tk
 
 from i6_core.datasets.tedlium2 import (
     DownloadTEDLIUM2CorpusJob,
@@ -8,17 +11,32 @@ from i6_core.datasets.tedlium2 import (
 )
 
 
+@dataclass()
+class TedLium2Data:
+    """Class for storing the TedLium2 data"""
+
+    data_dir: Dict[str, tk.Path]
+    lm_dir: tk.Path
+    vocab: tk.Path
+    bliss_nist: Dict[str, tk.Path]
+    stm: Dict[str, tk.Path]
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {
+            "data_dir": self.data_dir,
+            "lm_dir": self.lm_dir,
+            "vocab": self.vocab,
+            "bliss_nist": self.bliss_nist,
+            "stm": self.stm,
+        }
+
+
 @lru_cache()
 def download_data_dict(output_prefix: str = "datasets") -> Dict[str, Any]:
-    data_dict = {}
-
     download_tedlium2_job = DownloadTEDLIUM2CorpusJob()
     download_tedlium2_job.add_alias(
         os.path.join(output_prefix, "download", "raw_corpus_job")
     )
-    data_dict["data_dir"] = download_tedlium2_job.out_corpus_folders
-    data_dict["lm_dir"] = download_tedlium2_job.out_lm_folder
-    data_dict["vocab"] = download_tedlium2_job.out_vocab_dict
 
     bliss_corpus_tedlium2_job = CreateTEDLIUM2BlissCorpusJob(
         download_tedlium2_job.out_corpus_folders
@@ -26,7 +44,13 @@ def download_data_dict(output_prefix: str = "datasets") -> Dict[str, Any]:
     bliss_corpus_tedlium2_job.add_alias(
         os.path.join(output_prefix, "create_bliss", "bliss_corpus_job")
     )
-    data_dict["bliss_nist"] = bliss_corpus_tedlium2_job.out_corpus_files
-    data_dict["stm"] = bliss_corpus_tedlium2_job.out_stm_files
 
-    return data_dict
+    tl2_data = TedLium2Data(
+        data_dir=download_tedlium2_job.out_corpus_folders,
+        lm_dir=download_tedlium2_job.out_lm_folder,
+        vocab=download_tedlium2_job.out_vocab_dict,
+        bliss_nist=bliss_corpus_tedlium2_job.out_corpus_files,
+        stm=bliss_corpus_tedlium2_job.out_corpus_files,
+    )
+
+    return tl2_data.as_dict()
