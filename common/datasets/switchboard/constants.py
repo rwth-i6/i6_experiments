51dfdb5ccb95792d4bd7853eebc7ac4d7d961bd5
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2023-01-17 15:53:22 +0100

    change constants to 7,12,12

--- common/datasets/switchboard/constants.py
+++ common/datasets/switchboard/constants.py
@@ -2,11 +2,12 @@ SUBDIR_PREFIX = "Switchboard"
 
 durations = {"train": 311.62, "hub5e00": 3.61, "hub5e01": 6.17, "rt03s": 6.27}
 
+# about 1 per 0.5 hours for test and 1 per 5 hours for train
 concurrent = {
     "train": 60,
-    "hub5e00": 10,
-    "hub5e01": 10,
-    "rt03s": 10,
+    "hub5e00": 7,
+    "hub5e01": 12,
+    "rt03s": 12,
 }
 
 num_segments = {
