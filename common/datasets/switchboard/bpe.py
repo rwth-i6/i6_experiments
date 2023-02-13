e0a3ccee5aa78b99e4034e005b2e6ed40158be34
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2023-02-13 17:50:50 +0100

    get_subword_nmt_bpe update docs/typing

--- common/datasets/switchboard/bpe.py
+++ common/datasets/switchboard/bpe.py
@@ -9,18 +9,16 @@ from i6_experiments.common.helpers.text_labels.subword_nmt_bpe import (
 
 
 def get_subword_nmt_bpe(
-    bpe_size: int, unk_label: str = "<unk>", subdir_prefix=""
+    bpe_size: int, unk_label: str = "<unk>", subdir_prefix: str = ""
 ) -> BPESettings:
     """
     Get the BPE tokens via the Returnn subword-nmt for a Switchboard setup.
 
-    WARNING!
+    WARNING: This will not give compatible BPE to old setups!
 
-    This will not give compatible BPE to old setups!
-
-    :param int bpe_size: the number of BPE merge operations. This is NOT the resulting vocab size!
-    :param str unk_label: unknown label symbol
-    :param str subdir_prefix:
+    :param bpe_size: the number of BPE merge operations. This is NOT the resulting vocab size!
+    :param unk_label: unknown label symbol
+    :param subdir_prefix:
     """
     subword_nmt_repo = get_returnn_subword_nmt(output_prefix=subdir_prefix)
     train_corpus = get_train_bliss_corpus_ldc()
