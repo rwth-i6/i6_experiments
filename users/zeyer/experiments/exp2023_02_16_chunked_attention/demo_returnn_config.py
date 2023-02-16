d7a5adc49cd4a0117de1b8d552df2fd4a51edcc0
Albert Zeyer <albzey@gmail.com>
2023-02-16 13:54:05 +0100

    chunked attention, simplify demo

--- users/zeyer/experiments/exp2023_02_16_chunked_attention/demo_returnn_config.py
+++ users/zeyer/experiments/exp2023_02_16_chunked_attention/demo_returnn_config.py
@@ -17,55 +17,6 @@ from i6_experiments.users.zeineldeen.models.asr.encoder.conformer_encoder import
 from i6_experiments.users.zeineldeen.models.asr.decoder.rnn_decoder import RNNDecoder
 
 
-def _create_config():
-    exp_config = {}
-
-    # -------------------------- network -------------------------- #
-
-    conformer_encoder = ConformerEncoder(
-        target=target,
-        input_layer=None,
-        num_blocks=1,
-        specaug=False,
-        ff_dim=64,
-        enc_key_dim=32,
-        att_num_heads=1,
-        dropout=0.0,
-        att_dropout=0.0,
-        dropout_in=0.0,
-    )
-    conformer_encoder.create_network()
-
-    transformer_decoder = RNNDecoder(
-        base_model=conformer_encoder,
-        target=target,
-        embed_dim=32,
-        lstm_num_units=32,
-        output_num_units=64,
-        enc_key_dim=32,
-        lstm_lm_proj_dim=32,
-        dropout=0.0,
-        att_dropout=0.0,
-        att_num_heads=1,
-        embed_dropout=0.0,
-        softmax_dropout=0.0,
-        rec_weight_dropout=0.0,
-        label_smoothing=0.0,
-    )
-    transformer_decoder.create_network()
-
-    decision_layer_name = transformer_decoder.decision_layer_name
-    exp_config["search_output_layer"] = decision_layer_name
-
-    # add full network
-    exp_config["network"] = conformer_encoder.network.get_net()  # type: dict
-    exp_config["network"].update(transformer_decoder.network.get_net())
-
-    # -------------------------- end network -------------------------- #
-
-    return exp_config
-
-
 config = get_global_config()
 task = config.value("task", "train")
 use_tensorflow = True
@@ -88,10 +39,47 @@ extern_data = {
     "classes": {"dim": 2, "sparse": True},
 }
 
-globals().update(_create_config())
-
 
 batch_size = 10000
 optimizer = {"class": "adam", "epsilon": 1e-8}
 learning_rate = 0.01
 num_epochs = 100
+
+
+conformer_encoder = ConformerEncoder(
+    target=target,
+    input_layer=None,
+    num_blocks=1,
+    specaug=False,
+    ff_dim=64,
+    enc_key_dim=32,
+    att_num_heads=1,
+    dropout=0.0,
+    att_dropout=0.0,
+    dropout_in=0.0,
+)
+conformer_encoder.create_network()
+
+transformer_decoder = RNNDecoder(
+    base_model=conformer_encoder,
+    target=target,
+    embed_dim=32,
+    lstm_num_units=32,
+    output_num_units=64,
+    enc_key_dim=32,
+    lstm_lm_proj_dim=32,
+    dropout=0.0,
+    att_dropout=0.0,
+    att_num_heads=1,
+    embed_dropout=0.0,
+    softmax_dropout=0.0,
+    rec_weight_dropout=0.0,
+    label_smoothing=0.0,
+)
+transformer_decoder.create_network()
+
+search_output_layer = transformer_decoder.decision_layer_name
+
+# add full network
+network = conformer_encoder.network.get_net()  # type: dict
+network.update(transformer_decoder.network.get_net())
