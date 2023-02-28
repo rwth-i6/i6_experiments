2abee627d3a1001a725883341f64c6219022624f
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-02-28 12:37:26 +0000

    black

--- users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/data.py
+++ users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/data.py
@@ -3,10 +3,18 @@ from functools import lru_cache
 from typing import Dict, Any, Optional
 
 from i6_core.returnn import CodeWrapper
-from i6_experiments.common.datasets.librispeech import get_subword_nmt_bpe, get_ogg_zip_dict, get_bliss_corpus_dict
-from i6_experiments.users.rossenbach.common_setups.returnn.datastreams.audio import AudioRawDatastream, \
-    ReturnnAudioRawOptions
-from i6_experiments.users.rossenbach.common_setups.returnn.datastreams.vocabulary import BpeDatastream
+from i6_experiments.common.datasets.librispeech import (
+    get_subword_nmt_bpe,
+    get_ogg_zip_dict,
+    get_bliss_corpus_dict,
+)
+from i6_experiments.users.rossenbach.common_setups.returnn.datastreams.audio import (
+    AudioRawDatastream,
+    ReturnnAudioRawOptions,
+)
+from i6_experiments.users.rossenbach.common_setups.returnn.datastreams.vocabulary import (
+    BpeDatastream,
+)
 
 from returnn_common.datasets import Dataset, OggZipDataset, MetaDataset, HDFDataset
 
@@ -23,11 +31,11 @@ def get_bpe_datastream(bpe_size, is_recog):
     :return:
     """
     # build dataset
-    bpe_settings = get_subword_nmt_bpe(corpus_key="train-other-960", bpe_size=bpe_size, unk_label='<unk>')
+    bpe_settings = get_subword_nmt_bpe(
+        corpus_key="train-other-960", bpe_size=bpe_size, unk_label="<unk>"
+    )
     bpe_targets = BpeDatastream(
-        available_for_inference=False,
-        bpe_settings=bpe_settings,
-        use_unk_label=is_recog
+        available_for_inference=False, bpe_settings=bpe_settings, use_unk_label=is_recog
     )
     return bpe_targets
 
@@ -36,7 +44,9 @@ def get_bpe_datastream(bpe_size, is_recog):
 def get_audio_raw_datastream(preemphasis=None):
     audio_datastream = AudioRawDatastream(
         available_for_inference=True,
-        options=ReturnnAudioRawOptions(peak_normalization=True, preemphasis=preemphasis)
+        options=ReturnnAudioRawOptions(
+            peak_normalization=True, preemphasis=preemphasis
+        ),
     )
     return audio_datastream
 
@@ -50,14 +60,14 @@ class TrainingDatasets:
 
 
 def build_training_datasets(
-        bpe_size=10000,
-        partition_epoch=20,
-        epoch_wise_filter=None,
-        seq_ordering="laplace:.1000",
-        link_speed_perturbation=False,
-        use_raw_features=False,
-        preemphasis=None,  # TODO: by default is None, but we want to experiment
-    ):
+    bpe_size=10000,
+    partition_epoch=20,
+    epoch_wise_filter=None,
+    seq_ordering="laplace:.1000",
+    link_speed_perturbation=False,
+    use_raw_features=False,
+    preemphasis=None,  # TODO: by default is None, but we want to experiment
+):
     """
 
     :param returnn_python_exe:
@@ -69,9 +79,9 @@ def build_training_datasets(
     :return:
     """
     ogg_zip_dict = get_ogg_zip_dict("corpora")
-    train_clean_960_ogg = ogg_zip_dict['train-other-960']
-    dev_clean_ogg = ogg_zip_dict['dev-clean']
-    dev_other_ogg = ogg_zip_dict['dev-other']
+    train_clean_960_ogg = ogg_zip_dict["train-other-960"]
+    dev_clean_ogg = ogg_zip_dict["dev-clean"]
+    dev_other_ogg = ogg_zip_dict["dev-other"]
 
     train_bpe_datastream = get_bpe_datastream(bpe_size=bpe_size, is_recog=False)
 
@@ -81,13 +91,14 @@ def build_training_datasets(
         raise NotImplementedError
 
     extern_data = {
-        'audio_features': audio_datastream.as_returnn_extern_data_opts(),
-        'bpe_labels': train_bpe_datastream.as_returnn_extern_data_opts()
+        "audio_features": audio_datastream.as_returnn_extern_data_opts(),
+        "bpe_labels": train_bpe_datastream.as_returnn_extern_data_opts(),
     }
 
-    data_map = {"audio_features": ("zip_dataset", "data"),
-                "bpe_labels": ("zip_dataset", "classes")}
-
+    data_map = {
+        "audio_features": ("zip_dataset", "data"),
+        "bpe_labels": ("zip_dataset", "classes"),
+    }
 
     training_audio_opts = audio_datastream.as_returnn_audio_opts()
     if link_speed_perturbation:
@@ -95,9 +106,11 @@ def build_training_datasets(
 
     additional_opts = {}
     if epoch_wise_filter:
-        additional_opts['epoch_wise_filter'] = {}
+        additional_opts["epoch_wise_filter"] = {}
         for fr, to, max_mean_len in epoch_wise_filter:
-            additional_opts['epoch_wise_filter'][(fr, to)] = {"max_mean_len": max_mean_len}
+            additional_opts["epoch_wise_filter"][(fr, to)] = {
+                "max_mean_len": max_mean_len
+            }
 
     train_zip_dataset = OggZipDataset(
         files=train_clean_960_ogg,
@@ -110,21 +123,24 @@ def build_training_datasets(
     train_dataset = MetaDataset(
         data_map=data_map,
         datasets={"zip_dataset": train_zip_dataset},
-        seq_order_control_dataset="zip_dataset"
+        seq_order_control_dataset="zip_dataset",
     )
 
-    from i6_experiments.users.rossenbach.datasets.librispeech import get_mixed_cv_segments
+    from i6_experiments.users.rossenbach.datasets.librispeech import (
+        get_mixed_cv_segments,
+    )
+
     cv_zip_dataset = OggZipDataset(
         files=[dev_clean_ogg, dev_other_ogg],
         audio_options=audio_datastream.as_returnn_audio_opts(),
         target_options=train_bpe_datastream.as_returnn_targets_opts(),
         segment_file=get_mixed_cv_segments(),
-        seq_ordering="sorted_reverse"
+        seq_ordering="sorted_reverse",
     )
     cv_dataset = MetaDataset(
         data_map=data_map,
         datasets={"zip_dataset": cv_zip_dataset},
-        seq_order_control_dataset="zip_dataset"
+        seq_order_control_dataset="zip_dataset",
     )
 
     devtrain_zip_dataset = OggZipDataset(
@@ -137,14 +153,14 @@ def build_training_datasets(
     devtrain_dataset = MetaDataset(
         data_map=data_map,
         datasets={"zip_dataset": devtrain_zip_dataset},
-        seq_order_control_dataset="zip_dataset"
+        seq_order_control_dataset="zip_dataset",
     )
 
     return TrainingDatasets(
         train=train_dataset,
         cv=cv_dataset,
         devtrain=devtrain_dataset,
-        extern_data=extern_data
+        extern_data=extern_data,
     )
 
 
@@ -159,101 +175,111 @@ def build_chunkwise_training_datasets(
     use_raw_features=False,
     preemphasis=None,  # TODO: by default is None, but we want to experiment
 ):
-  """
+    """
 
-  :param returnn_python_exe:
-  :param returnn_root:
-  :param output_path:
-  :param int bpe_size:
-  :param int bpe_partition_epoch:
-  :param str seq_ordering:
-  :return:
-  """
-  ogg_zip_dict = get_ogg_zip_dict("corpora")
-  train_clean_960_ogg = ogg_zip_dict['train-other-960']
-  dev_clean_ogg = ogg_zip_dict['dev-clean']
-  dev_other_ogg = ogg_zip_dict['dev-other']
+    :param returnn_python_exe:
+    :param returnn_root:
+    :param output_path:
+    :param int bpe_size:
+    :param int bpe_partition_epoch:
+    :param str seq_ordering:
+    :return:
+    """
+    ogg_zip_dict = get_ogg_zip_dict("corpora")
+    train_clean_960_ogg = ogg_zip_dict["train-other-960"]
+    dev_clean_ogg = ogg_zip_dict["dev-clean"]
+    dev_other_ogg = ogg_zip_dict["dev-other"]
 
-  train_bpe_datastream = get_bpe_datastream(bpe_size=bpe_size, is_recog=False)
+    train_bpe_datastream = get_bpe_datastream(bpe_size=bpe_size, is_recog=False)
 
-  if use_raw_features:
-    audio_datastream = get_audio_raw_datastream(preemphasis)
-  else:
-    raise NotImplementedError
+    if use_raw_features:
+        audio_datastream = get_audio_raw_datastream(preemphasis)
+    else:
+        raise NotImplementedError
 
-  extern_data = {
-    'audio_features': audio_datastream.as_returnn_extern_data_opts(),
-    'bpe_labels': train_bpe_datastream.as_returnn_extern_data_opts()  # TODO: +1 for CTC alignment
-  }
+    extern_data = {
+        "audio_features": audio_datastream.as_returnn_extern_data_opts(),
+        "bpe_labels": train_bpe_datastream.as_returnn_extern_data_opts(),  # TODO: +1 for CTC alignment
+    }
 
-  data_map = {
-    "audio_features": ("zip_dataset", "data"),
-    "bpe_labels": ("chunk_hdf_dataset", "data")
-  }
+    data_map = {
+        "audio_features": ("zip_dataset", "data"),
+        "bpe_labels": ("chunk_hdf_dataset", "data"),
+    }
 
-  training_audio_opts = audio_datastream.as_returnn_audio_opts()
-  if link_speed_perturbation:
-    training_audio_opts["pre_process"] = CodeWrapper("speed_pert")
+    training_audio_opts = audio_datastream.as_returnn_audio_opts()
+    if link_speed_perturbation:
+        training_audio_opts["pre_process"] = CodeWrapper("speed_pert")
 
-  additional_opts = {}
-  if epoch_wise_filter:
-    additional_opts['epoch_wise_filter'] = {}
-    for fr, to, max_mean_len in epoch_wise_filter:
-      additional_opts['epoch_wise_filter'][(fr, to)] = {"max_mean_len": max_mean_len}
+    additional_opts = {}
+    if epoch_wise_filter:
+        additional_opts["epoch_wise_filter"] = {}
+        for fr, to, max_mean_len in epoch_wise_filter:
+            additional_opts["epoch_wise_filter"][(fr, to)] = {
+                "max_mean_len": max_mean_len
+            }
 
-  train_zip_dataset = OggZipDataset(
-    files=train_clean_960_ogg,
-    audio_options=training_audio_opts,
-    target_options=None,
-    partition_epoch=partition_epoch,
-    seq_ordering=seq_ordering,
-    additional_options=additional_opts,
-  )
+    train_zip_dataset = OggZipDataset(
+        files=train_clean_960_ogg,
+        audio_options=training_audio_opts,
+        target_options=None,
+        partition_epoch=partition_epoch,
+        seq_ordering=seq_ordering,
+        additional_options=additional_opts,
+    )
 
-  train_hdf_dataset = HDFDataset(
-    files=[train_fixed_alignment],
-  )
+    train_hdf_dataset = HDFDataset(
+        files=[train_fixed_alignment],
+    )
 
-  train_dataset = MetaDataset(
-    data_map=data_map,
-    datasets={"zip_dataset": train_zip_dataset, 'chunk_hdf_dataset': train_hdf_dataset},
-    seq_order_control_dataset="zip_dataset",
-  )
+    train_dataset = MetaDataset(
+        data_map=data_map,
+        datasets={
+            "zip_dataset": train_zip_dataset,
+            "chunk_hdf_dataset": train_hdf_dataset,
+        },
+        seq_order_control_dataset="zip_dataset",
+    )
 
-  from i6_experiments.users.rossenbach.datasets.librispeech import get_mixed_cv_segments
-  cv_zip_dataset = OggZipDataset(
-    files=[dev_clean_ogg, dev_other_ogg],
-    audio_options=audio_datastream.as_returnn_audio_opts(),
-    target_options=None,
-    segment_file=get_mixed_cv_segments(),
-    seq_ordering="sorted_reverse"
-  )
+    from i6_experiments.users.rossenbach.datasets.librispeech import (
+        get_mixed_cv_segments,
+    )
 
-  cv_hdf_dataset = HDFDataset(
-    files=[cv_fixed_alignment],
-  )
+    cv_zip_dataset = OggZipDataset(
+        files=[dev_clean_ogg, dev_other_ogg],
+        audio_options=audio_datastream.as_returnn_audio_opts(),
+        target_options=None,
+        segment_file=get_mixed_cv_segments(),
+        seq_ordering="sorted_reverse",
+    )
 
-  cv_dataset = MetaDataset(
-    data_map=data_map,
-    datasets={"zip_dataset": cv_zip_dataset, "chunk_hdf_dataset": cv_hdf_dataset},
-    seq_order_control_dataset="zip_dataset"
-  )
+    cv_hdf_dataset = HDFDataset(
+        files=[cv_fixed_alignment],
+    )
 
-  return TrainingDatasets(
-    train=train_dataset,
-    cv=cv_dataset,
-    devtrain=None,
-    extern_data=extern_data
-  )
+    cv_dataset = MetaDataset(
+        data_map=data_map,
+        datasets={"zip_dataset": cv_zip_dataset, "chunk_hdf_dataset": cv_hdf_dataset},
+        seq_order_control_dataset="zip_dataset",
+    )
+
+    return TrainingDatasets(
+        train=train_dataset, cv=cv_dataset, devtrain=None, extern_data=extern_data
+    )
+
 
 @lru_cache()
-def build_test_dataset(dataset_key, bpe_size=10000, use_raw_features=False, preemphasis=None):
-
+def build_test_dataset(
+    dataset_key, bpe_size=10000, use_raw_features=False, preemphasis=None
+):
     ogg_zip_dict = get_ogg_zip_dict("corpora")
     bliss_dict = get_bliss_corpus_dict()
     test_ogg = ogg_zip_dict[dataset_key]
     from i6_core.corpus.convert import CorpusToTextDictJob
-    test_reference_dict_file = CorpusToTextDictJob(bliss_dict[dataset_key]).out_dictionary
+
+    test_reference_dict_file = CorpusToTextDictJob(
+        bliss_dict[dataset_key]
+    ).out_dictionary
 
     train_bpe_datastream = get_bpe_datastream(bpe_size=bpe_size, is_recog=True)
 
@@ -262,19 +288,21 @@ def build_test_dataset(dataset_key, bpe_size=10000, use_raw_features=False, pree
     else:
         raise NotImplementedError
 
-    data_map = {"audio_features": ("zip_dataset", "data"),
-                "bpe_labels": ("zip_dataset", "classes")}
+    data_map = {
+        "audio_features": ("zip_dataset", "data"),
+        "bpe_labels": ("zip_dataset", "classes"),
+    }
 
     test_zip_dataset = OggZipDataset(
         files=[test_ogg],
         audio_options=audio_datastream.as_returnn_audio_opts(),
         target_options=train_bpe_datastream.as_returnn_targets_opts(),
-        seq_ordering="sorted_reverse"
+        seq_ordering="sorted_reverse",
     )
     test_dataset = MetaDataset(
         data_map=data_map,
         datasets={"zip_dataset": test_zip_dataset},
-        seq_order_control_dataset="zip_dataset"
+        seq_order_control_dataset="zip_dataset",
     )
 
     return test_dataset, test_reference_dict_file
