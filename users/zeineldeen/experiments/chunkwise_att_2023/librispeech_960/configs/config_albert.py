ef35555a91e9aa82ade1068e7cfbadb6994e1670
Albert Zeyer <albzey@gmail.com>
2023-03-02 23:44:20 +0100

    more

--- users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/config_albert.py
+++ users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/config_albert.py
@@ -10,57 +10,64 @@ from .chunkwise_att_base import get_ctc_rna_based_chunk_alignments, prefix_name,
 
 def sis_config_main():
     """sis config function"""
-    chunk_size = 20
-    chunk_step_factor = 0.9
-
     total_epochs = 40
     start_lr = 1e-4
     decay_pt_factor = 1 / 3
 
     for chunk_level in ["encoder", "input"]:
-        ctc_align_wo_speed_pert = get_ctc_rna_based_chunk_alignments(
-            chunk_sizes=[chunk_size],
-            chunk_step_factors=[chunk_step_factor],
-        )
+        for chunk_size in (
+            [20]
+            if chunk_level == "encoder"
+            else [
+                20,
+                50,
+            ]
+        ):
+            chunk_step_factor = 0.9
 
-        # train with ctc chunk-sync alignment
-        train_args = copy.deepcopy(default_args)
-        train_args["speed_pert"] = False  # no speed pert
-        train_args["search_type"] = None  # fixed alignment
+            ctc_align_wo_speed_pert = get_ctc_rna_based_chunk_alignments(
+                chunk_sizes=[chunk_size],
+                chunk_step_factors=[chunk_step_factor],
+            )
 
-        train_args["max_seq_length"] = None  # no filtering!
+            # train with ctc chunk-sync alignment
+            train_args = copy.deepcopy(default_args)
+            train_args["speed_pert"] = False  # no speed pert
+            train_args["search_type"] = None  # fixed alignment
 
-        train_args["encoder_args"].with_ctc = False  # No CTC
+            train_args["max_seq_length"] = None  # no filtering!
 
-        decay_pt = int(total_epochs * decay_pt_factor)
+            train_args["encoder_args"].with_ctc = False  # No CTC
 
-        train_args["chunk_size"] = chunk_size
+            decay_pt = int(total_epochs * decay_pt_factor)
 
-        chunk_step = max(1, int(chunk_size * chunk_step_factor))
-        train_args["chunk_step"] = chunk_step
+            train_args["chunk_size"] = chunk_size
 
-        train_args["chunk_level"] = chunk_level
+            chunk_step = max(1, int(chunk_size * chunk_step_factor))
+            train_args["chunk_step"] = chunk_step
 
-        train_args["learning_rates_list"] = [start_lr] * decay_pt + list(
-            numpy.linspace(start_lr, 1e-6, total_epochs - decay_pt)
-        )
+            train_args["chunk_level"] = chunk_level
 
-        run_exp(
-            prefix_name=prefix_name,
-            exp_name=f"chunk{chunk_level}_att"
-            f"_chunk-{chunk_size}_step-{chunk_step}"
-            f"_linDecay{total_epochs}_{start_lr}_decayPt{decay_pt_factor}"
-            f"_fixed_align",
-            train_args=train_args,
-            num_epochs=total_epochs,
-            train_fixed_alignment=ctc_align_wo_speed_pert["train"][f"{chunk_size}_{chunk_step}"],
-            cv_fixed_alignment=ctc_align_wo_speed_pert["dev"][f"{chunk_size}_{chunk_step}"],
-            epoch_wise_filter=None,
-            time_rqmt=72,
-            selected_datasets=["dev-other"],
-            key="dev_score",
-            use_sclite=True,
-        )
+            train_args["learning_rates_list"] = [start_lr] * decay_pt + list(
+                numpy.linspace(start_lr, 1e-6, total_epochs - decay_pt)
+            )
+
+            run_exp(
+                prefix_name=prefix_name,
+                exp_name=f"chunk{chunk_level}_att"
+                f"_chunk-{chunk_size}_step-{chunk_step}"
+                f"_linDecay{total_epochs}_{start_lr}_decayPt{decay_pt_factor}"
+                f"_fixed_align",
+                train_args=train_args,
+                num_epochs=total_epochs,
+                train_fixed_alignment=ctc_align_wo_speed_pert["train"][f"{chunk_size}_{chunk_step}"],
+                cv_fixed_alignment=ctc_align_wo_speed_pert["dev"][f"{chunk_size}_{chunk_step}"],
+                epoch_wise_filter=None,
+                time_rqmt=72,
+                selected_datasets=["dev-other"],
+                key="dev_score",
+                use_sclite=True,
+            )
 
 
 py = sis_config_main  # `py` is the default sis config function name
