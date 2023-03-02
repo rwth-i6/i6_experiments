cb0b948f47fa16274160e6b96d5dd47b37e88a77
Albert Zeyer <albzey@gmail.com>
2023-03-02 11:37:15 +0100

    black, prepare seq_postfix logic

--- users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/data.py
+++ users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/data.py
@@ -20,7 +20,7 @@ from returnn_common.datasets import Dataset, OggZipDataset, MetaDataset, HDFData
 
 
 @lru_cache()
-def get_bpe_datastream(bpe_size, is_recog):
+def get_bpe_datastream(bpe_size, is_recog, seq_postfix=0):
     """
     Returns the datastream for the bpe labels
 
@@ -28,15 +28,13 @@ def get_bpe_datastream(bpe_size, is_recog):
 
     :param int bpe_size: size for the bpe labels
     :param bool is_recog: removes the UNK label when not in training
+    :param seq_postfix: postfix for all sequences
     :return:
     """
     # build dataset
-    bpe_settings = get_subword_nmt_bpe(
-        corpus_key="train-other-960", bpe_size=bpe_size, unk_label="<unk>"
-    )
-    bpe_targets = BpeDatastream(
-        available_for_inference=False, bpe_settings=bpe_settings, use_unk_label=is_recog
-    )
+    bpe_settings = get_subword_nmt_bpe(corpus_key="train-other-960", bpe_size=bpe_size, unk_label="<unk>")
+    bpe_targets = BpeDatastream(available_for_inference=False, bpe_settings=bpe_settings, use_unk_label=is_recog)
+    bpe_targets.seq_postfix = seq_postfix
     return bpe_targets
 
 
@@ -44,9 +42,7 @@ def get_bpe_datastream(bpe_size, is_recog):
 def get_audio_raw_datastream(preemphasis=None):
     audio_datastream = AudioRawDatastream(
         available_for_inference=True,
-        options=ReturnnAudioRawOptions(
-            peak_normalization=True, preemphasis=preemphasis
-        ),
+        options=ReturnnAudioRawOptions(peak_normalization=True, preemphasis=preemphasis),
     )
     return audio_datastream
 
@@ -108,9 +104,7 @@ def build_training_datasets(
     if epoch_wise_filter:
         additional_opts["epoch_wise_filter"] = {}
         for fr, to, max_mean_len in epoch_wise_filter:
-            additional_opts["epoch_wise_filter"][(fr, to)] = {
-                "max_mean_len": max_mean_len
-            }
+            additional_opts["epoch_wise_filter"][(fr, to)] = {"max_mean_len": max_mean_len}
 
     train_zip_dataset = OggZipDataset(
         files=train_clean_960_ogg,
@@ -215,9 +209,7 @@ def build_chunkwise_training_datasets(
     if epoch_wise_filter:
         additional_opts["epoch_wise_filter"] = {}
         for fr, to, max_mean_len in epoch_wise_filter:
-            additional_opts["epoch_wise_filter"][(fr, to)] = {
-                "max_mean_len": max_mean_len
-            }
+            additional_opts["epoch_wise_filter"][(fr, to)] = {"max_mean_len": max_mean_len}
 
     train_zip_dataset = OggZipDataset(
         files=train_clean_960_ogg,
@@ -263,23 +255,17 @@ def build_chunkwise_training_datasets(
         seq_order_control_dataset="zip_dataset",
     )
 
-    return TrainingDatasets(
-        train=train_dataset, cv=cv_dataset, devtrain=None, extern_data=extern_data
-    )
+    return TrainingDatasets(train=train_dataset, cv=cv_dataset, devtrain=None, extern_data=extern_data)
 
 
 @lru_cache()
-def build_test_dataset(
-    dataset_key, bpe_size=10000, use_raw_features=False, preemphasis=None
-):
+def build_test_dataset(dataset_key, bpe_size=10000, use_raw_features=False, preemphasis=None):
     ogg_zip_dict = get_ogg_zip_dict("corpora")
     bliss_dict = get_bliss_corpus_dict()
     test_ogg = ogg_zip_dict[dataset_key]
     from i6_core.corpus.convert import CorpusToTextDictJob
 
-    test_reference_dict_file = CorpusToTextDictJob(
-        bliss_dict[dataset_key]
-    ).out_dictionary
+    test_reference_dict_file = CorpusToTextDictJob(bliss_dict[dataset_key]).out_dictionary
 
     train_bpe_datastream = get_bpe_datastream(bpe_size=bpe_size, is_recog=True)
 
