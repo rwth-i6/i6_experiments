efce28c0c2155696e9467962c7ee01648cbb0c7e
Albert Zeyer <albzey@gmail.com>
2023-03-04 21:12:54 +0100

    more

--- users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/config_albert.py
+++ users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/config_albert.py
@@ -3,12 +3,65 @@ For Albert
 """
 
 from __future__ import annotations
+from typing import Optional
 import numpy
 import copy
 from .chunkwise_att_base import get_ctc_rna_based_chunk_alignments, prefix_name, default_args, run_exp
 
 
-def _run_exp_v1(
+def _run_exp_baseline_v1(
+    *,
+    enc_stream_type: Optional[str],
+    total_epochs: int,
+):
+    start_lr = 1e-4
+    decay_pt_factor = 1 / 3
+
+    # train with ctc chunk-sync alignment
+    train_args = copy.deepcopy(default_args)
+    train_args["speed_pert"] = False  # no speed pert
+    train_args["search_type"] = None  # fixed alignment
+
+    train_args["max_seq_length"] = None  # no filtering!
+
+    if enc_stream_type == "causal" or enc_stream_type.startswith("causal-"):
+        train_args["encoder_args"].use_causal_layers = True
+        if enc_stream_type == "causal-reset-conv":
+            train_args["encoder_args"].conv_alternative_name = "depthwise_conv2_causal"
+
+    decay_pt = int(total_epochs * decay_pt_factor)
+
+    if enc_stream_type == "chunked":
+        train_args["chunk_level"] = "input-encoder-only"  # TODO...
+
+        # It needs more memory because there are mini batches
+        # where the chunk size is larger than the sequences,
+        # thus increasing the overall memory consumption of the whole encoder.
+        train_args["batch_size"] = int(train_args["batch_size"] * 0.75)
+        train_args["accum_grad"] = int(train_args.get("accum_grad", 2) * 1.5)
+        raise NotImplementedError  # TODO
+
+    train_args["learning_rates_list"] = [start_lr] * decay_pt + list(
+        numpy.linspace(start_lr, 1e-6, total_epochs - decay_pt)
+    )
+
+    run_exp(
+        prefix_name=prefix_name,
+        exp_name=f"baseline"
+        f"_enc-{enc_stream_type}-conf"
+        f"_linDecay{total_epochs}_{start_lr}_decayPt{decay_pt_factor}"
+        f"_fixed_align",
+        train_args=train_args,
+        num_epochs=total_epochs,
+        epoch_wise_filter=None,
+        time_rqmt=72,
+        selected_datasets=["dev-other"],
+        key="dev_score",
+        use_sclite=True,
+    )
+
+
+def _run_exp_chunked_v1(
     *,
     enc_stream_type: str,
     chunk_size: int,
@@ -79,17 +132,18 @@ def _run_exp_v1(
 
 def sis_config_main():
     """sis config function"""
-    _run_exp_v1(enc_stream_type="chunked", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="causal", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="causal-reset-conv", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="chunked", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="causal", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="global", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
-    _run_exp_v1(enc_stream_type="chunked", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
-    _run_exp_v1(enc_stream_type="causal", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
-    _run_exp_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
-    _run_exp_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.5, total_epochs=40)
+    _run_exp_baseline_v1(enc_stream_type="causal", total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="chunked", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="causal", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="causal-reset-conv", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="chunked", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="causal", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="global", chunk_size=50, chunk_step_factor=0.9, total_epochs=40)
+    _run_exp_chunked_v1(enc_stream_type="chunked", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
+    _run_exp_chunked_v1(enc_stream_type="causal", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
+    _run_exp_chunked_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.9, total_epochs=100)
+    _run_exp_chunked_v1(enc_stream_type="global", chunk_size=20, chunk_step_factor=0.5, total_epochs=40)
 
 
 py = sis_config_main  # `py` is the default sis config function name
