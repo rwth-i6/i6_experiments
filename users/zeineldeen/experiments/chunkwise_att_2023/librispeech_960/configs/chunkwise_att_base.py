c0cc14a24f3c9eccc6e61a78b4e1fad8d620b3ae
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-03-09 20:09:52 +0100

    add approx full sum

--- users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/chunkwise_att_base.py
+++ users/zeineldeen/experiments/chunkwise_att_2023/librispeech_960/configs/chunkwise_att_base.py
@@ -982,7 +982,7 @@ def run_chunkwise_train(
     chunk_sizes=None,
     chunk_step_factors=None,
     suffix="",
-    enable_check_align=False,
+    enable_check_align=True,
     **kwargs,
 ):
     # train with ctc chunk-sync alignment
@@ -1054,6 +1054,86 @@ def run_chunkwise_train(
                             )
 
 
+def _run_exp_full_sum_simple_approx(
+    *,
+    enc_stream_type: Optional[str],
+    chunk_size: int,
+    chunk_step_factor: float,
+    total_epochs: int,
+    with_ctc: bool = False,
+):
+    start_lr = 1e-4
+    decay_pt_factor = 1 / 3
+    train_args = copy.deepcopy(default_args)
+
+    train_args["speed_pert"] = False  # no speed pert
+    train_args["search_type"] = None
+    train_args["max_seq_length"] = None  # no filtering!
+
+    train_args["encoder_args"].with_ctc = with_ctc
+
+    if enc_stream_type == "causal" or enc_stream_type.startswith("causal-"):
+        train_args["encoder_args"].use_causal_layers = True
+        if enc_stream_type == "causal-reset-conv":
+            train_args["encoder_args"].conv_alternative_name = "depthwise_conv2_causal"
+            train_args.setdefault("retrain_checkpoint_opts", {}).setdefault("ignore_params_prefixes", []).extend(
+                [
+                    "conformer_block_%02i_conv_mod_depthwise_conv2_causal/" % (i + 1)
+                    for i in range(train_args["encoder_args"].num_blocks)
+                ]
+            )
+
+    decay_pt = int(total_epochs * decay_pt_factor)
+
+    train_args["chunk_size"] = chunk_size
+
+    chunk_step = max(1, int(chunk_size * chunk_step_factor))
+    train_args["chunk_step"] = chunk_step
+
+    chunk_level = "input" if enc_stream_type == "chunked" else "encoder"
+    train_args["chunk_level"] = chunk_level
+    train_args["eoc_idx"] = 0
+    train_args["decoder_args"].prev_target_embed_direct = True
+    train_args["decoder_args"].full_sum_simple_approx = True
+
+    if chunk_level == "input":
+        # It needs more memory because there are mini batches
+        # where the chunk size is larger than the sequences,
+        # thus increasing the overall memory consumption of the whole encoder.
+        train_args["batch_size"] = int(train_args["batch_size"] * 0.75)
+        train_args["accum_grad"] = int(train_args.get("accum_grad", 2) * 1.5)
+
+    train_args["learning_rates_list"] = [start_lr] * decay_pt + list(
+        numpy.linspace(start_lr, 1e-6, total_epochs - decay_pt)
+    )
+
+    train_args["enable_check_align"] = False
+
+    train_args["batch_size"] = int(0.75 * train_args["batch_size"])
+    train_args["accum_grad"] = int(1.5 * train_args.get("accum_grad", 2))
+
+    exp_name_parts = [
+        "chunk_att_simpleFS",
+        f"enc-{enc_stream_type}-conf",
+        f"chunksize-{chunk_size}",
+        f"chunkstep-{chunk_step}",
+        f"linDecay{total_epochs}_{start_lr}_decayPt{decay_pt_factor}",
+        f"ctc{with_ctc}",
+    ]
+
+    run_exp(
+        prefix_name=prefix_name,
+        exp_name="_".join(exp_name_parts),
+        train_args=train_args,
+        num_epochs=total_epochs,
+        epoch_wise_filter=None,
+        time_rqmt=72,
+        selected_datasets=["dev-other"],
+        key="dev_score_output/output_prob" if with_ctc else "dev_score",
+        use_sclite=True,
+    )
+
+
 def baseline():
     ctc_align_wo_speed_pert = get_ctc_rna_based_chunk_alignments(
         fixed_ctc_rna_align_without_eos=False,
@@ -1064,18 +1144,12 @@ def baseline():
     # imported from `/work/asr4/zeyer/setups-data/combined/2021-05-31/work`
     fixed_ctc_align_wo_speed_pert = get_ctc_rna_based_chunk_alignments(fixed_ctc_rna_align_without_eos=True)
 
-    run_chunkwise_train(ctc_align_wo_speed_pert, chunk_step_factors=[1, 1 / 2, 3 / 4])
+    run_chunkwise_train(ctc_align_wo_speed_pert, chunk_step_factors=[1, 1 / 2, 3 / 4], enable_check_align=False)
 
     # TODO: run exps with fixed align
-    run_chunkwise_train(fixed_ctc_align_wo_speed_pert, suffix="_wo_eos", run_all_for_best_last_avg=True)
-
-    # for more chunks
-    # run_chunkwise_train(
-    #     fixed_ctc_align_wo_speed_pert,
-    #     suffix="_wo_eos",
-    #     run_all_for_best_last_avg=True,
-    #     chunk_sizes=[15, 25, 35, 45],
-    # )
+    run_chunkwise_train(
+        fixed_ctc_align_wo_speed_pert, suffix="_wo_eos", run_all_for_best_last_avg=True, enable_check_align=False
+    )
 
     # TODO: longer training for chunk sizes 1 and 2
     run_chunkwise_train(
@@ -1084,6 +1158,7 @@ def baseline():
         chunk_sizes=[1, 2],
         total_epochs=[150, 200, 250],
         run_all_for_best_last_avg=True,
+        enable_check_align=False,
     )
 
     # TODO: alignment on-the-fly
@@ -1093,6 +1168,11 @@ def baseline():
         run_all_for_best_last_avg=True,
         chunk_step_factors=[1, 0.75],
         total_epochs=[100],
+        enable_check_align=False,
     )
 
-    # TODO: causal conformer encoder
+    # TODO: approximated full-sum
+    for chunk_size in [10, 20, 30]:
+        _run_exp_full_sum_simple_approx(
+            enc_stream_type="global", chunk_size=chunk_size, chunk_step_factor=1.0, total_epochs=40
+        )
