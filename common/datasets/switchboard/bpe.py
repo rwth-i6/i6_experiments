929dc403c14c4ecdacb48b5188d3212898cf6d30
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2023-03-28 16:43:51 +0200

    add use_spoken_form option to switchboard bpe

--- common/datasets/switchboard/bpe.py
+++ common/datasets/switchboard/bpe.py
@@ -1,5 +1,6 @@
 from i6_experiments.common.datasets.switchboard.corpus_train import (
     get_spoken_form_train_bliss_corpus_ldc,
+    get_train_bliss_corpus_ldc,
 )
 from i6_experiments.common.helpers.text_labels.subword_nmt_bpe import (
     get_returnn_subword_nmt,
@@ -8,18 +9,24 @@ from i6_experiments.common.helpers.text_labels.subword_nmt_bpe import (
 )
 
 
-def get_subword_nmt_bpe(bpe_size: int, unk_label: str = "<unk>", subdir_prefix: str = "") -> BPESettings:
+def get_subword_nmt_bpe(
+    bpe_size: int, use_spoken_form: bool = True, unk_label: str = "<unk>", subdir_prefix: str = ""
+) -> BPESettings:
     """
     Get the BPE tokens via the Returnn subword-nmt for a Switchboard setup.
 
     WARNING: This will not give compatible BPE to old setups!
 
     :param bpe_size: the number of BPE merge operations. This is NOT the resulting vocab size!
+    :param use_spoken_form: use the legacy conversion table to convert words into their spoken form
     :param unk_label: unknown label symbol
     :param subdir_prefix:
     """
     subword_nmt_repo = get_returnn_subword_nmt(output_prefix=subdir_prefix)
-    train_corpus = get_spoken_form_train_bliss_corpus_ldc()
+    if use_spoken_form:
+        train_corpus = get_spoken_form_train_bliss_corpus_ldc()
+    else:
+        train_corpus = get_train_bliss_corpus_ldc()
     bpe_settings = get_bpe_settings(
         train_corpus,
         bpe_size=bpe_size,
