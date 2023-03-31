b3a7a6248cb9d2153b08da25e580f301f2819e9b
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2023-03-31 09:25:56 +0200

    ls960 pretrain manifest from change bliss encoding

--- users/vieting/experiments/librispeech/librispeech_960_pretraining/wav2vec2/config_01_fairseq_large.py
+++ users/vieting/experiments/librispeech/librispeech_960_pretraining/wav2vec2/config_01_fairseq_large.py
@@ -1,26 +1,66 @@
 """
 Config for pre-training experiments on LibriSpeech using wav2vec 2.0.
 """
+import os.path
+
 from sisyphus import tk
-from i6_core.datasets.librispeech import DownloadLibriSpeechCorpusJob
+import i6_core.datasets.librispeech as librispeech
+from i6_core.audio.encoding import BlissChangeEncodingJob
 from i6_core.tools.git import CloneGitRepositoryJob
 from i6_experiments.users.engler.fairseq.training import FairseqHydraConfig, FairseqHydraTrainingJob
 from i6_experiments.users.dierkes.preprocessing.wav2vec import FairseqAudioManifestCreationJob
 
 
+def get_manifest(valid_percent=0.001, audio_format="ogg", output_prefix="datasets"):
+    assert audio_format in ["ogg", "wav"], f"audio format not implemented: '{audio_format}'"
+    audio_dirs = {}
+
+    output_prefix = os.path.join(output_prefix, "LibriSpeech")
+
+    download_metadata_job = librispeech.DownloadLibriSpeechMetadataJob()
+    download_metadata_job.add_alias(os.path.join(output_prefix, "download", "metadata_job"))
+    for corpus_name in ["train-clean-100", "train-clean-360", "train-other-500"]:
+        download_corpus_job = librispeech.DownloadLibriSpeechCorpusJob(corpus_key=corpus_name)
+        download_corpus_job.add_alias(os.path.join(output_prefix, "download", corpus_name))
+        create_bliss_corpus_job = librispeech.LibriSpeechCreateBlissCorpusJob(
+            corpus_folder=download_corpus_job.out_corpus_folder,
+            speaker_metadata=download_metadata_job.out_speakers,
+        )
+        create_bliss_corpus_job.add_alias(os.path.join(output_prefix, "create_bliss", corpus_name))
+        audio_format_options = {
+            "wav": {
+                "output_format": "wav",
+                "codec": "pcm_s16le",
+            },
+            "ogg": {"output_format": "ogg", "codec": "libvorbis"},
+        }
+        bliss_change_encoding_job = BlissChangeEncodingJob(
+            corpus_file=create_bliss_corpus_job.out_corpus,
+            sample_rate=16000,
+            **audio_format_options[audio_format],
+        )
+        bliss_change_encoding_job.add_alias(
+            os.path.join(
+                output_prefix,
+                "%s_conversion" % audio_format,
+                corpus_name,
+            )
+        )
+        audio_dirs[corpus_name] = bliss_change_encoding_job.out_audio_folder
+
+    manifest_creation_job = FairseqAudioManifestCreationJob(
+        audio_dir_path=list(audio_dirs.values()),
+        file_extension=audio_format,
+        valid_percent=valid_percent,
+    )
+    manifest_creation_job.rqmt["time"] = 4
+    manifest = manifest_creation_job.out_manifest_path
+    return manifest
+
+
 def get_fairseq_args(num_gpus=1):
     # create wav2vec manifest for training
-    audio_dirs = [
-      DownloadLibriSpeechCorpusJob(corpus_key=key).out_corpus_folder for key in [
-        "train-clean-100", "train-clean-360", "train-other-500"]
-    ]
-    job = FairseqAudioManifestCreationJob(
-        audio_dir_path=audio_dirs,
-        file_extension="flac",
-        valid_percent=0.001,
-    )
-    job.rqmt["time"] = 8
-    manifest = job.out_manifest_path
+    manifest = get_manifest()
 
     # Set training and model parameters
     sample_rate = 16000
