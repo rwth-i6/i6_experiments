d132bdcf67b98810841efdfb19428b699f51ae83
Judyxujj <judyxujj@yahoo.com>
2023-04-04 10:13:48 +0200

    add multilingual setup (#121)
    * add conformer hybrid swb setup
    
    * add multilingual setup
    
    ---------
    
    Co-authored-by: Jingjing Xu <jxu@i6.informatik.rwth-aachen.de>
    Co-authored-by: Jingjing Xu <jxu@hltpr.rwth-aachen.de>

---
+++ users/jxu/experiments/fairseq_multilingual/baseline_args.py
@@ -0,0 +1,118 @@
+import copy
+import os
+import numpy as np
+
+from torch import nn
+
+from i6_core.returnn.config import CodeWrapper, ReturnnConfig
+from i6_core.returnn.oggzip import BlissToOggZipJob
+import i6_experiments.users.engler.fairseq.training as runFairseq
+from returnn_common.datasets import OggZipDataset
+from i6_experiments.common.setups.returnn_common.serialization import (
+    Collection,
+    Import
+)
+
+def prepare_hdf_dataset(hdf_files, partition_epoch):
+    dataset = {
+            'class': "HDFDataset",
+            'files': hdf_files,
+            'partition_epoch': partition_epoch,
+            "seq_ordering": "random"
+            }
+    return dataset
+
+
+def prepare_zip_dataset(bliss_corpus, segment_file, returnn_python_exe, returnn_root):
+    ogg_zip_job = BlissToOggZipJob(
+            bliss_corpus,
+            returnn_python_exe=returnn_python_exe,
+            returnn_root=returnn_root,
+            )
+    tk.register_output("ogg_corpora", ogg_zip_job.out_ogg_zip)
+
+    zip_dataset = OggZipDatase(
+            files = [ogg_zip_job.out_ogg_zip],
+            segment_file=segment_file,
+            partition_epoch=2,
+            seq_ordering="laplace:.1000"
+            )
+    return zip_dataset
+
+def returnn_config_get_extern_data():
+    from returnn.tf.util.data import Dim, batch_dim, single_step_dim, SpatialDim, FeatureDim
+    time_data_dim = SpatialDim('time:data')
+    extern_data = {'data': {
+        'dim_tags': [
+            batch_dim,
+            time_data_dim,
+            FeatureDim('feat', 1),
+        ],
+        'dtype': 'float32',
+        }}
+    return extern_data
+
+
+def get_returnn_configs_pytorch(data_train, data_dev, wav2vec2_args, base_config_args):
+    wav2vec2_base_config = {}
+    wav2vec2_base_config["train"] = data_train
+    wav2vec2_base_config["dev"] = data_dev
+    wav2vec2_base_config["wav2vec2_args"] = wav2vec2_args
+    wav2vec2_base_config["extern_data"] = CodeWrapper("returnn_config_get_extern_data()")
+
+    base_post_config = {
+        "backend": "torch",
+        "use_tensorflow": False,
+        "debug_print_layer_output_template": True,
+        "log_batch_size": True,
+        "cache_size": "0",
+    }
+
+    wav2vec2_base_config.update(
+        {
+            "batching": "random",
+            "batch_size": 2080000, # raw audio input with 16kHz
+            "optimizer": {"class": "nadam", "epsilon": 1e-8},
+            "learning_rate_file": "learning_rates",
+            "learning_rates": list(np.linspace(2.5e-5, 2.5e-4, 10)),
+            "learning_rate_control": "newbob_multi_epoch",
+            "learning_rate_control_min_num_epochs_per_new_lr": 3,
+            "learning_rate_control_relative_error_relative_lr": True,
+            "min_learning_rate": 1e-5,
+            "newbob_learning_rate_decay": 0.707,
+            "newbob_multi_num_epochs": 40,
+            "newbob_multi_update_interval": 1,
+        }
+    )
+
+    wav2vec2_base_config.update(base_config_args)
+    
+    returnn_config = ReturnnConfig(
+        config=wav2vec2_base_config,
+        post_config=base_post_config,
+        pprint_kwargs={"sort_dicts": False},
+        python_prolog = [returnn_config_get_extern_data,
+                         "import fairseq.models.wav2vec as wav2vec",
+                         "import torch",
+                         "from torch import nn",
+                         "import torch.nn.functional as F"],
+        python_epilog=[
+            "def get_model(**_kwargs):\n"
+            "    fairseq_config = wav2vec.Wav2Vec2Config(**wav2vec2_args)\n"
+            "    model =  wav2vec.Wav2Vec2Model(fairseq_config)\n"
+            "    return model\n",
+
+            "def train_step(*, model:wav2vec.Wav2Vec2Model, data, train_ctx, **_kwargs):\n"
+            "   waveforms = torch.squeeze(data[\"data\"])\n"
+            "   waveforms = waveforms.type(torch.float32)\n"
+            "   net_output = model(waveforms)\n"
+            "   logits = model.get_logits(net_output).float()\n"
+            "   x = net_output[\"x\"]\n"
+            "   target = x.new_zeros(x.size(1) * x.size(2), dtype=torch.long)\n"
+            "   loss = F.cross_entropy(logits, target, reduction=\"sum\")\n"
+            "   train_ctx.mark_as_loss(name=\"ce\", loss=loss)\n"],
+    )
+
+    return {
+        "wav2vec_base": returnn_config,
+    }
