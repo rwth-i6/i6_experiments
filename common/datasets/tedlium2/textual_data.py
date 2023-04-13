cbb177c2b8bf83859b87636c7cf4582ac3e56ab9
Christoph M. Lüscher <chris.luscher@gmx.de>
2023-04-13 16:52:02 +0200

    cleanup

--- common/datasets/tedlium2/textual_data.py
+++ common/datasets/tedlium2/textual_data.py
@@ -1,7 +1,5 @@
-import os
 from functools import lru_cache
 from typing import Dict
-import pathlib
 
 from sisyphus import tk
 
@@ -10,8 +8,6 @@ from .download import download_data_dict
 
 @lru_cache()
 def get_text_data_dict(output_prefix: str = "datasets") -> Dict[str, tk.Path]:
-    txt_dict = {}
-
     lm_dir = download_data_dict(output_prefix=output_prefix)["lm_dir"]
 
     text_corpora = [
