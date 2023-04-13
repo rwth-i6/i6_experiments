bda3c69574ecad35a45e5a5071dd42b16ce0e6fa
Christoph M. Lüscher <chris.luscher@gmx.de>
2023-04-13 16:40:38 +0200

    add Wei's LM

--- common/baselines/tedlium2/data.py
+++ common/baselines/tedlium2/data.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from typing import Dict
 
+from sisyphus import tk
+
 from i6_experiments.common.datasets.tedlium2.constants import CONCURRENT
 from i6_experiments.common.datasets.tedlium2.corpus import get_corpus_object_dict
 from i6_experiments.common.datasets.tedlium2.lexicon import (
@@ -11,6 +13,7 @@ from i6_experiments.common.setups.rasr.util import RasrDataInput
 from i6_experiments.users.luescher.setups.rasr.config.lex_config import (
     LexiconRasrConfig,
 )
+from i6_experiments.users.luescher.setups.rasr.config.lm_config import ArpaLmRasrConfig
 
 
 def get_corpus_data_inputs() -> Dict[str, Dict[str, RasrDataInput]]:
@@ -22,6 +25,13 @@ def get_corpus_data_inputs() -> Dict[str, Dict[str, RasrDataInput]]:
         get_g2p_augmented_bliss_lexicon(output_prefix="lexicon"), False
     )
 
+    wei_lm = ArpaLmRasrConfig(  # TODO replace with complete sisyphus pipeline
+        lm_path=tk.Path(
+            "/work/asr4/zhou/data/ted-lium2/lm-data/model-kazuki/countLM/4-gram.lm.dev_opt3.bgd.interpolation.pruned.5e-10.gz",
+            cached=True,
+        ),
+    )
+
     rasr_data_input_dict = defaultdict(dict)
 
     for name, crp_obj in corpus_object_dict.items():
@@ -29,6 +39,7 @@ def get_corpus_data_inputs() -> Dict[str, Dict[str, RasrDataInput]]:
             corpus_object=crp_obj,
             lexicon=train_lexicon.get_dict(),
             concurrent=CONCURRENT[name],
+            lm=wei_lm.get_dict() if name == "dev" or name == "test" else None,
         )
 
     return rasr_data_input_dict
