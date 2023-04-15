fb87d2e34044762603944d0242a322053ad1b0cd
Daniel Mann <mann@i6.informatik.rwth-aachen.de>
2023-04-15 13:40:18 +0200

    extend get_data_dict function for ReturnnRasrDataInput

--- common/setups/rasr/util/nn.py
+++ common/setups/rasr/util/nn.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple, Type, TypedDict, Union
 
 from sisyphus import tk
+from sisyphus.delayed_ops import DelayedFormat
 
 import i6_core.am as am
 import i6_core.rasr as rasr
@@ -39,6 +40,15 @@ class ReturnnRasrDataInput:
         shuffle_data: bool = True,
         stm: Optional[tk.Path] = None,
         glm: Optional[tk.Path] = None,
+        # parameters for the ExternSprintDataset
+        num_classes: Optional[int] = None,
+        disregarded_classes: Optional[tk.Path] = None,
+        class_label_file: Optional[tk.Path] = None,
+        buffer_size: int = 200 * 1024,
+        partition_epochs: Optional[int] = None,
+        extra_rasr_config: Optional[rasr.RasrConfig] = None,
+        extra_rasr_post_config: Optional[rasr.RasrConfig] = None,
+        use_python_control: bool = True,
         **kwargs,
     ):
         self.name = name
@@ -52,14 +62,51 @@ class ReturnnRasrDataInput:
         self.stm = stm
         self.glm = glm
 
-    @staticmethod
-    def get_data_dict():
-        return {
-            "class": "ExternSprintDataset",
-            "sprintTrainerExecPath": "sprint-executables/nn-trainer",
-            "sprintConfigStr": "",
-            "suppress_load_seqs_print": True,
+        self.num_classes = num_classes
+        self.disregarded_classes = disregarded_classes
+        self.class_label_file = class_label_file
+        self.buffer_size = buffer_size
+        self.partition_epochs = partition_epochs
+        self.extra_rasr_config = extra_rasr_config
+        self.extra_rasr_post_config = extra_rasr_post_config
+        self.use_python_control = use_python_control
+
+    def get_training_feature_flow(self) -> tk.Path:
+        feature_flow = returnn.ReturnnRasrTrainingJob.create_flow(self.feature_flow, self.alignments)
+        write_feature_flow = rasr.WriteFlowNetworkJob(feature_flow)
+        return write_feature_flow.out_flow_file
+    
+    def get_training_rasr_config(self) -> tk.Path:
+        config, post_config = returnn.ReturnnRasrTrainingJob.create_config(
+            self.crp,
+            self.alignments,
+            self.num_classes,
+            self.buffer_size,
+            self.disregarded_classes,
+            self.class_label_file,
+            self.extra_rasr_config,
+            self.extra_rasr_post_config,
+            self.use_python_control,
+        )
+        config.neural_network_trainer.feature_extraction.file = self.get_training_feature_flow()
+        write_rasr_config = rasr.WriteRasrConfigJob(config, post_config)
+        return write_rasr_config.out_config
+    
+    def get_data_dict(self) -> Dict[str, Union[str, DelayedFormat, tk.Path]]:
+        """Returns the data dict for the ExternSprintDataset to be used in a training ReturnnConfig."""
+        config_file = self.get_rasr_training_config()
+        config_str = DelayedFormat(
+            "--config={} --*.LOGFILE=nn-trainer.{}.log --*.TASK=1",
+            config_file, self.name
+        )
+        dataset = { 
+            'class'                 : 'ExternSprintDataset',
+            'sprintTrainerExecPath' : rasr.RasrCommand.select_exe(self.crp.nn_trainer_exe, 'nn-trainer'),
+            'sprintConfigStr'       : config_str,
         }
+        if self.partition_epochs is not None:
+            dataset["partitionEpoch"] = self.partition_epochs
+        return dataset
 
     def build_crp(
         self,
@@ -152,46 +199,44 @@ class OggZipHdfDataInput:
     def __init__(
         self,
         oggzip_files: List[tk.Path],
-        alignments: List[tk.Path],
+        alignments: tk.Path,
+        context_window: Dict,
         audio: Dict,
+        targets: Optional[str] = None,
         partition_epoch: int = 1,
         seq_ordering: str = "laplace:.1000",
-        meta_args: Optional[Dict[str, Any]] = None,
         ogg_args: Optional[Dict[str, Any]] = None,
-        hdf_args: Optional[Dict[str, Any]] = None,
         acoustic_mixtures: Optional[tk.Path] = None,
     ):
         """
-        :param oggzip_files: zipped ogg files which contain the audio
-        :param alignments: hdf files which contain dumped RASR alignments
+        :param oggzip_files:
+        :param alignments:
+        :param context_window: {"classes": 1, "data": 242}
         :param audio: e.g. {"features": "raw", "sample_rate": 16000} for raw waveform input with a sample rate of 16 kHz
-        :param partition_epoch: if >1, split the full dataset into multiple sub-epochs
-        :param seq_ordering: sort the sequences in the dataset, e.g. "random" or "laplace:.100"
-        :param meta_args: parameters for the `MetaDataset`
-        :param ogg_args: parameters for the `OggZipDataset`
-        :param hdf_args: parameters for the `HdfDataset`
-        :param acoustic_mixtures: path to a RASR acoustic mixture file (used in System classes, not RETURNN training)
+        :param partition_epoch:
+        :param seq_ordering:
+        :param targets:
         """
         self.oggzip_files = oggzip_files
         self.alignments = alignments
+        self.context_window = context_window
         self.audio = audio
         self.partition_epoch = partition_epoch
         self.seq_ordering = seq_ordering
-        self.meta_args = meta_args
+        self.targets = targets
         self.ogg_args = ogg_args
-        self.hdf_args = hdf_args
         self.acoustic_mixtures = acoustic_mixtures
 
     def get_data_dict(self):
         return {
             "class": "MetaDataset",
+            "context_window": self.context_window,
             "data_map": {"classes": ("hdf", "classes"), "data": ("ogg", "data")},
             "datasets": {
                 "hdf": {
                     "class": "HDFDataset",
-                    "files": self.alignments,
+                    "files": [self.alignments.get_path()],
                     "use_cache_manager": True,
-                    **(self.hdf_args or {}),
                 },
                 "ogg": {
                     "class": "OggZipDataset",
@@ -199,12 +244,12 @@ class OggZipHdfDataInput:
                     "partition_epoch": self.partition_epoch,
                     "path": self.oggzip_files,
                     "seq_ordering": self.seq_ordering,
+                    "targets": self.targets,
                     "use_cache_manager": True,
                     **(self.ogg_args or {}),
                 },
             },
             "seq_order_control_dataset": "ogg",
-            **(self.meta_args or {}),
         }
 
 
