dc7d3875b30117d2e74c38f63fda6d7d20c6649a
Robin Schmitt <schmitt@hltpr.rwth-aachen.de>
2023-04-27 14:26:24 +0200

    add returnn exe for torch

--- users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/dependencies/general/returnn/exes.py
+++ users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/dependencies/general/returnn/exes.py
@@ -2,3 +2,5 @@ from sisyphus import Path
 
 RETURNN_EXE = Path("/u/rossenbach/bin/returnn_tf2.3_launcher.sh")
 RETURNN_ROOT = Path("/u/schmitt/src/returnn")
+
+RETURNN_EXE_TORCH = Path("/u/rossenbach/bin/returnn/returnn_pt20_experimental.sh")
