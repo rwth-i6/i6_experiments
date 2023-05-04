59cddd638a4646ac813b7b334095e35aa1ed97a0
Robin Schmitt <schmitt@hltpr.rwth-aachen.de>
2023-05-04 08:18:10 +0200

    add up-to-date returnn root

--- users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/dependencies/general/returnn/exes.py
+++ users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/dependencies/general/returnn/exes.py
@@ -2,5 +2,6 @@ from sisyphus import Path
 
 RETURNN_EXE = Path("/u/rossenbach/bin/returnn_tf2.3_launcher.sh")
 RETURNN_ROOT = Path("/u/schmitt/src/returnn")
+RETURNN_CURRENT_ROOT = Path("/u/schmitt/src/returnn_new")
 
 RETURNN_EXE_TORCH = Path("/u/rossenbach/bin/returnn/returnn_pt20_experimental.sh")
