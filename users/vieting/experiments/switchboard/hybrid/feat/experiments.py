ab299307deb1d8fd693acec8ed286ca5759ffd2e
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2023-05-10 11:50:54 +0200

    swb: remove run_test_gt

--- users/vieting/experiments/switchboard/hybrid/feat/experiments.py
+++ users/vieting/experiments/switchboard/hybrid/feat/experiments.py
@@ -70,50 +70,6 @@ def get_hybrid_nn_system(
     return hybrid_nn_system
 
 
-def run_test_gt():
-    gs.ALIAS_AND_OUTPUT_SUBDIR = "experiments/switchboard/hybrid/feat/"
-
-    gmm_system = run_gmm_system_from_common()
-    rasr_init_args = copy.deepcopy(gmm_system.rasr_init_args)
-
-    # Gammatone args
-    # noinspection PyTypeChecker
-    (
-        nn_train_data_inputs,
-        nn_cv_data_inputs,
-        nn_devtrain_data_inputs,
-        nn_dev_data_inputs,
-        nn_test_data_inputs,
-    ) = get_corpus_data_inputs_oggzip(
-        gmm_system,
-        partition_epoch={"train": 6, "dev": 1},
-        context_window={"classes": 1, "data": 441},
-        returnn_root=RETURNN_ROOT,
-        returnn_python_exe=RETURNN_EXE,
-    )
-
-    nn_args = get_nn_args_baseline({"conformer_test_setup": {}}, num_epochs=260, prefix="gt_")
-    nn_steps = RasrSteps()
-    nn_steps.add_step("nn", nn_args)
-
-    # Gammatone NN system
-    hybrid_nn_gt_system = HybridSystem(
-        returnn_root=RETURNN_ROOT,
-        returnn_python_exe=RETURNN_EXE,
-        rasr_binary_path=RASR_BINARY_PATH,
-    )
-    hybrid_nn_gt_system.init_system(
-        rasr_init_args=rasr_init_args,
-        train_data=nn_train_data_inputs,
-        cv_data=nn_cv_data_inputs,
-        devtrain_data=nn_devtrain_data_inputs,
-        dev_data=nn_dev_data_inputs,
-        test_data=nn_test_data_inputs,
-        train_cv_pairing=[tuple(["switchboard.train", "switchboard.cv"])],
-    )
-    hybrid_nn_gt_system.run(nn_steps)
-
-
 def run_baseline_gt():
     gs.ALIAS_AND_OUTPUT_SUBDIR = "experiments/switchboard/hybrid/feat/"
 
