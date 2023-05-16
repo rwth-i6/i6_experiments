32bde02a535b81f8bbf2ba811bd167459bdaefde
marvin84 <tinaraissi@gmail.com>
2023-05-16 18:33:50 +0200

    adjust new space

--- common/setups/rasr/gmm_system.py
+++ common/setups/rasr/gmm_system.py
@@ -1361,7 +1361,6 @@ class GmmSystem(RasrSystem):
 
             # ---------- CaRT ----------
             if step_name.startswith("cart"):
-
                 self.cart_questions = step_args.cart_questions
                 for trn_c in self.train_corpora:
                     self.cart_and_lda(
