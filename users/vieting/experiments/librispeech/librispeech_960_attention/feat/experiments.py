c3c2acd80a6ccc441af52b033d5e4c98b6f9d63b
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2023-05-17 14:14:50 +0200

    ls 960 att: simplify config

--- users/vieting/experiments/librispeech/librispeech_960_attention/feat/experiments.py
+++ users/vieting/experiments/librispeech/librispeech_960_attention/feat/experiments.py
@@ -37,18 +37,15 @@ BPE_10K = 10000
 BPE_5K = 5000
 BPE_1K = 1000
 
-# --------------------------- LM --------------------------- #
-
+# LM
 lstm_10k_lm_opts = {
     "lm_subnet": generic_lm.libri_lstm_bpe10k_net,
     "lm_model": generic_lm.libri_lstm_bpe10k_model,
     "name": "lstm",
 }
-
 lstm_lm_opts_map = {
     BPE_10K: lstm_10k_lm_opts,
 }
-
 trafo_lm_net = TransformerLM(source="prev:output", num_layers=24, vocab_size=10025, use_as_ext_lm=True)
 trafo_lm_net.create_network()
 trafo_10k_lm_opts = {
@@ -62,7 +59,6 @@ trafo_10k_lm_opts = {
     },
     "name": "trafo",
 }
-
 bpe5k_lm = get_lm("ls960_trafo24_bs3000_5ep_5kbpe")  # type: ZeineldeenLM
 trafo_5k_lm_opts = {
     "lm_subnet": bpe5k_lm.combination_network,
@@ -73,21 +69,19 @@ trafo_5k_lm_opts = {
     },
     "name": "trafo",
 }
-
 trafo_lm_opts_map = {
     BPE_10K: trafo_10k_lm_opts,
     BPE_5K: trafo_5k_lm_opts,
 }
 
-# ----------------------------------------------------------- #
-
 
 def conformer_baseline():
+    """
+    Baseline attention system with conformer encoder and LSTM decoder. Based on Mohammad's setup.
+    """
     gs.ALIAS_AND_OUTPUT_SUBDIR = "experiments/librispeech/librispeech_960_attention/feat/"
-    prefix_name = ""
 
-    # --------------------------- General Settings --------------------------- #
-
+    # model (encoder/decoder) args
     conformer_enc_args = ConformerEncoderArgs(
         num_blocks=12,
         input_layer="conv-6",
@@ -99,87 +93,48 @@ def conformer_baseline():
         dropout=0.1,
         att_dropout=0.1,
         l2=0.0001,
+        ctc_loss_scale=1.0,
+        use_sqrd_relu=True,
     )
     apply_fairseq_init_to_conformer(conformer_enc_args)
-    conformer_enc_args.ctc_loss_scale = 1.0
 
-    rnn_dec_args = RNNDecoderArgs()
+    rnn_dec_args = RNNDecoderArgs(use_zoneout_output=True)
 
-    trafo_dec_args = TransformerDecoderArgs(
-        num_layers=6,
-        embed_dropout=0.1,
-        label_smoothing=0.1,
-        apply_embed_weight=True,
-        pos_enc="rel",
-    )
-    apply_fairseq_init_to_transformer_decoder(trafo_dec_args)
-
-    conformer_dec_args = ConformerDecoderArgs()
-    apply_fairseq_init_to_conformer(conformer_dec_args)
-
-    training_args = dict()
-
-    # LR scheduling
-    training_args["const_lr"] = [42, 100]  # use const LR during pretraining
-    training_args["wup_start_lr"] = 0.0002
-    training_args["wup"] = 20
-    training_args["with_staged_network"] = True
-    training_args["speed_pert"] = True
-
-    trafo_training_args = copy.deepcopy(training_args)
-    trafo_training_args["pretrain_opts"] = {
-        "variant": 3,
-        "initial_batch_size": 20000 * 160,
+    # basic training args
+    training_args = {
+        "const_lr": [42, 100],  # use const LR during pretraining
+        "wup_start_lr": 0.0002,
+        "wup": 20,
+        "with_staged_network": True,
+        "speed_pert": True,
+        "oclr_opts": {
+            "peak_lr": 9e-4,
+            "final_lr": 1e-6,
+            "cycle_ep": 285,
+            "total_ep": 635,
+            "n_step": 1350,
+            "learning_rates": [8e-5] * 35,
+        },
+        "batch_size": 15000 * 160,  # frames * samples per frame
+        "pretrain_reps": 5,
+        "pretrain_opts": {
+            "variant": 3,
+            "initial_batch_size": 22500 * 160,
+        },
     }
-    trafo_training_args["pretrain_reps"] = 5
-    trafo_training_args["batch_size"] = 12000 * 160  # frames * samples per frame
 
-    trafo_dec_exp_args = copy.deepcopy(
+    base_args = copy.deepcopy(
         {
-            **trafo_training_args,
-            "encoder_args": conformer_enc_args,
-            "decoder_args": trafo_dec_args,
-        }
-    )
-
-    conformer_dec_exp_args = copy.deepcopy(trafo_dec_exp_args)
-    conformer_dec_exp_args["decoder_args"] = conformer_dec_args
-
-    lstm_training_args = copy.deepcopy(training_args)
-    lstm_training_args["pretrain_opts"] = {
-        "variant": 3,
-        "initial_batch_size": 22500 * 160,
-    }
-    lstm_training_args["pretrain_reps"] = 5
-    lstm_training_args["batch_size"] = 15000 * 160  # frames * samples per frame
-
-    lstm_dec_exp_args = copy.deepcopy(
-        {
-            **lstm_training_args,
+            **training_args,
             "encoder_args": conformer_enc_args,
             "decoder_args": rnn_dec_args,
         }
     )
 
-    # --------------------------- Experiments --------------------------- #
+    # experiments
     report_list = []
 
-    oclr_args = copy.deepcopy(lstm_dec_exp_args)
-    oclr_args["oclr_opts"] = {
-        "peak_lr": 9e-4,
-        "final_lr": 1e-6,
-        "cycle_ep": 915,
-        "total_ep": 2035,  # 20 epochs
-        "n_step": 1350,
-        "learning_rates": [8e-5] * 35,
-    }
-    oclr_args["encoder_args"].input_layer = "conv-6"
-    oclr_args["encoder_args"].use_sqrd_relu = True
-
-    args = copy.deepcopy(oclr_args)
-    args["oclr_opts"]["cycle_ep"] = 285
-    args["oclr_opts"]["total_ep"] = 635
-    args["decoder_args"].use_zoneout_output = True
+    args = copy.deepcopy(base_args)
     _, _, report = run_exp(
         "baseline",
         train_args=args,
@@ -187,6 +142,7 @@ def conformer_baseline():
     )
     report_list.append(report)
 
+    args = copy.deepcopy(base_args)
     _, _, report = run_exp(
         "scf",
         train_args=args,
