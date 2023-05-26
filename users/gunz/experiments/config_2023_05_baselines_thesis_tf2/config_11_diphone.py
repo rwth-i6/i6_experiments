74c5727dbfa71d073239b80273c5a9d389088d89
Moritz Gunz <moritz.gunz@gmail.com>
2023-05-26 11:49:36 +0200

    fix config

--- users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_11_diphone.py
+++ users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_11_diphone.py
@@ -406,7 +406,7 @@ def run_single(
         epoch=keep_epochs[-2],
         output_layer_name="output",
         smoothen=True,
-        returnn_config=remove_label_pops_and_losses_from_returnn_config(prior_returnn_config),
+        returnn_config=prior_returnn_config,
         via_hdf=True,
     )
 
