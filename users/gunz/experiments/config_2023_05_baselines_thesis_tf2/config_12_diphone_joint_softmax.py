d68ee773031d6434cc9208c3dbb66a0e033259cc
Moritz Gunz <moritz.gunz@gmail.com>
2023-05-29 12:48:09 +0200

    leave losses on

--- users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_12_diphone_joint_softmax.py
+++ users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_12_diphone_joint_softmax.py
@@ -302,7 +302,7 @@ def run_single(
         epoch=keep_epochs[-2],
         output_layer_name="output",
         smoothen=True,
-        returnn_config=remove_label_pops_and_losses_from_returnn_config(returnn_config),
+        returnn_config=returnn_config,
         via_hdf=True,
     )
 
