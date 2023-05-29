1d65626262c2f93a6cccc47a44a6d9f0006206e6
Moritz Gunz <moritz.gunz@gmail.com>
2023-05-29 12:40:52 +0200

    black

--- users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config.py
+++ users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config.py
@@ -8,9 +8,7 @@ CART_TREE_TRI = "/work/asr3/raissi/shared_workspaces/gunz/dependencies/cart-tree
 CART_TREE_TRI_NUM_LABELS = "/work/asr3/raissi/shared_workspaces/gunz/dependencies/cart-trees/ls960/tri.num_labels"
 
 GMM_TRI_ALIGNMENT = "/work/asr4/raissi/setups/librispeech/960-ls/work/i6_core/mm/alignment/AlignmentJob.hK21a0UU4iiJ/output/alignment.cache.bundle"
-SCRATCH_ALIGNMENT = (
-    "/u/mgunz/gunz/dependencies/alignments/ls-960/scratch/10ms/alignment.cache.bundle"
-)
+SCRATCH_ALIGNMENT = "/u/mgunz/gunz/dependencies/alignments/ls-960/scratch/10ms/alignment.cache.bundle"
 SCRATCH_ALIGNMENT_DANIEL = (
     "/u/mgunz/gunz/dependencies/alignments/ls-960/scratch/daniel-with-dc-detection/alignment.cache.bundle"
 )
