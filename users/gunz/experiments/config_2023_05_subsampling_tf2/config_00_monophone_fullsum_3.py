cb1de6cb3dda93a7cae8bba1daf1c0f49f1691f4
Moritz Gunz <moritz.gunz@gmail.com>
2023-05-31 13:20:51 +0200

    bugfix

--- users/gunz/experiments/config_2023_05_subsampling_tf2/config_00_monophone_fullsum_3.py
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_00_monophone_fullsum_3.py
@@ -115,6 +115,7 @@ def run(returnn_root: tk.Path):
             dc_detection=exp.dc_detection,
             feature_time_shift=exp.feature_time_shift,
             focal_loss=exp.focal_loss,
+            load_checkpoints_from=exp.load_checkpoints_from,
             lr=exp.lr,
             multitask=exp.multitask,
             returnn_root=returnn_root,
