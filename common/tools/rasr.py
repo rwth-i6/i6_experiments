e740703c6e48c53710ef2566eaf6325fd4d6a86c
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-06-01 08:19:44 +0200

    add option to configure u22 compilation

--- common/tools/rasr.py
+++ common/tools/rasr.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sisyphus import tk
 
@@ -11,6 +11,7 @@ def compile_rasr_binaries_i6mode(
     commit: Optional[str] = None,
     rasr_git_repository: str = "https://github.com/rwth-i6/rasr",
     rasr_arch: str = "linux-x86_64-standard",
+    configure_options: Optional[List[str]] = None
 ) -> tk.Path:
     """
     Compile RASR for i6-internal usage
@@ -19,13 +20,16 @@ def compile_rasr_binaries_i6mode(
     :param commit: specify a specific commit
     :param rasr_git_repository: where to clone RASR from, usually does not need to be altered
     :param rasr_arch: RASR compile architecture string
+    :param configure_options: if given, runs ./configure with these options before make
     :return: path to the binary folder
     """
+    if configure_options is None:
+        configure_options = ["--i6"]
     rasr_repo = CloneGitRepositoryJob(rasr_git_repository, branch=branch, commit=commit).out_repository
     make_job = MakeJob(
         folder=rasr_repo,
         make_sequence=["build", "install"],
-        configure_opts=["--i6"],
+        configure_opts=configure_options,
         num_processes=8,
         link_outputs={"binaries": f"arch/{rasr_arch}/"},
     )
