d2af7825c44d3ec0ffafb21a2afd28e4f603d863
Moritz Gunz <moritz.gunz@gmail.com>
2023-06-13 14:17:09 +0200

    remove wrong assertion

--- users/gunz/setups/common/nn/chunking.py
+++ users/gunz/setups/common/nn/chunking.py
@@ -7,7 +7,6 @@ def subsample_chunking(
     ch: str, factor: int, data_key: str = "data", subsampled_key: str = "classes"
 ) -> typing.Tuple[typing.Dict[str, int], typing.Dict[str, int]]:
     parts = [int(p.strip()) for p in ch.strip().split(":")]
-    assert all((p % factor == 0 for p in parts)), "factor must evenly divide chunk size"
     size, step = parts
 
     # ceil div
