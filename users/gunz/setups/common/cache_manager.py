bdb37946a0f1005219c013fa6e4f7435de2bab6f
Moritz Gunz <moritz.gunz@gmail.com>
2023-06-20 17:21:05 +0200

    bugfix

--- users/gunz/setups/common/cache_manager.py
+++ users/gunz/setups/common/cache_manager.py
@@ -15,5 +15,5 @@ cm = importlib.import_module("cache-manager")
 
 
 def cache_file(file: typing.Union["tk.Path", str]) -> str:
-    path = file.get_path() if isinstance(file, tk.Path) else file
+    path = file.get_path() if not isinstance(file, str) else file
     return cm.cacheFile(path)
