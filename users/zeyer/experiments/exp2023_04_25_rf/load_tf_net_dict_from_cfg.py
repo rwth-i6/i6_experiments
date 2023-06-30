81dc6c23650a30f633bd7ba75da229aca6756635
Albert Zeyer <albzey@gmail.com>
2023-06-30 22:13:03 +0200

    more

--- users/zeyer/experiments/exp2023_04_25_rf/load_tf_net_dict_from_cfg.py
+++ users/zeyer/experiments/exp2023_04_25_rf/load_tf_net_dict_from_cfg.py
@@ -16,11 +16,18 @@ def load_net_dict_from_cfg(cfg_filename: str, *, output_probs_output_layer: bool
         net_dict["source"]["class"] = "copy"
         del net_dict["source"]["eval"]
 
+    rec_layer = net_dict["output"]
+    assert rec_layer["class"] == "rec"
+    subnet = rec_layer["unit"]
+    assert isinstance(subnet, dict)
+    assert subnet["output_prob"]["class"] == "softmax", subnet["output_prob"]
+    subnet["output_prob"]["class"] = "linear"
+    subnet["output_prob"]["activation"] = None
+    subnet["output_prob"]["loss_opts"]["input_type"] = "logits"
     if output_probs_output_layer:
-        rec_layer = net_dict["output"]
-        assert rec_layer["class"] == "rec"
-        subnet = rec_layer["unit"]
-        assert isinstance(subnet, dict)
         subnet["output_prob"]["is_output_layer"] = True
+    assert subnet["output"]["class"] == "choice", subnet["output"]
+    assert subnet["output"]["from"] == "output_prob", subnet["output"]
+    subnet["output"]["input_type"] = "logits"
 
     return net_dict
