b96b87c48466b56f064f413aa7edff211274060b
Moritz Gunz <moritz.gunz@gmail.com>
2023-07-03 17:08:37 +0200

    add experiments evaluating search space

---
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_90_eval_search_space.py
@@ -0,0 +1,53 @@
+__all__ = ["run"]
+
+import dataclasses
+import os
+
+# -------------------- Sisyphus --------------------
+from sisyphus import gs, tk, Path
+
+from ...setups.common.rasr.search_space import AllophoneDetails, VisualizeBestTraceJob
+from ...setups.fh.factored import LabelInfo, RasrStateTying
+
+
+def run():
+    # ******************** Settings ********************
+    gs.ALIAS_AND_OUTPUT_SUBDIR = os.path.splitext(os.path.basename(__file__))[0][7:]
+
+    # ********************* Plots **********************
+
+    li = dataclasses.replace(LabelInfo.default_ls(), state_tying=RasrStateTying.monophone, n_states_per_phone=3)
+    li_ss = dataclasses.replace(li, n_states_per_phone=1)
+
+    segments = [
+        "dev-other/116-288045-0015",
+        "dev-other/116-288045-0025",
+        "dev-other/116-288046-0005",
+        "dev-other/6123-59150-0012",
+        "dev-other/6123-59150-0006",
+    ]
+    non_ss = VisualizeBestTraceJob(
+        rasr_logs=Path("/u/mgunz/gunz/ma/state-space-comparison/recog-normal/rasr.log"),
+        state_tying=Path("/u/mgunz/gunz/ma/state-space-comparison/monophone-dense-state-tying-n3"),
+        num_tied_phonemes=li.get_n_of_dense_classes(),
+        segments_to_process=segments,
+        allophone_detail_level=AllophoneDetails.MONOPHONE,
+        x_steps_per_log_time_step=1,
+    )
+    for i, out in enumerate(non_ss.out_print_files.values()):
+        tk.register_output(f"non-ss/out.{i}.txt", out)
+    for i, out in enumerate(non_ss.out_plot_files.values()):
+        tk.register_output(f"non-ss/out.{i}.png", out)
+
+    ss = VisualizeBestTraceJob(
+        rasr_logs=Path("/u/mgunz/gunz/ma/state-space-comparison/recog-ss/rasr.log"),
+        state_tying=Path("/u/mgunz/gunz/ma/state-space-comparison/monophone-dense-state-tying-n1"),
+        num_tied_phonemes=li_ss.get_n_of_dense_classes(),
+        segments_to_process=segments,
+        allophone_detail_level=AllophoneDetails.MONOPHONE,
+        x_steps_per_log_time_step=4,
+    )
+    for i, out in enumerate(ss.out_print_files.values()):
+        tk.register_output(f"non-ss/out.{i}.txt", out)
+    for i, out in enumerate(ss.out_plot_files.values()):
+        tk.register_output(f"non-ss/out.{i}.png", out)
---
+++ users/gunz/setups/common/rasr/search_space.py
@@ -0,0 +1,163 @@
+__all__ = ["AllophoneDetails", "Hyp", "VisualizeBestTraceJob"]
+
+import logging
+from dataclasses import dataclass
+from enum import Enum
+import gzip
+import typing
+from xml.etree import ElementTree as ET
+
+from sisyphus import Job, Path, Task
+
+
+@dataclass
+class Hyp:
+    l: int
+    pr: float
+    sc: float
+    st: int
+
+    @classmethod
+    def from_node(cls, n: ET.Element) -> "Hyp":
+        return cls(
+            l=int(n.attrib["l"]),
+            pr=float(n.attrib["pr"]),
+            sc=float(n.attrib["sc"]),
+            st=int(n.attrib["st"]),
+        )
+
+
+class AllophoneDetails(Enum):
+    FULL = "full"
+    CONCISE = "concise"
+    MONOPHONE = "mono"
+
+    def apply(self, a: str) -> str:
+        if self == AllophoneDetails.FULL:
+            return a
+        elif self == AllophoneDetails.CONCISE:
+            return a.split("}")[0] + "}"
+        elif self == AllophoneDetails.MONOPHONE:
+            return a.split("{")[0]
+        else:
+            assert False, "unknown details"
+
+
+class VisualizeBestTraceJob(Job):
+    def __init__(
+        self,
+        rasr_logs: typing.Union[Path, typing.List[Path], typing.Dict[typing.Any, Path]],
+        segments_to_process: typing.List[str],
+        state_tying: Path,
+        num_tied_phonemes: int,
+        x_steps_per_log_time_step: int,
+        allophone_detail_level: AllophoneDetails,
+    ):
+        super().__init__()
+
+        self.allophone_detail_level = allophone_detail_level
+        self.num_tied_phonemes = num_tied_phonemes
+        self.rasr_logs = (
+            rasr_logs
+            if isinstance(rasr_logs, list)
+            else list(rasr_logs.values())
+            if isinstance(rasr_logs, dict)
+            else [rasr_logs]
+        )
+        self.segments_to_process = segments_to_process
+        self.state_tying = state_tying
+        self.x_steps_per_log_time_step = x_steps_per_log_time_step
+
+        self.out_print_files = {seg: self.output_path(f"segment.{i}.txt") for i, seg in enumerate(segments_to_process)}
+        self.out_plot_files = {seg: self.output_path(f"segment.{i}.png") for i, seg in enumerate(segments_to_process)}
+
+        self.rqmt = {"cpu": 1, "mem": 6}
+
+    def tasks(self):
+        yield Task("run", rqmt=self.rqmt, args=range(len(self.rasr_logs)))
+
+    def run(self, index: int):
+        import matplotlib.pyplot as plt
+
+        logging.info(f"loading state tying {self.state_tying}")
+        parsed_tying = VisualizeBestTraceJob.parse_state_tying(self.state_tying, self.allophone_detail_level)
+
+        log_file = self.rasr_logs[index]
+        logging.info(f"loading search log {log_file}")
+        parsed_search_space = VisualizeBestTraceJob.parse_search_space(log_file)
+
+        segments_done = set()
+
+        for segment, search_space in parsed_search_space.items():
+            if segment not in self.segments_to_process:
+                continue
+
+            logging.info(f"processing {segment}")
+
+            best_hyps = {t: min(hyps, key=lambda hyp: hyp.sc) for t, hyps in search_space.items()}
+
+            keys = sorted(best_hyps.keys())
+            scores = [best_hyps[k] for k in keys]
+            pad_w = max((len(parsed_tying.get(hyp.l, "N/A")) for hyp in scores))
+
+            with open(self.out_print_files[segment], "wt") as f:
+                for hyp in scores:
+                    allophone = parsed_tying.get(hyp.l, "N/A")
+                    w = pad_w * self.x_steps_per_log_time_step + (self.x_steps_per_log_time_step - 1) * 2
+
+                    f.write(f"{allophone.ljust(w)}||")
+
+            plt.clf()
+            plt.imshow([[hyp.l for hyp in scores]], vmin=0, vmax=self.num_tied_phonemes)
+            plt.savefig(self.out_plot_files[segment])
+
+            segments_done.add(segment)
+
+        not_processed = set(self.segments_to_process) - segments_done
+        if len(not_processed) > 0:
+            raise AttributeError(f"did not process all requested segments: {not_processed}")
+
+    @classmethod
+    def parse_search_space(
+        cls, log_file: typing.Union[str, Path]
+    ) -> typing.Dict[str, typing.Dict[int, typing.List[Hyp]]]:
+        p = log_file.get_path() if isinstance(log_file, Path) else log_file
+
+        if p.endswith(".gz"):
+            with gzip.open(p, "rt") as f:
+                xml = ET.parse(f)
+        else:
+            with open(p, "rt") as f:
+                xml = ET.parse(f)
+
+        root = xml.getroot()
+        obj = {
+            segment.attrib["name"]: {
+                int(step.attrib["time"]): [Hyp.from_node(n) for n in step.findall("./hyp")]
+                for step in segment.findall(".//step")
+            }
+            for segment in root.findall(".//segment")
+        }
+
+        return obj
+
+    @classmethod
+    def parse_state_tying(
+        cls,
+        path: typing.Union[str, Path],
+        allophone_detail_level: AllophoneDetails,
+    ) -> typing.Dict[int, str]:
+        tying = {}
+
+        with open(path, "rt") as f:
+            for line in f:
+                if not line.strip() or line.strip().startswith("#"):
+                    continue
+
+                allophone, label = line.strip().split()
+                label = int(label)
+
+                if label not in tying:
+                    tying[int(label)] = allophone_detail_level.apply(allophone)
+
+        return tying
