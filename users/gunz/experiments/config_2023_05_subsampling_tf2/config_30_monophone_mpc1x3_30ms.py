7801274c727e98aaf749adf7726043ed255b4a92
Moritz Gunz <moritz.gunz@gmail.com>
2023-07-05 12:02:54 +0200

    try higher beam size

--- users/gunz/experiments/config_2023_05_subsampling_tf2/config_30_monophone_mpc1x3_30ms.py
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_30_monophone_mpc1x3_30ms.py
@@ -320,7 +320,7 @@ def run_single(
         )
         recog_args = recog_args.with_lm_scale(round(recog_args.lm_scale / float(ss_factor), 2)).with_tdp_scale(0.1)
 
-        for cfg in [recog_args.with_prior_scale(0.6)]:
+        for cfg in [recog_args.with_prior_scale(0.6), recog_args.with_prior_scale(0.6).with_beam_size(24)]:
             recognizer.recognize_count_lm(
                 label_info=s.label_info,
                 search_parameters=cfg,
