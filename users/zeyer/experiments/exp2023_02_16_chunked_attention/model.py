02bd5038db0a3b72763cdb1d718e87d8f9928d71
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-07-14 15:32:23 +0000

    remove unused param

--- users/zeyer/experiments/exp2023_02_16_chunked_attention/model.py
+++ users/zeyer/experiments/exp2023_02_16_chunked_attention/model.py
@@ -151,7 +151,6 @@ class RNNDecoder:
         masked_computation_blank_idx: Optional[int] = None,
         full_sum_simple_approx: bool = False,
         prev_target_embed_direct: bool = False,
-        end_slice_size: Optional[int] = None,
     ):
         """
         :param base_model: base/encoder model instance
