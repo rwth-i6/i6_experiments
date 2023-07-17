c70a0d2897a143761ef7bf4755e85037ff43f154
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-07-17 14:40:31 +0000

    update default tools

--- users/zeineldeen/experiments/conformer_att_2023/swb_300/default_tools.py
+++ users/zeineldeen/experiments/conformer_att_2023/swb_300/default_tools.py
@@ -7,6 +7,6 @@ RETURNN_CPU_EXE = RETURNN_EXE
 
 # old: b8187f47437a368eda8ad369631521515ee5e209
 RETURNN_ROOT = CloneGitRepositoryJob(
-    "https://github.com/rwth-i6/returnn", commit="d343279bd4aa34638692a230d2258579ef51682c"
+    "https://github.com/rwth-i6/returnn", commit="3f7bd3b9c712c6b92ed7ccd8dd925a595cf73716"
 ).out_repository
 RETURNN_ROOT.hash_overwrite = "LIBRISPEECH_DEFAULT_RETURNN_ROOT"
