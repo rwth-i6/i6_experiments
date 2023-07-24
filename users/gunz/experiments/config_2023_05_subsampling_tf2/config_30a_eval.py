c6f60d8d65b96ffd44d069731b0cd085211ce31a
Moritz Gunz <moritz.gunz@gmail.com>
2023-07-24 15:32:02 +0200

    use search

--- users/gunz/experiments/config_2023_05_subsampling_tf2/config_30a_eval.py
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_30a_eval.py
@@ -11,19 +11,19 @@ _, out_dir = sys.argv
 dtl_files = glob.glob(os.path.join(out_dir, "*.wer", "sclite.dtl"))
 
 for file in dtl_files:
-    print(f"processing {file}")
-
     with open(file, "rt") as dtl:
         dtl_contents = " ".join(itertools.islice(dtl, 50))
 
-        wer, total_errs = re.match(
-            "Percent Total Error\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE
+        wer, total_errs = re.search(
+            r"Percent Total Error\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE
         ).groups()
-        _, total_subst = re.match(
+        _, total_subst = re.search(
             "Percent Substitutions\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE
         ).groups()
-        _, total_del = re.match("Percent Deletions\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE).groups()
-        _, total_insrt = re.match(
+        _, total_del = re.search(
+            "Percent Deletions\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE
+        ).groups()
+        _, total_insrt = re.search(
             "Percent Insertions\s+=\s+(\d+\.\d+)%\s+\((\d+)\)", dtl_contents, re.IGNORECASE
         ).groups()
 
@@ -32,4 +32,6 @@ for file in dtl_files:
             os.path.dirname(file),
         ).groups()
 
-        print(f"{tdpScale};{spLoop};{spFwd};{spExit};{silLoop};{silFwd};{silExit};{wer};{total_errs};{total_subst};{total_del};{total_insrt}")
+        print(
+            f"{tdpScale};{spLoop};{spFwd};{spExit};{silLoop};{silFwd};{silExit};{wer};{total_errs};{total_subst};{total_del};{total_insrt}"
+        )
