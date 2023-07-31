def1acaa6af756f376573b1865e31d24bb77898c
Moritz Gunz <moritz.gunz@gmail.com>
2023-07-31 12:13:14 +0200

    check TDPs from study

--- users/gunz/experiments/config_2023_05_subsampling_tf2/config_35_monophone_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_35_monophone_mpc1x4_40ms.py
@@ -334,7 +334,7 @@ def run_single(
             *(
                 recog_args.with_tdp_scale(sc).with_tdp_speech(tdp_sp).with_tdp_silence(tdp_sil)
                 for sc, tdp_sp, tdp_sil in good_values
-                if ep == max(keep_epochs)
+                if ep == max(keep_epochs) or ep == 100
             ),
         ]:
             recognizer.recognize_count_lm(
