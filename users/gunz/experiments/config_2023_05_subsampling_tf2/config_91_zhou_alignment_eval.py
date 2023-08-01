a0cc253034e604084c4f723508aa7b01822f0d48
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-01 14:04:21 +0200

    add Zhou alignment eval

---
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_91_zhou_alignment_eval.py
@@ -0,0 +1,17 @@
+import os
+
+from sisyphus import gs, tk, Path
+
+from ...setups.common.analysis import PlotPhonemeDurationsJob
+from .config import ZHOU_ALLOPHONES, ZHOU_SUBSAMPLED_ALIGNMENT
+
+
+def run():
+    gs.ALIAS_AND_OUTPUT_SUBDIR = os.path.splitext(os.path.basename(__file__))[0][7:]
+
+    plots = PlotPhonemeDurationsJob(
+        alignment_bundle_path=Path(ZHOU_SUBSAMPLED_ALIGNMENT, cached=True),
+        allophones_path=Path(ZHOU_ALLOPHONES),
+        time_step_s=40 / 1000,
+    )
+    tk.register_output(f"alignments/40ms-zhou-blstm/phoneme_durations.png", plots.out_plot)
