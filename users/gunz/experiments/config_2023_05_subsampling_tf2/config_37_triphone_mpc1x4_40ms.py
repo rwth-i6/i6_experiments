6a83521c215680be80cb866a41f651d067f22a41
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-05 16:33:56 +0200

    remove erroneous jobs

--- users/gunz/experiments/config_2023_05_subsampling_tf2/config_37_triphone_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_05_subsampling_tf2/config_37_triphone_mpc1x4_40ms.py
@@ -299,7 +299,7 @@ def run_single(
     )
 
     best_config = None
-    for ep, crp_k in itertools.product([400, 550, max(keep_epochs)], ["dev-other"]):
+    for ep, crp_k in itertools.product([550, max(keep_epochs)], ["dev-other"]):
         s.set_binaries_for_crp(crp_k, RASR_TF_BINARY_PATH)
 
         s.set_triphone_priors_returnn_rasr(
