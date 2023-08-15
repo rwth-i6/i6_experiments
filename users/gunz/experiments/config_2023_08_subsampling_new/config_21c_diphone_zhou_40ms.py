8d98cfc650d902f95fffa11b5ad85bc880c11813
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-15 17:02:06 +0200

    bugfix

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21c_diphone_zhou_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21c_diphone_zhou_40ms.py
@@ -2158,7 +2158,7 @@ def run_single(
             train_corpus_key=s.crp_names["train"],
             dev_corpus_key=s.crp_names["cvtrain"],
             smoothen=True,
-            returnn_config=remove_label_pops_and_losses_from_returnn_config(returnn_config),
+            returnn_config=remove_label_pops_and_losses_from_returnn_config(returnn_config_ft),
             output_layer_name="output",
         )
 
