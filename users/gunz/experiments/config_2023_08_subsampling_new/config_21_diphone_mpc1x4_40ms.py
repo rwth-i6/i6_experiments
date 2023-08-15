58c1635ec8eb94934874a900458c3ed7b0b7524b
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-15 17:56:58 +0200

    fine-tune diphones

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21_diphone_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21_diphone_mpc1x4_40ms.py
@@ -386,12 +386,12 @@ def run_single(
     # FINE TUNING
     # ###########
 
-    fine_tune_epochs = 300
-    keep_epochs = [150, 275, 300]
+    fine_tune_epochs = 450
+    keep_epochs = [225, 400, 450]
 
     bw_scales = [
         baum_welch.BwScales(label_posterior_scale=0.3, label_prior_scale=None, transition_scale=0.0),
-        baum_welch.BwScales(label_posterior_scale=1.0, label_prior_scale=None, transition_scale=0.0)
+        baum_welch.BwScales(label_posterior_scale=1.0, label_prior_scale=None, transition_scale=0.0),
     ]
 
     for bw_scale in bw_scales:
@@ -411,8 +411,19 @@ def run_single(
             returnn_config=returnn_config_ft,
             log_linear_scales=bw_scale,
         )
+        lrates = oclr.get_learning_rates(
+            lrate=5e-5,
+            increase=0,
+            constLR=np.floor(fine_tune_epochs * 0.45),
+            decay=np.floor(fine_tune_epochs * 0.45),
+            decMinRatio=0.1,
+            decMaxRatio=1,
+        )
         update_config = returnn.ReturnnConfig(
             config={
+                "learning_rates": list(
+                    np.concatenate([lrates, np.linspace(min(lrates), 1e-6, fine_tune_epochs - len(lrates))])
+                ),
                 "preload_from_files": {
                     "existing-model": {
                         "init_for_train": True,
@@ -422,6 +433,9 @@ def run_single(
                 },
             },
             post_config={"cleanup_old_models": {"keep_best_n": 3, "keep": keep_epochs}},
+            python_epilog={
+                "lr": returnn.CodeWrapper("dynamic_learning_rate = None"),
+            },
         )
         returnn_config_ft.update(update_config)
 
