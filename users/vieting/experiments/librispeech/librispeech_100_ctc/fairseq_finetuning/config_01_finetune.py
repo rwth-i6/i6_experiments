87a6e941e3b357b88c213b742bd7566cb22be001
Andreas <andreas.pletschko@rwth-aachen.de>
2023-08-15 18:42:23 +0200

    added Union type hint

--- users/vieting/experiments/librispeech/librispeech_100_ctc/fairseq_finetuning/config_01_finetune.py
+++ users/vieting/experiments/librispeech/librispeech_100_ctc/fairseq_finetuning/config_01_finetune.py
@@ -6,8 +6,6 @@ from typing import List, Optional, Union
 
 import logging
 
-import logging
-
 from sisyphus import tk, gs
 import recipe.i6_core.datasets.librispeech as librispeech
 from recipe.i6_core.audio.encoding import BlissChangeEncodingJob
