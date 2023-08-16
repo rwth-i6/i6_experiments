119bf24a7931d85c0f144c6de8249fbae857402b
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-16 13:44:51 +0200

    normal batch size

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21b_diphone_multi_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21b_diphone_multi_mpc1x4_40ms.py
@@ -457,7 +457,6 @@ def run_single(
             )
             update_config = returnn.ReturnnConfig(
                 config={
-                    "batch_size": 8000,
                     "learning_rates": list(
                         np.concatenate([lrates, np.linspace(min(lrates), 1e-6, fine_tune_epochs - len(lrates))])
                     ),
--- users/gunz/experiments/config_2023_08_subsampling_new/config_21c_diphone_zhou_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21c_diphone_zhou_40ms.py
@@ -2151,7 +2151,6 @@ def run_single(
         )
         update_config = returnn.ReturnnConfig(
             config={
-                "batch_size": 8e3,
                 "learning_rates": list(
                     np.concatenate([lrates, np.linspace(min(lrates), 1e-6, fine_tune_epochs - len(lrates))])
                 ),
