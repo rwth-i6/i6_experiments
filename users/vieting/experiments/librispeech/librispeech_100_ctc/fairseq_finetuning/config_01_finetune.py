f06efb4adb3b88b6d57541e688138fa83cdec62e
Andreas <andreas.pletschko@rwth-aachen.de>
2023-08-17 13:41:53 +0200

    fairseq finetune restructuring and tidy up

--- users/vieting/experiments/librispeech/librispeech_100_ctc/fairseq_finetuning/config_01_finetune.py
+++ users/vieting/experiments/librispeech/librispeech_100_ctc/fairseq_finetuning/config_01_finetune.py
@@ -2,7 +2,7 @@
 Config for finetune experiments on LibriSpeech using wav2vec 2.0.
 """
 import os.path
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Dict
 
 import logging
 
@@ -11,28 +11,98 @@ import recipe.i6_core.datasets.librispeech as librispeech
 from recipe.i6_core.audio.encoding import BlissChangeEncodingJob
 from recipe.i6_core.tools.git import CloneGitRepositoryJob
 from recipe.i6_core.tools.download import DownloadJob 
+from recipe.i6_core.corpus.segments import ShuffleAndSplitSegmentsJob, SegmentCorpusJob
+from recipe.i6_core.corpus.filter import FilterCorpusBySegmentsJob
 from recipe.i6_experiments.common.datasets.librispeech.corpus import get_bliss_corpus_dict
 from recipe.i6_experiments.users.engler.fairseq.training import FairseqHydraConfig, FairseqHydraTrainingJob
-from recipe.i6_experiments.users.vieting.jobs.fairseq import CreateFairseqLabeledDataJob
+from recipe.i6_experiments.users.vieting.jobs.fairseq import CreateFairseqLabeledDataJob, MergeLabeledFairseqDataJob
 from recipe.i6_experiments.users.vieting.experiments.librispeech.librispeech_960_pretraining.wav2vec2.fairseq \
     import SetupFairseqJob
 
 
-def get_task(
+def get_task_dev_sampled(
+    corpus_name: str,
     valid_percent: float = 0.01, 
     audio_format: str = "ogg", 
-    output_prefix: str = "datasets", 
-    corpus_names: List[str] = ["train-clean-100", "train-clean-360", "train-other-500"]
+    output_prefix: str = "datasets",
 ):
     """
+    :param corpus_name: name of the corpora to be used for training and to sample the dev set from
     :param valid_percent: percentage of the training data to be used as validation set. 
-        If <= 0, the validation set is taken from the dev corpus.
     :param audio_format: audio format of the output files
     :param output_prefix: prefix of the output files
-    :param corpus_names: list of names of the corpora to be used for training
+    """
+    # check input
+    assert audio_format in ["ogg", "wav", "flac"], f"audio format not implemented: '{audio_format}'"
+    assert corpus_name in {
+        "train-clean-100", 
+        "train-clean-360", 
+        "train-clean-460", 
+        "train-other-500", 
+        "train-other-960",
+        "dev-clean",
+        "dev-other",
+        "test-clean",
+        "test-other",
+        }, f"unknown corpus name: {corpus_name}"
+    assert 0 <= valid_percent <= 1, f"invalid percentage: {valid_percent}"
+
+    # get corpus
+    corpus_dict = get_bliss_corpus_dict(audio_format=audio_format, output_prefix=output_prefix)
+    corpus_path = corpus_dict[corpus_name]
+
+    # split corpus into train and dev set
+    segment_corpus_job = SegmentCorpusJob(corpus_path, 1)
+
+    split = {"train": 1 - valid_percent, "dev": valid_percent}
+    shuffle_job = ShuffleAndSplitSegmentsJob(segment_corpus_job.out_single_segment_files[1])
+
+    train_filter_corpus_job = FilterCorpusBySegmentsJob(
+        bliss_corpus=corpus_path,
+        segment_file=shuffle_job.out_segments["train"], 
+        compressed=True,
+        delete_empty_recordings=True
+        )
+    dev_filter_corpus_job = FilterCorpusBySegmentsJob(
+        bliss_corpus=corpus_path,
+        segment_file=shuffle_job.out_segments["dev"],
+        compressed=True,
+        delete_empty_recordings=True
+        )    
+
+    # create labeled data for training and dev set
+    train_labels = get_labels(
+        dest_name="train",
+        corpus_paths=train_filter_corpus_job.out_corpus,
+    )
+    dev_labels = get_labels(
+        dest_name="dev",
+        corpus_paths=dev_filter_corpus_job.out_corpus,
+    )
+
+    # merge labeled data into single task directory
+    merge_job = MergeLabeledFairseqDataJob(
+        labeled_data_paths=[train_labels, dev_labels],
+        create_letter_dict=True,
+    )
+
+    task = merge_job.out_task_path
+    return task
+
+def get_task_dev_separate(
+    train_corpus_names: List[str],
+    dev_corpus_name: str,
+    audio_format: str = "ogg",
+    output_prefix: str = "datasets",
+):
+    """
+    :param train_corpus_names: list of names of the corpora to be used for training set
+    :param dev_corpus_name: name of the corpus to be used for validation set
+    :param audio_format: audio format of the output files
+    :param output_prefix: prefix of the output files
     """
     assert audio_format in ["ogg", "wav", "flac"], f"audio format not implemented: '{audio_format}'"
-    assert set(corpus_names).issubset(
+    assert set(train_corpus_names).issubset(
         {"train-clean-100", 
          "train-clean-360", 
          "train-clean-460", 
@@ -42,23 +112,49 @@ def get_task(
          "dev-other", 
          "test-clean", 
          "test-other"}
-    ), f"unknown corpus names: {corpus_names}"
+    ), f"unknown corpus names: {train_corpus_names}"
+
+    assert dev_corpus_name in {"dev-clean", "dev-other"}, f"unknown corpus name: {dev_corpus_name}"
 
     corpus_dict = get_bliss_corpus_dict(audio_format=audio_format, output_prefix=output_prefix)
-    # filter out corpora that are not in corpus_names
-    corpus_dict = {corpus: path for corpus, path in corpus_dict.items() if corpus in corpus_names}
+    train_corpus_paths = [corpus_dict[name] for name in train_corpus_names]
+    dev_corpus_path = corpus_dict[dev_corpus_name]
 
-    task_creation_job = CreateFairseqLabeledDataJob(
-        train_corpus_paths=list(corpus_dict.values()),
-        file_extension=audio_format,
-        sample_valid_percent=valid_percent,
-        train_dest_name="train",
-        valid_dest_name="valid",
+    train_labels = get_labels(
+        dest_name="train",
+        corpus_paths=train_corpus_paths,
+    )
+    dev_labels = get_labels(
+        dest_name=dev_corpus_name,
+        corpus_paths=dev_corpus_path,
+    )
+
+    merge_job = MergeLabeledFairseqDataJob(
+        labeled_data_paths=[train_labels, dev_labels],
         create_letter_dict=True,
     )
-    task_creation_job.rqmt["time"] = 4
-    task = task_creation_job.out_task_path
+    merge_job.rqmt["time"] = 4
+    task = merge_job.out_task_path
     return task
+
+def get_labels(
+    dest_name: str,
+    corpus_paths: Union[List[tk.Path], tk.Path],
+):
+    """
+    :param dest_name: name of the output file
+    :param corpus_paths: path to the corpora
+    """
+    if isinstance(corpus_paths, tk.Path):
+        corpus_paths = [corpus_paths]
+
+    label_data_job = CreateFairseqLabeledDataJob(
+        corpus_paths=corpus_paths,
+        dest_name=dest_name,
+    )
+    label_data_job.rqmt["time"] = 4
+    
+    return label_data_job.out_labels_path
 
 def get_fairseq_root(fairseq_python_exe: Optional[tk.Path] = None):
     """
@@ -72,15 +168,26 @@ def get_fairseq_root(fairseq_python_exe: Optional[tk.Path] = None):
     return fairseq_root
 
 
-def get_fairseq_args(w2v_path: tk.Path, corpus_names: List[str], num_gpus: int = 1):
+def get_fairseq_args(
+    labeled_data: tk.Path, 
+    w2v_path: tk.Path, 
+    dev_file_name: str = "dev", 
+    num_gpus: int = 1
+    ):
     """
+    :param labeled_data: path to the labeled data including the following files:
+        - train.tsv
+        - train.wrd
+        - train.ltr
+        - <dev_file_name>.tsv
+        - <dev_file_name>.wrd
+        - <dev_file_name>.ltr
+        - letter_dict.txt
+    :param dev_file_name: name of the dev label files
     :param w2v_path: path to the (pretrained) wav2vec model
     :param corpus_names: list of names of the corpora to be used for training
     :param num_gpus: number of gpus to be used for training
     """
-    # create wav2vec manifest for training
-    task = get_task(corpus_names=corpus_names)
-
     # Set training and model parameters
     fairseq_args = {
         "common": {
@@ -94,7 +201,7 @@ def get_fairseq_args(w2v_path: tk.Path, corpus_names: List[str], num_gpus: int =
         },
         "task": {
             "_name": "audio_finetuning",
-            "data": task,
+            "data": labeled_data,
             "normalize": False,
             "labels": "ltr"
         },
@@ -102,7 +209,7 @@ def get_fairseq_args(w2v_path: tk.Path, corpus_names: List[str], num_gpus: int =
             "num_workers": 2 * num_gpus,
             "max_tokens": 3200000,  # length of tokens in one batch
             "skip_invalid_size_inputs_valid_test": True,
-            "valid_subset": "valid",
+            "valid_subset": dev_file_name,
         },
         "distributed_training": {
             "distributed_world_size": num_gpus,
@@ -158,19 +265,37 @@ def get_pretrained_model(model_path: Optional[Union[str, tk.Path]] = None):
 
 
 def main():
-    prefix_name = "experiments/librispeech/librispeech_100_ctc/fairseq/"
-    # run finetuning
-    exp_name = "base"
-    num_gpus = 1
-    gpu_mem_rqmt = 24
-    corpus_names = ["train-clean-100"]
+    # defines
+    PREFIX_NAME = "experiments/librispeech/librispeech_100_ctc/fairseq/"
+    EXP_NAME = "base"
+    NUM_GPUS = 1
+    GPU_MEM_RQMT = 24
+    CORPUS_NAMES = ["train-clean-100"]
+    FAIRSEQ_PYTHON_EXE = tk.Path("/work/asr3/vieting/hiwis/pletschko/miniconda3/envs/fairseq_python38/bin/python")
+    USE_SAMPLED_DEV = False
 
+    # get pretrained model
     w2v_path = get_pretrained_model()
-    fairseq_python_exe = tk.Path("/work/asr3/vieting/hiwis/pletschko/miniconda3/envs/fairseq_python38/bin/python")
 
-    fairseq_args = get_fairseq_args(corpus_names=corpus_names, num_gpus=num_gpus, w2v_path=w2v_path)
+    # create wav2vec labeled data
+    if USE_SAMPLED_DEV:
+        dev_set = "dev"
+        labeled_data = get_task_dev_sampled(corpus_name=CORPUS_NAMES[0], valid_percent=0.01)
+    else:
+        dev_set = "dev-other"
+        labeled_data = get_task_dev_separate(train_corpus_names=CORPUS_NAMES, dev_corpus_name=dev_set)
+
+    # create fairseq config for finetuning
+    fairseq_args = get_fairseq_args(
+        labeled_data=labeled_data,
+        w2v_path=w2v_path,
+        dev_file_name=dev_set,
+        num_gpus=NUM_GPUS,
+    )
     fairseq_config = FairseqHydraConfig(fairseq_args)
-    fairseq_root = get_fairseq_root(fairseq_python_exe=fairseq_python_exe)
+    fairseq_root = get_fairseq_root(fairseq_python_exe=FAIRSEQ_PYTHON_EXE)
+
+    # run finetuning
     job = FairseqHydraTrainingJob(
         fairseq_config,
         max_epoch=300,
@@ -178,14 +303,14 @@ def main():
         time_rqmt=100,
         mem_rqmt=16,
         cpu_rqmt=2,
-        gpu_rqmt=num_gpus,
-        gpu_mem_rqmt=gpu_mem_rqmt,
+        gpu_rqmt=NUM_GPUS,
+        gpu_mem_rqmt=GPU_MEM_RQMT,
         fairseq_root=fairseq_root,
-        fairseq_python_exe=fairseq_python_exe,
-        use_cache_manager=True,
+        fairseq_python_exe=FAIRSEQ_PYTHON_EXE,
+        use_cache_manager=False,
     )
     
-    job.add_alias(os.path.join(prefix_name, exp_name, "finetune"))
-    tk.register_output(os.path.join(prefix_name, exp_name, "finetune", "scores.png"), job.out_plot_se)
+    job.add_alias(os.path.join(PREFIX_NAME, EXP_NAME, "finetune"))
+    tk.register_output(os.path.join(PREFIX_NAME, EXP_NAME, "finetune", "scores.png"), job.out_plot_se)
 
 main()
