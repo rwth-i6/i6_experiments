9b5a60fec22e9ce03fa55ab17120302dc2061750
Moritz Gunz <moritz.gunz@gmail.com>
2023-08-17 11:13:11 +0200

    smaller batch for fine-tuning

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21_diphone_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21_diphone_mpc1x4_40ms.py
@@ -535,6 +535,7 @@ def run_single(
             )
             update_config = returnn.ReturnnConfig(
                 config={
+                    "batch_size": 11000,
                     "learning_rates": list(
                         np.concatenate([lrates, np.linspace(min(lrates), 1e-6, fine_tune_epochs - len(lrates))])
                     ),
