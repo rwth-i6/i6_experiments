83b5ec2441f2d68671a2064d2d899aa017cff4dc
Albert Zeyer <albzey@gmail.com>
2023-09-02 12:05:12 +0200

    comment

--- users/zeineldeen/models/asr/encoder/conformer_encoder.py
+++ users/zeineldeen/models/asr/encoder/conformer_encoder.py
@@ -398,6 +398,7 @@ class ConformerEncoder:
             # which would be difficult otherwise.
             # C is approx 15-20.
             # Then we can concat it to K and V.
+            # Note on prefix_name: The outer _create_mhsa_module adds the additional "_self_att" prefix.
             mem_bank = self._block_prefix_name(layer_index - 1) + "_self_att_emformer_mem"  # [B*C, D]
             mem_bank = self.network.add_generic_layer(
                 f"{prefix_name}_emformer_mem_split_batch_time",
