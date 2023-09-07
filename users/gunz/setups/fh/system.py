321534cee193c83cec3c58ac6bfe45af137778d2
Moritz Gunz <moritz.gunz@gmail.com>
2023-09-07 14:23:03 +0200

    fix stats per epoch

--- users/gunz/setups/fh/system.py
+++ users/gunz/setups/fh/system.py
@@ -1624,9 +1624,7 @@ class FactoredHybridSystem(NnSystem):
                 tdp_nonspeech=to_tdp(params.tdp_non_word),
                 altas=params.altas,
             )
-            stats_alias = (
-                f"statistics-nn-pch/{self.experiments[key]['name']}/{exp_str}_beam{params.beam}_bl{params.beam_limit}"
-            )
+            stats_alias = f"statistics-nn-pch/{self.experiments[key]['name']}/ep{epoch}/{exp_str}_beam{params.beam}_bl{params.beam_limit}"
 
             stats_job.add_alias(stats_alias)
             tk.register_output(f"{stats_alias}/avg_states", stats_job.avg_states)
