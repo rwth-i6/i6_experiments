d420a6ab86f4a509fff9af08db694670e05c71e5
Moritz Gunz <moritz.gunz@gmail.com>
2023-09-08 09:37:37 +0200

    more mem for training

--- users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_11_diphone.py
+++ users/gunz/experiments/config_2023_05_baselines_thesis_tf2/config_11_diphone.py
@@ -584,7 +584,7 @@ def run_single(
             train_args = {
                 **s.initial_train_args,
                 "num_epochs": fine_tune_epochs,
-                "mem_rqmt": 10,
+                "mem_rqmt": 12,
                 "partition_epochs": partition_epochs,
                 "returnn_config": copy.deepcopy(returnn_config_ft),
             }
