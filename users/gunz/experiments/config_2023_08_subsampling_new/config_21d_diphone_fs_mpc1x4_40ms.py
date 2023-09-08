b074df2fae801917879ae2783a2ad21fdae6f712
Moritz Gunz <moritz.gunz@gmail.com>
2023-09-08 12:25:18 +0200

    train 1200 epochs full-sum

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21d_diphone_fs_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21d_diphone_fs_mpc1x4_40ms.py
@@ -103,7 +103,7 @@ def run(returnn_root: tk.Path, alignment: tk.Path, a_name: str):
             fine_tune=a_name == "40ms-FF-v8",
             lr="v13",
             # 200 is the equivalent number of epochs in training time of training an FF-NN for alignment
-            num_epochs=600 + 200,
+            num_epochs=600 + 600,
             run_performance_study=a_name == "40ms-FF-v8",
             tune_decoding=a_name == "40ms-FF-v8",
             run_tdp_study=False,
