d070e982115e3123fbcabc745f408ee161a410ee
Moritz Gunz <moritz.gunz@gmail.com>
2023-09-16 17:14:56 +0200

    lay out exps for the plan

--- users/gunz/experiments/config_2023_08_subsampling_new/__init__.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/__init__.py
@@ -261,6 +261,8 @@ def the_plan():
 
     returnn_root = _clone_returnn_safe()
 
+    # FS vs. MP
+
     phmm_30ms_mp_a = get_n_blstm_a(feature_stacking=False, transition_scale=0.3, adapted_tdps=False, t_step=30 / 1000)
     phmm_30ms_fs_a = get_n_blstm_a(feature_stacking=True, transition_scale=0.3, adapted_tdps=False, t_step=30 / 1000)
 
@@ -278,6 +280,29 @@ def the_plan():
     config_21_diphone_mpc1x4_40ms.run(returnn_root=returnn_root, alignment=phmm_40ms_fs_a, a_name="40ms-Bfs-v8")
     config_21h_diphone_fs1x4_40ms.run(returnn_root=returnn_root, alignment=phmm_40ms_mp_a, a_name="40ms-Bmp-v8")
     config_21h_diphone_fs1x4_40ms.run(returnn_root=returnn_root, alignment=phmm_40ms_fs_a, a_name="40ms-Bfs-v8")
+
+    # P-HMM ADAPTED TDPs
+
+    phmm_40ms_mp_adapted_a = get_n_blstm_a(
+        feature_stacking=False, transition_scale=0.3, adapted_tdps=True, t_step=40 / 1000
+    )
+    config_21_diphone_mpc1x4_40ms.run(returnn_root=returnn_root, alignment=phmm_40ms_mp_adapted_a, a_name="40ms-Ba-v8")
+
+    # P-HMM-S
+
+    phmms_30ms_mp_a = get_n_blstm_a(feature_stacking=False, transition_scale=0.0, adapted_tdps=None, t_step=30 / 1000)
+    phmms_40ms_mp_a = get_n_blstm_a(feature_stacking=False, transition_scale=0.0, adapted_tdps=None, t_step=40 / 1000)
+
+    config_11_diphone_mpc1x3_30ms.run(returnn_root=returnn_root, alignments=[(phmms_30ms_mp_a, "30ms-Bs-v8")])
+    config_21_diphone_mpc1x4_40ms.run(returnn_root=returnn_root, alignment=phmms_40ms_mp_a, a_name="40ms-Bs-v8")
+
+    # P-HMM-S FF-NN
+
+    phmms_30ms_ffnn_a = get_30ms_linear_a(t_scale=0.0)
+    phmms_40ms_ffnn_a = get_40ms_linear_a(t_scale=0.0)
+
+    config_11_diphone_mpc1x3_30ms.run(returnn_root=returnn_root, alignments=[(phmms_30ms_ffnn_a, "30ms-FFs-v8")])
+    config_21_diphone_mpc1x4_40ms.run(returnn_root=returnn_root, alignment=phmms_40ms_ffnn_a, a_name="40ms-FFs-v8")
 
 
 def main():
