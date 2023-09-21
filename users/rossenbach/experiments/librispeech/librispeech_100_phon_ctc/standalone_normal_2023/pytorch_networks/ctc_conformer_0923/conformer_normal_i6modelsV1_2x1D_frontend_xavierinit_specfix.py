49a0e92c2bb0b971ec30879330aa5213f1843776
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2023-09-21 17:19:09 +0200

    add ls100 flashlight phoneme ctc

---
+++ users/rossenbach/experiments/librispeech/librispeech_100_phon_ctc/standalone_normal_2023/pytorch_networks/ctc_conformer_0923/conformer_normal_i6modelsV1_2x1D_frontend_xavierinit_specfix.py
@@ -0,0 +1,407 @@
+"""
+Trying to make the aligner more AppTek-Like
+
+Extended weight init code
+"""
+
+from dataclasses import dataclass
+import numpy as np
+import torch
+from torch import nn
+from typing import Any, Dict, Optional, Tuple, Union
+import math
+
+from i6_models.parts.conformer.norm import LayerNormNC
+from i6_models.assemblies.conformer.conformer_v1 import ConformerEncoderV1Config, ConformerEncoderV1
+from i6_models.assemblies.conformer.conformer_v1 import ConformerBlockV1Config, ConformerBlockV1
+from i6_models.config import ModuleFactoryV1, ModelConfiguration
+from i6_models.primitives.feature_extraction import LogMelFeatureExtractionV1, LogMelFeatureExtractionV1Config
+from i6_models.parts.frontend.common import mask_pool
+
+from i6_models.parts.conformer.convolution import ConformerConvolutionV1Config, ConformerConvolutionV1
+from i6_models.parts.conformer.feedforward import ConformerPositionwiseFeedForwardV1Config, ConformerPositionwiseFeedForwardV1
+from i6_models.parts.conformer.mhsa import ConformerMHSAV1Config, ConformerMHSAV1
+
+from returnn.torch.context import get_run_ctx
+
+from .conformer_transparent_i6modelsV1_2x1D_frontend_xavierinit_cfg import TwoLayer1DFrontendConfig, ModelConfig
+from .specaugment_fixed import returnn_specaugment_by_length
+
+
+def mask_tensor(tensor: torch.Tensor, seq_len: torch.Tensor) -> torch.Tensor:
+    """
+    mask a tensor with a "positive" mask (boolean true means position is used)
+
+    This function is traceable.
+
+    :param tensor: [B,T,....]
+    :param seq_len: [B]
+    :return: [B,T]
+    """
+    seq_len = seq_len.to(device=tensor.device)
+    r = torch.arange(tensor.shape[1], device=tensor.device)  # [T]
+    seq_mask = torch.less(r[None, :], seq_len[:, None])  # broadcast to [B,T]
+    return seq_mask
+
+
+class ESPNetPositionalEncoding(torch.nn.Module):
+    """
+    Absolute positional encoding taken from ESPNet, reformulated in i6-style
+    https://github.com/espnet/espnet/blob/5d0758e2a7063b82d1f10a8ac2de98eb6cf8a352/espnet/nets/pytorch_backend/transformer/embedding.py#L35
+
+    :param d_model: Embedding dimension.
+    :param dropout_rate: Dropout rate.
+    :param max_len: Maximum input length.
+    """
+
+    def __init__(self, d_model: int, dropout_rate: float, max_len: int = 5000):
+        super(ESPNetPositionalEncoding, self).__init__()
+        self.d_model = d_model
+        self.xscale = math.sqrt(self.d_model)
+        self.dropout = torch.nn.Dropout(p=dropout_rate)
+        self.pe = None
+        self.extend_pe(torch.tensor(0.0).expand(1, max_len))
+
+    def extend_pe(self, x):
+        """
+        Reset the positional encodings.
+
+        :param x:
+        """
+        if self.pe is not None:
+            if self.pe.size(1) >= x.size(1):
+                if self.pe.dtype != x.dtype or self.pe.device != x.device:
+                    self.pe = self.pe.to(dtype=x.dtype, device=x.device)
+                return
+        pe = torch.zeros(x.size(1), self.d_model)
+        position = torch.arange(0, x.size(1), dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, self.d_model, 2, dtype=torch.float32)
+            * -(math.log(10000.0) / self.d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.pe = pe.to(device=x.device, dtype=x.dtype)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Add positional encoding.
+
+        :param x: Input tensor [B, T, *]
+        :returns: Masked tensor [B, T, *]
+        """
+        self.extend_pe(x)
+        x = x * self.xscale + self.pe[:, : x.size(1)]
+        return self.dropout(x)
+
+
+class TwoLayer1DFrontend(nn.Module):
+    """
+    Convolutional Front-End using two 1-D Convolutions
+
+     - Contains Batch-Norm, but no activation functions.
+     - Applies absolute positional encoding on the output.
+     - Contains no linear mapping, so conv2_channels need to be equivalent to Model size
+    """
+
+    def __init__(self, model_cfg: TwoLayer1DFrontendConfig):
+        """
+        :param model_cfg: model configuration for this module
+        """
+        super().__init__()
+
+        model_cfg.check_valid()
+
+        self.cfg = model_cfg
+
+        self.conv1 = nn.Conv1d(
+            in_channels=model_cfg.in_features,
+            out_channels=model_cfg.conv1_channels,
+            kernel_size=model_cfg.conv1_kernel_size,
+            stride=model_cfg.conv1_stride
+        )
+        self.conv2 = nn.Conv1d(
+            in_channels=model_cfg.conv1_channels,
+            out_channels=model_cfg.conv2_channels,
+            kernel_size=model_cfg.conv2_kernel_size,
+            stride=model_cfg.conv2_stride
+        )
+
+        self.bn1 = nn.BatchNorm1d(num_features=model_cfg.conv1_channels)
+        self.bn2 = nn.BatchNorm1d(num_features=model_cfg.conv2_channels)
+        self.pos_encoding = ESPNetPositionalEncoding(model_cfg.conv2_channels, model_cfg.dropout)
+
+
+    def forward(self, tensor: torch.Tensor, sequence_mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        T might be reduced to T' or T'' depending on stride of the layers
+
+        stride is only allowed for the pool1 and pool2 operation.
+        other ops do not have stride configurable -> no update of mask sequence required but added anyway
+
+        :param tensor: input tensor of shape [B,T,F]
+        :param sequence_mask: the sequence mask for the tensor
+        :return: torch.Tensor of shape [B,T",F'] and the shape of the sequence mask
+        """
+        tensor = tensor.permute(0, 2, 1)  # [B,T,F] -> [B,C,T]
+
+        tensor = self.conv1(tensor)
+        tensor = self.bn1(tensor)
+        sequence_mask = mask_pool(
+            seq_mask=sequence_mask,
+            kernel_size=self.conv1.kernel_size[0],
+            stride=self.conv1.stride[0],
+            padding=self.conv1.padding[0],
+        )
+
+        tensor = self.conv2(tensor)
+        tensor = self.bn2(tensor)
+        sequence_mask = mask_pool(
+            sequence_mask,
+            kernel_size=self.conv2.kernel_size[0],
+            stride=self.conv2.stride[0],
+            padding=self.conv2.padding[0],
+        )
+
+        tensor = tensor.permute(0, 2, 1) # [B,C,T] -> [B, T, hidden]
+        tensor = self.pos_encoding(tensor)
+
+        return tensor, sequence_mask
+
+    def _calculate_dim(self) -> int:
+        return self.conv2.out_channels
+
+
+class Model(torch.nn.Module):
+    def __init__(self, model_config_dict, **kwargs):
+        super().__init__()
+        self.cfg = ModelConfig.from_dict(model_config_dict)
+        fe_config = LogMelFeatureExtractionV1Config(
+            sample_rate=16000,
+            win_size=0.025,
+            hop_size=0.01,
+            f_min=60,
+            f_max=7600,
+            min_amp=1e-10,
+            num_filters=80,
+            center=False,
+        )
+        frontend_config = self.cfg.frontend_config
+        conformer_size = self.cfg.conformer_size
+        conformer_config = ConformerEncoderV1Config(
+            num_layers=self.cfg.num_layers,
+            frontend=ModuleFactoryV1(module_class=TwoLayer1DFrontend, cfg=frontend_config),
+            block_cfg=ConformerBlockV1Config(
+                ff_cfg=ConformerPositionwiseFeedForwardV1Config(
+                    input_dim=conformer_size,
+                    hidden_dim=conformer_size,
+                    dropout=self.cfg.ff_dropout,
+                    activation=nn.functional.silu,
+                ),
+                mhsa_cfg=ConformerMHSAV1Config(
+                    input_dim=conformer_size,
+                    num_att_heads=self.cfg.num_heads,
+                    att_weights_dropout=self.cfg.att_weights_dropout,
+                    dropout=self.cfg.mhsa_dropout,
+                ),
+                conv_cfg=ConformerConvolutionV1Config(
+                    channels=conformer_size, kernel_size=self.cfg.conv_kernel_size, dropout=self.cfg.conv_dropout, activation=nn.functional.silu,
+                    norm=LayerNormNC(conformer_size)
+                ),
+            ),
+        )
+
+        run_ctx = get_run_ctx()
+        dataset = run_ctx.engine.train_dataset or run_ctx.engine.forward_dataset
+        # self.label_target_size = len(dataset.datasets["zip_dataset"].targets.labels)
+        self.label_target_size = 79
+
+        self.feature_extraction = LogMelFeatureExtractionV1(cfg=fe_config)
+        self.conformer = ConformerEncoderV1(cfg=conformer_config)
+        self.final_linear = nn.Linear(conformer_size, self.label_target_size + 1)  # + CTC blank
+        self.final_dropout = nn.Dropout(p=self.cfg.final_dropout)
+
+        self.export_mode = False
+        
+        # initialize weights
+        self.apply(self._weight_init)
+
+    @staticmethod
+    def _weight_init(module: torch.nn.Module):
+        if isinstance(module, (torch.nn.Conv1d, torch.nn.Linear)):
+            print("apply weight init for %s" % str(module))
+            nn.init.xavier_uniform_(module.weight)
+
+    def forward(
+            self,
+            raw_audio: torch.Tensor,
+            raw_audio_len: torch.Tensor,
+    ):
+        """
+
+        :param raw_audio:
+        :param raw_audio_len:
+        :return: logprobs [B, T, #labels + blank]
+        """
+        
+        squeezed_features = torch.squeeze(raw_audio)
+        with torch.no_grad():
+            audio_features, audio_features_len = self.feature_extraction(squeezed_features, raw_audio_len)
+
+            if self.training:
+                audio_features_masked_2 = returnn_specaugment_by_length(
+                    audio_features,
+                    repeat_per_n_frames=self.cfg.specaug_config.repeat_per_n_frames,
+                    max_dim_time=self.cfg.specaug_config.max_dim_time,
+                    num_repeat_feat=self.cfg.specaug_config.num_repeat_feat,
+                    max_dim_feat=self.cfg.specaug_config.max_dim_feat)
+            else:
+                audio_features_masked_2 = audio_features
+
+        conformer_in = audio_features_masked_2
+        # create the mask for the conformer input
+        mask = mask_tensor(conformer_in, audio_features_len)
+
+        conformer_out, out_mask = self.conformer(conformer_in, mask)
+        conformer_out = self.final_dropout(conformer_out)
+        logits = self.final_linear(conformer_out)
+
+        log_probs = torch.log_softmax(logits, dim=2)
+
+        return log_probs, torch.sum(out_mask, dim=1)
+
+
+def train_step(*, model: Model, data, run_ctx, **kwargs):
+
+    raw_audio = data["raw_audio"]  # [B, T', F]
+    raw_audio_len = data["raw_audio:size1"]  # [B]
+
+    bpe_labels = data["phon_labels"]  # [B, N] (sparse)
+    bpe_labels_len = data["phon_labels:size1"]  # [B, N]
+
+    logprobs, audio_features_len = model(
+        raw_audio=raw_audio,
+        raw_audio_len=raw_audio_len,
+    )
+    transposed_logprobs = torch.permute(logprobs, (1, 0, 2))  # CTC needs [T, B, F]
+    ctc_loss = nn.functional.ctc_loss(
+        transposed_logprobs,
+        bpe_labels,
+        input_lengths=audio_features_len,
+        target_lengths=bpe_labels_len,
+        blank=model.label_target_size,
+        reduction="sum",
+    )
+    num_phonemes = torch.sum(bpe_labels_len)
+    run_ctx.mark_as_loss(name="ctc", loss=ctc_loss, inv_norm_factor=num_phonemes)
+
+
+def forward_init_hook(run_ctx, **kwargs):
+    # we are storing durations, but call it output.hdf to match
+    # the default output of the ReturnnForwardJob
+    from torchaudio.models.decoder import ctc_decoder
+    run_ctx.recognition_file = open("search_out.py", "wt")
+    run_ctx.recognition_file.write("{\n")
+    import subprocess
+    if kwargs["arpa_lm"] is not None:
+        lm = subprocess.check_output(["cf", kwargs["arpa_lm"]]).decode().strip()
+    else:
+        lm = None
+    from returnn.datasets.util.vocabulary import Vocabulary
+    vocab = Vocabulary.create_vocab(
+        vocab_file="/work/asr4/rossenbach/sisyphus_work_folders/tts_asr_2023_work/i6_core/returnn/vocabulary/ReturnnVocabFromPhonemeInventory.paQ9HU4t5srU/output/vocab.pkl", unknown_label="[UNKNOWN]")
+    labels = vocab.labels
+    run_ctx.ctc_decoder = ctc_decoder(
+        lexicon=kwargs["lexicon"],
+        lm=lm,
+        lm_weight=kwargs["lm_weight"],
+        tokens=labels + ["[blank]", "[SILENCE]"],
+        blank_token="[blank]",
+        sil_token="[SILENCE]",
+        unk_word="[UNKNOWN]",
+        nbest=1,
+        beam_size=kwargs["beam_size"],
+        beam_threshold=kwargs["beam_threshold"],
+        sil_score=kwargs.get("sil_score", 0.0),
+        word_score=kwargs.get("word_score", 0.0),
+    )
+    run_ctx.labels = labels
+    run_ctx.blank_log_penalty = kwargs.get("blank_log_penalty", None)
+
+    if kwargs.get("prior_file", None):
+        run_ctx.prior = np.loadtxt(kwargs["prior_file"], dtype="float32")
+        run_ctx.prior_scale = kwargs["prior_scale"]
+    else:
+        run_ctx.prior = None
+
+
+def forward_finish_hook(run_ctx, **kwargs):
+    run_ctx.recognition_file.write("}\n")
+    run_ctx.recognition_file.close()
+
+    # write empty HDF until new ForwardJob exists
+    f = open("output.hdf", "wt")
+    f.write(" ")
+    f.close()
+
+
+def search_step(*, model: Model, data, run_ctx, **kwargs):
+    raw_audio = data["raw_audio"]  # [B, T', F]
+    raw_audio_len = data["raw_audio:size1"]  # [B]
+
+    logprobs, audio_features_len = model(
+        raw_audio=raw_audio,
+        raw_audio_len=raw_audio_len,
+    )
+
+    tags = data["seq_tag"]
+
+    logprobs_cpu = logprobs.cpu()
+    if run_ctx.blank_log_penalty is not None:
+        # assumes blank is last
+        logprobs_cpu[:, :, -1] -= run_ctx.blank_log_penalty
+    if run_ctx.prior is not None:
+        logprobs_cpu -= run_ctx.prior_scale * run_ctx.prior
+
+    hypothesis = run_ctx.ctc_decoder(logprobs_cpu, audio_features_len.cpu())
+
+    for hyp, tag in zip(hypothesis, tags):
+        words = hyp[0].words
+        sequence = " ".join([word for word in words if not word.startswith("[")])
+        print(sequence)
+        run_ctx.recognition_file.write("%s: %s,\n" % (repr(tag), repr(sequence)))
+        
+        
+def prior_init_hook(run_ctx, **kwargs):
+    # we are storing durations, but call it output.hdf to match
+    # the default output of the ReturnnForwardJob
+    run_ctx.sum_probs = None
+    run_ctx.sum_frames = 0
+
+
+def prior_finish_hook(run_ctx, **kwargs):
+    all_frames = run_ctx.sum_frames.detach().cpu().numpy()
+    all_probs = run_ctx.sum_probs.detach().cpu().numpy()
+    average_probs = all_probs / all_frames
+    log_average_probs = np.log(average_probs)
+    print("Prior sum in std-space (should be close to 1.0):", np.sum(average_probs))
+    with open("prior.txt", 'w') as f:
+        np.savetxt(f, log_average_probs, delimiter=' ')
+    print("Saved prior in prior.txt in +log space.")
+
+
+def prior_step(*, model: Model, data, run_ctx, **kwargs):
+    raw_audio = data["raw_audio"]  # [B, T', F]
+    raw_audio_len = data["raw_audio:size1"]  # [B]
+
+    logprobs, audio_features_len = model(
+        raw_audio=raw_audio,
+        raw_audio_len=raw_audio_len,
+    )
+
+    probs = torch.exp(logprobs)
+    run_ctx.sum_frames = run_ctx.sum_frames + torch.sum(audio_features_len)
+    if run_ctx.sum_probs is None:
+        run_ctx.sum_probs = torch.sum(probs, dim=(0, 1))
+    else:
+        run_ctx.sum_probs += torch.sum(probs, dim=(0, 1))
