13a658fa3f5ec370363c1c4fdacbcf3dd7f83ddc
Moritz Gunz <moritz.gunz@gmail.com>
2023-10-04 14:34:50 +0200

    test

--- users/gunz/setups/common/analysis/tse.py
+++ users/gunz/setups/common/analysis/tse.py
@@ -110,8 +110,8 @@ class ComputeTimestampErrorJob(Job):
                     f"len mismatch in {seg} of {len(a_states_dedup)} vs. {len(a_states_ref_dedup)}, skipping due to different pronunciation"
                 )
 
-                logging.info(f"align: {a_states}")
-                logging.info(f"ref align: {a_states_ref}")
+                logging.info(f"align: {a_states_dedup}")
+                logging.info(f"ref align: {a_states_ref_dedup}")
 
                 return
 
