f741a08d623c3691d3b3961f3206a8d97813ed55
Moritz Gunz <moritz.gunz@gmail.com>
2023-10-18 10:22:42 +0200

    pop more keys

--- users/gunz/experiments/config_2023_08_subsampling_new/config_21j_diphone_realign_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_21j_diphone_realign_mpc1x4_40ms.py
@@ -792,8 +792,8 @@ def run_single(
             s._update_crp_am_setting("train-other-960.train", tdp_type="default", add_base_allophones=False)
 
             graph_cfg = copy.deepcopy(returnn_config)
-            to_pop = [k for k in graph_cfg.config["network"].keys() if "aux_" in k]
-            for k in to_pop:
+            aux_to_pop = [k for k in graph_cfg.config["network"].keys() if "aux_" in k]
+            for k in [*aux_to_pop, "currentState", "linear1-triphone", "linear2-triphone", "right-output"]:
                 graph_cfg.config["network"].pop(k)
 
             s.set_returnn_config_for_experiment("fh-fs", graph_cfg)
