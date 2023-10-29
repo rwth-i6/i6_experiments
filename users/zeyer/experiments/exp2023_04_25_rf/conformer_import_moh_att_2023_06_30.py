5e0a8af977f2ae48272caa9291eb76d2a5c62588
Albert Zeyer <albzey@gmail.com>
2023-10-29 11:48:02 +0100

    no normalized loss

--- users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
+++ users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
@@ -91,9 +91,14 @@ def sis_run_with_prefix(prefix_name: str = None):
     _train_exp("base-24gb-v3-adam", config_24gb_v3, config_updates={"optimizer.class": "adam"})
     _train_exp("base-24gb-v3-lr1e_3", config_24gb_v3, config_updates={"learning_rate": 0.001})
     _train_exp(
-        "base-24gb-v3-adam-lossscales",
+        "base-24gb-v3-adam-lossscales-lossnonorm",
         config_24gb_v3,
-        config_updates={"optimizer.class": "adam", "aux_loss_scales": [0.1, 0.2], "aed_loss_scale": 0.1},
+        config_updates={
+            "optimizer.class": "adam",
+            "aux_loss_scales": [0.1, 0.2],
+            "aed_loss_scale": 0.1,
+            "use_normalized_loss": False,
+        },
     )
 
 
@@ -514,6 +519,7 @@ def from_scratch_training(
     aux_loss_layers = config.typed_value("aux_loss_layers")
     aux_loss_scales = config.typed_value("aux_loss_scales", ([1.0] * len(aux_loss_layers)) if aux_loss_layers else None)
     aed_loss_scale = config.float("aed_loss_scale", 1.0)
+    use_normalized_loss = config.bool("use_normalized_loss", True)
 
     if data.feature_dim and data.feature_dim.dimension == 1:
         data = rf.squeeze(data, axis=data.feature_dim)
@@ -538,7 +544,7 @@ def from_scratch_training(
                 f"ctc_{layer_idx}",
                 scale=aux_loss_scales[i],
                 custom_inv_norm_factor=targets_spatial_dim.get_size_tensor(),
-                use_normalized_loss=True,
+                use_normalized_loss=use_normalized_loss,
             )
             # decoded, decoded_spatial_dim = rf.ctc_greedy_decode(aux_logits, in_spatial_dim=enc_spatial_dim)
             # error = rf.edit_distance(
@@ -581,7 +587,7 @@ def from_scratch_training(
     loss = rf.cross_entropy(
         target=targets_packed, estimated=log_prob, estimated_type="log-probs", axis=model.target_dim
     )
-    loss.mark_as_loss("ce", scale=aed_loss_scale, use_normalized_loss=True)
+    loss.mark_as_loss("ce", scale=aed_loss_scale, use_normalized_loss=use_normalized_loss)
 
     best = rf.reduce_argmax(logits_packed, axis=model.target_dim)
     frame_error = best != targets_packed
