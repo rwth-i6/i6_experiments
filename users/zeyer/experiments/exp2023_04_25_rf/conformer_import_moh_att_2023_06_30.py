49c6acd70e1f086dc685e4bd427acbaf92babe1d
Albert Zeyer <albzey@gmail.com>
2023-10-31 11:46:33 +0100

    cleanup

--- users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
+++ users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
@@ -93,11 +93,6 @@ def sis_run_with_prefix(prefix_name: str = None):
     _train_exp("base-24gb-v3-lr1e_3", config_24gb_v3, config_updates={"learning_rate": 0.001})
     _train_exp("base-24gb-v3-aux12", config_24gb_v3, config_updates={"aux_loss_layers": [12]})
     _train_exp(
-        "base-24gb-v3-lossscalesF",  # does not work?
-        config_24gb_v3,
-        config_updates={"aux_loss_scales": [0.1, 0.2], "aed_loss_scale": 0.7},
-    )
-    _train_exp(
         "base-24gb-v3-adam-lossscales-lossnonorm",  # works? but took very long
         config_24gb_v3,
         config_updates={
