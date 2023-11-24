1e3358c3b3c58c37b13b003aadd81347be2af047
Albert Zeyer <albzey@gmail.com>
2023-11-24 21:52:03 +0100

    runnable jobs debugging

--- users/zeyer/experiments/exp2023_04_25_rf/baseline.py
+++ users/zeyer/experiments/exp2023_04_25_rf/baseline.py
@@ -8,12 +8,3 @@ from .conformer_import_moh_att_2023_06_30 import train_exp, config_24gb_v4
 # run directly via `sis m ...`
 def py():
     train_exp("base-24gb-v4", config_24gb_v4)
-
-    from sisyphus import tk
-    from i6_core.audio.encoding import BlissChangeEncodingJob
-
-    for target in list(tk.sis_graph.targets):
-        job = target._sis_path.creator
-        if isinstance(job, BlissChangeEncodingJob):
-            tk.sis_graph.targets.remove(target)
-            tk.sis_graph.active_targets.remove(target)
