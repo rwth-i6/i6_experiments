4917f797eedab5ecac005a38c81e8f6421211de6
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-06 12:52:50 +0100

    cleanup

--- users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
+++ users/zeyer/experiments/exp2023_04_25_rf/conformer_import_moh_att_2023_06_30.py
@@ -312,33 +312,24 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
     )
     train_exp(
         "v6-11gb-f32-bs15k-accgrad4-mgpu4-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad4_mgpu,
-        config_updates={
-            "optimizer.weight_decay": 1e-4,
-            **_cfg_lrlin1e_5_295k,  # total steps after 500 epochs: ~652k
-        },
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
+        config_updates={"accum_grad_multiple_step": 4},
         gpu_mem=11,
         num_processes=4,  # multi-GPU
         num_epochs=500,  # because of multi-GPU, 1 subepoch here is like 4 subepochs in single-GPU
     )
     train_exp(
         "v6-11gb-f32-bs15k-accgrad1-mgpu4-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu,
-        config_updates={
-            "optimizer.weight_decay": 1e-4,
-            **_cfg_lrlin1e_5_295k,  # total steps after 500 epochs: ~652k
-        },
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
         gpu_mem=11,
         num_processes=4,  # multi-GPU
         num_epochs=500,  # because of multi-GPU, 1 subepoch here is like 4 subepochs in single-GPU
     )
     train_exp(
         "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg4-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu,
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
         config_updates={
             "torch_distributed": {"reduce_type": "param", "param_sync_step": 4},  # multi-GPU
-            "optimizer.weight_decay": 1e-4,
-            **_cfg_lrlin1e_5_295k,  # total steps after 500 epochs: ~652k
         },
         gpu_mem=11,
         num_processes=4,  # multi-GPU
@@ -346,11 +337,9 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
     )
     train_exp(
         "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg10-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu,
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
         config_updates={
             "torch_distributed": {"reduce_type": "param", "param_sync_step": 10},  # multi-GPU
-            "optimizer.weight_decay": 1e-4,
-            **_cfg_lrlin1e_5_295k,  # total steps after 500 epochs: ~652k
         },
         gpu_mem=11,
         num_processes=4,  # multi-GPU
@@ -358,11 +347,9 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
     )
     train_exp(
         "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu,
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
         config_updates={
             "torch_distributed": {"reduce_type": "param", "param_sync_step": 100},  # multi-GPU
-            "optimizer.weight_decay": 1e-4,
-            **_cfg_lrlin1e_5_295k,
         },
         gpu_mem=11,
         num_processes=4,  # multi-GPU
@@ -657,6 +644,13 @@ config_11gb_v6_f32_bs15k_accgrad1_mgpu = dict_update_deep(
     config_11gb_v6_f32_bs15k_accgrad4_mgpu,
     {
         "accum_grad_multiple_step": 1,
+    },
+)
+config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k = dict_update_deep(
+    config_11gb_v6_f32_bs15k_accgrad1_mgpu,
+    {
+        "optimizer.weight_decay": 1e-4,
+        **_cfg_lrlin1e_5_295k,
     },
 )
 
