d4b5bad172439f460d55bacb7d65d6ed1ef7965d
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-12-13 14:06:07 +0100

    black

--- common/baselines/tedlium2/default_tools.py
+++ common/baselines/tedlium2/default_tools.py
@@ -14,7 +14,9 @@ from i6_core.tools.git import CloneGitRepositoryJob
 
 PACKAGE = __package__
 
-RASR_BINARY_PATH = compile_rasr_binaries_i6mode(configure_options=["--apptainer-patch=2023-05-08_tensorflow-2.8_v1"])  #  use most recent RASR
+RASR_BINARY_PATH = compile_rasr_binaries_i6mode(
+    configure_options=["--apptainer-patch=2023-05-08_tensorflow-2.8_v1"]
+)  #  use most recent RASR
 assert RASR_BINARY_PATH, "Please set a specific RASR_BINARY_PATH before running the pipeline"
 RASR_BINARY_PATH.hash_overwrite = "TEDLIUM2_DEFAULT_RASR_BINARY_PATH"
 
