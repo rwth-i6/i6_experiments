1778fa3cb7755b9911de1f89646469cd2b55f012
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-14 12:27:36 +0100

    more on chunked AED, cleanup

--- users/zeyer/experiments/exp2023_04_25_rf/chunked_conformer.py
+++ users/zeyer/experiments/exp2023_04_25_rf/chunked_conformer.py
@@ -351,27 +351,6 @@ class ChunkedRelPosSelfAttention(rf.RelPosSelfAttention):
         # TODO actually wrong...! but wrong just like in orig cfg? should be end_chunk_size_dim?
         query_offset = self.chunk_history * axis.dimension
 
-        if True:  # inefficient but simpler
-            pos_emb = self.learned_pos_emb.full_matrix(
-                query_spatial_dim=axis, key_value_spatial_dim=hist_dim_, query_offset=query_offset
-            )
-
-            matrix_ac = rf.matmul(q_with_bias_u, k, reduce=self.key_dim_per_head)  # (batch, head, time1, time2)
-            matrix_bd = rf.matmul(q_with_bias_v, pos_emb, reduce=self.key_dim_per_head)  # (batch, head, time1, time2)
-
-            scores = matrix_ac + matrix_bd  # (batch, head, time1, time2)
-            scores *= self.key_dim_per_head.dimension**-0.5
-
-            att_weights = rf.softmax(scores, axis=hist_dim_)
-            att_weights = rf.dropout(att_weights, self.att_dropout, axis=self.att_dropout_broadcast and hist_dim_)
-            # Masking not needed because softmax should already have masked,
-            # so we have 0.0 att weights for padded frames.
-            att = rf.matmul(att_weights, v, reduce=hist_dim_, use_mask=False)
-            output, _ = rf.merge_dims(att, dims=(self.num_heads, self.value_dim_per_head), out_dim=self.value_dim_total)
-            if self.proj:
-                output = self.proj(output)
-            return output
-
         if self.learned_pos_emb is not None:
             pos_emb, pos_emb_spatial_dim = self.learned_pos_emb(
                 query_spatial_dim=axis, key_value_spatial_dim=hist_dim_, query_offset=query_offset
@@ -400,7 +379,7 @@ class ChunkedRelPosSelfAttention(rf.RelPosSelfAttention):
         # compute matrix b and matrix d
         # (batch, head, time1, 2*time1-1)
         matrix_bd = rf.matmul(q_with_bias_v, pos_emb, reduce=self.key_dim_per_head)
-        matrix_bd = self._rel_shift(matrix_bd, axis, pos_emb_spatial_dim, hist_dim_)  # TODO fix...
+        matrix_bd = self._rel_shift(matrix_bd, axis, pos_emb_spatial_dim, hist_dim_)
 
         scores = matrix_ac + matrix_bd  # (batch, head, time1, time2)
         scores *= self.key_dim_per_head.dimension**-0.5
