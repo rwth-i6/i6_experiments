ff16680bf35f91f771865055fd93e3bfff2c1787
schmitt <robin.schmitt1@rwth-aachen.de>
2023-12-14 12:31:18 +0100

    add dump_frame_probs as a more general for dumping frame wise probabilities

--- users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/att_weights.py
+++ users/schmitt/experiments/config/pipelines/global_vs_segmental_2022_23/att_weights.py
@@ -1,8 +1,10 @@
 import copy
 
 # from i6_experiments.users.schmitt.experiments.config.pipelines.global_vs_segmental_2022_23.dependencies.returnn.config_builder import ConfigBuilder, GlobalConfigBuilder, SegmentalConfigBuilder
-from i6_experiments.users.schmitt.experiments.config.pipelines.global_vs_segmental_2022_23.dependencies.returnn.config_builder.base import ConfigBuilder
-from i6_experiments.users.schmitt.experiments.config.pipelines.global_vs_segmental_2022_23.dependencies.returnn.config_builder.segmental import SegmentalConfigBuilder
+from i6_experiments.users.schmitt.experiments.config.pipelines.global_vs_segmental_2022_23.dependencies.returnn.config_builder.base import \
+  ConfigBuilder
+from i6_experiments.users.schmitt.experiments.config.pipelines.global_vs_segmental_2022_23.dependencies.returnn.config_builder.segmental import \
+  SegmentalConfigBuilder
 
 from i6_experiments.users.schmitt.visualization.visualization import PlotAttentionWeightsJobV2
 
@@ -13,8 +15,7 @@ from i6_core.text.processing import WriteToTextFileJob
 
 from sisyphus import Path, tk
 
-from typing import Dict, List, Optional
-
+from typing import Dict, List, Optional, Callable
 
 write_cv_segments_to_file_job = WriteToTextFileJob(
   content=[
@@ -50,6 +51,39 @@ def dump_att_weights(
         alias: str,
         seq_tags_to_analyse: Optional[List[str]] = None,
 ):
+  def _get_dump_att_weight_config(corpus_key, opts):
+    return config_builder.get_dump_att_weight_config(corpus_key=corpus_key, opts=opts)
+
+  dump_frame_probs(
+    config_builder=config_builder,
+    variant_params=variant_params,
+    checkpoint=checkpoint,
+    hdf_targets=hdf_targets,
+    corpus_key=corpus_key,
+    hdf_alias=hdf_alias,
+    ref_alignment=ref_alignment,
+    ref_alignment_blank_idx=ref_alignment_blank_idx,
+    alias=alias,
+    seq_tags_to_analyse=seq_tags_to_analyse,
+    probs_name="att_weights",
+    get_dump_frame_probs_config_func=_get_dump_att_weight_config
+  )
+
+
+def dump_frame_probs(
+        config_builder: ConfigBuilder,
+        variant_params: Dict,
+        checkpoint: Checkpoint,
+        hdf_targets: Path,
+        corpus_key: str,
+        hdf_alias: str,
+        ref_alignment: Path,
+        ref_alignment_blank_idx: int,
+        alias: str,
+        probs_name: str,
+        get_dump_frame_probs_config_func: Callable,
+        seq_tags_to_analyse: Optional[List[str]] = None,
+):
   if seq_tags_to_analyse is None:
     if corpus_key == "cv":
       seq_tags_to_analyse = [
@@ -72,7 +106,7 @@ def dump_att_weights(
     content=seq_tags_to_analyse
   )
 
-  dump_att_weights_opts = {
+  dump_frame_probs_opts = {
     "dataset_opts": {
       "segment_paths": {corpus_key: segment_file.out_file},
       "hdf_targets": {corpus_key: hdf_targets}
@@ -80,12 +114,12 @@ def dump_att_weights(
   }
 
   hdf_filenames = {
-    "att_weights": "att_weights.hdf",
+    probs_name: "%s.hdf" % probs_name,
     "targets": "targets.hdf"
   }
 
   if isinstance(config_builder, SegmentalConfigBuilder):
-    dump_att_weights_opts["use_train_net"] = True
+    dump_frame_probs_opts["use_train_net"] = True
 
     hdf_filenames.update({
       "seg_starts": "seg_starts.hdf",
@@ -98,30 +132,30 @@ def dump_att_weights(
         "center_positions": "center_positions.hdf"
       })
 
-  dump_att_weights_opts["hdf_filenames"] = hdf_filenames
+  dump_frame_probs_opts["hdf_filenames"] = hdf_filenames
 
-  dump_att_weight_config = config_builder.get_dump_att_weight_config(
+  dump_frame_probs_config = get_dump_frame_probs_config_func(
     corpus_key=corpus_key,
-    opts=dump_att_weights_opts
+    opts=dump_frame_probs_opts
   )
 
   forward_job = ReturnnForwardJob(
     model_checkpoint=checkpoint,
-    returnn_config=dump_att_weight_config,
+    returnn_config=dump_frame_probs_config,
     returnn_root=variant_params["returnn_root"],
     returnn_python_exe=variant_params["returnn_python_exe"],
     hdf_outputs=list(hdf_filenames.values()),
     eval_mode=True
   )
-  forward_job.add_alias("%s/analysis/att_weights/%s/%s/dump_hdf" % (alias, corpus_key, hdf_alias))
+  forward_job.add_alias("%s/analysis/%s/%s/%s/dump_hdf" % (alias, probs_name, corpus_key, hdf_alias))
 
   if isinstance(config_builder, SegmentalConfigBuilder):
     target_blank_idx = config_builder.dependencies.model_hyperparameters.blank_idx
   else:
     target_blank_idx = None
 
-  plot_att_weights_job = PlotAttentionWeightsJobV2(
-    att_weight_hdf=forward_job.out_hdf_files["att_weights.hdf"],
+  plot_frame_probs_job = PlotAttentionWeightsJobV2(
+    att_weight_hdf=forward_job.out_hdf_files["%s.hdf" % probs_name],
     targets_hdf=forward_job.out_hdf_files["targets.hdf"],
     seg_lens_hdf=forward_job.out_hdf_files.get(hdf_filenames.get("seg_lens")),
     seg_starts_hdf=forward_job.out_hdf_files.get(hdf_filenames.get("seg_starts")),
@@ -131,5 +165,6 @@ def dump_att_weights(
     ref_alignment_hdf=ref_alignment,
     json_vocab_path=config_builder.dependencies.vocab_path,
   )
-  plot_att_weights_job.add_alias("%s/analysis/att_weights/%s/%s/plot" % (alias, corpus_key, hdf_alias))
-  tk.register_output(("%s/analysis/att_weights/%s/%s/plot" % (alias, corpus_key, hdf_alias)), plot_att_weights_job.out_plot_dir)
+  plot_frame_probs_job.add_alias("%s/analysis/%s/%s/%s/plot" % (alias, probs_name, corpus_key, hdf_alias))
+  tk.register_output(("%s/analysis/%s/%s/%s/plot" % (alias, probs_name, corpus_key, hdf_alias)),
+                     plot_frame_probs_job.out_plot_dir)
