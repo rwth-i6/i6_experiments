94c3d70a6df2984b2b009a467ea0dc2b30fb5ca0
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-15 11:53:48 +0100

    fix

--- users/zeyer/experiments/exp2023_04_25_rf/chunked_aed_import.py
+++ users/zeyer/experiments/exp2023_04_25_rf/chunked_aed_import.py
@@ -848,10 +848,10 @@ def model_recog_v2(
 
 
 # RecogDef API
-model_recog: RecogDef[Model]
-model_recog.output_with_beam = True
-model_recog.output_blank_label = "<s>"  # EOC
-model_recog.batch_size_dependent = False
+model_recog_v2: RecogDef[Model]
+model_recog_v2.output_with_beam = True
+model_recog_v2.output_blank_label = "<s>"  # EOC
+model_recog_v2.batch_size_dependent = False
 
 
 def _transform_chunk_opts(opts: Optional[Dict[str, Any]]) -> Dict[str, Any]:
