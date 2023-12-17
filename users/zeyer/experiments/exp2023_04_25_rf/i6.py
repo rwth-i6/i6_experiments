fff163687ab16d78e85249fcf3f8e0126ea622e6
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-17 15:40:25 +0100

    monkey patch i6_core util path logic

--- users/zeyer/experiments/exp2023_04_25_rf/i6.py
+++ users/zeyer/experiments/exp2023_04_25_rf/i6.py
@@ -5,6 +5,10 @@ Sisyphus config for i6 cluster
 
 def py():
     """run via Sisyphus"""
+    from i6_experiments.users.zeyer import tools_paths
+
+    tools_paths.monkey_patch_i6_core()
+
     from . import conformer_import_moh_att_2023_06_30
 
     conformer_import_moh_att_2023_06_30.py()
