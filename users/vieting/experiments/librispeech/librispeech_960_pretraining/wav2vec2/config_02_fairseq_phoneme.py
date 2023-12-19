4d3186b3e345ae486af6490f8ca38560b8e2cd3d
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2023-12-19 16:52:06 +0100

    ls960 pretrain: cleanup pretraining with mono align

--- users/vieting/experiments/librispeech/librispeech_960_pretraining/wav2vec2/config_02_fairseq_phoneme.py
+++ users/vieting/experiments/librispeech/librispeech_960_pretraining/wav2vec2/config_02_fairseq_phoneme.py
@@ -6,6 +6,7 @@ import os.path
 from sisyphus import tk
 from i6_core.tools.git import CloneGitRepositoryJob
 from i6_core.fairseq.training import FairseqHydraConfig, FairseqHydraTrainingJob
+from i6_core.returnn.hdf import RasrAlignmentDumpHDFJob
 from .fairseq import SetupFairseqJob
 from .config_01_fairseq_main import get_fairseq_args
 
@@ -16,47 +17,25 @@ def get_alignment_hdf():
         commit="1a6554570058dd632c65b6d3328cadeab018d2d5",
     ).out_repository
     returnn_root.hash_overwrite = "TEDLIUM_HYBRID_RETURNN_ROOT"
-    from i6_core.returnn.hdf import RasrAlignmentDumpHDFJob
+
+    dependency_dir = "/work/asr4/vieting/setups/librispeech/dependencies/alignments/"
     alignment_caches = [tk.Path(
-        "/work/asr4/vieting/setups/librispeech/dependencies/alignments/monophone_10ms_gmm_fix/output/"
-        f"alignment.cache.{idx}",
+        os.path.join(dependency_dir, f"monophone_10ms_gmm_fix/output/alignment.cache.{idx}"),
         hash_overwrite=f"ls960_monophone_10ms_gmm_fix_alignment_{idx}",
     ) for idx in range(1, 201)]
     allophone_file = tk.Path(
-        "/work/asr4/vieting/setups/librispeech/dependencies/alignments/monophone_10ms_gmm_fix/dependencies/"
-        "StoreAllophonesJob.68JjXthmrl8y/output/allophones",
+        os.path.join(
+            dependency_dir,
+            f"monophone_10ms_gmm_fix/dependencies/StoreAllophonesJob.68JjXthmrl8y/output/allophones"
+        ),
         hash_overwrite="ls960_monophone_10ms_gmm_fix_allophones",
     )
-    # import i6_core.am as am
-    # import i6_core.rasr as rasr
-    # from i6_core.lexicon.allophones import DumpStateTyingJob
-    # from i6_experiments.common.datasets.librispeech import get_bliss_lexicon
-    # from i6_experiments.common.baselines.tedlium2.default_tools import RASR_BINARY_PATH
-    # crp = rasr.CommonRasrParameters()
-    # rasr.crp_add_default_output(crp)
-    # am_args = {
-    #     "state_tying": "monophone",
-    #     "states_per_phone": 1,
-    #     "state_repetitions": 1,
-    #     "across_word_model": True,
-    #     "early_recombination": False,
-    #     "tdp_scale": 1.0,
-    #     "tdp_transition": (3.0, 0.0, "infinity", 0.0),  # loop, forward, skip, exit
-    #     "tdp_silence": (0.0, 3.0, "infinity", 20.0),
-    #     "tying_type": "global",
-    #     "nonword_phones": "",
-    # }
-    # crp.acoustic_model_config = am.acoustic_model_config(**am_args)
-    # crp.acoustic_model_config.allophones.add_from_lexicon = False
-    # crp.acoustic_model_config.allophones.add_from_file = allophone_file
-    # crp.allophone_tool_exe = RASR_BINARY_PATH + "/allophone-tool.linux-x86_64-standard"
-    # crp.lexicon_config = rasr.RasrConfig()
-    # crp.lexicon_config.file = get_bliss_lexicon()
-    # crp.lexicon_config.normalize_pronunciation = False
-    # state_tying_file = DumpStateTyingJob(crp).out_state_tying
     state_tying_file = tk.Path(
-        "/work/asr4/vieting/setups/librispeech/dependencies/alignments/monophone_10ms_gmm_fix/dependencies/"
-        "state-tying-map-to-single-state",
+        os.path.join(
+            dependency_dir,
+            "/work/asr4/vieting/setups/librispeech/dependencies/alignments/monophone_10ms_gmm_fix/dependencies/"
+            "state-tying-map-to-single-state"
+        ),
         hash_overwrite="ls960_monophone_10ms_gmm_state_tying_monophone_1",
     )
     job = RasrAlignmentDumpHDFJob(
@@ -71,9 +50,9 @@ def get_alignment_hdf():
 
 def get_fairseq_root():
     fairseq_root = CloneGitRepositoryJob(
-        "https://github.com/facebookresearch/fairseq",
+        "https://github.com/vieting/fairseq_phoneme",
         checkout_folder_name="fairseq",
-        commit="176cd934982212a4f75e0669ee81b834ee71dbb0").out_repository
+        commit="b9fd659d427a946e9d5dc675fd88e26c3ef1ba23").out_repository
     fairseq_root = SetupFairseqJob(fairseq_root).out_fairseq_root
     return fairseq_root
 
