843b03942a677db7179a427c5012002f85d5fe0e
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-20 14:11:42 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -44,6 +44,14 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
         },
     )
 
+    train_exp(
+        "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_295k",
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_wd1e_4_lrlin1e_5_295k,
+        config_updates={
+            "torch_distributed": {"reduce_type": "param", "param_sync_step": 100},  # multi-GPU
+        },
+    )
+
     # TODO pretrain with specaugment_steps=(0, 15k, 25k)?
 
 
