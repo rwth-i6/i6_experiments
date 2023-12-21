a8171bbd17901fa829835ee09d726928730d5133
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-21 21:12:46 +0100

    chunked CTC recog

--- users/zeyer/experiments/exp2023_04_25_rf/chunked_ctc.py
+++ users/zeyer/experiments/exp2023_04_25_rf/chunked_ctc.py
@@ -7,14 +7,12 @@ https://arxiv.org/abs/2309.08436
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional, Any, Union, Tuple, Dict, Sequence, List
-import tree
 import math
 import numpy as np
 import hashlib
 
-from returnn.tensor import Tensor, Dim, single_step_dim
+from returnn.tensor import Tensor, Dim
 import returnn.frontend as rf
-from returnn.frontend.tensor_array import TensorArray
 from .chunked_conformer import ChunkedConformerEncoder, ConformerConvSubsample
 
 from i6_experiments.users.zeyer.utils.dict_update import dict_update_deep
@@ -582,7 +580,6 @@ def model_recog_v2(
     model: Model,
     data: Tensor,
     data_spatial_dim: Dim,
-    max_seq_len: Optional[int] = None,
 ) -> Tuple[Tensor, Tensor, Dim, Dim]:
     """
     Function is run within RETURNN.
@@ -598,52 +595,36 @@ def model_recog_v2(
         final beam_dim
     """
     from returnn.config import get_global_config
+    from returnn.frontend.tensor_array import TensorArray
 
     config = get_global_config(return_empty_if_none=True)
 
     batch_dims = data.remaining_dims((data_spatial_dim, data.feature_dim))
     enc, enc_spatial_dim, chunked_time_dim = model.encode(data, in_spatial_dim=data_spatial_dim)
+    enc, _ = rf.slice(enc, axis=enc_spatial_dim, size=model.end_chunk_size_dim)
+    enc, enc_spatial_dim_ = rf.merge_dims(enc, dims=(chunked_time_dim, model.end_chunk_size_dim))
+    logits = model.logits(enc)
+    label_log_probs = rf.log_softmax(logits, axis=model.target_dim)
+    label_log_probs_ta = TensorArray.unstack(label_log_probs, axis=enc_spatial_dim_)
+
     beam_size = config.int("beam_size", 12)
-    length_normalization_exponent = config.float("length_normalization_exponent", 0.0)
-    if max_seq_len is None:
-        max_seq_len = enc_spatial_dim.get_size_tensor() * chunked_time_dim.get_size_tensor()
-    else:
-        max_seq_len = rf.convert_to_tensor(max_seq_len, dtype="int32")
-    print("** max seq len:", max_seq_len.raw_tensor)
 
     # Eager-mode implementation of beam search.
     # Initial state.
     beam_dim = Dim(1, name="initial-beam")
     batch_dims_ = [beam_dim] + batch_dims
-    decoder_state = model.decoder_default_initial_state(batch_dims=batch_dims_, chunked_time_dim=chunked_time_dim)
-    target = rf.constant(model.bos_idx, dims=batch_dims_, sparse_dim=model.target_dim)
-    ended = rf.constant(False, dims=batch_dims_)
-    out_seq_len = rf.constant(0, dims=batch_dims_)
     seq_log_prob = rf.constant(0.0, dims=batch_dims_)
 
-    # TODO ...
     i = 0
     seq_targets = []
     seq_backrefs = []
     while True:
-        if i == 0:
-            input_embed = rf.zeros(batch_dims_ + [model.target_embed.out_dim], feature_dim=model.target_embed.out_dim)
-        else:
-            input_embed = model.target_embed(target)
-        step_out, decoder_state = model.loop_step(
-            **enc_args,
-            enc_spatial_dim=enc_spatial_dim,
-            input_embed=input_embed,
-            input_label=target,
-            state=decoder_state,
-        )
-        logits = model.decode_logits(input_embed=input_embed, **step_out)
-        label_log_prob = rf.log_softmax(logits, axis=model.target_dim)
+        label_log_prob = label_log_probs_ta[i]
         # Filter out finished beams
         label_log_prob = rf.where(
-            ended,
-            rf.sparse_to_dense(model.eos_idx, axis=model.target_dim, label_value=0.0, other_value=-1.0e30),
+            i < enc_spatial_dim_.get_size_tensor(),
             label_log_prob,
+            rf.sparse_to_dense(model.blank_idx, axis=model.target_dim, label_value=0.0, other_value=-1.0e30),
         )
         seq_log_prob = seq_log_prob + label_log_prob  # Batch, InBeam, Vocab
         seq_log_prob, (backrefs, target), beam_dim = rf.top_k(
@@ -651,36 +632,10 @@ def model_recog_v2(
         )  # seq_log_prob, backrefs, target: Batch, Beam
         seq_targets.append(target)
         seq_backrefs.append(backrefs)
-        decoder_state = tree.map_structure(lambda s: rf.gather(s, indices=backrefs), decoder_state)
-        ended = rf.gather(ended, indices=backrefs)
-        out_seq_len = rf.gather(out_seq_len, indices=backrefs)
         i += 1
 
-        ended = rf.logical_or(
-            ended,
-            rf.logical_and(
-                target == model.blank_idx,
-                decoder_state.chunk_idx
-                == chunked_time_dim.get_dyn_size_ext_for_device(decoder_state.chunk_idx.device) - 1,
-            ),
-        )
-        ended = rf.logical_or(ended, rf.copy_to_device(i >= max_seq_len))
-        if bool(rf.reduce_all(ended, axis=ended.dims).raw_tensor):
+        if bool(i >= enc_spatial_dim_.get_dim_value()):
             break
-        out_seq_len = out_seq_len + rf.where(ended, 0, 1)
-
-        if i > 1 and length_normalization_exponent != 0:
-            # Length-normalized scores, so we evaluate score_t/len.
-            # If seq ended, score_i/i == score_{i-1}/(i-1), thus score_i = score_{i-1}*(i/(i-1))
-            # Because we count with EOS symbol, shifted by one.
-            seq_log_prob *= rf.where(
-                ended,
-                (i / (i - 1)) ** length_normalization_exponent,
-                1.0,
-            )
-
-    if i > 0 and length_normalization_exponent != 0:
-        seq_log_prob *= (1 / i) ** length_normalization_exponent
 
     # Backtrack via backrefs, resolve beams.
     seq_targets_ = []
@@ -694,7 +649,7 @@ def model_recog_v2(
     seq_targets__ = TensorArray(seq_targets_[0])
     for target in seq_targets_:
         seq_targets__ = seq_targets__.push_back(target)
-    out_spatial_dim = Dim(out_seq_len, name="out-spatial")
+    out_spatial_dim = enc_spatial_dim_
     seq_targets = seq_targets__.stack(axis=out_spatial_dim)
 
     return seq_targets, seq_log_prob, out_spatial_dim, beam_dim
