bcbe0bc4924997080f3e8bc2b390dc66897515cc
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-28 10:24:32 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -68,6 +68,11 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
         config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_100k,
         config_updates={"optimizer.weight_decay": 1e-2},
     )
+    train_exp(
+        "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_1-lrlin1e_5_100k",
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_100k,
+        config_updates={"optimizer.weight_decay": 1e-1},
+    )
 
     train_exp(
         "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_100k-mixup",
