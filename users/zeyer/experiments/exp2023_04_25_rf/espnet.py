7b378b7a4981f8e37e3c8435ca1f86a0adcf8cc8
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-12-29 17:00:00 +0100

    espnet, wip

---
+++ users/zeyer/experiments/exp2023_04_25_rf/espnet.py
@@ -0,0 +1,405 @@
+"""
+Attention-based encoder-decoder (AED) experiments, using ESPnet models
+"""
+
+from __future__ import annotations
+
+import os
+import copy
+import functools
+from typing import TYPE_CHECKING, Optional, Any, Union, Tuple, Dict, Sequence, List
+
+import torch.nn
+import tree
+import math
+import numpy as np
+import hashlib
+import contextlib
+
+from returnn.tensor import Tensor, Dim, single_step_dim
+import returnn.frontend as rf
+from returnn.frontend.tensor_array import TensorArray
+from returnn.frontend.encoder.conformer import ConformerEncoder, ConformerConvSubsample
+from returnn.frontend.decoder.transformer import TransformerDecoder
+
+from i6_experiments.users.zeyer.returnn.models.rf_layerdrop import SequentialLayerDrop
+from i6_experiments.users.zeyer.speed_pert.librosa_config import speed_pert_librosa_config
+
+from .configs import *
+
+if TYPE_CHECKING:
+    from i6_experiments.users.zeyer.model_interfaces import ModelDef, RecogDef, TrainDef
+    from i6_experiments.users.zeyer.model_with_checkpoints import ModelWithCheckpoints, ModelWithCheckpoint
+    from i6_experiments.users.zeyer.datasets.task import Task
+
+
+# The model gets raw features (16khz) and does feature extraction internally.
+_log_mel_feature_dim = 80
+
+
+def sis_run_with_prefix(prefix_name: Optional[str] = None):
+    """run the exp"""
+    _sis_setup_global_prefix(prefix_name)
+
+    # TODO ...
+    # train_exp(
+    #    "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_295k",
+    #    config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
+    # )
+
+
+_sis_prefix: Optional[str] = None
+
+
+def _sis_setup_global_prefix(prefix_name: Optional[str] = None):
+    if not prefix_name:
+        from .sis_setup import get_prefix_for_config
+
+        prefix_name = get_prefix_for_config(__file__)
+    global _sis_prefix
+    _sis_prefix = prefix_name
+
+
+def _recog(name: str, model_with_checkpoint: ModelWithCheckpoint):
+    from sisyphus import tk
+    from i6_experiments.users.zeyer.recog import recog_model
+
+    task = _get_ls_task()
+
+    res = recog_model(task, model_with_checkpoint, model_recog)
+    tk.register_output(_sis_prefix + "/" + name, res.output)
+
+
+# noinspection PyShadowingNames
+def train_exp(
+    name: str,
+    config: Dict[str, Any],
+    *,
+    config_updates: Optional[Dict[str, Any]] = None,
+    config_deletes: Optional[Sequence[str]] = None,
+    post_config_updates: Optional[Dict[str, Any]] = None,
+    num_epochs: int = 2000,
+    gpu_mem: Optional[int] = 24,
+    num_processes: Optional[int] = None,
+    time_rqmt: Optional[int] = None,  # set this to 1 or below to get the fast test queue
+) -> ModelWithCheckpoints:
+    """
+    Train experiment
+    """
+    from .train import train
+    from i6_experiments.users.zeyer.recog import recog_training_exp
+
+    if _sis_prefix is None:
+        _sis_setup_global_prefix()
+
+    prefix = _sis_prefix + "/" + name
+    task = _get_ls_task()
+    config = config.copy()
+    config = dict_update_deep(config, config_updates, config_deletes)
+    if "__num_epochs" in config:
+        num_epochs = config.pop("__num_epochs")
+    if "__gpu_mem" in config:
+        gpu_mem = config.pop("__gpu_mem")
+    if "__num_processes" in config:
+        num_processes = config.pop("__num_processes")
+    if "__train_audio_preprocess" in config:
+        task: Task = copy.copy(task)
+        task.train_dataset = copy.copy(task.train_dataset)
+        task.train_dataset.train_audio_preprocess = config.pop("__train_audio_preprocess")
+
+    model_with_checkpoint = train(
+        prefix,
+        task=task,
+        config=config,
+        post_config=dict_update_deep(post_config, post_config_updates),
+        model_def=from_scratch_model_def,
+        train_def=from_scratch_training,
+        num_epochs=num_epochs,
+        gpu_mem=gpu_mem,
+        num_processes=num_processes,
+        distributed_launch_cmd="torchrun" if num_processes else "mpirun",
+        time_rqmt=time_rqmt,
+    )
+    recog_training_exp(prefix, task, model_with_checkpoint, recog_def=model_recog)
+
+    return model_with_checkpoint
+
+
+_ls_task = None
+
+
+def _get_ls_task():
+    global _ls_task
+    if _ls_task:
+        return _ls_task
+
+    from i6_experiments.users.zeyer.datasets.librispeech import get_librispeech_task_bpe10k_raw
+
+    _ls_task = get_librispeech_task_bpe10k_raw(with_eos_postfix=True)
+    return _ls_task
+
+
+py = sis_run_with_prefix  # if run directly via `sis m ...`
+
+_batch_size_factor = 160
+
+
+def _get_bos_idx(target_dim: Dim) -> int:
+    """for non-blank labels"""
+    assert target_dim.vocab
+    if target_dim.vocab.bos_label_id is not None:
+        bos_idx = target_dim.vocab.bos_label_id
+    elif target_dim.vocab.eos_label_id is not None:
+        bos_idx = target_dim.vocab.eos_label_id
+    elif "<sil>" in target_dim.vocab.user_defined_symbol_ids:
+        bos_idx = target_dim.vocab.user_defined_symbol_ids["<sil>"]
+    else:
+        raise Exception(f"cannot determine bos_idx from vocab {target_dim.vocab}")
+    return bos_idx
+
+
+def _get_eos_idx(target_dim: Dim) -> int:
+    """for non-blank labels"""
+    assert target_dim.vocab
+    if target_dim.vocab.eos_label_id is not None:
+        eos_idx = target_dim.vocab.eos_label_id
+    else:
+        raise Exception(f"cannot determine eos_idx from vocab {target_dim.vocab}")
+    return eos_idx
+
+
+def from_scratch_model_def(*, epoch: int, in_dim: Dim, target_dim: Dim) -> torch.nn.Module:
+    """Function is run within RETURNN."""
+    from returnn.config import get_global_config
+
+    in_dim, epoch  # noqa
+    config = get_global_config()  # noqa
+
+    # Load some train yaml file for model def.
+    # References:
+    # https://github.com/espnet/espnet/blob/master/espnet2/bin/asr_train.py
+    # https://github.com/espnet/espnet/blob/master/espnet2/tasks/asr.py
+    # https://github.com/espnet/espnet/blob/master/espnet2/tasks/abs_task.py
+
+    import espnet2
+
+    espnet_repo_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(espnet2.__file__)))
+
+    from espnet2.tasks.asr import ASRTask
+    import yaml
+    import argparse
+
+    enc_aux_logits = config.typed_value("aux_loss_layers")
+    pos_emb_dropout = config.float("pos_emb_dropout", 0.0)
+    # real input is raw audio, internally it does logmel
+    in_dim = Dim(name="logmel", dimension=_log_mel_feature_dim, kind=Dim.Types.Feature)
+
+    config_file = f"{espnet_repo_root_dir}/egs2/librispeech/asr1/conf/tuning/train_asr_e_branchformer.yaml"
+    with open(config_file, "r", encoding="utf-8") as f:
+        args = yaml.safe_load(f)
+    args = argparse.Namespace(**args)
+    model = ASRTask.build_model(args)
+    return model
+
+
+from_scratch_model_def: ModelDef[Model]
+from_scratch_model_def.behavior_version = 16
+from_scratch_model_def.backend = "torch"
+from_scratch_model_def.batch_size_factor = _batch_size_factor
+
+
+def from_scratch_training(
+    *, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: rf.Tensor, targets_spatial_dim: Dim
+):
+    """Function is run within RETURNN."""
+    from returnn.config import get_global_config
+
+    config = get_global_config()  # noqa
+    aux_loss_layers = config.typed_value("aux_loss_layers")
+    aux_loss_scales = config.typed_value("aux_loss_scales", ([1.0] * len(aux_loss_layers)) if aux_loss_layers else None)
+    aed_loss_scale = config.float("aed_loss_scale", 1.0)
+    use_normalized_loss = config.bool("use_normalized_loss", True)
+
+    if data.feature_dim and data.feature_dim.dimension == 1:
+        data = rf.squeeze(data, axis=data.feature_dim)
+    assert not data.feature_dim  # raw audio
+
+    collected_outputs = {}
+    enc, enc_spatial_dim = model.encode(data, in_spatial_dim=data_spatial_dim, collected_outputs=collected_outputs)
+    if aux_loss_layers:
+        for i, layer_idx in enumerate(aux_loss_layers):
+            if layer_idx > len(model.encoder.layers):
+                continue
+            linear = getattr(model, f"enc_aux_logits_{layer_idx}")
+            aux_logits = linear(collected_outputs[str(layer_idx - 1)])
+            aux_loss = rf.ctc_loss(
+                logits=aux_logits,
+                targets=targets,
+                input_spatial_dim=enc_spatial_dim,
+                targets_spatial_dim=targets_spatial_dim,
+                blank_index=model.blank_idx,
+            )
+            aux_loss.mark_as_loss(
+                f"ctc_{layer_idx}",
+                scale=aux_loss_scales[i],
+                custom_inv_norm_factor=targets_spatial_dim.get_size_tensor(),
+                use_normalized_loss=use_normalized_loss,
+            )
+            # decoded, decoded_spatial_dim = rf.ctc_greedy_decode(aux_logits, in_spatial_dim=enc_spatial_dim)
+            # error = rf.edit_distance(
+            #     a=decoded, a_spatial_dim=decoded_spatial_dim, b=targets, b_spatial_dim=targets_spatial_dim
+            # )
+            # error.mark_as_loss("label", as_error=True, custom_inv_norm_factor=targets_spatial_dim.get_size_tensor())
+
+    batch_dims = data.remaining_dims(data_spatial_dim)
+    input_labels = rf.shift_right(targets, axis=targets_spatial_dim, pad_value=model.bos_idx)
+
+    logits, _ = model.decoder(
+        input_labels,
+        spatial_dim=targets_spatial_dim,
+        encoder=enc,
+        state=model.decoder.default_initial_state(batch_dims=batch_dims),
+    )
+
+    logits_packed, pack_dim = rf.pack_padded(logits, dims=batch_dims + [targets_spatial_dim], enforce_sorted=False)
+    targets_packed, _ = rf.pack_padded(
+        targets, dims=batch_dims + [targets_spatial_dim], enforce_sorted=False, out_dim=pack_dim
+    )
+
+    log_prob = rf.log_softmax(logits_packed, axis=model.target_dim)
+    log_prob = rf.label_smoothed_log_prob_gradient(log_prob, 0.1, axis=model.target_dim)
+    loss = rf.cross_entropy(
+        target=targets_packed, estimated=log_prob, estimated_type="log-probs", axis=model.target_dim
+    )
+    loss.mark_as_loss("ce", scale=aed_loss_scale, use_normalized_loss=use_normalized_loss)
+
+    best = rf.reduce_argmax(logits_packed, axis=model.target_dim)
+    frame_error = best != targets_packed
+    frame_error.mark_as_loss(name="fer", as_error=True)
+
+
+from_scratch_training: TrainDef[Model]
+from_scratch_training.learning_rate_control_error_measure = "dev_score_full_sum"
+
+
+def model_recog(
+    *,
+    model: Model,
+    data: Tensor,
+    data_spatial_dim: Dim,
+    max_seq_len: Optional[int] = None,
+) -> Tuple[Tensor, Tensor, Dim, Dim]:
+    """
+    Function is run within RETURNN.
+
+    Earlier we used the generic beam_search function,
+    but now we just directly perform the search here,
+    as this is overall simpler and shorter.
+
+    :return:
+        recog results including beam {batch, beam, out_spatial},
+        log probs {batch, beam},
+        out_spatial_dim,
+        final beam_dim
+    """
+    batch_dims = data.remaining_dims((data_spatial_dim, data.feature_dim))
+    enc, enc_spatial_dim = model.encode(data, in_spatial_dim=data_spatial_dim)
+    beam_size = 12
+    length_normalization_exponent = 1.0
+    if max_seq_len is None:
+        max_seq_len = enc_spatial_dim.get_size_tensor()
+    else:
+        max_seq_len = rf.convert_to_tensor(max_seq_len, dtype="int32")
+    print("** max seq len:", max_seq_len.raw_tensor)
+
+    # Eager-mode implementation of beam search.
+    # Initial state.
+    beam_dim = Dim(1, name="initial-beam")
+    batch_dims_ = [beam_dim] + batch_dims
+    decoder_state = model.decoder.default_initial_state(batch_dims=batch_dims_)
+    target = rf.constant(model.bos_idx, dims=batch_dims_, sparse_dim=model.target_dim)
+    ended = rf.constant(False, dims=batch_dims_)
+    out_seq_len = rf.constant(0, dims=batch_dims_)
+    seq_log_prob = rf.constant(0.0, dims=batch_dims_)
+
+    i = 0
+    seq_targets = []
+    seq_backrefs = []
+    while True:
+        logits, decoder_state = model.decoder(
+            target,
+            spatial_dim=single_step_dim,
+            encoder=enc,
+            state=decoder_state,
+        )
+        label_log_prob = rf.log_softmax(logits, axis=model.target_dim)
+        # Filter out finished beams
+        label_log_prob = rf.where(
+            ended,
+            rf.sparse_to_dense(model.eos_idx, axis=model.target_dim, label_value=0.0, other_value=-1.0e30),
+            label_log_prob,
+        )
+        seq_log_prob = seq_log_prob + label_log_prob  # Batch, InBeam, Vocab
+        seq_log_prob, (backrefs, target), beam_dim = rf.top_k(
+            seq_log_prob, k_dim=Dim(beam_size, name=f"dec-step{i}-beam"), axis=[beam_dim, model.target_dim]
+        )  # seq_log_prob, backrefs, target: Batch, Beam
+        seq_targets.append(target)
+        seq_backrefs.append(backrefs)
+        decoder_state = tree.map_structure(functools.partial(_gather_backrefs, backrefs=backrefs), decoder_state)
+        ended = rf.gather(ended, indices=backrefs)
+        out_seq_len = rf.gather(out_seq_len, indices=backrefs)
+        i += 1
+
+        ended = rf.logical_or(ended, target == model.eos_idx)
+        ended = rf.logical_or(ended, rf.copy_to_device(i >= max_seq_len))
+        if bool(rf.reduce_all(ended, axis=ended.dims).raw_tensor):
+            break
+        out_seq_len = out_seq_len + rf.where(ended, 0, 1)
+
+        if i > 1 and length_normalization_exponent != 0:
+            # Length-normalized scores, so we evaluate score_t/len.
+            # If seq ended, score_i/i == score_{i-1}/(i-1), thus score_i = score_{i-1}*(i/(i-1))
+            # Because we count with EOS symbol, shifted by one.
+            seq_log_prob *= rf.where(
+                ended,
+                (i / (i - 1)) ** length_normalization_exponent,
+                1.0,
+            )
+
+    if i > 0 and length_normalization_exponent != 0:
+        seq_log_prob *= (1 / i) ** length_normalization_exponent
+
+    # Backtrack via backrefs, resolve beams.
+    seq_targets_ = []
+    indices = rf.range_over_dim(beam_dim)  # FinalBeam -> FinalBeam
+    for backrefs, target in zip(seq_backrefs[::-1], seq_targets[::-1]):
+        # indices: FinalBeam -> Beam
+        # backrefs: Beam -> PrevBeam
+        seq_targets_.insert(0, rf.gather(target, indices=indices))
+        indices = rf.gather(backrefs, indices=indices)  # FinalBeam -> PrevBeam
+
+    seq_targets__ = TensorArray(seq_targets_[0])
+    for target in seq_targets_:
+        seq_targets__ = seq_targets__.push_back(target)
+    out_spatial_dim = Dim(out_seq_len, name="out-spatial")
+    seq_targets = seq_targets__.stack(axis=out_spatial_dim)
+
+    return seq_targets, seq_log_prob, out_spatial_dim, beam_dim
+
+
+def _gather_backrefs(s, *, backrefs: Tensor):
+    if isinstance(s, Tensor):
+        if backrefs.sparse_dim in s.dims:
+            return rf.gather(s, indices=backrefs)  # really the default case
+        return s  # e.g. scalar or so, independent from beam
+    if isinstance(s, Dim):
+        assert s.dimension or backrefs not in s.dyn_size_ext.dims  # currently not supported, also not expected
+        return s
+    raise TypeError(f"_gather_backrefs: unexpected type ({type(s)})")
+
+
+# RecogDef API
+model_recog: RecogDef[Model]
+model_recog.output_with_beam = True
+model_recog.output_blank_label = None
+model_recog.batch_size_dependent = False
