279b26f3328d79c5e2753fd367557c056fb0a7e6
Moritz Gunz <moritz.gunz@gmail.com>
2024-01-08 10:14:42 +0100

    remove label pops

--- users/gunz/experiments/config_2023_08_subsampling_new/config_20d_monophone_pp_mpc1x4_40ms.py
+++ users/gunz/experiments/config_2023_08_subsampling_new/config_20d_monophone_pp_mpc1x4_40ms.py
@@ -339,7 +339,7 @@ def run_single(
             train_corpus_key=s.crp_names["train"],
             dev_corpus_key=s.crp_names["cvtrain"],
             smoothen=True,
-            returnn_config=returnn_config,
+            returnn_config=remove_label_pops_and_losses_from_returnn_config(returnn_config),
         )
 
         recognizer, recog_args = s.get_recognizer_and_args(
