f470bab0e82a8cfc9d9a16803d543477218987d2
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-01-14 05:13:55 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -125,6 +125,20 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
         },
     )
 
+    train_exp(
+        "v6-bhv20-nenc17-11gb-f32-bs10k-accgrad4-mgpu4-pavg100-wd1e_4-lrlin1e_5_443k-aux17",
+        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
+        model_config={
+            "behavior_version": 20,  # new Trafo decoder defaults
+            "num_enc_layers": 17,
+            "aux_loss_layers": [17],
+        },
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep(10_000, 500),
+            "accum_grad_multiple_step": 4,
+        },
+    )
+
     # TODO...
     #   - more specaug?
     #   - more speed pert?
