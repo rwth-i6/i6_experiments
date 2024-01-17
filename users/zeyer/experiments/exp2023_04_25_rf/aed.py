91fbfc0aa4ec339aa6304bdf1792570281f1332e
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-01-17 21:26:19 +0100

    cleanup

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -41,17 +41,17 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
     _sis_setup_global_prefix(prefix_name)
 
     # 5.60
-    train_exp("base-24gb-v6-lrlin1e_5_450k", config_24gb_v6, config_updates=_get_cfg_lrlin_oclr_by_bs_nep(40_000, 2000))
-
-    train_exp(  # 5.64
-        "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
-    )
-    train_exp(  # 5.51
-        "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k",
-        config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
-        config_updates={"optimizer.weight_decay": 1e-2},
-    )
+    # train_exp("base-24gb-v6-lrlin1e_5_450k", config_24gb_v6, config_updates=_get_cfg_lrlin_oclr_by_bs_nep(40_000, 2000))
+    #
+    # train_exp(  # 5.64
+    #     "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_4-lrlin1e_5_295k",
+    #     config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
+    # )
+    # train_exp(  # 5.51
+    #     "v6-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k",
+    #     config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
+    #     config_updates={"optimizer.weight_decay": 1e-2},
+    # )
     train_exp(  # 5.45
         "v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k",
         config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
