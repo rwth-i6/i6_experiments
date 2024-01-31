8986eb679da2d9b1fb665df58a8bb035a38086d5
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-01-31 23:00:46 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/configs.py
+++ users/zeyer/experiments/exp2023_04_25_rf/configs.py
@@ -121,7 +121,9 @@ _lrlin_oclr_steps_by_bs_nep = {
     (8, 500): [558_000, 1_117_000, 1_242_000],  # ~2485steps/ep, 500 eps -> 1.242k steps in total
     (10, 500): [443_000, 887_000, 986_000],  # ~1973 steps/epoch, total steps after 500 epochs: ~986k
     (15, 500): [295_000, 590_000, 652_000],  # total steps after 500 epochs: ~652k
-    (30, 2000): [587_000, 1_174_000, 1_305_000],  # total steps: 1.305.182
+    (20, 1000): [438_000, 877_000, 974_000],  # total steps after 1000 epochs: 974.953
+    (20, 2000): [878_000, 1_757_000, 1_952_000],  # total steps after 2000 epochs: 1.952.394
+    (30, 2000): [587_000, 1_174_000, 1_305_000],  # total steps after 2000 epochs: 1.305.182
     (40, 2000): [450_000, 900_000, 982_000],  # total steps after 2000 epochs: 982.312
 }
 
--- users/zeyer/experiments/exp2023_04_25_rf/rz.py
+++ users/zeyer/experiments/exp2023_04_25_rf/rz.py
@@ -3,9 +3,16 @@ Experiments in RWTH ITC
 """
 
 from __future__ import annotations
-from .configs import config_24gb_v4, config_24gb_v6, _batch_size_factor
+from .configs import (
+    config_24gb_v4,
+    config_24gb_v6,
+    _batch_size_factor,
+    _get_cfg_lrlin_oclr_by_bs_nep,
+)
 from .conformer_import_moh_att_2023_06_30 import train_exp as train_exp_aed_lstm
+from .aed import train_exp as train_exp_aed_trafo
 from i6_experiments.users.zeyer.utils.dict_update import dict_update_deep
+from i6_experiments.users.zeyer.speed_pert.librosa_config import speed_pert_librosa_config
 
 
 # run directly via `sis m ...`
@@ -63,6 +70,22 @@ def py():
         num_processes=2,
     )
 
+    train_exp_aed_trafo(
+        "v6-bhv20-f32-bs20k-accgrad1-mgpu2-wd1e_2-lrlin1e_5_438k-speedpertV2",
+        config_v6_f32_bs20k,
+        model_config={"behavior_version": 20},
+        config_updates={
+            "__gpu_mem": 16,
+            "__num_processes": 2,
+            **_get_cfg_lrlin_oclr_by_bs_nep(20_000, 1000),
+            "accum_grad_multiple_step": 1,
+            "torch_distributed": {},
+            "optimizer.weight_decay": 1e-2,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+        },
+    )
+
     # currently NVLINK seems broken, always NCCL error... or I could fallback to slower communication...
     # train_exp(
     #     "v4-f32-mgpu16",
@@ -78,7 +101,7 @@ def py():
     #     gpu_mem=32,
     #     num_processes=2,
     # )
-    # train_exp(
+    # train_exp(  # 6.12
     #     "v6-f32-accgrad1-mgpu4-wd1e_4-lrlin1e_5_111k",
     #     config_v6_f32,
     #     config_updates={
