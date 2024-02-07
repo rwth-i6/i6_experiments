dd5f5c5a9062f8db7c9dfe67d6c5ca29f0d9eadc
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-07 09:36:01 +0100

    comment

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -82,7 +82,7 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
         },
     )
 
-    train_exp(  # 5.44 ("test-other": 6.34)
+    train_exp(  # 5.44 ("test-other": 6.34), worse than speedpertV2 (5.11)
         "v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-speedpertV3",
         config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
         model_config={"behavior_version": 20},  # new Trafo decoder defaults
