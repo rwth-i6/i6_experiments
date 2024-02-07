1e87d3039b0212093a724876ba1c391b35507654
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2024-02-07 20:36:17 +0100

    add returnn util data input instanciator

--- users/vieting/util/returnn.py
+++ users/vieting/util/returnn.py
@@ -2,6 +2,7 @@
 Collection of utils for RETURNN.
 """
 from typing import Any, Dict, Iterable
+from i6_experiments.common.setups.rasr.util.nn.data import AllowedReturnnTrainingDataInput
 
 
 def iterate_returnn_datasets(datasets: Dict[str, Dict]) -> Iterable[Dict[str, Any]]:
@@ -22,3 +23,22 @@ def iterate_returnn_datasets(datasets: Dict[str, Dict]) -> Iterable[Dict[str, An
         else:
             yield dataset
 
+
+def instanciate_returnn_data_inputs(datasets: Dict[str, AllowedReturnnTrainingDataInput]):
+    """
+    RETURNN datasets are often stored in a nested dict, e.g., like
+
+    datasets = {
+        "train": {...},
+        "dev": {...},
+        "eval_datasets": {"devtrain": {...}},
+    }
+
+    This helper iterates over all datasets and instanciates them by calling their `get_data_dict()` method, assuming 
+    they are data input classes e.g. from i6_experiments.common.setups.rasr.util.nn.data.
+    """
+    for name, dataset in datasets.items():
+        if name == "eval_datasets":
+            instanciate_returnn_data_inputs(dataset)
+        else:
+            datasets[name] = dataset.get_data_dict()
