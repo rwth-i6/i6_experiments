8a0d0c9820ea049d6a8c993731e8b145550b36d6
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-10 23:56:13 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/espnet.py
+++ users/zeyer/experiments/exp2023_04_25_rf/espnet.py
@@ -84,7 +84,7 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
         },
     )
 
-    train_exp(
+    train_exp(  # 5.58
         "v6-11gb-f32-bs8k-accgrad10-mgpu4-pavg100-wd1e_4-lrlin1e_5_558k-EBranchformer",
         config_11gb_v6_f32_bs15k_accgrad1_mgpu4_pavg100_wd1e_4_lrlin1e_5_295k,
         {
