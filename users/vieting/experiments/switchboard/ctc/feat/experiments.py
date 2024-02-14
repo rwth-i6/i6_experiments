089c966b4ac73f8a98e4be1e7898cda1cbc57624
Max-Ryujin <max.kannen@gmail.com>
2024-02-14 11:24:13 +0100

    remove all experiments

--- users/vieting/experiments/switchboard/ctc/feat/experiments.py
+++ users/vieting/experiments/switchboard/ctc/feat/experiments.py
@@ -1,100 +1,33 @@
 import copy
 import os.path
 from sisyphus import tk, gs
-from sisyphus.delayed_ops import DelayedFormat
 from typing import Any, Dict
 
-from i6_core.meta.system import CorpusObject
-from i6_core.lexicon.modification import AddEowPhonemesToLexiconJob
 from i6_core.returnn.config import CodeWrapper
 from i6_core.recognition import Hub5ScoreJob
 from i6_core.tools import CloneGitRepositoryJob
-from i6_experiments.common.datasets.switchboard.corpus_eval import get_hub5e00
-from i6_experiments.common.setups.rasr.util import RasrDataInput
-from i6_experiments.users.berger.recipe.lexicon.modification import DeleteEmptyOrthJob, MakeBlankLexiconJob
 from i6_experiments.users.vieting.tools.report import Report
-
-# TODO: run_gmm_system_from_common might be copied here for stability
-from i6_experiments.users.vieting.experiments.switchboard.hybrid.feat.experiments import run_gmm_system_from_common
+from i6_experiments.users.vieting.experiments.switchboard.transducer.feat.data import (
+    get_switchboard_data,
+    get_returnn_ogg_datasets,
+)
 from i6_experiments.users.vieting.experiments.switchboard.ctc.feat.transducer_system_v2 import (
     TransducerSystem,
     ReturnnConfigs,
     ScorerInfo,
     SearchTypes,
 )
-
 from .baseline_args import get_nn_args as get_nn_args_baseline
-from .data import get_corpus_data_inputs_oggzip  # TODO: might be copied here for stability
 from .default_tools import RASR_BINARY_PATH, RETURNN_ROOT, RETURNN_EXE, SCTK_BINARY_PATH
 
 
-def get_datasets(use_multi_proc_dataset=False, **kwargs):
-    gmm_system = run_gmm_system_from_common()
-
-    # TODO: get oggzip independent of GMM system
-    # noinspection PyTypeChecker
-    (
-        nn_train_data_inputs,
-        nn_cv_data_inputs,
-        nn_devtrain_data_inputs,
-        nn_dev_data_inputs,
-        nn_test_data_inputs,
-        train_corpus_path,
-        traincv_segments,
-    ) = get_corpus_data_inputs_oggzip(
-        gmm_system,
-        partition_epoch={"train": 6, "dev": 1},
-        returnn_root=RETURNN_ROOT,
-        returnn_python_exe=RETURNN_EXE,
-        **kwargs,
-    )
-
-    returnn_datasets = {
-        "train": nn_train_data_inputs["switchboard.train"].get_data_dict()["datasets"]["ogg"],
-        "dev": nn_cv_data_inputs["switchboard.cv"].get_data_dict()["datasets"]["ogg"],
-        "eval_datasets": {
-            "devtrain": nn_devtrain_data_inputs["switchboard.devtrain"].get_data_dict()["datasets"]["ogg"],
-        },
-    }
-
-    if use_multi_proc_dataset:
-        returnn_datasets["train"] = {
-            "class": "MultiProcDataset",
-            "dataset": nn_train_data_inputs["switchboard.train"].get_data_dict()["datasets"]["ogg"],
-            "num_workers": 2,
-            "buffer_size": 5,
-        }
-
-    lexicon = gmm_system.crp["switchboard"].lexicon_config.file
-    lexicon = DeleteEmptyOrthJob(lexicon).out_lexicon
-    rasr_loss_lexicon = MakeBlankLexiconJob(lexicon).out_lexicon
-    nonword_phones = ["[LAUGHTER]", "[NOISE]", "[VOCALIZEDNOISE]"]
-    recog_lexicon = AddEowPhonemesToLexiconJob(rasr_loss_lexicon, nonword_phones=nonword_phones).out_lexicon
-
-    rasr_loss_corpus = train_corpus_path
-    rasr_loss_segments = traincv_segments
-
-    hub5e00 = get_hub5e00()
-    corpus_object = CorpusObject()
-    corpus_object.corpus_file = hub5e00.bliss_corpus
-    corpus_object.audio_format = nn_dev_data_inputs["hub5e00"].crp.audio_format
-    corpus_object.duration = nn_dev_data_inputs["hub5e00"].crp.corpus_duration
-    dev_corpora = {
-        "hub5e00": RasrDataInput(
-            corpus_object=corpus_object,
-            lexicon={
-                "filename": recog_lexicon,
-                "normalize_pronunciation": False,
-                "add_all": True,
-                "add_from_lexicon": False,
-            },
-            lm={"filename": nn_dev_data_inputs["hub5e00"].crp.language_model_config.file, "type": "ARPA"},
-            stm=hub5e00.stm,
-            glm=hub5e00.glm,
-        )
-    }
-
-    return returnn_datasets, rasr_loss_corpus, rasr_loss_segments, rasr_loss_lexicon, dev_corpora
+def get_datasets():
+    returnn_datasets = get_returnn_ogg_datasets()
+    train_corpus, dev_corpora, segments = get_switchboard_data()
+    rasr_loss_lexicon = train_corpus.lexicon["filename"]
+    rasr_loss_corpus = train_corpus.corpus_object.corpus_file
+    rasr_loss_segments = segments["all_filtered"]
+    return returnn_datasets, rasr_loss_corpus, rasr_loss_segments, rasr_loss_lexicon, dev_corpora["ctc"]
 
 
 def run_test_mel():
@@ -666,7 +599,7 @@ def run_nn_args(nn_args, report_args_collection, dev_corpora, report_name="", re
             values=report.get_values(),
             template=report.get_template(),
         )
-    return report
+    return report, ctc_nn_system
 
 
 def run_mel_baseline():
@@ -713,8 +646,8 @@ def run_mel_baseline():
         num_epochs=450,
         prefix="conformer_bs10k_",
     )
-    report = run_nn_args(nn_args, report_args_collection, dev_corpora)
-    return report
+    report, ctc_nn_system = run_nn_args(nn_args, report_args_collection, dev_corpora)
+    return report, ctc_nn_system
 
 
 def run_scf_baseline():
@@ -781,126 +714,10 @@ def run_scf_baseline():
         commit="c4d36d06f6465e82a50d400d114259e07b8b0709",
     ).out_repository
     returnn_root.hash_overwrite = "returnn_conv_padding"
-    report = run_nn_args(
+    report, ctc_nn_system = run_nn_args(
         nn_args,
         report_args_collection,
         dev_corpora,
-        returnn_root=returnn_root,
-        recog_args={"epochs": [350, 400, 450, "best"]},
-    )
-    return report
-
-
-def run_scf_frozen_features():
-    gs.ALIAS_AND_OUTPUT_SUBDIR = "experiments/switchboard/ctc/feat/"
-
-    (
-        returnn_datasets,
-        rasr_loss_corpus_path,
-        rasr_loss_corpus_segments,
-        rasr_loss_lexicon_path,
-        dev_corpora,
-    ) = get_datasets()
-    returnn_args = {
-        "batch_size": 5000,
-        "rasr_binary_path": RASR_BINARY_PATH,
-        "rasr_loss_corpus_path": rasr_loss_corpus_path,
-        "rasr_loss_corpus_segments": rasr_loss_corpus_segments,
-        "rasr_loss_lexicon_path": rasr_loss_lexicon_path,
-        "datasets": returnn_datasets,
-        "conformer_type": "wei",
-        "specaug_old": {"max_feature": 15},
-    }
-    feature_args = {
-        "class": "ScfNetwork",
-        "size_tf": 256 // 2,
-        "stride_tf": 10 // 2,
-        "preemphasis": 0.97,
-        "wave_norm": True,
-    }
-    lr_args={
-        "peak_lr": 4e-4,
-        "start_lr": 1.325e-05,
-        "end_lr": 1e-5,
-        "increase_epochs": 180,
-        "decrease_epochs": 180,
-        "final_epochs": 0,
-    }
-
-    nn_args, report_args_collection = get_nn_args_baseline(
-        nn_base_args={
-            "scf_baseline": dict(
-                returnn_args={
-                    "staged_opts": {
-                        1: "freeze_features",
-                        16: "unfreeze_features",
-                    },
-                    "extra_args": {
-                        "accum_grad_multiple_step": 2,
-                        "conv_pad_seq_len_to_power": 1.5,
-                        "preload_from_files": {
-                            "existing-model": {
-                                "filename": "/u/maximilian.kannen/setups/20230406_feat/work/i6_core/returnn/training/ReturnnTrainingJob.xUBplWqSsRII/output/models/epoch.016.index",
-                                "init_for_train": True,
-                                "var_name_mapping": {
-                                    "/conv_h_filter/conv_h_filter": "features/conv_h_filter/conv_h_filter",
-                                    "/conv_l/W": "features/conv_l/W",
-                                    "/conv_l_act/bias": "features/conv_l_act/bias",
-                                    "/conv_l_act/scale": "features/conv_l_act/scale",
-                                },
-                                "prefix": "features",
-                            },
-                        },
-                    },
-                    **returnn_args,
-                },
-                feature_args=feature_args,
-                lr_args=lr_args,
-                report_args={
-                    "architecture": "conf-wei",
-                    "lr": "wei_peak_4e-4_e450_cycle360",
-                    "specaug": "wei_adapt_80dim",
-                    "wave_norm": "True",
-                    "preemphasis": "0.97",
-                },
-            ),
-            "epoch_256_freezing": dict(
-                returnn_args={
-                    "staged_opts": {
-                        256: "freeze_features",
-                    },
-                    "extra_args": {
-                        "accum_grad_multiple_step": 2,
-                        "conv_pad_seq_len_to_power": 1.5,
-                    },
-                    **returnn_args,
-                },
-                feature_args=feature_args,
-                lr_args=lr_args,
-                report_args={
-                    "architecture": "conf-wei",
-                    "lr": "wei_peak_4e-4_e450_cycle360",
-                    "specaug": "wei_adapt_80dim",
-                    "wave_norm": "True",
-                    "preemphasis": "0.97",
-                },
-            ),
-        },
-        num_epochs=450,
-        evaluation_epochs=[350, 400, 450],
-        prefix="conformer_bs2x5k_frozen_features_",
-    )
-
-    returnn_root = CloneGitRepositoryJob(
-        "https://github.com/rwth-i6/returnn",
-        commit="c4d36d06f6465e82a50d400d114259e07b8b0709",
-    ).out_repository
-    returnn_root.hash_overwrite = "returnn_conv_padding"
-    report = run_nn_args(
-        nn_args,
-        report_args_collection,
-        dev_corpora,
-        "report_scf_frozen_features.csv",
         returnn_root=returnn_root,
         recog_args={"epochs": [350, 400, 450, "best"]},
     )
@@ -1022,7 +839,7 @@ def run_scf_audio_perturbation():
         commit="c4d36d06f6465e82a50d400d114259e07b8b0709",
     ).out_repository
     returnn_root.hash_overwrite = "returnn_conv_padding"
-    report = run_nn_args(
+    report, ctc_nn_system = run_nn_args(
         nn_args,
         report_args_collection,
         dev_corpora,
@@ -1108,7 +925,7 @@ def run_scf_specaug_sort():
         commit="c4d36d06f6465e82a50d400d114259e07b8b0709",
     ).out_repository
     returnn_root.hash_overwrite = "returnn_conv_padding"
-    report = run_nn_args(
+    report, ctc_nn_system = run_nn_args(
         nn_args, report_args_collection, dev_corpora, returnn_root=returnn_root,
         recog_args={"epochs": [350, 400, 450, "best"]},
     )
@@ -1250,7 +1067,7 @@ def py():
     """
     called if the file is passed to sis manager, used to run all experiments (replacement for main)
     """
-    report_mel = run_mel_baseline()
+    report_mel, _ = run_mel_baseline()
     report_scf = run_scf_baseline()
     report_scf_specaug_sort = run_scf_specaug_sort()
 
