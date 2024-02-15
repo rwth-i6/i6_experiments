dbc9c605a66dc07bcef0ca7b62fbbbc8a9aaf347
Lukas Rilling <lukas.rilling@rwth-aachen.de>
2024-02-15 11:02:48 +0100

    GlowTTS ASR: Added joint training with given alignments

---
+++ users/rilling/experiments/librispeech/librispeech_joint_training_given_alignments/pytorch_networks/shared/configs.py
@@ -0,0 +1,187 @@
+from dataclasses import dataclass
+
+import torch
+from torch import nn
+from typing import Callable, Optional, Type, Union, Tuple
+from sisyphus import tk
+
+from i6_models.assemblies.conformer.conformer_v1 import ConformerBlockV1Config, ConformerBlockV1
+from i6_models.parts.frontend.vgg_act import VGG4LayerActFrontendV1Config
+from i6_models.config import ModuleFactoryV1, ModelConfiguration
+
+
+@dataclass
+class DbMelFeatureExtractionConfig():
+    """
+
+    :param sample_rate: audio sample rate in Hz
+    :param win_size: window size in seconds
+    :param hop_size: window shift in seconds
+    :param f_min: minimum mel filter frequency in Hz
+    :param f_max: maximum mel fitler frequency in Hz
+    :param min_amp: minimum amplitude for safe log
+    :param num_filters: number of mel windows
+    :param center: centered STFT with automatic padding
+    :param norm: tuple optional of mean & std_dev for feature normalization
+    """
+    sample_rate: int
+    win_size: float
+    hop_size: float
+    f_min: int
+    f_max: int
+    min_amp: float
+    num_filters: int
+    center: bool
+    norm: Optional[Tuple[float, float]] = None
+
+    @classmethod
+    def from_dict(cls, d):
+        return DbMelFeatureExtractionConfig(**d)
+
+
+@dataclass(kw_only=True)
+class VGG4LayerActFrontendV1Config_mod(VGG4LayerActFrontendV1Config):
+    activation_str: str = ""
+    activation: Optional[Union[nn.Module, Callable[[torch.Tensor], torch.Tensor]]] = None
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        activation_str = d.pop("activation_str")
+        if activation_str == "ReLU":
+            from torch.nn import ReLU
+
+            activation = ReLU()
+        else:
+            assert False, "Unsupported activation %s" % d["activation_str"]
+        d["activation"] = activation
+        return VGG4LayerActFrontendV1Config(**d)
+
+
+@dataclass
+class ConformerEncoderV1Config(ModelConfiguration):
+    """
+    Attributes:
+        num_layers: Number of conformer layers in the conformer encoder
+        frontend: A pair of ConformerFrontend and corresponding config
+        block_cfg: Configuration for ConformerBlockV1
+    """
+
+    num_layers: int
+
+    # nested configurations
+    frontend: ModuleFactoryV1
+    block_cfg: ConformerBlockV1Config
+
+
+@dataclass
+class SpecaugConfig(ModelConfiguration):
+    repeat_per_n_frames: int
+    max_dim_time: int
+    num_repeat_feat: int
+    max_dim_feat: int
+
+    @classmethod
+    def from_dict(cls, d):
+        if d is not None:
+            d = d.copy()
+            return SpecaugConfig(**d)
+        else:
+            return None
+
+
+@dataclass
+class FlowDecoderConfig(ModelConfiguration):
+    hidden_channels: int
+    kernel_size: int
+    dilation_rate: int
+    n_blocks: int
+    n_layers: int
+    p_dropout: float
+    n_split: int
+    n_sqz: int
+    sigmoid_scale: bool
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        return FlowDecoderConfig(**d)
+
+
+@dataclass
+class TextEncoderConfig(ModelConfiguration):
+    n_vocab: Union[tk.Variable, int]
+    hidden_channels: int
+    filter_channels: int
+    filter_channels_dp: int
+    n_heads: int
+    n_layers: int
+    kernel_size: int
+    p_dropout: float
+    window_size: int
+    block_length: Union[int, None]
+    mean_only: bool
+    prenet: bool
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        return TextEncoderConfig(**d)
+
+
+@dataclass
+class PhonemePredictionConfig(ModelConfiguration):
+    n_channels: int
+    n_layers: int
+    p_dropout: float
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        return PhonemePredictionConfig(**d)
+
+
+@dataclass
+class ModelConfigV1:
+    frontend_config: VGG4LayerActFrontendV1Config
+    specaug_config: Union[SpecaugConfig, None]
+    decoder_config: FlowDecoderConfig
+    text_encoder_config: TextEncoderConfig
+    ffn_layers: int
+    ffn_channels: int
+    specauc_start_epoch: int
+    label_target_size: int
+    out_channels: int
+    gin_channels: int
+    n_speakers: Union[tk.Variable, int]
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        d["frontend_config"] = VGG4LayerActFrontendV1Config_mod.from_dict(d["frontend_config"])
+        d["specaug_config"] = SpecaugConfig.from_dict(d["specaug_config"])
+        d["decoder_config"] = FlowDecoderConfig.from_dict(d["decoder_config"])
+        d["text_encoder_config"] = TextEncoderConfig.from_dict(d["text_encoder_config"])
+        return ModelConfigV1(**d)
+
+
+@dataclass
+class ModelConfigV2:
+    specaug_config: Union[SpecaugConfig, None]
+    decoder_config: FlowDecoderConfig
+    text_encoder_config: TextEncoderConfig
+    phoneme_prediction_config: Optional[PhonemePredictionConfig]
+    specauc_start_epoch: int
+    label_target_size: int
+    out_channels: int
+    gin_channels: int
+    n_speakers: Union[tk.Variable, int]
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        d["specaug_config"] = SpecaugConfig.from_dict(d["specaug_config"])
+        d["decoder_config"] = FlowDecoderConfig.from_dict(d["decoder_config"])
+        d["text_encoder_config"] = TextEncoderConfig.from_dict(d["text_encoder_config"])
+        d["phoneme_prediction_config"] = PhonemePredictionConfig.from_dict(d["phoneme_prediction_config"])
+        return ModelConfigV2(**d)
