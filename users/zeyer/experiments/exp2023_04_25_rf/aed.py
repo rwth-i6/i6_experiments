ff389c132f62a914147c47c35951f79c3736c062
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-20 16:11:01 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -233,6 +233,16 @@ def sis_run_with_prefix(prefix_name: Optional[str] = None):
                 "attention_coverage_opts": {"type": "log", "eps": 0.1, "clip_min": 0.0},
             },
         },
+        "beam60-lenNorm02-cov02-covInd-batch50": {
+            "beam_size": 60,
+            "max_seqs": 50,
+            "batch_size": 5000 * _batch_size_factor,
+            "beam_search_opts": {
+                "length_normalization_exponent": 0.2,
+                "attention_coverage_scale": 0.2,
+                "attention_coverage_opts": {"type": "indicator"},
+            },
+        },
         "beam60-batch1": {
             # {"dev-clean": 2.89, "dev-other": 6.21, "test-clean": 2.84, "test-other": 6.58}
             "beam_size": 60,
@@ -1522,6 +1532,9 @@ def get_label_scorer_and_coverage_scorer_pure_torch(
                 eps = coverage_opts.get("eps", 0.0)
                 clip_min = coverage_opts.get("clip_min", 0.01)
                 coverage_score = rf.log(rf.clip_by_value(accum_att_weights, clip_min, 1.0) + eps)
+            elif cov_type == "indicator":
+                threshold = coverage_opts.get("threshold", 0.5)
+                coverage_score = rf.where(accum_att_weights >= threshold, 1.0, 0.0)
             else:
                 raise ValueError(f"invalid coverage opts type {cov_type!r}")
             coverage_score = rf.reduce_sum(coverage_score, axis=enc_spatial_dim)
