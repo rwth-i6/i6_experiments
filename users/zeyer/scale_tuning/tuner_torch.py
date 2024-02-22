a620a2cfb8edf79a82da28ec5ab39ba83b1ed886
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-22 01:37:17 +0100

    auto scale tuner fixes

--- users/zeyer/scale_tuning/tuner_torch.py
+++ users/zeyer/scale_tuning/tuner_torch.py
@@ -26,13 +26,14 @@ def main():
     entries_num_err = []  # list of sequences over tensor shape [beam]
     total_num_ref_words = 0
 
-    print("* Reading entries.")
     for fn in args.recog_output_dir:
-        with gzip.GzipFile(fn + "/output.py.gz", "rt") as f:
+        print(f"* Reading entries from {fn}...")
+        with gzip.open(fn + "/output.py.gz", "rt") as f:
             hyps = eval(f.read())
-        with gzip.GzipFile(fn + "/output_ext.py.gz", "rt") as f:
+        with gzip.open(fn + "/output_ext.py.gz", "rt") as f:
             exts = eval(f.read())
         assert isinstance(hyps, dict) and isinstance(exts, dict) and set(hyps) == set(exts)
+        print(f"* Collecting data...")
         for seq_tag in hyps:
             hyps_ = hyps[seq_tag]
             exts_ = exts[seq_tag]
@@ -57,12 +58,13 @@ def main():
             entries_num_err.append(torch.tensor(num_err_ls, device=device))
     entries_num_err = [num_errors_ / total_num_ref_words for num_errors_ in entries_num_err]
 
+    print("* Start training...")
+
     scales = torch.nn.Parameter(torch.ones([len(keys) + 1], device=device))
 
     def _logits(entries_):
         seq_scores = []
         for scores, hyp_num_tokens in entries_:
-            assert len(scores) == len(scales)
             seq_score = torch.dot(scales[:-1], scores)
             seq_score /= hyp_num_tokens ** scales[-1]
             seq_scores.append(seq_score)
@@ -74,10 +76,10 @@ def main():
     for step in range(args.num_steps):
         opt.zero_grad()
         loss = torch.zeros((), device=device)
-        for entries_, num_errors_ in zip(entries):
+        for entries_, num_errors_ in zip(entries, entries_num_err):
             seq_scores = _logits(entries_)
-            seq_probs = torch.nn.functional.softmax(seq_scores)
-            loss += seq_probs * num_errors_
+            seq_probs = torch.nn.functional.softmax(seq_scores, dim=0)
+            loss += torch.dot(seq_probs, num_errors_)
         loss.backward()
         opt.step()
         print(f"step {step}, loss: {loss}")
@@ -85,11 +87,11 @@ def main():
         if step % 100 == 0:
             with torch.no_grad():
                 err = torch.zeros((), device=device)
-                for entries_, num_errors_ in zip(entries):
+                for entries_, num_errors_ in zip(entries, entries_num_err):
                     seq_scores = _logits(entries_)
                     best = seq_scores.argmax()
                     err += num_errors_[best]
-                print(f"err: {err}, scales {scales}")
+                print(f"err: {err}, scales {scales.detach().cpu().numpy().tolist()}")
 
 
 if __name__ == "__main__":
