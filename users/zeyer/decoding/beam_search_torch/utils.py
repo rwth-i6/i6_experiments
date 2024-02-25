0354dfdcd52c110c934f6fe504be838460197257
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-25 23:33:21 +0100

    batch_gather more generic

--- users/zeyer/decoding/beam_search_torch/utils.py
+++ users/zeyer/decoding/beam_search_torch/utils.py
@@ -39,16 +39,23 @@ def top_k_nd(
     return values, indices_out
 
 
-def batch_gather(values: torch.Tensor, *, indices: torch.Tensor) -> torch.Tensor:
+def batch_gather(
+    values: torch.Tensor, *, indices: torch.Tensor, batch_dim: int = 0, index_dim: int = 1
+) -> torch.Tensor:
     """
     :param values: shape [Batch,Indices,ValuesDims...], e.g. [Batch,InBeam,...]
     :param indices: shape [Batch,IndicesDims...] -> Indices, e.g. [Batch,OutBeam] -> InBeam
-    :return: shape [Batch,IndicesDims...,ValuesDims...], e.g. [Batch,OutBeam,...]
+    :param batch_dim: in values. in indices, batch is assumed first.
+    :param index_dim: in values. must be >batch_dim (not implemented otherwise).
+        in indices, index dims are expected after batch.
+    :return: shape [Batch,IndicesDims...,ValuesDims...], e.g. [Batch,OutBeam,...],
+        if batch_dim=0 and index_dim=1.
+        Batch and index dim stays at the same place, index dim is replaced by indices dims from indices.
     """
     # Derived from returnn.torch.frontend._backend.TorchBackend.gather.
     # Case indices.dims_set.intersection(source.dims_set - {axis}).
     # We cannot use index_select in this case. Need to fallback to gather.
-    assert indices.shape[0] == values.shape[0]
+    assert indices.shape[0] == values.shape[batch_dim] and batch_dim < index_dim
     num_index_own_dims = indices.ndim - 1
     if num_index_own_dims == 1:
         indices_flat = indices  # good, [Batch,IndexDim]
@@ -56,16 +63,27 @@ def batch_gather(values: torch.Tensor, *, indices: torch.Tensor) -> torch.Tensor
         indices_flat = indices[:, None]  # [Batch,IndexDim=1]
     else:
         indices_flat = indices.flatten(1)  # [Batch,FlatIndexDim]
-    indices_flat_bc = indices_flat.reshape(list(indices_flat.shape) + [1] * (values.ndim - 2))  # [Batch,IndexDim,1s...]
-    indices_flat_exp = indices_flat_bc.expand(indices_flat.shape + values.shape[2:])  # [Batch,IndexDim,ValuesDims...]
-    out = torch.gather(values, dim=1, index=indices_flat_exp.type(torch.int64))
+    indices_flat_bc = indices_flat.reshape(
+        [
+            indices_flat.shape[0] if i == batch_dim else (indices_flat.shape[1] if i == index_dim else 1)
+            for i, d in enumerate(values.shape)
+        ]
+    )  # batch_dim=0, index_dim=1 -> [Batch,IndexDim,1s...].
+    indices_flat_exp = indices_flat_bc.expand(
+        [
+            indices_flat.shape[0] if i == batch_dim else (indices_flat.shape[1] if i == index_dim else d)
+            for i, d in enumerate(values.shape)
+        ]
+    )  # batch_dim=0, index_dim=1 -> [Batch,IndexDim,ValuesDims...]
+    out = torch.gather(values, dim=index_dim, index=indices_flat_exp.type(torch.int64))
     if num_index_own_dims == 1:
         pass  # nothing to do
     elif num_index_own_dims == 0:
-        out = out.squeeze(1)
+        out = out.squeeze(index_dim)
     else:
-        out = out.unflatten(1, indices.shape[1:])
-    assert out.shape == indices.shape + values.shape[2:]
+        out = out.unflatten(index_dim, indices.shape[1:])
+    if batch_dim == 0 and index_dim == 1:
+        assert out.shape == indices.shape + values.shape[2:]
     return out
 
 
