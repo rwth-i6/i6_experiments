18e7e1dabffb7f16e5771e530d86ebc96da47387
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-02-27 12:39:55 +0100

    length reward with dynamic beam

--- users/zeyer/decoding/beam_search_torch/scorers/length_reward.py
+++ users/zeyer/decoding/beam_search_torch/scorers/length_reward.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from typing import Any, Tuple
 import torch
 from ..interface import LabelScorerIntf
+from ..interface_dyn_beam import LabelScorerDynBeamIntf
 
 
 class LengthRewardScorer(LabelScorerIntf):
@@ -25,3 +26,23 @@ class LengthRewardScorer(LabelScorerIntf):
     ) -> Tuple[torch.Tensor, Any]:
         """score, state"""
         return torch.ones((1, 1, 1), device=prev_label.device), None
+
+
+class LengthRewardDynBeamScorer(LabelScorerDynBeamIntf):
+    """
+    Length reward
+    """
+
+    def get_initial_state(self, *, batch_size: int, device: torch.device) -> Any:
+        """initial state"""
+        return None
+
+    def score_and_update_state(
+        self,
+        *,
+        batch_idx: torch.Tensor,
+        prev_state: Any,
+        prev_label: torch.Tensor,
+    ) -> Tuple[torch.Tensor, Any]:
+        """score, state"""
+        return torch.ones((1, 1), device=prev_label.device), None
