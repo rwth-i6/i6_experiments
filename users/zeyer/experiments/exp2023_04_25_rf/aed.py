0805c6374280a14a0949bd085e6a16dc8eee0f50
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-03-03 01:47:58 +0100

    more

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -1695,6 +1695,7 @@ def model_recog_pure_torch(
 
     config = get_global_config()
 
+    torch.cuda.set_sync_debug_mode(1)  # debug CUDA sync. does not hurt too much to leave this always in?
     start_time = time.perf_counter_ns()
 
     data_concat_zeros = config.float("data_concat_zeros", 0)
