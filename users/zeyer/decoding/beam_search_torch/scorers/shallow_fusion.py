47eeda9b66bb0863213ef40f4566dc2fecc3c808
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-03-06 11:01:17 +0100

    prepare for adaptive threshold pruning (shallow fusion)

--- users/zeyer/decoding/beam_search_torch/scorers/shallow_fusion.py
+++ users/zeyer/decoding/beam_search_torch/scorers/shallow_fusion.py
@@ -64,6 +64,17 @@ class ShallowFusedLabelScorers(LabelScorerIntf):
         seq_score_ext = seq_score if seq_score_ext is None else (seq_score + seq_score_ext)
         return seq_score_ext, individual_scores, state
 
+    def max_remaining_seq_score(
+        self, *, state: Any, max_remaining_steps: torch.Tensor, device: torch.device
+    ) -> torch.Tensor:
+        """max remaining"""
+        score = None
+        for k, (v, scale) in self.label_scorers.items():
+            score_ = v.max_remaining_seq_score(state=state[k], max_remaining_steps=max_remaining_steps, device=device)
+            score_ = score_ * scale
+            score = score_ if score is None else (score + score_)
+        return score
+
 
 class ShallowFusedDynBeamLabelScorers(LabelScorerDynBeamIntf):
     def __init__(self, label_scorers: Dict[str, Tuple[LabelScorerDynBeamIntf, float]] = None):
