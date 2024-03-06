e11e89b65e9e0a78ed49b151cf1e3adf5b0722e6
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-03-06 22:11:10 +0100

    search_rqmt, dev_sets

--- users/zeyer/experiments/exp2023_04_25_rf/aed.py
+++ users/zeyer/experiments/exp2023_04_25_rf/aed.py
@@ -1316,6 +1316,9 @@ def _recog(
     model_with_checkpoint: ModelWithCheckpoint,
     recog_def: RecogDef,
     recog_config: Optional[Dict[str, Any]] = None,
+    *,
+    search_rqmt: Optional[Dict[str, Any]] = None,
+    dev_sets: Optional[Collection[str]] = None,
 ):
     from sisyphus import tk
     from i6_experiments.users.zeyer.recog import recog_model
@@ -1324,11 +1327,25 @@ def _recog(
 
     cheating = recog_config and recog_config.pop("__with_cheating", False)
 
-    res = recog_model(task, model_with_checkpoint, recog_def=recog_def, config=recog_config)
+    res = recog_model(
+        task,
+        model_with_checkpoint,
+        recog_def=recog_def,
+        config=recog_config,
+        search_rqmt=search_rqmt,
+        dev_sets=dev_sets,
+    )
     tk.register_output(_sis_prefix + "/" + name, res.output)
 
     if cheating:  # TODO just always add...?
-        res = recog_model(task, model_with_checkpoint, recog_def=recog_def, config={"cheating": True, **recog_config})
+        res = recog_model(
+            task,
+            model_with_checkpoint,
+            recog_def=recog_def,
+            config={"cheating": True, **recog_config},
+            search_rqmt=search_rqmt,
+            dev_sets=dev_sets,
+        )
         tk.register_output(_sis_prefix + "/" + name + ".d/cheating", res.output)
 
 
