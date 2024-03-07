707ab668107b7a4a62461530e8939031ba6159a9
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2024-03-07 13:49:17 +0000

    fix

--- users/zeineldeen/experiments/aed_beam_search/espnet_beam_search.py
+++ users/zeineldeen/experiments/aed_beam_search/espnet_beam_search.py
@@ -134,7 +134,7 @@ if args.pylasr_recog_args:
             recog_config.device = args.device
 
             # Trafo external LM
-            lm_model = speech2text.lm
+            lm_model = speech2text.lm_model  # required change in espnet asr_inferece.py code
             if lm_model:
                 lm_model = lm_model.eval()
 
