384b7de3bdb3e8845f2cd684c80c07534ae248a9
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-04-03 14:46:23 +0200

    doc

--- users/zeyer/utils/job_log.py
+++ users/zeyer/utils/job_log.py
@@ -8,13 +8,23 @@ from . import sis_path
 
 @contextmanager
 def open_job_log(job: str, task: str = "run", index: int = 1) -> Tuple[Optional[TextIO], Optional[str]]:
-    work_dir_prefix = sis_path.get_work_dir_prefix()
+    """
+    :param job: dir
+    :param task:
+    :param index:
+    """
+    if job.startswith("work/") or job.startswith("/"):
+        assert os.path.isdir(job), f"job dir not valid: {job}"
+    else:
+        work_dir_prefix = sis_path.get_work_dir_prefix()
+        assert os.path.isdir(work_dir_prefix + job), f"job dir not valid: job {job}, work dir prefix {work_dir_prefix}"
+        job = work_dir_prefix + job
     log_base_fn = f"log.{task}.{index}"
-    log_fn = f"{work_dir_prefix}{job}/{log_base_fn}"
+    log_fn = f"{job}/{log_base_fn}"
     if os.path.exists(log_fn):
         yield open(log_fn), log_fn
         return
-    tar_fn = f"{work_dir_prefix}{job}/finished.tar.gz"
+    tar_fn = f"{job}/finished.tar.gz"
     if os.path.exists(tar_fn):
         with tarfile.open(tar_fn) as tarf:
             while True:
