c7b2b63fbab277f80611b06a3f6b5ccddcd8857c
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2024-04-03 15:49:20 +0200

    initial ctc/rnn-t standalone example setup

---
+++ example_setups/librispeech/ctc_rnnt_standalone_2024/experiments/ctc_phon/baseline.py
@@ -0,0 +1,222 @@
+import copy
+from dataclasses import asdict
+import numpy as np
+from typing import cast, List
+
+from i6_core.tools.parameter_tuning import GetOptimalParametersAsVariableJob
+
+from i6_experiments.common.setups.returnn.datastreams.vocabulary import LabelDatastream
+
+from ...data.common import DatasetSettings, build_test_dataset
+from ...data.phon import build_eow_phon_training_datasets, get_text_lexicon
+from ...default_tools import RETURNN_EXE, MINI_RETURNN_ROOT
+from ...lm import get_4gram_binary_lm
+from ...pipeline import training, prepare_asr_model, search, ASRModel
+
+
+def eow_phon_ls960_1023_base():
+    prefix_name = "experiments/librispeech/ctc_rnnt_standalone_2024/ls960_ctc_eow_phon"
+
+    train_settings = DatasetSettings(
+        preemphasis=0.97,  # TODO: Check if this is really useful
+        peak_normalization=True,  # TODO: Also check if really useful, older Attention setups did not have that
+        # training
+        train_partition_epoch=10,
+        train_seq_ordering="laplace:.1000",
+    )
+
+    # build the training datasets object containing train, cv, dev-train and the extern_data dict
+    train_data = build_eow_phon_training_datasets(
+        prefix=prefix_name,
+        librispeech_key="train-other-960",
+        settings=train_settings,
+    )
+    label_datastream = cast(LabelDatastream, train_data.datastreams["labels"])
+    vocab_size_without_blank = label_datastream.vocab_size
+
+    dev_dataset_tuples = {}
+    for testset in ["dev-clean", "dev-other"]:
+        dev_dataset_tuples[testset] = build_test_dataset(
+            dataset_key=testset,
+            settings=train_settings,
+        )
+
+    test_dataset_tuples = {}
+    for testset in ["test-clean", "test-other"]:
+        test_dataset_tuples[testset] = build_test_dataset(
+            dataset_key=testset,
+            settings=train_settings,
+        )
+
+    arpa_4gram_lm = get_4gram_binary_lm(prefix_name=prefix_name)
+
+    default_returnn = {
+        "returnn_exe": RETURNN_EXE,
+        "returnn_root": MINI_RETURNN_ROOT,
+    }
+
+    from ...pytorch_networks.ctc.decoder.flashlight_ctc_v1 import DecoderConfig
+
+    def tune_and_evaluate_helper(
+        training_name: str,
+        asr_model: ASRModel,
+        base_decoder_config: DecoderConfig,
+        lm_scales: List[float],
+        prior_scales: List[float],
+    ):
+        """
+        Example helper to execute tuning over lm_scales and prior scales.
+        With the best values runs test-clean and test-other.
+
+        This is just a reference helper and can (should) be freely changed, copied, modified etc...
+
+        :param training_name: for alias and output names
+        :param asr_model: ASR model to use
+        :param base_decoder_config: any decoder config dataclass
+        :param lm_scales: lm scales for tuning
+        :param prior_scales: prior scales for tuning, same length as lm scales
+        """
+        tune_parameters = []
+        tune_values_clean = []
+        tune_values_other = []
+        for lm_weight in lm_scales:
+            for prior_scale in prior_scales:
+                decoder_config = copy.deepcopy(base_decoder_config)
+                decoder_config.lm_weight = lm_weight
+                decoder_config.prior_scale = prior_scale
+                search_name = training_name + "/search_lm%.1f_prior%.1f" % (lm_weight, prior_scale)
+                search_jobs, wers = search(
+                    search_name,
+                    forward_config={},
+                    asr_model=asr_model,
+                    decoder_module="ctc.decoder.flashlight_ctc_v1",
+                    decoder_args={"config": asdict(decoder_config)},
+                    test_dataset_tuples=dev_dataset_tuples,
+                    **default_returnn,
+                )
+                tune_parameters.append((lm_weight, prior_scale))
+                tune_values_clean.append((wers[search_name + "/dev-clean"]))
+                tune_values_other.append((wers[search_name + "/dev-other"]))
+
+        for key, tune_values in [("test-clean", tune_values_clean), ("test-other", tune_values_other)]:
+            pick_optimal_params_job = GetOptimalParametersAsVariableJob(
+                parameters=tune_parameters, values=tune_values, mode="minimize"
+            )
+            pick_optimal_params_job.add_alias(training_name + f"/pick_best_{key}")
+            decoder_config = copy.deepcopy(base_decoder_config)
+            decoder_config.lm_weight = pick_optimal_params_job.out_optimal_parameters[0]
+            decoder_config.prior_scale = pick_optimal_params_job.out_optimal_parameters[1]
+            search_jobs, wers = search(
+                training_name,
+                forward_config={},
+                asr_model=asr_model,
+                decoder_module="ctc.decoder.flashlight_ctc_v1",
+                decoder_args={"config": asdict(decoder_config)},
+                test_dataset_tuples={key: test_dataset_tuples[key]},
+                **default_returnn,
+            )
+
+    default_decoder_config = DecoderConfig(
+        lexicon=get_text_lexicon(),
+        returnn_vocab=label_datastream.vocab,
+        beam_size=1024,
+        beam_size_token=12,  # makes it much faster
+        arpa_lm=arpa_4gram_lm,
+        beam_threshold=14,
+    )
+
+    from ...pytorch_networks.ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v6_cfg import (
+        SpecaugConfig,
+        VGG4LayerActFrontendV1Config_mod,
+        ModelConfig,
+        LogMelFeatureExtractionV1Config,
+    )
+
+    fe_config = LogMelFeatureExtractionV1Config(
+        sample_rate=16000,
+        win_size=0.025,
+        hop_size=0.01,
+        f_min=60,
+        f_max=7600,
+        min_amp=1e-10,
+        num_filters=80,
+        center=False,
+    )
+    specaug_config = SpecaugConfig(
+        repeat_per_n_frames=25,
+        max_dim_time=20,
+        max_dim_feat=8,  # Jingjing style
+        num_repeat_feat=5,
+    )
+    frontend_config = VGG4LayerActFrontendV1Config_mod(
+        in_features=80,
+        conv1_channels=32,
+        conv2_channels=64,
+        conv3_channels=64,
+        conv4_channels=32,
+        conv_kernel_size=(3, 3),
+        conv_padding=None,
+        pool1_kernel_size=(2, 1),
+        pool1_stride=(2, 1),
+        pool1_padding=None,
+        pool2_kernel_size=(2, 1),
+        pool2_stride=(2, 1),
+        pool2_padding=None,
+        activation_str="ReLU",
+        out_features=512,
+        activation=None,
+    )
+
+    model_config = ModelConfig(
+        feature_extraction_config=fe_config,
+        frontend_config=frontend_config,
+        specaug_config=specaug_config,
+        label_target_size=vocab_size_without_blank,
+        conformer_size=512,
+        num_layers=12,
+        num_heads=8,
+        ff_dim=2048,
+        att_weights_dropout=0.1,
+        conv_dropout=0.1,
+        ff_dropout=0.1,
+        mhsa_dropout=0.1,
+        conv_kernel_size=31,
+        final_dropout=0.1,
+        specauc_start_epoch=1,
+    )
+
+    train_config_24gbgpu_amp = {
+        "optimizer": {"class": "adamw", "epsilon": 1e-16, "weight_decay": 1e-3},
+        "learning_rates": list(np.linspace(7e-6, 5e-4, 240))
+        + list(np.linspace(5e-4, 5e-5, 240))
+        + list(np.linspace(5e-5, 1e-7, 20)),
+        #############
+        "batch_size": 360 * 16000,
+        "max_seq_length": {"audio_features": 35 * 16000},
+        "accum_grad_multiple_step": 1,
+        "torch_amp_options": {"dtype": "bfloat16"},
+    }
+
+    network_module = "ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v6"
+    train_args = {
+        "config": train_config_24gbgpu_amp,
+        "network_module": network_module,
+        "net_args": {"model_config_dict": asdict(model_config)},
+        "debug": False,
+    }
+
+    training_name = prefix_name + "/" + network_module + ".512dim_sub4_24gbgpu_50eps"
+    train_job = training(training_name, train_data, train_args, num_epochs=500, **default_returnn)
+    train_job.rqmt["gpu_mem"] = 24
+    asr_model = prepare_asr_model(
+        training_name, train_job, train_args, with_prior=True, datasets=train_data, get_specific_checkpoint=500
+    )
+    tune_and_evaluate_helper(
+        training_name, asr_model, default_decoder_config, lm_scales=[2.3, 2.5, 2.7], prior_scales=[0.2, 0.3, 0.4]
+    )
+
+    # No improvement, just as example
+    asr_model_best4 = prepare_asr_model(
+        training_name+ "/best4", train_job, train_args, with_prior=True, datasets=train_data, get_best_averaged_checkpoint=(4, "dev_loss_ctc")
+    )
+    tune_and_evaluate_helper(training_name + "/best4", asr_model_best4, default_decoder_config, lm_scales=[2.3, 2.5, 2.7], prior_scales=[0.2, 0.3, 0.4])
---
+++ example_setups/librispeech/ctc_rnnt_standalone_2024/pipeline.py
@@ -0,0 +1,284 @@
+"""
+Pipeline parts to create the necessary jobs for training / forwarding / search etc...
+"""
+import copy
+import enum
+from dataclasses import dataclass, asdict
+import os.path
+from typing import Any, Dict, List, Optional, Tuple
+
+from sisyphus import tk
+
+from i6_core.corpus.convert import CorpusToStmJob
+from i6_core.recognition.scoring import ScliteJob
+
+from i6_core.returnn.config import ReturnnConfig
+from i6_core.returnn.search import SearchWordsToCTMJob
+from i6_core.returnn.training import ReturnnTrainingJob, AverageTorchCheckpointsJob, GetBestPtCheckpointJob
+from i6_core.returnn.forward import ReturnnForwardJobV2
+
+from i6_experiments.common.setups.returnn.datasets import Dataset
+
+from .config import get_forward_config, get_training_config, get_prior_config, TrainingDatasets
+from .default_tools import SCTK_BINARY_PATH, RETURNN_EXE, MINI_RETURNN_ROOT
+
+
+@dataclass
+class ASRModel:
+    checkpoint: tk.Path
+    net_args: Dict[str, Any]
+    network_module: str
+    prior_file: Optional[tk.Path]
+
+
+def search_single(
+    prefix_name: str,
+    returnn_config: ReturnnConfig,
+    checkpoint: tk.Path,
+    recognition_dataset: Dataset,
+    recognition_bliss_corpus: tk.Path,
+    returnn_exe: tk.Path,
+    returnn_root: tk.Path,
+    mem_rqmt: float = 10,
+    use_gpu: bool = False,
+):
+    """
+    Run search for a specific test dataset
+
+    :param prefix_name: prefix folder path for alias and output files
+    :param returnn_config: the RETURNN config to be used for forwarding
+    :param Checkpoint checkpoint: path to RETURNN PyTorch model checkpoint
+    :param recognition_dataset: Dataset to perform recognition on
+    :param recognition_bliss_corpus: path to bliss file used as Sclite evaluation reference
+    :param returnn_exe: The python executable to run the job with (when using container just "python3")
+    :param returnn_root: Path to a checked out RETURNN repository
+    :param mem_rqmt: some search jobs might need more memory
+    :param use_gpu: if to do GPU decoding
+    """
+    returnn_config = copy.deepcopy(returnn_config)
+    returnn_config.config["forward"] = recognition_dataset.as_returnn_opts()
+    search_job = ReturnnForwardJobV2(
+        model_checkpoint=checkpoint,
+        returnn_config=returnn_config,
+        log_verbosity=5,
+        mem_rqmt=mem_rqmt,
+        time_rqmt=24,
+        device="gpu" if use_gpu else "cpu",
+        cpu_rqmt=2,
+        returnn_python_exe=returnn_exe,
+        returnn_root=returnn_root,
+        output_files=["search_out.py"],
+    )
+    search_job.add_alias(prefix_name + "/search_job")
+
+    search_ctm = SearchWordsToCTMJob(
+        recog_words_file=search_job.out_files["search_out.py"],
+        bliss_corpus=recognition_bliss_corpus,
+    ).out_ctm_file
+
+    stm_file = CorpusToStmJob(bliss_corpus=recognition_bliss_corpus).out_stm_path
+
+    sclite_job = ScliteJob(ref=stm_file, hyp=search_ctm, sctk_binary_path=SCTK_BINARY_PATH)
+    tk.register_output(prefix_name + "/sclite/wer", sclite_job.out_wer)
+    tk.register_output(prefix_name + "/sclite/report", sclite_job.out_report_dir)
+
+    return sclite_job.out_wer, search_job
+
+
+@tk.block()
+def search(
+    prefix_name: str,
+    forward_config: Dict[str, Any],
+    asr_model: ASRModel,
+    decoder_module: str,
+    decoder_args: Dict[str, Any],
+    test_dataset_tuples: Dict[str, Tuple[Dataset, tk.Path]],
+    returnn_exe: tk.Path,
+    returnn_root: tk.Path,
+    use_gpu: bool = False,
+    debug: bool = False,
+):
+    """
+    Run search over multiple datasets and collect statistics
+
+    :param prefix_name: prefix folder path for alias and output files
+    :param forward_config: returnn config parameter for the forward job
+    :param asr_model: the ASRModel from the training
+    :param decoder_module: path to the file containing the decoder definition
+    :param decoder_args: arguments for the decoding forward_init_hook
+    :param test_dataset_tuples: tuple of (Dataset, tk.Path) for the dataset object and the reference bliss
+    :param returnn_exe: The python executable to run the job with (when using container just "python3")
+    :param returnn_root: Path to a checked out RETURNN repository
+    :param use_gpu: run search with GPU
+    """
+    if asr_model.prior_file is not None:
+        decoder_args["config"]["prior_file"] = asr_model.prior_file
+
+    returnn_search_config = get_forward_config(
+        network_module=asr_model.network_module,
+        config=forward_config,
+        net_args=asr_model.net_args,
+        decoder_args=decoder_args,
+        decoder=decoder_module,
+        debug=debug,
+    )
+
+    # use fixed last checkpoint for now, needs more fine-grained selection / average etc. here
+    wers = {}
+    search_jobs = []
+    for key, (test_dataset, test_dataset_reference) in test_dataset_tuples.items():
+        search_name = prefix_name + "/%s" % key
+        wers[search_name], search_job = search_single(
+            search_name,
+            returnn_search_config,
+            asr_model.checkpoint,
+            test_dataset,
+            test_dataset_reference,
+            returnn_exe,
+            returnn_root,
+            use_gpu=use_gpu,
+        )
+        search_jobs.append(search_job)
+
+    return search_jobs, wers
+
+
+@tk.block()
+def compute_prior(
+    prefix_name: str,
+    returnn_config: ReturnnConfig,
+    checkpoint: tk.Path,
+    returnn_exe: tk.Path,
+    returnn_root: tk.Path,
+    mem_rqmt: int = 16,
+):
+    """
+    Run search for a specific test dataset
+
+    :param prefix_name: prefix folder path for alias and output files
+    :param returnn_config: the RETURNN config to be used for forwarding
+    :param Checkpoint checkpoint: path to RETURNN PyTorch model checkpoint
+    :param returnn_exe: The python executable to run the job with (when using container just "python3")
+    :param returnn_root: Path to a checked out RETURNN repository
+    :param mem_rqmt: override the default memory requirement
+    """
+    search_job = ReturnnForwardJobV2(
+        model_checkpoint=checkpoint,
+        returnn_config=returnn_config,
+        log_verbosity=5,
+        mem_rqmt=mem_rqmt,
+        time_rqmt=2,
+        device="gpu",
+        cpu_rqmt=8,
+        returnn_python_exe=returnn_exe,
+        returnn_root=returnn_root,
+        output_files=["prior.txt"],
+    )
+    search_job.add_alias(prefix_name + "/prior_job")
+    return search_job.out_files["prior.txt"]
+
+
+def training(training_name, datasets, train_args, num_epochs, returnn_exe, returnn_root):
+    """
+    :param training_name:
+    :param datasets:
+    :param train_args:
+    :param num_epochs:
+    :param returnn_exe: The python executable to run the job with (when using container just "python3")
+    :param returnn_root: Path to a checked out RETURNN repository
+    """
+    returnn_config = get_training_config(training_datasets=datasets, **train_args)
+    default_rqmt = {
+        "mem_rqmt": 24,
+        "time_rqmt": 168,
+        "cpu_rqmt": 6,
+        "log_verbosity": 5,
+        "returnn_python_exe": returnn_exe,
+        "returnn_root": returnn_root,
+    }
+
+    train_job = ReturnnTrainingJob(returnn_config=returnn_config, num_epochs=num_epochs, **default_rqmt)
+    train_job.add_alias(training_name + "/training")
+    tk.register_output(training_name + "/learning_rates", train_job.out_learning_rates)
+    return train_job
+
+
+def prepare_asr_model(
+    training_name,
+    train_job,
+    train_args,
+    with_prior,
+    datasets: Optional[TrainingDatasets] = None,
+    get_specific_checkpoint: Optional[int] = None,
+    get_best_averaged_checkpoint: Optional[Tuple[int, str]] = None,
+    get_last_averaged_checkpoint: Optional[int] = None,
+):
+    """
+    :param training_name:
+    :param train_job: output of training
+    :param train_args: same args as for training
+    :param with_prior: If prior should be used (yes for CTC, no for RNN-T)
+    :param datasets: Needed if with_prior == True
+    :param get_specific_checkpoint: return a specific epoch (set one get_*)
+    :param get_best_averaged_checkpoint: return the average with (n checkpoints, loss-key), n checkpoints can be 1
+    :param get_last_averaged_checkpoint: return the average of the last n checkpoints
+    :return:
+    """
+
+    params = [get_specific_checkpoint, get_last_averaged_checkpoint, get_best_averaged_checkpoint]
+    assert sum([p is not None for p in params]) == 1
+    assert not with_prior or datasets is not None
+
+    if get_best_averaged_checkpoint is not None:
+        n_checkpoints, loss_key = get_best_averaged_checkpoint
+        checkpoints = []
+        for index in range(n_checkpoints):
+            best_job = GetBestPtCheckpointJob(
+                train_job.out_model_dir,
+                train_job.out_learning_rates,
+                key=loss_key,
+                index=index,
+            )
+            best_job.add_alias(training_name + f"/get_best_job_{index}")
+            checkpoints.append(best_job.out_checkpoint)
+        if n_checkpoints > 1:
+            # perform averaging
+            avg = AverageTorchCheckpointsJob(
+                checkpoints=checkpoints, returnn_python_exe=RETURNN_EXE, returnn_root=MINI_RETURNN_ROOT
+            )
+            checkpoint = avg.out_checkpoint
+        else:
+            # we only have one
+            checkpoint = checkpoints[0]
+    elif get_last_averaged_checkpoint is not None:
+        assert get_last_averaged_checkpoint >= 2, "For the single last checkpoint use get_specific_checkpoint instead"
+        num_checkpoints = len(train_job.out_checkpoints)
+        avg = AverageTorchCheckpointsJob(
+            checkpoints=[train_job.out_checkpoints[num_checkpoints - i] for i in range(get_last_averaged_checkpoint)],
+            returnn_python_exe=RETURNN_EXE,
+            returnn_root=MINI_RETURNN_ROOT,
+        )
+        checkpoint = avg.out_checkpoint
+    else:
+        checkpoint = train_job.out_checkpoints[get_specific_checkpoint]
+
+    prior_file = None
+    if with_prior:
+        returnn_config = get_prior_config(training_datasets=datasets, **train_args)
+        prior_file = compute_prior(
+            training_name,
+            returnn_config,
+            checkpoint=checkpoint,
+            returnn_exe=RETURNN_EXE,
+            returnn_root=MINI_RETURNN_ROOT,
+        )
+        tk.register_output(training_name + "/prior.txt", prior_file)
+
+    asr_model = ASRModel(
+        checkpoint=checkpoint,
+        network_module=train_args["network_module"],
+        net_args=train_args["net_args"],
+        prior_file=prior_file,
+    )
+
+    return asr_model
