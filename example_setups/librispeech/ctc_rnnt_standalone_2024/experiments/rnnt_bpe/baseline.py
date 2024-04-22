b5371a5a794a5c2a987a3e270b0f2e25d4fdf68b
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2024-04-22 17:43:43 +0200

    add RNN-T init from CTC baseline
    i6_models + i6_native_ops based pure PyTorch RNN-T with torchaudio beam search

--- example_setups/librispeech/ctc_rnnt_standalone_2024/experiments/rnnt_bpe/baseline.py
+++ example_setups/librispeech/ctc_rnnt_standalone_2024/experiments/rnnt_bpe/baseline.py
@@ -14,11 +14,11 @@ from ...data.bpe import build_bpe_training_datasets, get_text_lexicon
 from ...default_tools import RETURNN_EXE, MINI_RETURNN_ROOT
 from ...lm import get_4gram_binary_lm
 from ...pipeline import training, prepare_asr_model, search, ASRModel
-from ...storage import add_ctc_model
+from ...storage import get_ctc_model
 
 
-def bpe_ls960_1023_base():
-    prefix_name = "example_setups/librispeech/ctc_rnnt_standalone_2024/ls960_ctc_bpe_5k"
+def rnnt_bpe_ls960_1023_base():
+    prefix_name = "example_setups/librispeech/ctc_rnnt_standalone_2024/ls960_rnnt_bpe_5k"
 
     train_settings = DatasetSettings(
         preemphasis=0.97,  # TODO: Check if this is really useful
@@ -34,7 +34,7 @@ def bpe_ls960_1023_base():
         librispeech_key="train-other-960",
         bpe_size=5000,
         settings=train_settings,
-        use_postfix=False,
+        use_postfix=True,  # RNN-T now, use postfix
     )
     label_datastream_bpe5000 = cast(LabelDatastream, train_data_bpe5000.datastreams["labels"])
     vocab_size_without_blank = label_datastream_bpe5000.vocab_size
@@ -53,21 +53,18 @@ def bpe_ls960_1023_base():
             settings=train_settings,
         )
 
-    arpa_4gram_lm = get_4gram_binary_lm(prefix_name=prefix_name)
-
     default_returnn = {
         "returnn_exe": RETURNN_EXE,
         "returnn_root": MINI_RETURNN_ROOT,
     }
 
-    from ...pytorch_networks.ctc.decoder.flashlight_ctc_v1 import DecoderConfig
+    from ...pytorch_networks.rnnt.decoder.experimental_rnnt_decoder import DecoderConfig
 
-    def tune_and_evaluate_helper(
+    def evaluate_helper(
         training_name: str,
         asr_model: ASRModel,
         base_decoder_config: DecoderConfig,
-        lm_scales: List[float],
-        prior_scales: List[float],
+        beam_size: int = 1,
     ):
         """
         Example helper to execute tuning over lm_scales and prior scales.
@@ -78,63 +75,36 @@ def bpe_ls960_1023_base():
         :param training_name: for alias and output names
         :param asr_model: ASR model to use
         :param base_decoder_config: any decoder config dataclass
-        :param lm_scales: lm scales for tuning
-        :param prior_scales: prior scales for tuning, same length as lm scales
+
         """
-        tune_parameters = []
-        tune_values_clean = []
-        tune_values_other = []
-        for lm_weight in lm_scales:
-            for prior_scale in prior_scales:
-                decoder_config = copy.deepcopy(base_decoder_config)
-                decoder_config.lm_weight = lm_weight
-                decoder_config.prior_scale = prior_scale
-                search_name = training_name + "/search_lm%.1f_prior%.1f" % (lm_weight, prior_scale)
-                search_jobs, wers = search(
-                    search_name,
-                    forward_config={},
-                    asr_model=asr_model,
-                    decoder_module="ctc.decoder.flashlight_ctc_v1",
-                    decoder_args={"config": asdict(decoder_config)},
-                    test_dataset_tuples=dev_dataset_tuples,
-                    **default_returnn,
-                )
-                tune_parameters.append((lm_weight, prior_scale))
-                tune_values_clean.append((wers[search_name + "/dev-clean"]))
-                tune_values_other.append((wers[search_name + "/dev-other"]))
+        decoder_config = copy.deepcopy(base_decoder_config)
+        decoder_config.beam_size = beam_size
+        search_name = training_name + "/search_bs%i" % beam_size
+        search_jobs, wers = search(
+            search_name,
+            forward_config={},
+            asr_model=asr_model,
+            decoder_module="rnnt.decoder.experimental_rnnt_decoder",
+            decoder_args={"config": asdict(decoder_config)},
+            test_dataset_tuples={**dev_dataset_tuples, **test_dataset_tuples},
+            **default_returnn,
+        )
 
-        for key, tune_values in [("test-clean", tune_values_clean), ("test-other", tune_values_other)]:
-            pick_optimal_params_job = GetOptimalParametersAsVariableJob(
-                parameters=tune_parameters, values=tune_values, mode="minimize"
-            )
-            pick_optimal_params_job.add_alias(training_name + f"/pick_best_{key}")
-            decoder_config = copy.deepcopy(base_decoder_config)
-            decoder_config.lm_weight = pick_optimal_params_job.out_optimal_parameters[0]
-            decoder_config.prior_scale = pick_optimal_params_job.out_optimal_parameters[1]
-            search_jobs, wers = search(
-                training_name,
-                forward_config={},
-                asr_model=asr_model,
-                decoder_module="ctc.decoder.flashlight_ctc_v1",
-                decoder_args={"config": asdict(decoder_config)},
-                test_dataset_tuples={key: test_dataset_tuples[key]},
-                **default_returnn,
-            )
-
-    default_decoder_config_bpe5000 = DecoderConfig(
-        lexicon=get_text_lexicon(prefix=prefix_name, librispeech_key="train-other-960", bpe_size=5000),
-        returnn_vocab=label_datastream_bpe5000.vocab,
-        beam_size=1024,  # Untuned
-        beam_size_token=16,  # makes it much faster (0.3 search RTF -> 0.04 search RTF), but looses 0.1% WER over 128
-        arpa_lm=arpa_4gram_lm,
-        beam_threshold=14,  # Untuned
+    greedy_decoder_config_bpe5000 = DecoderConfig(
+        beam_size=1,  # greedy as default
+        returnn_vocab=label_datastream_bpe5000.vocab
+    )
+    bs10_decoder_config_bpe5000 = DecoderConfig(
+        beam_size=1,  # greedy as default
+        returnn_vocab=label_datastream_bpe5000.vocab
     )
 
-    from ...pytorch_networks.ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v6_cfg import (
+    from ...pytorch_networks.rnnt.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v9_cfg import (
         SpecaugConfig,
         VGG4LayerActFrontendV1Config_mod,
         ModelConfig,
         LogMelFeatureExtractionV1Config,
+        PredictorConfig
     )
 
     fe_config = LogMelFeatureExtractionV1Config(
@@ -153,7 +123,13 @@ def bpe_ls960_1023_base():
         max_dim_feat=8,  # Jingjing style
         num_repeat_feat=5,
     )
-    frontend_config_sub6 = VGG4LayerActFrontendV1Config_mod(
+    specaug_config_full = SpecaugConfig(
+        repeat_per_n_frames=25,
+        max_dim_time=20,
+        max_dim_feat=16,  # Old style
+        num_repeat_feat=5,
+    )
+    frontend_config = VGG4LayerActFrontendV1Config_mod(
         in_features=80,
         conv1_channels=32,
         conv2_channels=64,
@@ -162,7 +138,7 @@ def bpe_ls960_1023_base():
         conv_kernel_size=(3, 3),
         conv_padding=None,
         pool1_kernel_size=(3, 1),
-        pool1_stride=(2, 1),
+        pool1_stride=(3, 1),
         pool1_padding=None,
         pool2_kernel_size=(2, 1),
         pool2_stride=(2, 1),
@@ -171,11 +147,18 @@ def bpe_ls960_1023_base():
         out_features=512,
         activation=None,
     )
-
-    model_config = ModelConfig(
+    predictor_config = PredictorConfig(
+        symbol_embedding_dim=256,
+        emebdding_dropout=0.2,
+        num_lstm_layers=1,
+        lstm_hidden_dim=512,
+        lstm_dropout=0.1,
+    )
+    model_config_v5_sub6_512lstm = ModelConfig(
         feature_extraction_config=fe_config,
-        frontend_config=frontend_config_sub6,
+        frontend_config=frontend_config,
         specaug_config=specaug_config,
+        predictor_config=predictor_config,
         label_target_size=vocab_size_without_blank,
         conformer_size=512,
         num_layers=12,
@@ -187,40 +170,66 @@ def bpe_ls960_1023_base():
         mhsa_dropout=0.1,
         conv_kernel_size=31,
         final_dropout=0.1,
-        specauc_start_epoch=11,  # BPE does not converge otherwise
+        specauc_start_epoch=21,
+        joiner_dim=640,
+        joiner_activation="relu",
+        joiner_dropout=0.1,
+        ctc_output_loss=0.0
     )
+    model_config_v5_sub6_512lstm_start1 = copy.deepcopy(model_config_v5_sub6_512lstm)
+    model_config_v5_sub6_512lstm_start1.specauc_start_epoch = 1
+
+    model_config_v5_sub6_512lstm_start1_full_spec = copy.deepcopy(model_config_v5_sub6_512lstm_start1)
+    model_config_v5_sub6_512lstm_start1_full_spec.specaug_config = specaug_config_full
+
 
     train_config_24gbgpu_amp = {
         "optimizer": {"class": "adamw", "epsilon": 1e-16, "weight_decay": 1e-3},
-        "learning_rates": list(np.linspace(7e-6, 5e-4, 240))
+        "learning_rates": list(np.linspace(5e-5, 5e-4, 240))
         + list(np.linspace(5e-4, 5e-5, 240))
         + list(np.linspace(5e-5, 1e-7, 20)),
         #############
-        "batch_size": 360 * 16000,  # GPU MEM still very moderate, but larger batch did not help
+        "batch_size": 120 * 16000, # RNN-T has very high memory consumption
         "max_seq_length": {"audio_features": 35 * 16000},
-        "accum_grad_multiple_step": 1,
+        "accum_grad_multiple_step": 2,
         "torch_amp_options": {"dtype": "bfloat16"},
     }
 
-    network_module = "ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v6"
+    network_module = "rnnt.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v9"
     train_args = {
         "config": train_config_24gbgpu_amp,
         "network_module": network_module,
-        "net_args": {"model_config_dict": asdict(model_config)},
+        "net_args": {"model_config_dict": asdict(model_config_v5_sub6_512lstm)},
         "debug": False,
     }
 
-    training_name = prefix_name + "/" + network_module + ".512dim_sub6_24gbgpu_50eps"
-    train_job = training(training_name, train_data_bpe5000, train_args, num_epochs=500, **default_returnn)
+
+    network_module = "rnnt.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v9_i6_native"
+    train_args_warprnnt_accum2_fullspec1 = copy.deepcopy(train_args)
+    train_args_warprnnt_accum2_fullspec1["network_module"] = network_module
+    train_args_warprnnt_accum2_fullspec1["net_args"] = {"model_config_dict": asdict(model_config_v5_sub6_512lstm_start1_full_spec)}
+    train_args_warprnnt_accum2_fullspec1["include_native_ops"] = True
+    train_args_warprnnt_accum2_fullspec1["config"]["learning_rates"] = list(np.linspace(5e-5, 5e-4, 120)) + list(
+        np.linspace(5e-4, 5e-5, 120)) + list(np.linspace(5e-5, 1e-7, 10))
+    train_args_warprnnt_accum2_fullspec1["config"]["preload_from_files"] = {
+        "encoder": {
+            "filename": get_ctc_model(
+                "ls960_ctc_bpe_5k.ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_v6.512dim_sub6_24gbgpu_50eps_ckpt500"
+            ).checkpoint,
+            "init_for_train": True,
+            "ignore_missing": True,
+        }
+    }
+
+    training_name = prefix_name + "/" + network_module + ".512dim_sub6_24gbgpu_25eps_accum2_fullspec1_continue_from_ctc50eps"
+    train_job = training(training_name, train_data_bpe5000, train_args_warprnnt_accum2_fullspec1, num_epochs=250, **default_returnn)
     train_job.rqmt["gpu_mem"] = 24
+    train_job.set_env("PYTORCH_CUDA_ALLOC_CONF", "expandable_segments:True")
     asr_model = prepare_asr_model(
-        training_name, train_job, train_args, with_prior=True, datasets=train_data_bpe5000, get_specific_checkpoint=500
+        training_name, train_job, train_args_warprnnt_accum2_fullspec1, with_prior=False, datasets=train_data_bpe5000, get_specific_checkpoint=250
     )
-    add_ctc_model("ls960_ctc_bpe_5k." + network_module + ".512dim_sub6_24gbgpu_50eps_ckpt500", asr_model)
-    tune_and_evaluate_helper(
+    evaluate_helper(
         training_name,
         asr_model,
-        default_decoder_config_bpe5000,
-        lm_scales=[1.6, 1.8, 2.0],
-        prior_scales=[0.2, 0.3, 0.4],
+        greedy_decoder_config_bpe5000,
     )
