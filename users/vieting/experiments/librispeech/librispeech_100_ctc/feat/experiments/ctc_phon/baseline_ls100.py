d477af0f65d5ec949d31f61a8c511ce59f84b2bb
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2024-05-17 17:47:59 +0200

    ls100 ctc feat: adjust specaug for scf

--- users/vieting/experiments/librispeech/librispeech_100_ctc/feat/experiments/ctc_phon/baseline_ls100.py
+++ users/vieting/experiments/librispeech/librispeech_100_ctc/feat/experiments/ctc_phon/baseline_ls100.py
@@ -333,3 +333,78 @@ def eow_phon_ls100_1023_base():
     tune_and_evaluate_helper(
         training_name, asr_model, default_decoder_config, lm_scales=[3.5], prior_scales=[0.3, 0.5]
     )
+
+    # adjust SpecAugment in frequency dimension
+    scf_config = SupervisedConvolutionalFeatureExtractionV1Config(
+        wave_norm=True,
+        num_tf=150,
+        size_tf=256,
+        stride_tf=10,
+        num_env=5,
+        size_env=40,
+        stride_env=16
+    )
+    specaug_config = SpecaugConfig(
+        repeat_per_n_frames=25,
+        max_dim_time=20,
+        max_dim_feat=int(16 / 80 * 750),
+        num_repeat_feat=5,
+    )
+    frontend_config = VGG4LayerActFrontendV1Config_mod(
+        in_features=750,
+        conv1_channels=32,
+        conv2_channels=64,
+        conv3_channels=64,
+        conv4_channels=32,
+        conv_kernel_size=(3, 3),
+        conv_padding=None,
+        pool1_kernel_size=(2, 1),
+        pool1_stride=(2, 1),
+        pool1_padding=None,
+        pool2_kernel_size=(2, 1),
+        pool2_stride=(2, 1),
+        pool2_padding=None,
+        activation_str="ReLU",
+        out_features=384,
+        activation=None,
+    )
+    model_config = ScfModelConfig(
+        feature_extraction_config=scf_config,
+        frontend_config=frontend_config,
+        specaug_config=specaug_config,
+        label_target_size=vocab_size_without_blank,
+        conformer_size=384,
+        num_layers=12,
+        num_heads=4,
+        ff_dim=1536,
+        att_weights_dropout=0.2,
+        conv_dropout=0.2,
+        ff_dropout=0.2,
+        mhsa_dropout=0.2,
+        conv_kernel_size=31,
+        final_dropout=0.2,
+        specaug_start_epoch=1,
+        feature_training_start_epoch=0,
+        feature_training_end_epoch=-1,
+    )
+    network_module = "ctc.conformer_1023.i6modelsV1_VGG4LayerActFrontendV1_ScfV1_v1"
+    train_args = {
+        "config": {
+            **copy.deepcopy(train_config_24gbgpu_amp),
+            "batch_size": 180 * 16000,
+            "accum_grad_multiple_step": 2,
+        },
+        "network_module": network_module,
+        "net_args": {"model_config_dict": asdict(model_config)},
+        "debug": False,
+    }
+
+    training_name = prefix_name + "/" + network_module + ".384dim_sub4_24gbgpu_100eps_bs2x180_sa2"
+    train_job = training(training_name, train_data, train_args, num_epochs=300, **default_returnn)
+    train_job.rqmt["gpu_mem"] = 24
+    asr_model = prepare_asr_model(
+        training_name, train_job, train_args, with_prior=True, datasets=train_data, get_specific_checkpoint=300
+    )
+    tune_and_evaluate_helper(
+        training_name, asr_model, default_decoder_config, lm_scales=[3.5], prior_scales=[0.3, 0.5]
+    )
