41d336e42cd759f37edf9ebb40d1862283705668
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2024-05-21 14:39:59 +0000

    better layer names for ebranchformer

--- users/zeineldeen/models/asr/encoder/ebranchformer_encoder.py
+++ users/zeineldeen/models/asr/encoder/ebranchformer_encoder.py
@@ -10,38 +10,22 @@ class EBranchformerEncoder(ConformerEncoderV2):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _create_conv_gating_mlp(self, prefix_name, source, layer_index):
-        prefix_name = "{}_local_extractor".format(prefix_name)
+    def _create_conv_spatial_gating_unit(self, prefix_name, source, layer_index):
+        # see also here: https://github.com/espnet/espnet/blob/master/espnet2/asr/layers/cgmlp.py#L15
 
-        ln = self.network.add_layer_norm_layer("{}_ln".format(prefix_name), source)
-
-        ff1 = self.network.add_linear_layer(
-            "{}_ff_1".format(prefix_name),
-            ln,
-            n_out=6 * self.enc_key_dim,
-            l2=self.l2,
-            forward_weights_init=self.ff_init,
-            with_bias=False,
-            param_dropout=self.ff_weight_drop,
-            param_dropout_min_ndim=2,
-            param_variational_noise=self.ff_weight_noise,
+        branch_a = self.network.add_slice_layer(
+            "{}_branch_a".format(prefix_name), source, "F", slice_start=0, slice_end=self.enc_key_dim * 3
         )
 
-        gelu_act = self.network.add_activation_layer("{}_gelu".format(prefix_name), ff1, activation="gelu")
-
-        br_part_A = self.network.add_slice_layer(
-            "{}_branch_a".format(prefix_name), gelu_act, "F", slice_start=0, slice_end=self.enc_key_dim * 3
+        branch_b = self.network.add_slice_layer(
+            "{}_branch_b".format(prefix_name), source, "F", slice_start=self.enc_key_dim * 3
         )
 
-        br_part_B = self.network.add_slice_layer(
-            "{}_branch_b".format(prefix_name), gelu_act, "F", slice_start=self.enc_key_dim * 3
-        )
+        br_part_b_ln = self.network.add_layer_norm_layer("{}_branch_b_ln".format(prefix_name), branch_b)
 
-        br_part_B_ln = self.network.add_layer_norm_layer("{}_branch_b_ln".format(prefix_name), br_part_B)
-
-        br_part_B_dpt_conv = self.network.add_conv_layer(
-            "{}_branch_b_dpt_conv".format(prefix_name),
-            br_part_B_ln,
+        br_part_b_depthwise_conv = self.network.add_conv_layer(
+            "{}_branch_b_depthwise_conv".format(prefix_name),
+            br_part_b_ln,
             n_out=self.enc_key_dim * 3,
             filter_size=(self.conv_kernel_size,),
             groups=self.enc_key_dim * 3,
@@ -52,14 +36,37 @@ class EBranchformerEncoder(ConformerEncoderV2):
         )
 
         br_merge = self.network.add_eval_layer(
-            "{}_branch_merge".format(prefix_name), [br_part_A, br_part_B_dpt_conv], "source(0)*source(1)"
+            "{}_branch_merge".format(prefix_name), [branch_a, br_part_b_depthwise_conv], "source(0) * source(1)"
         )
 
         dropout = self.network.add_dropout_layer("{}_dropout".format(prefix_name), br_merge, dropout=self.dropout)
 
+        return dropout
+
+    def _create_conv_gating_mlp(self, prefix_name, source, layer_index):
+        prefix_name = "{}_cgmlp".format(prefix_name)
+
+        ln = self.network.add_layer_norm_layer("{}_ln".format(prefix_name), source)
+
+        ff1 = self.network.add_linear_layer(
+            "{}_ff_1".format(prefix_name),
+            ln,
+            n_out=6 * self.enc_key_dim,  # TODO: make it configurable
+            l2=self.l2,
+            forward_weights_init=self.ff_init,
+            with_bias=False,
+            param_dropout=self.ff_weight_drop,
+            param_dropout_min_ndim=2,
+            param_variational_noise=self.ff_weight_noise,
+        )
+
+        gelu_act = self.network.add_activation_layer("{}_gelu".format(prefix_name), ff1, activation="gelu")
+
+        csgu = self._create_conv_spatial_gating_unit(prefix_name, gelu_act, layer_index)
+
         br_merge_ff = self.network.add_linear_layer(
             "{}_ff_2".format(prefix_name),
-            dropout,
+            csgu,
             n_out=self.enc_key_dim,
             l2=self.l2,
             forward_weights_init=self.ff_init,
@@ -74,12 +81,13 @@ class EBranchformerEncoder(ConformerEncoderV2):
     def _create_merge_module(self, prefix_name, *, source, global_extracter, local_extracter, layer_index):
         prefix_name = "{}_merge_module".format(prefix_name)
 
+        # concat on feature dim
         glb_lcl_merge = self.network.add_copy_layer(
             "{}_global_local_merge".format(prefix_name), [global_extracter, local_extracter]
         )
 
-        dpt_conv = self.network.add_conv_layer(
-            "{}_dpt_conv".format(prefix_name),
+        depthwise_conv = self.network.add_conv_layer(
+            "{}_depthwise_conv".format(prefix_name),
             glb_lcl_merge,
             n_out=2 * self.enc_key_dim,
             filter_size=(self.conv_kernel_size,),
@@ -90,16 +98,16 @@ class EBranchformerEncoder(ConformerEncoderV2):
             param_variational_noise=self.conv_weight_noise,
         )
 
-        dpt_conv_res = self.network.add_combine_layer(
-            "{}_dpt_conv_res".format(prefix_name),
+        depthwise_conv_res = self.network.add_combine_layer(
+            "{}_depthwise_conv_res".format(prefix_name),
             kind="add",
-            source=[glb_lcl_merge, dpt_conv],
+            source=[glb_lcl_merge, depthwise_conv],
             n_out=2 * self.enc_key_dim,
         )
 
         ff = self.network.add_linear_layer(
             "{}_ff".format(prefix_name),
-            dpt_conv_res,
+            depthwise_conv_res,
             n_out=self.enc_key_dim,
             l2=self.l2,
             forward_weights_init=self.ff_init,
@@ -140,11 +148,11 @@ class EBranchformerEncoder(ConformerEncoderV2):
         mhsa = self._create_mhsa_module(prefix_name, ff_module1, i)
 
         # create branch 2: Convolutional gating MLP
-        conv_gating_mlp = self._create_conv_gating_mlp(prefix_name, ff_module1, i)
+        cgmlp = self._create_conv_gating_mlp(prefix_name, ff_module1, i)
 
         # merge two branches
         merge_module = self._create_merge_module(
-            prefix_name, source=ff_module1, global_extracter=mhsa, local_extracter=conv_gating_mlp, layer_index=i
+            prefix_name, source=ff_module1, global_extracter=mhsa, local_extracter=cgmlp, layer_index=i
         )
 
         ff_module2 = self._create_ff_module(prefix_name, 2, merge_module, i)
