a2e76f7731dfdd5499b5e546bab3c2d53dc1d186
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2024-05-21 15:43:49 +0200

    ls100 ctc feat: add report

--- users/vieting/experiments/librispeech/librispeech_100_ctc/feat/experiments/ctc_phon/baseline_ls100.py
+++ users/vieting/experiments/librispeech/librispeech_100_ctc/feat/experiments/ctc_phon/baseline_ls100.py
@@ -1,11 +1,13 @@
 import copy
+import os.path
 from dataclasses import asdict
 import numpy as np
 from typing import cast, List, Optional, Dict, Any
 
+from sisyphus import tk
 from i6_core.tools.parameter_tuning import GetOptimalParametersAsVariableJob
-
 from i6_experiments.common.setups.returnn.datastreams.vocabulary import LabelDatastream
+from i6_experiments.users.vieting.tools.report import Report
 
 from ...data.common import DatasetSettings, build_test_dataset
 from ...data.phon import build_eow_phon_training_datasets, get_text_lexicon
@@ -16,6 +18,11 @@ from ...pipeline import training, prepare_asr_model, search, ASRModel
 
 def eow_phon_ls100_1023_base():
     prefix_name = "experiments/librispeech/librispeech_100_ctc/feat"
+
+    report = Report(
+        columns_start=["train_name"],
+        columns_end=["lm_scale", "prior_scale", "wer"],
+    )
 
     train_settings = DatasetSettings(
         preemphasis=0.97,
@@ -98,6 +105,16 @@ def eow_phon_ls100_1023_base():
                 tune_parameters.append((lm_weight, prior_scale))
                 tune_values_clean.append((wers[search_name + "/dev-clean"]))
                 tune_values_other.append((wers[search_name + "/dev-other"]))
+                for key, wer in wers.items():
+                    report.add(
+                        {
+                            "train_name": training_name,
+                            "prior_scale": prior_scale,
+                            "lm_scale": lm_weight,
+                            "eval_set": key.split("/")[-1],
+                            "wer": wer,
+                        }
+                    )
 
         for key, tune_values in [("test-clean", tune_values_clean), ("test-other", tune_values_other)]:
             pick_optimal_params_job = GetOptimalParametersAsVariableJob(
@@ -116,6 +133,16 @@ def eow_phon_ls100_1023_base():
                 test_dataset_tuples={key: test_dataset_tuples[key]},
                 **default_returnn,
             )
+            for key, wer in wers.items():
+                report.add(
+                    {
+                        "train_name": training_name,
+                        "prior_scale": decoder_config.prior_scale.get(),
+                        "lm_scale": decoder_config.lm_weight.get(),
+                        "eval_set": key.split("/")[-1],
+                        "wer": wer,
+                    }
+                )
 
     default_decoder_config = DecoderConfig(
         lexicon=get_text_lexicon(),
@@ -496,3 +523,13 @@ def eow_phon_ls100_1023_base():
             training_name, asr_model, default_decoder_config, lm_scales=[3.5], prior_scales=[0.3, 0.5],
             forward_config={"batch_size": 100 * 16000},
         )
+
+    # finish report
+    report.delete_redundant_columns()
+    report.delete_redundant_rows()
+    report.merge_eval_sets("eval_set")
+    tk.register_report(
+        os.path.join(prefix_name, "report.csv"),
+        values=report.get_values(),
+        template=report.get_template(),
+    )
