ef244fffa290301fb0cf046a5fc8b987a84ffadb
Mohammad Zeineldeen <mzeineldeen@apptek.com>
2024-05-28 12:01:45 -0400

    more

--- users/zeineldeen/data_aug/speed_perturbation_generic.py
+++ users/zeineldeen/data_aug/speed_perturbation_generic.py
@@ -8,13 +8,11 @@ def speed_pert(audio, sample_rate, random_state, min_factor={min_factor}, max_fa
 """
 
 speed_pert_v2 = """
-def speed_pert(audio, sample_rate={sample_rate}, min_factor={min_factor}, max_factor={max_factor}, step={step}):
+def speed_pert(audio, sample_rate={sample_rate}, min_factor={min_factor}, max_factor={max_factor}, step={step}, random_state=numpy.random.RandomState(1)):
   import librosa
-  import numpy
-  
-  random_state = np.random.RandomState(1)
+ 
   new_sample_rate = int(sample_rate * (1 + random_state.randint(min_factor, max_factor) * step))
   if new_sample_rate != sample_rate:
-    audio = librosa.core.resample(audio, sample_rate, new_sample_rate, res_type="kaiser_fast")
+    audio = librosa.resample(audio, orig_sr=sample_rate, target_sr=new_sample_rate, res_type="kaiser_fast", axis=0)
   return audio
 """
