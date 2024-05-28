739d46ddd464bb032164c7b3522d7e44f3670424
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-05-28 09:11:29 +0200

    cleanup

--- users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
@@ -97,23 +97,8 @@ def py():
             train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": alpha}},
         )
 
-    for alpha in [
-        # 0.3,  # very bad?
-        0.7,
-    ]:
-        train_exp(
-            "v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-speedpertV2-spm_bpe10k"
-            f"-spmSample{str(alpha).replace('.', '')}",
-            config_11gb_v6_f32_accgrad1_mgpu4_pavg100_wd1e_4,
-            config_updates={
-                **_get_cfg_lrlin_oclr_by_bs_nep(15_000, 500),
-                "optimizer.weight_decay": 1e-2,
-                "__train_audio_preprocess": speed_pert_librosa_config,
-                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-            },
-            vocab="spm_bpe10k",
-            train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": alpha}},
-        )
+    # v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-speedpertV2
+    # with spm_bpe10k and enable_sampling, alpha in {0.3, 0.7} was both very bad (90% WER).
 
 
 # noinspection PyShadowingNames
