455fc74d6c7fa317965446ea1a8573dcecbaccca
mmz33 <mohammadzed5@gmail.com>
2024-05-31 11:17:11 -0400

    fix

--- users/zeineldeen/data_aug/speed_perturbation_generic.py
+++ users/zeineldeen/data_aug/speed_perturbation_generic.py
@@ -11,7 +11,7 @@ speed_pert_v2 = """
 import numpy
 def speed_pert(audio, sample_rate={sample_rate}, min_factor={min_factor}, max_factor={max_factor}, step={step}, random_state=numpy.random.RandomState(1)):
   import librosa
- 
+
   new_sample_rate = int(sample_rate * (1 + random_state.randint(min_factor, max_factor) * step))
   if new_sample_rate != sample_rate:
     audio = librosa.resample(audio, orig_sr=sample_rate, target_sr=new_sample_rate, res_type="kaiser_fast", axis=0)
