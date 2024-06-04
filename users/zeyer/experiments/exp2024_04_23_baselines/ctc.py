d6c45435fc88761dc09b3770db5760421a16ba55
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-06-04 23:59:03 +0200

    feature batch norm

--- users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
@@ -149,6 +149,20 @@ def py():
         train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
     )
 
+    train_exp(
+        "v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-featBN-speedpertV2-spm10k-spmSample07",
+        config_11gb_v6_f32_accgrad1_mgpu4_pavg100_wd1e_4,
+        model_config={"feature_batch_norm": True},
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep(15_000, 500),
+            "optimizer.weight_decay": 1e-2,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+        },
+        vocab="spm10k",
+        train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+    )
+
 
 # noinspection PyShadowingNames
 def train_exp(
@@ -545,6 +559,10 @@ class Model(rf.Module):
                 vocab_labels, user_defined_symbols={model_recog.output_blank_label: blank_idx}
             )
 
+        self.feature_batch_norm = None
+        if config.bool("feature_batch_norm", False):
+            self.feature_batch_norm = rf.BatchNorm(self.in_dim, affine=False, use_mask=True)
+
         self._specaugment_opts = {
             "steps": config.typed_value("specaugment_steps") or (0, 1000, 2000),
             "max_consecutive_spatial_dims": config.typed_value("specaugment_max_consecutive_spatial_dims") or 20,
@@ -574,6 +592,8 @@ class Model(rf.Module):
             out_dim=self.in_dim,
             sampling_rate=16_000,
         )
+        if self.feature_batch_norm:
+            source = self.feature_batch_norm(source)
         if self._mixup:
             source = self._mixup(source, spatial_dim=in_spatial_dim)
         # SpecAugment
