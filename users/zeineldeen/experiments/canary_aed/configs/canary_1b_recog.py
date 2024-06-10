3e688ba34cc734c21553e745c3c3194af61fc1b0
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2024-06-10 11:58:42 +0000

    add nemo model download job

--- users/zeineldeen/experiments/canary_aed/configs/canary_1b_recog.py
+++ users/zeineldeen/experiments/canary_aed/configs/canary_1b_recog.py
@@ -1,18 +1,24 @@
+from typing import Dict
+
 from sisyphus import *
 
 from i6_core.datasets.huggingface import DownloadAndPrepareHuggingFaceDatasetJob
+from i6_experiments.users.zeineldeen.experiments.canary_aed.nemo.download import DownloadNemoModel
 
-test_sets = ["ami", "earnings22", "gigaspeech"]
+TEST_DATASETS = ["ami", "earnings22", "gigaspeech"]
+MODEL_ID = "nvidia/canary-1b"
 
 
-def download_test_datasets():
+def download_test_datasets() -> Dict[str, tk.Path]:
     # for downloading gigaspeech, a token is required. I login to huggingface and generate a token and then
     # run the command `huggingface-cli login` and paste the token
 
-    for test_set in test_sets:
+    out_dirs = {}
+
+    for test_dataset in TEST_DATASETS:
         j = DownloadAndPrepareHuggingFaceDatasetJob(
             path="open-asr-leaderboard/datasets-test-only",
-            name=test_set,
+            name=test_dataset,
             split="test",
             time_rqmt=24,
             mem_rqmt=4,
@@ -20,8 +26,18 @@ def download_test_datasets():
             mini_task=True,
             token=True,
         )
-        tk.register_output(f"datasets/{test_set}", j.out_dir)
+        out_dirs[test_dataset] = j.out_dir
+        tk.register_output(f"datasets/{test_dataset}", j.out_dir)
+
+    return out_dirs
+
+
+def download_canary_1b_model():
+    j = DownloadNemoModel(model_id=MODEL_ID, device=-1)
+    tk.register_output("canary_1b_nemo_model", j.out_model_dir)
+    return j.out_model_dir
 
 
 def py():
     download_test_datasets()
+    download_canary_1b_model()
