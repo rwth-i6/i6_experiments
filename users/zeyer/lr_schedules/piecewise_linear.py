f23b1177d41c4f5ddc03a38f9bf94e8e2da0b346
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-06-14 22:45:14 +0200

    test_piecewise_linear

--- users/zeyer/lr_schedules/piecewise_linear.py
+++ users/zeyer/lr_schedules/piecewise_linear.py
@@ -25,3 +25,31 @@ def dyn_lr_piecewise_linear(*, global_train_step: int, learning_rate: float, **_
         last_step = step
 
     return learning_rate * lrs[-1]
+
+
+def test_piecewise_linear():
+    from numpy.testing import assert_almost_equal, assert_equal
+
+    def _f(x, xs, ys):
+        assert isinstance(x, int)
+        assert len(xs) + 1 == len(ys)
+        last_step = 0
+        for i, step in enumerate(xs):
+            assert isinstance(step, int)
+            assert step > last_step
+            assert x >= last_step
+            if x < step:
+                factor = (x + 1 - last_step) / (step - last_step)
+                return ys[i + 1] * factor + ys[i] * (1 - factor)
+            last_step = step
+
+        return ys[-1]
+
+    assert_almost_equal(_f(0, [10, 20], [0, 1, 0.5]), 0.1)
+    assert_almost_equal(_f(5, [10, 20], [0, 1, 0.5]), 0.6)
+    assert_equal(_f(9, [10, 20], [0, 1, 0.5]), 1)
+    assert_almost_equal(_f(10, [10, 20], [0, 1, 0.5]), 0.95)
+    assert_almost_equal(_f(11, [10, 20], [0, 1, 0.5]), 0.90)
+    assert_almost_equal(_f(15, [10, 20], [0, 1, 0.5]), 0.70)
+    assert_almost_equal(_f(19, [10, 20], [0, 1, 0.5]), 0.5)
+    assert_equal(_f(20, [10, 20], [0, 1, 0.5]), 0.5)
