01b8c7f1011b0f872a0ac8693625e59b5cd95760
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2024-06-18 12:49:18 +0000

    add beam search

--- users/zeineldeen/experiments/canary_aed/configs/canary_1b_recog.py
+++ users/zeineldeen/experiments/canary_aed/configs/canary_1b_recog.py
@@ -49,10 +49,15 @@ def py():
     dataset_paths = download_test_datasets()
     model_path = download_canary_1b_model()
 
-    search_script = tk.Path(
+    huggface_search_script = tk.Path(
         "/u/zeineldeen/setups/ubuntu_22_setups/2024-06-07--canary-aed/recipe/i6_experiments/users/zeineldeen/experiments/canary_aed/nemo/run_eval.py",
         hash_overwrite="run_eval_v1",
     )
+    our_beam_search_script = tk.Path(
+        "/u/zeineldeen/setups/ubuntu_22_setups/2024-06-07--canary-aed/recipe/i6_experiments/users/zeineldeen/experiments/canary_aed/nemo/run_eval_beam_search.py",
+        hash_overwrite="run_eval_v2",
+    )
+
     # to run canary model, this env has installed nemo toolkit with:
     #   pip3 install git+https://github.com/NVIDIA/NeMo.git@r2.0.0rc0#egg=nemo_toolkit[all]
     # related issue: https://github.com/huggingface/open_asr_leaderboard/issues/26
@@ -76,7 +81,7 @@ def py():
             dataset_path=dataset_paths[test_set],
             dataset_name=test_set,
             split=split,
-            search_script=search_script,
+            search_script=huggface_search_script,
             search_args={"batch_size": 64, "pnc": False, "max_eval_samples": -1},
             python_exe=python_exe,
             device="gpu",
@@ -85,6 +90,30 @@ def py():
             cpu_rqmt=2,
         )
         search_job.rqmt["sbatch_args"] = ["-p", "gpu_24gb"]
-        search_job.add_alias(f"canary_1b/{test_set}_bs64_wo-pnc")
-        tk.register_output(f"canary_1b/{test_set}_bs64_wo-pnc/search_out", search_job.out_search_results)
-        tk.register_output(f"canary_1b/{test_set}_bs64_wo-pnc/wer", search_job.out_wer)
+        search_job.add_alias(f"canary_1b/huggingface/{test_set}_bs64_greedy")
+        tk.register_output(f"canary_1b/huggingface/{test_set}_bs64_greedy/search_out", search_job.out_search_results)
+        tk.register_output(f"canary_1b/huggingface/{test_set}_bs64_greedy/wer", search_job.out_wer)
+
+    # Run with our beam search
+    for beam_size in [4]:
+        for test_set, split in TEST_DATASETS.items():
+            search_job = SearchJob(
+                model_id=MODEL_ID,
+                model_path=model_path,
+                dataset_path=dataset_paths[test_set],
+                dataset_name=test_set,
+                split=split,
+                search_script=our_beam_search_script,
+                search_args={"batch_size": 64, "pnc": False, "max_eval_samples": -1, "beam_size": beam_size},
+                python_exe=python_exe,
+                device="gpu",
+                time_rqmt=24,
+                mem_rqmt=8,
+                cpu_rqmt=2,
+            )
+            search_job.rqmt["sbatch_args"] = ["-p", "gpu_24gb"]
+            search_job.add_alias(f"canary_1b/beam_search_v5/{test_set}_bs64_beam{beam_size}")
+            tk.register_output(
+                f"canary_1b/beam_search_v5/{test_set}_bs64_beam{beam_size}/search_out", search_job.out_search_results
+            )
+            tk.register_output(f"canary_1b/beam_search_v5/{test_set}_bs64_beam{beam_size}/wer", search_job.out_wer)
