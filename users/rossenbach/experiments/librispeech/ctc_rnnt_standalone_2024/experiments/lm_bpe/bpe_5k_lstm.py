75ba307147607abf59e033eafad36aa4ddc3cf9b
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2024-06-28 10:59:48 +0200

    LM updates

--- users/rossenbach/experiments/librispeech/ctc_rnnt_standalone_2024/experiments/lm_bpe/bpe_5k_lstm.py
+++ users/rossenbach/experiments/librispeech/ctc_rnnt_standalone_2024/experiments/lm_bpe/bpe_5k_lstm.py
@@ -62,16 +62,6 @@ def bpe5k_kazuki_lstm():
         init_args=default_init_args,
     )
     
-    lstm_default_init_config = ModelConfig(
-        vocab_dim=vocab_size_without_blank,
-        embed_dim=512,
-        hidden_dim=2048,
-        n_lstm_layers=2,
-        use_bottle_neck=False,
-        dropout=0.2,
-        init_args=None,
-    )
-
     train_config_modern_v1 = {
         "optimizer": {"class": "RAdam"},
         #############
@@ -98,6 +88,37 @@ def bpe5k_kazuki_lstm():
     from ...pipeline import NeuralLM
     add_lm("bpe5k_2x2024_kazuki_lstmlm_2ep", lm_model=NeuralLM(
         checkpoint=train_job.out_checkpoints[200],
+        net_args=train_args["net_args"],
+        network_module=network_module,
+        prefix_name=training_name
+    ))
+
+    train_config_modern_v1 = {
+        "optimizer": {"class": "RAdam"},
+        #############
+        "batch_size": 1280,  # BPE tokens
+        "accum_grad_multiple_step": 1,
+        "gradient_clip_norm": 1.0,
+        "learning_rates": ([1e-3] * 100) + list(np.linspace(1e-3, 1e-5, 200))
+    }
+
+    network_module = "lm.lstm.kazuki_lstm_zijian_variant_v1"
+    train_args = {
+        "config": train_config_modern_v1,
+        "post_config": {"num_workers_per_gpu": 1},
+        "network_module": network_module,
+        "net_args": {"model_config_dict": asdict(lstm_base_config)},
+        "debug": True,
+        "add_cache_manager": True,
+    }
+
+    training_name = prefix_name + "/" + network_module + ".2x2024_1k_RAdam_1e-3_3ep_reduce_gcn1.0"
+    train_job = training(training_name, train_data_bpe5k_part100, train_args, num_epochs=300, **lm_returnn)#
+
+    from ...storage import add_lm
+    from ...pipeline import NeuralLM
+    add_lm("bpe5k_2x2024_kazuki_lstmlm_3ep", lm_model=NeuralLM(
+        checkpoint=train_job.out_checkpoints[300],
         net_args=train_args["net_args"],
         network_module=network_module,
         prefix_name=training_name
