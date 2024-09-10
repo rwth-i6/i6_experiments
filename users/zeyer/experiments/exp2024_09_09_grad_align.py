9a8247fb29db656ab2b6f46af77814d217968d54
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-09-11 00:38:06 +0200

    more

--- users/zeyer/experiments/exp2024_09_09_grad_align.py
+++ users/zeyer/experiments/exp2024_09_09_grad_align.py
@@ -10,9 +10,11 @@ from sisyphus import tk, Job, Task, Path
 
 
 def py():
+    prefix = "exp2024_09_09_grad_align/"
     # * CalculateSilenceStatistics:
     # der job holt sich die word end positions von dem GMM alignment und rechnet die initial und final silence aus
     # * ForcedAlignOnScoreMatrixJob
+    alignment_hdf = None
     for apply_softmax_over_time in [True, False]:
         name = f"grad-align-sm{apply_softmax_over_time}"
         job = ForcedAlignOnScoreMatrixJob(
@@ -26,8 +28,34 @@ def py():
             ),
             apply_softmax_over_time=apply_softmax_over_time,
         )
-        job.add_alias(name)
-        tk.register_output(name, job.out_align)
+        job.add_alias(prefix + name)
+        tk.register_output(prefix + name, job.out_align)
+        if apply_softmax_over_time:
+            alignment_hdf = job.out_align
+
+    gmm_alignment_hdf = Path(
+        "/u/schmitt/experiments/03-09-24_aed_flipped_encoder/work/i6_core/returnn/hdf/ReturnnDumpHDFJob.nQ1YkjerObMO/output/data.hdf"
+    )
+    gmm_alignment_allophones = Path(
+        "/work/common/asr/librispeech/data/sisyphus_export_setup/work/i6_core/lexicon/allophones/StoreAllophonesJob.bY339UmRbGhr/output/allophones"
+    )
+    gmm_alignment_sprint_cache = Path(
+        "/work/common/asr/librispeech/data/sisyphus_work_dir/i6_core/mm/alignment/AlignmentJob.oyZ7O0XJcO20/output/alignment.cache.bundle"
+    )
+    features_sprint_cache = Path(  # for exact timings
+        "/work/common/asr/librispeech/data/sisyphus_work_dir/i6_core/features/extraction/FeatureExtractionJob.VTLN.upmU2hTb8dNH/output/vtln.cache.bundle"
+    )
+
+    name = "calc-alignment-metrics"
+    job = CalcAlignmentMetrics(
+        alignment_hdf=alignment_hdf,
+        features_sprint_cache=features_sprint_cache,
+        ref_alignment_sprint_cache=gmm_alignment_sprint_cache,
+        ref_alignment_allophones=gmm_alignment_allophones,
+        ref_alignment_len_factor=6,
+    )
+    job.add_alias(prefix + name)
+    tk.register_output(prefix + name, job.out_scores)
 
 
 class ForcedAlignOnScoreMatrixJob(Job):
@@ -214,6 +242,133 @@ class ForcedAlignOnScoreMatrixJob(Job):
 class CalcAlignmentMetrics(Job):
     """Calculate alignment metrics, e.g. time-stamp-error (TSE) for word boundaries and for word positions."""
 
-    def __init__(self):
+    def __init__(
+        self,
+        *,
+        alignment_hdf: Path,
+        ref_alignment_sprint_cache: Path,
+        ref_alignment_allophones: Path,
+        ref_alignment_len_factor: int,
+        features_sprint_cache: Optional[Path] = None,  # for exact timings
+        returnn_root: Optional[tk.Path] = None,
+    ):
         super().__init__()
-        # TODO...
+
+        self.alignment_hdf = alignment_hdf
+        self.ref_alignment_sprint_cache = ref_alignment_sprint_cache
+        self.ref_alignment_allophones = ref_alignment_allophones
+        self.ref_alignment_len_factor = ref_alignment_len_factor
+        self.features_sprint_cache = features_sprint_cache
+        self.returnn_root = returnn_root
+
+        self.out_scores = self.output_path("out_scores.json")
+
+    def tasks(self):
+        yield Task("run", rqmt={"cpu": 1, "mem": 4, "time": 1, "gpu": 0})
+
+    def run(self):
+        from typing import List, Tuple
+        import numpy as np
+        import subprocess
+        import i6_experiments
+
+        def _cf(path: Path) -> str:
+            return path.get_path()
+
+            try:
+                return subprocess.check_output(["cf", path.get_path()]).decode(sys.stdout.encoding).strip()
+            except subprocess.CalledProcessError:
+                return path.get_path()
+
+        recipe_dir = os.path.dirname(os.path.dirname(i6_experiments.__file__))
+        sys.path.insert(0, recipe_dir)
+
+        from i6_experiments.users.schmitt.hdf import load_hdf_data
+
+        print("Loading alignment HDF...")
+        alignments = load_hdf_data(Path(_cf(self.alignment_hdf)))
+
+        import i6_core.util as util
+
+        returnn_root = util.get_returnn_root(self.returnn_root)
+
+        sys.path.insert(0, returnn_root.get_path())
+
+        from returnn.sprint.cache import open_file_archive
+
+        print("Loading ref alignment Sprint cache...")
+        ref_align_sprint_cache = open_file_archive(_cf(self.ref_alignment_sprint_cache))
+        print("Loading ref alignment allophones...")
+        ref_align_sprint_cache.set_allophones(_cf(self.ref_alignment_allophones))
+
+        print("Loading features Sprint cache...")
+        features_sprint_cache = open_file_archive(_cf(self.features_sprint_cache))
+
+        def _ceil_div(a: int, b: int) -> int:
+            return -(-a // b)
+
+        def _floor_div(a: int, b: int) -> int:
+            return a // b
+
+        # noinspection PyShadowingNames
+        def _start_end_time_for_align_frame_idx(t: int) -> Tuple[float, float]:
+            # For the downsampling, assume same padding, thus pad:
+            stride = win_size = self.ref_alignment_len_factor
+            pad_total = win_size - 1
+            pad_left = pad_total // 2
+            t0 = t * stride - pad_left  # inclusive
+            t1 = t0 + win_size - 1  # inclusive
+            # Now about the log mel features.
+            window_len = 0.025  # 25 ms
+            step_len = 0.010  # 10 ms
+            sampling_rate = 16_000
+            window_num_frames = int(window_len * sampling_rate)
+            step_num_frames = int(step_len * sampling_rate)
+            t0 *= step_num_frames
+            t1 *= step_num_frames
+            t1 += window_num_frames  # exclusive
+            return max(0.0, t0 / sampling_rate), t1 / sampling_rate
+
+        out_scores = {
+            "per_seq": {"tse_word_boundaries": {}, "tse_word_positions": {}},
+            "avg": {"tse_word_boundaries": {}, "tse_word_positions": {}},
+            "total_num_words": 0,
+        }
+
+        for key, alignment in alignments.items():
+            print("seq tag:", key)
+            feat_times, _ = features_sprint_cache.read(key, typ="feat")
+            ref_align = ref_align_sprint_cache.read(key, typ="align")
+            assert len(feat_times) == len(ref_align), f"feat len {len(feat_times)} vs ref align len {len(ref_align)}"
+            print(f"  start time: {feat_times[0][0]} sec")
+            print(f"  end time: {feat_times[-1][1]} sec")
+            duration_sec = feat_times[-1][1] - feat_times[0][0]
+            sampling_rate = 16_000
+            len_samples = round(duration_sec * sampling_rate)  # 16 kHz
+            print(f"  num samples: {len_samples} (rounded from {duration_sec * sampling_rate} sec)")
+            # RETURNN uses log mel filterbank features, 10ms frame shift, via stft (valid padding)
+            window_len = 0.025  # 25 ms
+            step_len = 0.010  # 10 ms
+            window_num_frames = int(window_len * sampling_rate)
+            step_num_frames = int(step_len * sampling_rate)
+            len_feat = _ceil_div(len_samples - (window_num_frames - 1), step_num_frames)
+            print(f"  num features: {len_feat} (window {window_num_frames} step {step_num_frames})")
+            len_feat_downsampled = _ceil_div(len_feat, self.ref_alignment_len_factor)
+            print(f"  downsampled num features: {len_feat_downsampled} (factor {self.ref_alignment_len_factor})")
+            print(f"  actual align len: {len(alignment)}")
+
+            last_frame_start, align_dur = _start_end_time_for_align_frame_idx(len(alignment) - 1)
+            print(f"  last frame start: {last_frame_start} sec")
+            print(f"  align duration: {align_dur} sec")
+
+            # I'm not really sure on the calculation above, and also not really sure about the limit here...
+            assert (
+                abs(align_dur - duration_sec) < 0.0301
+            ), f"align duration {align_dur} vs duration {duration_sec}, diff {abs(align_dur - duration_sec)}"
+
+            # TODO...
+            # out_scores["total_num_words"] +=
+
+        import json
+
+        json.dump(out_scores, open(self.out_scores.get_path(), "w"))
