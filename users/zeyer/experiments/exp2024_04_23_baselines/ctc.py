26bbcc5da2ad09248d4eba035c22e4a8420f1460
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-10-07 15:46:12 +0200

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/ctc.py
@@ -757,7 +757,8 @@ def py():
             ],
         )
 
-    # Log prob normed gradient (lpNormedGrad) with blank separated (blankSep)
+    # (Baseline without lpNormedGrad: 5.77/6.03)
+    # Log prob normed gradient (lpNormedGrad) (excl blank) with blank separated (blankSep)
     train_exp(  # 6.05 (so lpNormedGrad is worse here, but specifically in combination with blankSep?)
         "v6-relPosAttDef-aedLoss-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-featBN"
         "-speedpertV2-spm10k-bpeSample001-blankSep-lpNormedGrad",
@@ -783,6 +784,37 @@ def py():
             serialization.NonhashedCode(f"sys.path.append({gs.BASE_DIR + '/projects/2024-alignment-analysis'!r})\n")
         ],
     )
+
+    # (Baseline without lpNormedGrad: 5.77/6.03)
+    # Log prob normed gradient (lpNormedGrad) (incl blank) with blank separated (blankSep)
+    train_exp(
+        "v6-relPosAttDef-aedLoss-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-featBN"
+        "-speedpertV2-spm10k-bpeSample001-blankSep-lpNormedGrad",
+        config_11gb_v6_f32_accgrad1_mgpu4_pavg100_wd1e_4,
+        model_config={
+            "enc_conformer_layer": enc_conformer_layer_default,
+            "feature_batch_norm": True,
+            "out_blank_separated": True,
+        },
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep(15_000, 500),
+            "optimizer.weight_decay": 1e-2,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "aux_attention_decoder": rf.build_dict(TransformerDecoder, num_layers=6),  # purely used for training
+            "log_prob_normed_grad": {
+                "func": {"clamp_min": 0.5, "clamp_max": 1.1, "scale_type": "inv_num_labels", "prior_exp": 1.0}
+            },
+            "log_prob_normed_grad_exclude_blank": False,
+        },
+        vocab="spm10k",
+        train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+        epilog=[
+            serialization.NonhashedCode(f"sys.path.append({gs.BASE_DIR + '/projects/2024-alignment-analysis'!r})\n")
+        ],
+    )
+
+    # TODO with prior... as alternative to lpNormedGrad
 
     # ffGated (and also noBias). (Baseline: 5.77)
     # train_exp(  # 6.01, so worse
