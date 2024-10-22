f32602270b0387405dda67133d75370282307996
Albert Zeyer <albzey@gmail.com>
2024-10-22 18:17:45 +0200

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -33,6 +33,9 @@ def py():
     # - more workers for data loading (__multi_proc_dataset_opts.num_workers=25) (check computation time in log!)
     # - __cpu_rqmt = 24 (the whole c23g node has 96 CPUs, and 4 GPUs)
     # - __mem_rqmt = 100 (the whole node should have more than 500GB)
+    # Due to the larger batch size, we have less steps per epoch. With bs200k, it is 2016 steps per full epoch.
+    # Baseline: SubEp21 start: 21660, SubEp40 end: 46627, thus 24967 steps per full epoch.
+    # (But first full epoch has a bit less due to filtering: 21660)
 
     # Baseline: v6-bhv20-11gb-f32-bs15k-accgrad1-mgpu4-pavg100-wd1e_2-lrlin1e_5_295k-speedpertV2-spm10k-spmSample07
     # {"dev-clean": 2.35, "dev-other": 4.98, "test-clean": 2.21, "test-other": 5.49}
@@ -66,6 +69,23 @@ def py():
             "optimizer.weight_decay": 1e-2,
             "__train_audio_preprocess": speed_pert_librosa_config,
             "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+        },
+        post_config_updates={"__multi_proc_dataset_opts": {"num_workers": 25}},
+        vocab="spm10k",
+        train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+    )
+
+    # SpecAugment adapted
+    aed_train_exp(
+        f"96gb-bf16-bs200k-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07",
+        config_96gb_bf16_accgrad1,
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep_v3(200_000, 100),
+            "optimizer.weight_decay": 1e-2,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "specaugment_steps": (500, 1_000, 2_000),
         },
         post_config_updates={"__multi_proc_dataset_opts": {"num_workers": 25}},
         vocab="spm10k",
