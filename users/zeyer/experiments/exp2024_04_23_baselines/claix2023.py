5e0a3f8caed7ddc5f69db52fd4ad5072dbdb30ba
Albert Zeyer <albzey@gmail.com>
2024-10-22 23:09:11 +0200

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -141,6 +141,28 @@ def py():
         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
     )
 
+    # bfloat16A with larger batch and max_seqs=500.
+    aed_train_exp(
+        f"96gb-bf16A-bs400k-bsSeq500-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07",
+        config_96gb_bf16_accgrad1,
+        config_updates={
+            "torch_amp": None,
+            "default_float_dtype": "bfloat16",
+            **_get_cfg_lrlin_oclr_by_bs_nep_v3(400_000, 100),
+            "max_seqs": 500,
+            "optimizer.weight_decay": 1e-2,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "specaugment_steps": (500, 1_000, 2_000),
+        },
+        post_config_updates={"__multi_proc_dataset_opts": {"num_workers": 25}},
+        vocab="spm10k",
+        train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
+    )
+
     # Higher peak LR
     aed_train_exp(
         f"96gb-bf16-bs200k-accgrad1-wd1e_2-lrlinEpCont-lr1e_2-noCrl-speedpertV2-spm10k-spmSample07",
