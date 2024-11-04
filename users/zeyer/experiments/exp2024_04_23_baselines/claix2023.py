4d00eeb97f04fe5cafa603de428c634056117b95
Albert Zeyer <albzey@gmail.com>
2024-11-04 14:08:57 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -86,6 +86,7 @@ def py():
     # TODO instead of having SpecAugment step-based, we can also make this dependent on the continuous epoch
 
     # SpecAugment adapted
+    # {"dev-clean": 2.32, "dev-other": 5.24, "test-clean": 2.41, "test-other": 5.66}
     aed_train_exp(
         f"96gb-bf16-bs200k-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07",
         config_96gb_bf16_accgrad1,
@@ -103,28 +104,15 @@ def py():
     )
 
     # Test default_float_dtype="bfloat16" (bfloat16A) instead of AMP.
+    # (96gb-bf16A-bs200k-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07)
     # Consumes about 40GB of GPU memory.
-    aed_train_exp(
-        f"96gb-bf16A-bs200k-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07",
-        config_96gb_bf16_accgrad1,
-        config_updates={
-            "torch_amp": None,
-            "default_float_dtype": "bfloat16",
-            **_get_cfg_lrlin_oclr_by_bs_nep_v3(200_000, 100, batch_size_factor=_batch_size_factor),
-            "optimizer.weight_decay": 1e-2,
-            "__train_audio_preprocess": speed_pert_librosa_config,
-            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-            "specaugment_steps": (500, 1_000, 2_000),
-        },
-        post_config_updates={"__multi_proc_dataset_opts": {"num_workers": 25}},
-        vocab="spm10k",
-        train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
-        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
-    )
+    # {"dev-clean": 4.37, "dev-other": 10.07, "test-clean": 4.39, "test-other": 10.32}
+    # TODO what's the problem?
 
     # bfloat16A with larger batch.
     # Batch size 400k: OOM after some epochs.
     # bfloat16A with larger batch V2.
+    # {"dev-clean": 4.28, "dev-other": 10.35, "test-clean": 4.22, "test-other": 10.08}
     aed_train_exp(
         f"96gb-bf16A-bs300k-bsSeq400-accgrad1-wd1e_2-lrlinEpCont-noCrl-specAug2k-speedpertV2-spm10k-spmSample07",
         config_96gb_bf16_accgrad1,
@@ -146,23 +134,12 @@ def py():
         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
     )
 
-    # Higher peak LR
-    aed_train_exp(
-        f"96gb-bf16-bs200k-accgrad1-wd1e_2-lrlinEpCont-lr1e_2-noCrl-speedpertV2-spm10k-spmSample07",
-        config_96gb_bf16_accgrad1,
-        config_updates={
-            **_get_cfg_lrlin_oclr_by_bs_nep_v3(200_000, 100, peak_lr=1e-2, batch_size_factor=_batch_size_factor),
-            "optimizer.weight_decay": 1e-2,
-            "__train_audio_preprocess": speed_pert_librosa_config,
-            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-        },
-        post_config_updates={"__multi_proc_dataset_opts": {"num_workers": 25}},
-        vocab="spm10k",
-        train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
-        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
-    )
+    # Higher peak LR (96gb-bf16-bs200k-accgrad1-wd1e_2-lrlinEpCont-lr1e_2-noCrl-speedpertV2-spm10k-spmSample07)
+    # {"dev-clean": 5.62, "dev-other": 12.41, "test-clean": 5.56, "test-other": 12.88} -> bad
 
     # More weight decay
+    # {"dev-clean": 2.4, "dev-other": 5.26, "test-clean": 2.54, "test-other": 5.63}
+    # -> unclear, maybe slightly better?
     aed_train_exp(
         f"96gb-bf16-bs200k-accgrad1-wd5e_2-lrlinEpCont-noCrl-speedpertV2-spm10k-spmSample07",
         config_96gb_bf16_accgrad1,
@@ -224,6 +201,7 @@ def py():
         # This here is now spm512 though.
         # Note: In the original CR paper, they don't have time-downsampling!
         {"num_enc_layers": 16, "batch_size": 10_000, "vocab": "spm512"},
+        {"num_enc_layers": 12, "batch_size": 200_000, "vocab": "spm512"},
     ]:
         for cr_ctc in [None, {"cr_loss_scale": 0.2}]:
             # TODO also adapt specaug for CR...
