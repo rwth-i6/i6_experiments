5e152adb8b66d0faea222ec67d9aff58596ad185
Marten Müller <14896015+Netramm@users.noreply.github.com>
2024-11-07 11:41:31 +0100

    Mueller ctc baselines (#256)
    * Add user mueller
    
    * Add support for training on librispeech-clean-100
    
    * FIrst try to add LM based decoding
    
    * Update LM decoding
    
    * Nearly fix LM loading job
    
    * Fix LM binary creation
    
    * Add lexicon and merge pseude labels into dataset
    
    * Fix LM usage and some pseude label support implementation
    
    * Add hyperparameters and change pseudo label extraction
    
    * Add meta dataset
    
    * Add greedy decoding

---
+++ users/mueller/datasets/utils.py
@@ -0,0 +1,113 @@
+import itertools
+
+from typing import Dict, Tuple, Union, Any, Optional
+
+from i6_core.lib import corpus
+from sisyphus import Job, Task as SisTask, tk
+from i6_core.util import uopen
+
+class CorpusReplaceOrthFromPyDictJob(Job):
+    """
+    Merge HDF pseudo labels back into a bliss corpus
+    """
+
+    def __init__(self, bliss_corpus, recog_words_file, segment_file=None):
+        """
+        :param Path bliss_corpus: Bliss corpus
+        :param Path recog_words_file: a recog_words file
+        :param Path|None segment_file: only replace the segments as specified in the segment file
+        """
+        self.bliss_corpus = bliss_corpus
+        self.recog_words_file = recog_words_file
+        self.segment_file = segment_file
+
+        self.out_corpus = self.output_path("corpus.xml.gz")
+
+    def tasks(self):
+        yield SisTask("run", mini_task=True)
+
+    def run(self):
+        c = corpus.Corpus()
+        c.load(self.bliss_corpus.get_path())
+
+        if self.segment_file:
+            with uopen(self.segment_file.get_path(), "rt") as f:
+                segments_whitelist = set(l.strip() for l in f.readlines() if len(l.strip()) > 0)
+            segment_iterator = filter(lambda s: s.fullname() in segments_whitelist, c.segments())
+        else:
+            segment_iterator = c.segments()
+            
+        # TODO look at returnn/search.py SearchWordsDummyTimesToCTMJob
+        d = eval(uopen(self.recog_words_file, "rt").read(), {"nan": float("nan"), "inf": float("inf")})
+        assert isinstance(d, dict), "Has to be a dict containing the path to the search output file"
+        
+        assert c.fullname() in d["path"], "Corpus not in search output"
+        
+        d = eval(uopen(d["path"][c.fullname()], "rt").read(), {"nan": float("nan"), "inf": float("inf")})
+        assert isinstance(d, dict), "only search output file with dict format is supported"
+        print(f"Words:\n {d}\n\n\n")
+        
+        i = 0
+        for segment in segment_iterator:
+            i += 1
+            assert segment.fullname() in d, f"Segment {segment.fullname()} not in search output"
+            line = d[segment.fullname()]
+            print(f"Segment Orth: {segment.orth}\nNew Orth: {line}")
+            assert len(line) > 0
+            segment.orth = line.strip()
+        assert len(d) == i, f"Number of segments in corpus ({i}) does not match number of segments in search output ({len(d)})"
+
+        c.dump(self.out_corpus.get_path())
+        
+def get_ogg_zip_dict_pseudo_labels(bliss_corpus_dict: Dict[str, tk.Path]) -> Dict[str, tk.Path]:
+    from i6_core.returnn.oggzip import BlissToOggZipJob
+    import os
+
+    ogg_zip_dict = {}
+    for name, bliss_corpus in bliss_corpus_dict.items():
+        ogg_zip_job = BlissToOggZipJob(
+            bliss_corpus,
+            no_audio=True,
+            returnn_python_exe=None,
+            returnn_root=None,
+        )
+        ogg_zip_job.add_alias(os.path.join("datasets", "LibriSpeech-PseudoLabels", "%s_ogg_zip_job" % name.replace('-', '_')))
+        ogg_zip_dict[name] = ogg_zip_job.out_ogg_zip
+
+    return ogg_zip_dict
+
+class MetaDataset():
+    """
+    Represents :class:`MetaDataset` in RETURNN
+
+    Only allows the MetaDataset to be used with an explicit control dataset.
+    """
+
+    def __init__(self,
+                 data_map: Dict[str, Tuple[str, str]],
+                 datasets: Dict[str, Dict],
+                 seq_order_control_dataset: str,
+                 other_opts: Optional[Dict[str, Any]] = None):
+        """
+        :param data_map:
+        :param datasets:
+        :param seq_order_control_dataset:
+        :param dict other_opts:
+        """
+        self.data_map = data_map
+        self.datasets = datasets
+        assert seq_order_control_dataset in datasets
+        self.seq_order_control_dataset = seq_order_control_dataset
+        if other_opts is None:
+            other_opts = {}
+        self.other_opts = other_opts
+
+    def as_returnn_opts(self):
+        d = {
+            'class': 'MetaDataset',
+            'data_map': self.data_map,
+            'datasets': self.datasets,
+            'seq_order_control_dataset': self.seq_order_control_dataset
+        }
+        d.update(self.other_opts)
+        return d
