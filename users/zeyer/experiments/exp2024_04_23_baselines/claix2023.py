0d9c306f7628bac1cacc7f26f7aa326b5d4bf7d0
Albert Zeyer <albzey@gmail.com>
2024-11-08 16:05:39 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -829,6 +829,7 @@ def py():
                 "calculate_exp_loss": True,
             },
         ),
+        post_config={"log_grad_norm": True},
         train_dataset=get_librispeech_lm_dataset(vocab=bpe1k, train_epoch_split=20),
         model_def=ModelDefWithCfg(
             lm_model_def,
@@ -845,6 +846,45 @@ def py():
             },
         ),
         train_def=lm_train_def,
+    )
+
+    train(
+        "lm/trafo-n32-d1024-noAbsPos-rmsNorm-ffGated-rope-noBias-drop0-b2k_30k-laplace100k-shuffleBatch100-bpe1k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v4(100),
+                "batch_size": 30_000,
+                "max_seqs": 2000,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "online_shuffle_batches": 100,
+            },
+        ),
+        post_config={"log_grad_norm": True},
+        train_dataset=get_librispeech_lm_dataset(
+            vocab=bpe1k, train_epoch_split=20, train_sort_laplace_num_seqs=100_000
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=32,
+                    model_dim=1024,
+                    pos_enc=None,
+                    norm=rf.build_dict(rf.RMSNorm),
+                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                    decoder_layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
     )
 
 
