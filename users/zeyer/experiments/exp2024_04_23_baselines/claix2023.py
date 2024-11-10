9377883fbc04c8e86c2c5e65172e438114f2f429
Albert Zeyer <albzey@gmail.com>
2024-11-10 02:35:33 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -430,7 +430,7 @@ def py():
     # ("use_normalized_loss": "seqs")
     # (Note: small bug, now the exp loss is not correctly calculated...)
     # -> CE 3.693, 40.16 PPL, unstable (vs baseline use_normalized_loss:True, CE 3.735, 41.91 PPL, unstable)
-    # Same with shuffleBatch100: ...
+    # Same with shuffleBatch100: -> 38.85 PPL, stable (vs 39.85 PPL, stable) (!)
     train(
         "lm/trafo-n24-d512-gelu-drop0-b2k_80k-laplace100k-shuffleBatch100-spm10k-lossSeqNorm",
         config=dict_update_deep(
@@ -441,6 +441,43 @@ def py():
                 "optimizer.weight_decay": 1e-2,
                 "calculate_exp_loss": True,
                 "use_normalized_loss": "seqs",
+                "online_shuffle_batches": 100,
+            },
+        ),
+        post_config={"log_grad_norm": True},
+        train_dataset=get_librispeech_lm_dataset(
+            vocab="spm10k", train_epoch_split=20, train_sort_laplace_num_seqs=100_000
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    ff_activation=rf.build_dict(rf.gelu),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+
+    # Again no norm (lossNoNorm).
+    train(
+        "lm/trafo-n24-d512-gelu-drop0-b2k_80k-laplace100k-shuffleBatch100-spm10k-lossNoNorm",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(80_000, 100, batch_size_factor=1),
+                "max_seqs": 2_000,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "use_normalized_loss": False,
                 "online_shuffle_batches": 100,
             },
         ),
