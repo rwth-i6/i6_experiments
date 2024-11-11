756908cf3bc9ee80612e84c23ee7a7680a0d5b81
Albert Zeyer <albzey@gmail.com>
2024-11-11 07:25:21 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -714,11 +714,11 @@ def py():
     # 20 -> 42.05 PPL, very unstable...
     #   (and avg grad norm way lower, starts at 1.5, goes fast down to 0.3, so rarely has an effect)
     # Now same with shuffleBatch100 (all are stable unless otherwise noted).
-    # 0.01
+    # 0.01 -> 39.01 PPL
     # 0.1 -> 39.32 PPL, faster convergence than grad clip 5
     # 1 -> 39.77 PPL
     # 5 -> 39.85 PPL
-    for grad_clip in [0.01, 0.1, 1.0, 5.0]:
+    for grad_clip in [1e-4, 1e-3, 0.01, 0.1, 1.0, 5.0]:
         train(
             f"lm/trafo-n24-d512-gelu-drop0-gradClip{grad_clip}-b2k_80k-laplace100k-shuffleBatch100-spm10k",
             config=dict_update_deep(
