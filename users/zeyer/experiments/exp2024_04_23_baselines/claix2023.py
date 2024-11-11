1c8bf011d164b64759d8d9f27dfe4685c95dcc2c
Albert Zeyer <albzey@gmail.com>
2024-11-11 09:45:19 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -646,61 +646,6 @@ def py():
     # shuffleBatch1 -> 43.05 PPL. unstable. (vs 41.91 PPL without warmupBs, also unstable.)
     # shuffleBatch100 -> 39.87 PPL, stable (vs 39.85 PPL without warmupBs, stable), initial convergence faster
 
-    # Llama (noAbsPos-rmsNorm-ffGated-rope-noBias) + optRAdam + lrNoWarmup + warmupBs + lossSeqNorm
-    # TODO also add better grad clip. or fix grad clip, it's wrong here now?
-    # TODO init out layer to all zeros
-    n_ep = 100
-    peak_lr, low_lr, lowest_lr = 1e-3, 1e-5, 1e-6
-    train(
-        "lm/trafo-n24-d512-noAbsPos-rmsNorm-ffGated-rope-noBias-drop0-b2k_80k"
-        "-warmupBs-laplace100k-optRAdam-lrNoWarmup-shuffleBatch100-spm10k-lossSeqNorm",
-        config=dict_update_deep(
-            config_96gb_bf16_accgrad1,
-            {
-                "__num_epochs": n_ep,
-                "batch_size": PiecewiseLinear(
-                    {0: 1_000, 5: 80_000}, kw_name="epoch_continuous", ignore_other_kwargs=True
-                ),
-                "max_seqs": 2_000,
-                "learning_rate": 1.0,
-                "dynamic_learning_rate": dyn_lr_piecewise_linear,
-                "learning_rate_piecewise_by_epoch_continuous": True,
-                "learning_rate_piecewise_steps": [0.45 * n_ep, 0.9 * n_ep, n_ep],
-                "learning_rate_piecewise_values": [peak_lr, peak_lr, low_lr, lowest_lr],
-                "optimizer.class": "RAdam",
-                "optimizer.decoupled_weight_decay": True,
-                "optimizer.weight_decay": 1e-2,
-                "calculate_exp_loss": True,
-                "use_normalized_loss": "seqs",
-                "online_shuffle_batches": 100,
-            },
-        ),
-        post_config={"log_grad_norm": True},
-        train_dataset=get_librispeech_lm_dataset(
-            vocab="spm10k", train_epoch_split=20, train_sort_laplace_num_seqs=100_000
-        ),
-        model_def=ModelDefWithCfg(
-            lm_model_def,
-            {
-                "_model_def_dict": rf.build_dict(
-                    TransformerDecoder,
-                    encoder_dim=None,
-                    num_layers=24,
-                    model_dim=512,
-                    pos_enc=None,
-                    norm=rf.build_dict(rf.RMSNorm),
-                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
-                    decoder_layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
-                    dropout=0.0,
-                    att_dropout=0.0,
-                )
-            },
-        ),
-        train_def=lm_train_def,
-        # avoid oom
-        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
-    )
-
     # Less grad clip.
     # (trafo-n24-d512-gelu-drop0-gradClip10-b2k_80k-laplace100k-spm10k-lossNoNorm)
     # (But values are way off with this lossNoNorm...)
@@ -1051,6 +996,63 @@ def py():
         # avoid oom
         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
     )
+
+    # Llama (noAbsPos-rmsNorm-ffGated-rope-noBias) + optRAdam + lrNoWarmup + warmupBs + lossSeqNorm
+    # (trafo-n24-d512-noAbsPos-rmsNorm-ffGated-rope-noBias-drop0-b2k_80k-warmupBs-laplace100k-optRAdam-lrNoWarmup-shuffleBatch100-spm10k-lossSeqNorm)
+    # -> 39.74 PPL (maybe suboptimal grad clip here)
+    # Now without lossSeqNorm.
+    n_ep = 100
+    peak_lr, low_lr, lowest_lr = 1e-3, 1e-5, 1e-6
+    train(
+        "lm/trafo-n24-d512-noAbsPos-rmsNorm-ffGated-rope-noBias-drop0-b2k_80k"
+        "-warmupBs-laplace100k-optRAdam-lrNoWarmup-shuffleBatch100-spm10k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                "__num_epochs": n_ep,
+                "batch_size": PiecewiseLinear(
+                    {0: 1_000, 5: 80_000}, kw_name="epoch_continuous", ignore_other_kwargs=True
+                ),
+                "max_seqs": 2_000,
+                "learning_rate": 1.0,
+                "dynamic_learning_rate": dyn_lr_piecewise_linear,
+                "learning_rate_piecewise_by_epoch_continuous": True,
+                "learning_rate_piecewise_steps": [0.45 * n_ep, 0.9 * n_ep, n_ep],
+                "learning_rate_piecewise_values": [peak_lr, peak_lr, low_lr, lowest_lr],
+                "optimizer.class": "RAdam",
+                "optimizer.decoupled_weight_decay": True,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "online_shuffle_batches": 100,
+            },
+        ),
+        post_config={"log_grad_norm": True},
+        train_dataset=get_librispeech_lm_dataset(
+            vocab="spm10k", train_epoch_split=20, train_sort_laplace_num_seqs=100_000
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    pos_enc=None,
+                    norm=rf.build_dict(rf.RMSNorm),
+                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                    decoder_layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+
+    # TODO init out layer to all zeros
 
     from i6_experiments.common.datasets.librispeech.vocab import get_subword_nmt_bpe
     from i6_experiments.users.zeyer.datasets.utils.bpe import Bpe
