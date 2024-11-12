3b32471e4b474c8d50ce387e62f991dbcf9aacac
Albert Zeyer <albzey@gmail.com>
2024-11-12 14:29:06 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -708,6 +708,7 @@ def py():
             # avoid oom
             env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
         )
+        del grad_clip
 
     # Try less seqs in batch, b1k_80k (max_seqs=1_000) instead of b2k_80k. Baseline (b2k_80k) has 39.01 PPL.
     # (lm/trafo-n24-d512-gelu-drop0-gradClip0.01-b1k_80k-laplace100k-shuffleBatch100-spm10k)
@@ -742,6 +743,44 @@ def py():
     # Even more grad accum (accgrad100) (no lossNoNorm) such that we cover the laplace100k.
     # (trafo-n24-d512-gelu-drop0-accgrad100-b2k_80k-laplace100k-spm10k)
     # -> 56.60 PPL, but very stable.
+
+    # accgrad2 again, now with shuffleBatch100 and gradClip0.01.
+    train(
+        f"lm/trafo-n24-d512-gelu-drop0-gradClip0.01-b2k_80k-laplace100k-shuffleBatch100-spm10k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(80_000, 100, batch_size_factor=1),
+                "max_seqs": 2_000,
+                "gradient_clip_global_norm": 0.01,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "accum_grad_multiple_step": 2,
+                "online_shuffle_batches": 100,
+            },
+        ),
+        post_config={"log_grad_norm": True},
+        train_dataset=get_librispeech_lm_dataset(
+            vocab="spm10k", train_epoch_split=20, train_sort_laplace_num_seqs=100_000
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    ff_activation=rf.build_dict(rf.gelu),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
 
     # laplace100k is maybe too much. Try laplace10k (train_sort_laplace_num_seqs=10_000).
     # (trafo-n24-d512-gelu-drop0-b2k_80k-accgrad2-laplace10k-spm10k-lossNoNorm)
