52654ba929d47d2e96953837ccbe662a585ad653
Albert Zeyer <albzey@gmail.com>
2024-11-13 11:05:27 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -452,6 +452,36 @@ def py():
         train_def=lm_train_def,
     )
 
+    # Use small batch size, just for reference (will be slow, totally underutilizing the GPU...).
+    train(
+        "lm/trafo-n24-d512-gelu-drop0-b100_5k-spm10k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(5_000, 100, batch_size_factor=1),
+                "max_seqs": 100,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+            },
+        ),
+        train_dataset=get_librispeech_lm_dataset(vocab="spm10k", train_epoch_split=20),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    ff_activation=rf.build_dict(rf.gelu),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+    )
+
     # trafo-n24-d512-gelu-drop0-b2k_80k-laplace100k-spm10k:
     #   train_sort_laplace_num_seqs larger.
     #   -> total num steps: 117,362
