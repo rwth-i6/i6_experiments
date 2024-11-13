db9f4f51620b7b93db12d5f3aed3de247cb98e3f
Albert Zeyer <albzey@gmail.com>
2024-11-13 11:07:15 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -452,7 +452,7 @@ def py():
         train_def=lm_train_def,
     )
 
-    # Use small batch size, just for reference (will be slow, totally underutilizing the GPU...).
+    # Use small batch size (b100_5k), just for reference (will be slow, totally underutilizing the GPU...).
     train(
         "lm/trafo-n24-d512-gelu-drop0-b100_5k-spm10k",
         config=dict_update_deep(
@@ -490,6 +490,39 @@ def py():
     #   -> see shuffleBatch100 below
     #   baseline without laplace100k: 38.69 PPL, stable (first subep already 213.2),
     #     total num steps 166,408, 974 sec / subep
+
+    # Again just for reference, small batch size (b100_5k) with laplace100k + shuffleBatch100.
+    train(
+        "lm/trafo-n24-d512-gelu-drop0-b100_5k-laplace100k-shuffleBatch100-spm10k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(5_000, 100, batch_size_factor=1),
+                "max_seqs": 100,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "online_shuffle_batches": 100,
+            },
+        ),
+        train_dataset=get_librispeech_lm_dataset(
+            vocab="spm10k", train_epoch_split=20, train_sort_laplace_num_seqs=100_000
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    ff_activation=rf.build_dict(rf.gelu),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+    )
 
     # For grad accum dtype, in RETURNN, create returnn/torch/optim/accum_grads_f32.py with:
     """    
