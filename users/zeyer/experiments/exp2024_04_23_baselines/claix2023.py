c9d5d06d5ddfbd3207388cb5c253b0b4fbead57f
Albert Zeyer <albzey@gmail.com>
2024-11-15 15:28:48 +0100

    fun

--- users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/claix2023.py
@@ -1264,6 +1264,48 @@ def py():
         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
     )
 
+    # Alternate laplace and random...
+    train(
+        "lm/trafo-n24-d512-gelu-drop0-b2k_80k-laplace100k_altNone-shuffleBatch100-spm10k",
+        config=dict_update_deep(
+            config_96gb_bf16_accgrad1,
+            {
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(80_000, 100, batch_size_factor=1),
+                "max_seqs": 2_000,
+                "optimizer.weight_decay": 1e-2,
+                "calculate_exp_loss": True,
+                "online_shuffle_batches": 100,
+            },
+        ),
+        post_config={"log_grad_norm": True},
+        train_dataset=get_librispeech_lm_dataset(
+            vocab="spm10k",
+            train_epoch_split=20,
+            train_sort_order=StepFunction(
+                {i: {0: "laplace:.100000", 1: "random"}[i % 2] for i in range(1, 101)},
+                kw_name="epoch",
+                ignore_other_kwargs=True,
+            ),
+        ),
+        model_def=ModelDefWithCfg(
+            lm_model_def,
+            {
+                "_model_def_dict": rf.build_dict(
+                    TransformerDecoder,
+                    encoder_dim=None,
+                    num_layers=24,
+                    model_dim=512,
+                    ff_activation=rf.build_dict(rf.gelu),
+                    dropout=0.0,
+                    att_dropout=0.0,
+                )
+            },
+        ),
+        train_def=lm_train_def,
+        # avoid oom
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+
     # ScaledAdam (optScaledAdam).
     # ("optimizer.class": rf.build_dict(ScaledAdam)["class"], "optimizer.clipping_scale": 2.0)
     # trafo-n24-d512-gelu-drop0-b2k_80k-optScaledAdam-laplace10k-shuffleBatch10-spm10k: 57.62
