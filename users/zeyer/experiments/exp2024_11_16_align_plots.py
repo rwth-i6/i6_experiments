c9f343d5f52334b0a6bb2a44985dd52c879bc264
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-11-18 01:58:37 +0100

    more

--- users/zeyer/experiments/exp2024_11_16_align_plots.py
+++ users/zeyer/experiments/exp2024_11_16_align_plots.py
@@ -16,10 +16,11 @@ Similar as :func:`i6_experiments.users.zeyer.experiments.exp2024_09_16_grad_alig
 """
 
 from __future__ import annotations
-from typing import Optional, Callable
+from typing import Optional, Callable, Tuple, List
 import os
 import sys
 import numpy as np
+import pickle
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib
@@ -97,9 +98,114 @@ def get_grad_scores():
     return score_matrix
 
 
+def get_ref_word_boundaries() -> List[Tuple[float, float, str]]:
+    out_fn_pickle = out_prefix + seq_tag + "/ref_word_boundaries.pkl"
+    if os.path.exists(out_fn_pickle):
+        print(f"Already exists: {out_fn_pickle}")
+        return pickle.load(open(out_fn_pickle, "rb"))
+
+    # adopted from i6_experiments.users.zeyer.experiments.exp2024_09_09_grad_align.CalcAlignmentMetrics
+    from returnn.sprint.cache import open_file_archive
+
+    ref_alignment_allophones = Path(
+        "/work/common/asr/librispeech/data/sisyphus_export_setup/work/i6_core/lexicon/allophones/StoreAllophonesJob.bY339UmRbGhr/output/allophones"
+    )
+    ref_alignment_sprint_cache = Path(
+        "/work/common/asr/librispeech/data/sisyphus_work_dir/i6_core/mm/alignment/AlignmentJob.oyZ7O0XJcO20/output/alignment.cache.bundle"
+    )
+    features_sprint_cache = Path(  # for exact timings
+        "/work/common/asr/librispeech/data/sisyphus_work_dir/i6_core/features/extraction/FeatureExtractionJob.VTLN.upmU2hTb8dNH/output/vtln.cache.bundle"
+    )
+
+    def _cf(fn: Path) -> str:
+        return fn.get_path()
+
+    print("Loading ref alignment Sprint cache...")
+    ref_align_sprint_cache = open_file_archive(_cf(ref_alignment_sprint_cache))
+    print("Loading ref alignment allophones...")
+    ref_align_sprint_cache.set_allophones(_cf(ref_alignment_allophones))
+    allophones = ref_align_sprint_cache.get_allophones_list()
+
+    print("Loading features Sprint cache...")
+    features_sprint_cache = open_file_archive(_cf(features_sprint_cache))
+
+    def _ceil_div(a: int, b: int) -> int:
+        return -(-a // b)
+
+    key = seq_tag
+    key_ref = "train-other-960/" + "/".join(seq_tag.split("/")[1:])
+
+    print("seq tag:", key)
+    feat_times, _ = features_sprint_cache.read(key_ref, typ="feat")
+    ref_align = ref_align_sprint_cache.read(key_ref, typ="align")
+    assert len(feat_times) == len(ref_align), f"feat len {len(feat_times)} vs ref align len {len(ref_align)}"
+    print(f"  start time: {feat_times[0][0]} sec")
+    print(f"  end time: {feat_times[-1][1]} sec")
+
+    ref_start_time = ref_align[0][0]
+    duration_sec = feat_times[-1][1] - ref_start_time
+    sampling_rate = 16_000
+    len_samples = round(duration_sec * sampling_rate)  # 16 kHz
+    print(f"  num samples: {len_samples} (rounded from {duration_sec * sampling_rate})")
+    # RETURNN uses log mel filterbank features, 10ms frame shift, via stft (valid padding)
+    window_len = 0.025  # 25 ms
+    step_len = 0.010  # 10 ms
+    window_num_frames = int(window_len * sampling_rate)
+    step_num_frames = int(step_len * sampling_rate)
+    len_feat = _ceil_div(len_samples - (window_num_frames - 1), step_num_frames)
+    print(f"  num features (10ms): {len_feat} (window {window_num_frames} step {step_num_frames})")
+    # ref_alignment_len_factor =
+    # len_feat_downsampled = _ceil_div(len_feat, self.ref_alignment_len_factor)
+    # print(f"  downsampled num features: {len_feat_downsampled} (factor {self.ref_alignment_len_factor})")
+
+    ref_word_boundaries: List[Tuple[float, float]] = []
+    cur_word_start_frame = None
+    prev_allophone_idx = None
+    ref_words_phones = []
+    num_sil_frames_ref = 0
+    for t, (t_, allophone_idx, hmm_state_idx, _) in enumerate(ref_align):
+        assert t == t_
+        if "[SILENCE]" in allophones[allophone_idx]:
+            num_sil_frames_ref += 1
+            continue
+        if cur_word_start_frame is None:
+            cur_word_start_frame = t  # new word starts here
+            ref_words_phones.append([])
+        if prev_allophone_idx != allophone_idx:
+            ref_words_phones[-1].append(allophones[allophone_idx])
+        if "@f" in allophones[allophone_idx] and (
+            t == len(ref_align) - 1 or ref_align[t + 1][1] != allophone_idx or ref_align[t + 1][2] < hmm_state_idx
+        ):
+            # end of word
+            start_time = feat_times[cur_word_start_frame][0] - ref_start_time
+            end_time = feat_times[t][1] - ref_start_time
+            # take center 10ms of the 25ms window
+            # (or not, as we also don't do for the alignment)
+            # start_time += (window_len - step_len) / 2
+            # end_time -= (window_len - step_len) / 2
+            ref_word_boundaries.append((start_time, end_time))
+            cur_word_start_frame = None
+        prev_allophone_idx = allophone_idx
+    assert cur_word_start_frame is None  # word should have ended
+    assert len(ref_words_phones) == len(ref_word_boundaries)
+
+    ref_word_boundaries_ = [
+        (float(start_time), float(end_time), " ".join(p[: p.index("{")] for p in phones))
+        for (start_time, end_time), phones in zip(ref_word_boundaries, ref_words_phones)
+    ]
+
+    print(f"  num words: {len(ref_word_boundaries)}")
+    print(f"  num silence frames: {num_sil_frames_ref}")
+    print("save to:", out_fn_pickle)
+    pickle.dump(ref_word_boundaries_, open(out_fn_pickle, "wb"))
+
+    return ref_word_boundaries_
+
+
 def plot_audio_features(*, plotter: Optional[Plotter] = None):
     out_fn_pdf = out_prefix + seq_tag + "/audio_features.pdf"
     audio_features = get_audio_features()
+    ref_word_boundaries = get_ref_word_boundaries()
 
     if not plotter:
         plotter = Plotter(plot_at_del=True, out_filename=out_fn_pdf)
@@ -119,7 +225,7 @@ def plot_audio_features(*, plotter: Optional[Plotter] = None):
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plotter.fig.colorbar(mat_, cax=cax, orientation="vertical")
 
-    plotter.add_plot("audio", _plot, rate=100)
+    plotter.add_plot("audio", _plot, ref_word_boundaries, rate=100)
 
 
 def plot_grad_scores(*, plotter: Optional[Plotter] = None):
@@ -165,16 +271,25 @@ class Plotter:
         self.out_filename = out_filename
 
         self.num_figs = 0
-        self.plot_titles = []
-        self.plot_callbacks = []
-        self.plot_rates = []
+        self.plot_titles: List[str] = []
+        self.plot_callbacks: List[Callable] = []
+        self.plot_word_boundaries: List[Optional[List[Tuple[float, float, str]]]] = []
+        self.plot_rates: List[int] = []
 
         self.fig = None
         self.ax = None
 
-    def add_plot(self, title: str, callback: Callable, *, rate: int):
+    def add_plot(
+        self,
+        title: str,
+        callback: Callable,
+        word_boundaries: Optional[List[Tuple[float, float, str]]] = None,
+        *,
+        rate: int,
+    ):
         self.plot_titles.append(title)
         self.plot_callbacks.append(callback)
+        self.plot_word_boundaries.append(word_boundaries)
         self.plot_rates.append(rate)
         self.num_figs += 1
 
@@ -183,10 +298,27 @@ class Plotter:
         if self.num_figs == 1:
             self.ax = [self.ax]
 
-        for i, (title, callback, rate) in enumerate(zip(self.plot_titles, self.plot_callbacks, self.plot_rates)):
+        for i, (title, callback, word_boundaries, rate) in enumerate(
+            zip(self.plot_titles, self.plot_callbacks, self.plot_word_boundaries, self.plot_rates)
+        ):
             ax = self.ax[i]
 
             callback(ax)
+
+            if word_boundaries:
+                for start, end, word in word_boundaries:
+                    # ax.axvline(start * rate, color="black", linestyle="--")
+                    # ax.axvline(end * rate, color="black", linestyle="--")
+                    ax.axvspan(start * rate, end * rate, color="gray", alpha=0.2)
+                    ax.text(
+                        (start + end) * rate / 2,
+                        ax.get_ylim()[1],
+                        word,
+                        rotation=90,
+                        verticalalignment="bottom",
+                        horizontalalignment="center",
+                        fontsize=12,
+                    )
 
             ax.set_xlabel("time (sec)")
             ticks = ax.get_xticks() / rate
