bd16708cbb8a9c5a0d5360ef7b39d34ddf0bb3b0
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-11-20 14:20:00 +0100

    more

--- users/zeyer/experiments/exp2024_09_16_grad_align.py
+++ users/zeyer/experiments/exp2024_09_16_grad_align.py
@@ -292,7 +292,7 @@ def py():
                 for ctc_scale in [0]  # [0, 0.1, 0.2, 0.3, 1.0]
             ],
             (
-                "-blankStopGrad-inclBlankState-p0.1-shift-10-am1.0-prior1.0",
+                "-shift-10-am1.0-prior1.0-blankStopGrad-inclBlankState-p0.1",
                 {
                     "stop_grad_blank": True,
                     "ctc_partial_scores_include_next_blank": True,
@@ -303,6 +303,57 @@ def py():
                     "ctc_am_scale": 1.0,
                     "ctc_prior_scale": 1.0,
                 },
+            ),
+            *(
+                [
+                    (
+                        "-lpNormedGradUsed-blankStopGrad-inclBlankState-p0.1",
+                        {
+                            "log_prob_normed_grad": {
+                                "lpNormedGradC05_11P1": {
+                                    "func": {
+                                        "clamp_min": 0.5,
+                                        "clamp_max": 1.1,
+                                        "scale_type": "inv_num_labels",
+                                        "prior_exp": 1.0,
+                                    }
+                                }
+                            }[shortname],
+                            "_log_prob_normed_grad_mod_import_hack": _HackImportAlignmentModule(),
+                            "_log_prob_normed_grad_version": 2,
+                            "stop_grad_blank": True,
+                            "ctc_partial_scores_include_next_blank": True,
+                            "grad_norm_p": 0.1,
+                        },
+                    ),
+                    (
+                        "-lpNormedGradUsed-shift-10-am1.0-prior1.0-blankStopGrad-inclBlankState-p0.1",
+                        {
+                            "blank_logit_shift": -10,
+                            "ctc_prior_type": "static",
+                            "static_prior": {"type": "prob", "file": prior_stats.mean},
+                            "ctc_am_scale": 1.0,
+                            "ctc_prior_scale": 1.0,
+                            "log_prob_normed_grad": {
+                                "lpNormedGradC05_11P1": {
+                                    "func": {
+                                        "clamp_min": 0.5,
+                                        "clamp_max": 1.1,
+                                        "scale_type": "inv_num_labels",
+                                        "prior_exp": 1.0,
+                                    }
+                                }
+                            }[shortname],
+                            "_log_prob_normed_grad_mod_import_hack": _HackImportAlignmentModule(),
+                            "_log_prob_normed_grad_version": 2,
+                            "stop_grad_blank": True,
+                            "ctc_partial_scores_include_next_blank": True,
+                            "grad_norm_p": 0.1,
+                        },
+                    ),
+                ]
+                if "lpNormedGrad" in shortname
+                else []
             ),
         ]:
             grad_opts = grad_opts.copy()
@@ -321,23 +372,6 @@ def py():
                     **({"fixed_blank_sep_v1": True} if "blankSep" in shortname else {}),
                     **grad_opts,
                     **({"batch_size": 5_000 * _batch_size_factor} if shortname == "ebranchformer" else {}),
-                    **(
-                        {
-                            "log_prob_normed_grad": {
-                                "lpNormedGradC05_11P1": {
-                                    "func": {
-                                        "clamp_min": 0.5,
-                                        "clamp_max": 1.1,
-                                        "scale_type": "inv_num_labels",
-                                        "prior_exp": 1.0,
-                                    }
-                                }
-                            }[shortname],
-                            "_log_prob_normed_grad_mod_import_hack": _HackImportAlignmentModule(),
-                        }
-                        if "lpNormedGrad" in shortname
-                        else {}
-                    ),
                 },
             )
             if shortname == "blankSep":
