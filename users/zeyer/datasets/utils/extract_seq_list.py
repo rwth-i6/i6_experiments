deee8f66ce52e47752dcbf330e9edd435729756a
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-11-29 16:09:31 +0100

    ExtractNumLinesFromTextFileJob and WriteLmDatasetSeqListJob

--- users/zeyer/datasets/utils/extract_seq_list.py
+++ users/zeyer/datasets/utils/extract_seq_list.py
@@ -1,5 +1,10 @@
-from typing import Optional, Any, Dict
+"""
+Extract/generate seq lists.
+"""
+
+from typing import Optional, Union, Any, Dict
 from sisyphus import tk, Task, Job
+from i6_core.util import uopen
 
 
 class ExtractSeqListJob(Job):
@@ -56,3 +61,51 @@ class ExtractSeqListJob(Job):
         with open(self.out_seq_list.get_path(), "w") as f:
             for seq_tag in dataset.get_all_tags():
                 print(seq_tag, file=f)
+
+
+class ExtractNumLinesFromTextFileJob(Job):
+    """
+    Extracts the number of lines from a (potentially gzipped) text file.
+    """
+
+    def __init__(self, *, text_file: tk.Path, skip_empty_lines: bool = True):
+        self.text_file = text_file
+        self.skip_empty_lines = skip_empty_lines
+        self.out_num_lines = self.output_var("out_num_lines.txt")
+
+    def tasks(self):
+        yield Task("run", rqmt={"cpu": 1, "mem": 4, "time": 1, "gpu": 0})
+
+    def run(self):
+        num_lines = 0
+        with uopen(self.text_file.get_path(), "rt") as f:
+            for line in f:
+                if self.skip_empty_lines and not line.strip():
+                    continue
+                num_lines += 1
+        self.out_num_lines.set(num_lines)
+
+
+class WriteLmDatasetSeqListJob(Job):
+    """
+    LmDataset has simple seq tags like "line-{line_nr}".
+    So to write out the seq list, we just need to know the number of lines.
+    """
+
+    def __init__(self, *, num_lines: Union[int, tk.Variable]):
+        super().__init__()
+        assert isinstance(num_lines, (int, tk.Variable))
+        self.num_lines = num_lines
+        self.out_seq_list = self.output_path("out_seq_list.txt")
+
+    def tasks(self):
+        yield Task("run", rqmt={"cpu": 1, "mem": 4, "time": 1, "gpu": 0})
+
+    def run(self):
+        num_lines = self.num_lines
+        if isinstance(num_lines, tk.Variable):
+            num_lines = num_lines.get()
+        assert isinstance(num_lines, int)
+        with open(self.out_seq_list.get_path(), "w") as f:
+            for line_nr in range(num_lines):
+                print(f"line-{line_nr}", file=f)
