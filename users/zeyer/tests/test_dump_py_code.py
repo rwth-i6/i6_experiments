d67fb6dc8b70002280724e06793b40ac1decfce1
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-12-05 15:55:45 +0100

    test dump_py_code, test frozen dataclasses

--- users/zeyer/tests/test_dump_py_code.py
+++ users/zeyer/tests/test_dump_py_code.py
@@ -7,6 +7,8 @@ Als see test_serialization_v2 for very similar tests.
 from typing import Any
 from textwrap import dedent
 from io import StringIO
+import dataclasses
+
 from i6_experiments.common.utils.dump_py_code import PythonCodeDumper
 
 
@@ -14,7 +16,11 @@ def serialize(obj: Any) -> str:
     out = StringIO()
     dumper = PythonCodeDumper(file=out)
     dumper.dump(obj, lhs="obj")
-    return out.getvalue()
+    code = out.getvalue()
+    print("*** generated code: {")
+    print(code)
+    print("*** }")
+    return code
 
 
 def test_basic():
@@ -39,7 +45,6 @@ def test_builtin():
         }
         """
     )
-    print(code)
     scope = {}
     exec(code, scope)
     assert scope["obj"]["func"] is sum
@@ -52,7 +57,6 @@ def _demo_func(*args):
 def test_func_in_dict():
     d = {"func": _demo_func}
     code = serialize(d)
-    print(code)
     scope = {}
     exec(code, scope)
     assert scope["obj"]["func"] is _demo_func
@@ -60,7 +64,41 @@ def test_func_in_dict():
 
 def test_func():
     code = serialize(_demo_func)
-    print(code)
     scope = {}
     exec(code, scope)
     assert scope["obj"] is _demo_func
+
+
+@dataclasses.dataclass
+class _DemoData:
+    value: int
+
+
+def test_dataclass():
+    obj = _DemoData(42)
+    code = serialize(obj)
+    scope = {}
+    exec(code, scope)
+    obj_ = scope["obj"]
+    assert obj_ is not obj
+    assert isinstance(obj_, _DemoData)
+    assert obj_.value == 42
+    assert obj_ == obj
+
+
+@dataclasses.dataclass(frozen=True)
+class _FrozenDemoData:
+    value: int
+
+
+def test_dataclass_frozen():
+    obj = _FrozenDemoData(42)
+    code = serialize(obj)
+    print(code)
+    scope = {}
+    exec(code, scope)
+    obj_ = scope["obj"]
+    assert obj_ is not obj
+    assert isinstance(obj_, _FrozenDemoData)
+    assert obj_.value == 42
+    assert obj_ == obj
