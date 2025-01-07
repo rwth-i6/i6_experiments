87595e3cde6e1d40dbf7168f47feefdaf634d774
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2025-01-07 10:53:08 +0100

    move analyze_recog script to i6_experiments

---
+++ users/zeyer/sis_tools/analyze_recog.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python3
+
+"""
+Given some (whatever) job (e.g. JoinScoreResultsJob)
+or job output,
+go through its inputs
+to find the search jobs (e.g. "i6_core/returnn/forward/ReturnnForwardJobV2.06Moa9am7DMj")
+and scoring jobs (e.g. "i6_core/recognition/scoring/ScliteJob.OcYJiwXkWAUI").
+Analyze the results:
+- Find
+"""
+
+from __future__ import annotations
+import argparse
+import os
+import sys
+import re
+from functools import reduce
+
+# It will take the dir of the checked out git repo.
+# So you can also only use it there...
+_my_dir = os.path.dirname(os.path.realpath(__file__))
+_base_recipe_dir = reduce(lambda p, _: os.path.dirname(p), range(4), _my_dir)
+_setup_base_dir = os.path.dirname(_base_recipe_dir)
+_sis_dir = f"{_setup_base_dir}/tools/sisyphus"
+
+
+def _setup():
+    # In case the user started this script directly.
+    if not globals().get("__package__"):
+        globals()["__package__"] = "i6_experiments.users.zeyer.sis_tools"
+        if _base_recipe_dir not in sys.path:
+            sys.path.append(_base_recipe_dir)
+        if _sis_dir not in sys.path:
+            sys.path.append(_sis_dir)
+
+        os.environ["SIS_GLOBAL_SETTINGS_FILE"] = f"{_setup_base_dir}/settings.py"
+
+        try:
+            import sisyphus  # noqa
+            import i6_experiments  # noqa
+        except ImportError:
+            print("setup base dir:", _setup_base_dir)
+            print("sys.path:")
+            for path in sys.path:
+                print(f"  {path}")
+            raise
+
+
+_setup()
+
+
+from . import sis_common
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("job", help="path to job, with or without 'work/' prefix")
+    args = arg_parser.parse_args()
+    job = sis_common.get_job_from_arg(args.job)
+
+    visited = set()
+    # (job, corpus, scoring)
+    queue = [(job, None, None)]
+    scoring_per_corpus = {}
+    search_per_corpus = {}
+    while queue:
+        job, corpus, scoring = queue.pop(0)
+        if job in visited:
+            continue
+        visited.add(job)
+        print(f"{corpus or '?'}: {job}")
+        if "/JoinScoreResultsJob." in job:
+            _report_join_score_results_job(job)
+        elif job.startswith("i6_core/recognition/scoring/ScliteJob."):
+            print(
+                f"* {corpus or '?'}: Found scoring. Aliases:", " ".join(sis_common.get_job_aliases(job) or ["<None?>"])
+            )
+            scoring = job
+            scoring_per_corpus[corpus] = job
+        elif job.startswith("i6_core/returnn/forward/ReturnnForwardJobV2."):
+            print(
+                f"* {corpus or '?'}: Found search. Aliases:", " ".join(sis_common.get_job_aliases(job) or ["<None?>"])
+            )
+            search_per_corpus[corpus] = job
+            continue  # deps of this don't matter
+        if not scoring:
+            for corpus_, job_ in sis_common.get_inputs_per_key(job).items():
+                queue.append((job_, corpus if scoring else corpus_, scoring))
+        for job_ in sis_common.get_inputs(job):
+            queue.append((job_, corpus, scoring))
+
+    if not scoring_per_corpus and not search_per_corpus:
+        print("Nothing found. Exit.")
+        sys.exit(1)
+
+    for corpus, sclite_job in scoring_per_corpus.items():
+        sclite_pra_report_worst_seqs(corpus or "?", sclite_job)
+
+    for corpus, search_job in search_per_corpus.items():
+        search_log_report_timings_summary(corpus or "?", search_job)
+
+
+def _report_join_score_results_job(job: str):
+    fn = sis_common.get_work_dir_prefix() + job + "/output/score_results.json"
+    if os.path.exists(fn):
+        with open(fn) as f:
+            print("joined score results:", f.read().strip())
+
+
+def sclite_pra_report_worst_seqs(
+    prefix: str,
+    sclite_job: str,
+    *,
+    n: int = 2,
+    broken_seq_threshold_rel: float = 0.5,
+    broken_seq_threshold_abs: int = 10,
+):
+    fn = sis_common.get_work_dir_prefix() + sclite_job + "/output/reports/sclite.pra"
+    print(f"({prefix}: read {fn})")
+    with open(fn) as f:
+        lines = f.read().splitlines()
+    total_ref_num_token = 0
+    total_num_err = 0
+    total_num_sub = 0
+    total_num_del = 0
+    total_num_ins = 0
+    total_num_seqs = 0
+    total_broken_num_seqs = 0
+    total_broken_seq_len = 0
+    worst_seqs = []  # (num_err, cur_seq)
+    cur_seq = []
+    for line in lines:
+        if not line.strip():
+            cur_seq = []
+        else:
+            cur_seq.append(line)
+        if line.startswith("Scores: (#C #S #D #I) "):
+            line = line[len("Scores: (#C #S #D #I) ") :]
+            num_correct, num_err_sub, num_err_del, num_err_ins = map(int, line.split())
+            ref_num_token = num_correct + num_err_sub + num_err_del
+            assert ref_num_token > 0, f"unexpected empty: line: {line}\n" + "\n".join(cur_seq)
+            total_ref_num_token += ref_num_token
+            num_err = num_err_sub + num_err_del + num_err_ins
+            total_num_err += num_err
+            total_num_sub += num_err_sub
+            total_num_del += num_err_del
+            total_num_ins += num_err_ins
+            total_num_seqs += 1
+            if len(worst_seqs) < n or worst_seqs[-1][0] < num_err:
+                worst_seqs.append((num_err, cur_seq))
+                worst_seqs.sort(key=lambda item: -item[0])
+                worst_seqs = worst_seqs[:n]
+            if num_err / ref_num_token >= broken_seq_threshold_rel and num_err >= broken_seq_threshold_abs:
+                total_broken_num_seqs += 1
+                total_broken_seq_len += ref_num_token
+    print(
+        f"{prefix}: WER: {total_num_err / total_ref_num_token * 100.:.2f}%"
+        f" (sub: {total_num_sub / total_ref_num_token * 100.:.2f}%,"
+        f" del {total_num_del / total_ref_num_token * 100.:.2f}%,"
+        f" ins {total_num_ins / total_ref_num_token * 100.:.2f}%)"
+    )
+    print(
+        f"{prefix}: num broken (>={broken_seq_threshold_rel*100.:.0f}% WER, >={broken_seq_threshold_abs} errs) seqs:"
+        f" {total_broken_num_seqs}/{total_num_seqs} = {total_broken_num_seqs/total_num_seqs*100.:.1f}%,"
+        f" avg seq len: {total_broken_seq_len / total_broken_num_seqs if total_broken_num_seqs else 0:.1f} words"
+    )
+    print(f"{prefix}: worst {len(worst_seqs)} seqs:")
+    for num_err, cur_seq in worst_seqs:
+        print(f"--- ({num_err} errors):")
+        # TODO would be good if we can also find the individual seq scores in search ext output for this seq...
+        print("\n".join(cur_seq))
+    print("---")
+
+
+def search_log_report_timings_summary(prefix: str, search_job: str):
+    with sis_common.open_job_log(search_job) as (f, log_fn):
+        print(f"({prefix}: opened log {log_fn})")
+        lines = f.read().splitlines()
+    data_len_frame_factor = None
+    data_len_num_frames = 0
+    enc_num_frames = 0
+    dec_num_frames = 0
+    time_enc_ns = 0
+    time_dec_ns = 0
+    num_steps = 0
+    dev_ls_s = []
+    for line in lines:
+        if line.startswith("Hostname: ") or line.startswith("Using gpu device ") or line.startswith("Total GPU "):
+            dev_ls_s.append(line)
+            continue
+        if not line.startswith("TIMINGS: "):
+            continue
+        # example:
+        # TIMINGS: batch size 2, data len max 369120 (23.07 secs), data len sum 736960 (46.06 secs),
+        #   enc 89626378 ns, enc len max 385, dec 2212957542 ns, out len max 77
+        line = line[len("TIMINGS: ") :]
+        parts = line.split(", ")
+        d = {
+            # key -> abs num, opt time in secs, opt unit
+            m.group(1): (int(m.group(2)), float(m.group(4)) if m.group(4) else None, m.group(5))
+            for part in parts
+            for m in [re.match(r"^([a-z ]+) ([0-9.]+) ?(\(([0-9.]+) secs\))? ?([a-z]+)?$", part)]
+        }
+        if data_len_frame_factor is None:
+            print(f"({prefix}: first timing log entry: {line})")
+            num_frames, time_in_secs, _ = d["data len sum"]
+            data_len_frame_factor = int(round(num_frames / time_in_secs, -2))
+            print(
+                f"{prefix}: timings first batch data len sum:"
+                f" {num_frames} frames, {time_in_secs} secs -> factor {data_len_frame_factor}"
+            )
+        data_len_num_frames += d["data len sum"][0]
+        time_enc_ns += d["enc"][0]
+        time_dec_ns += d["dec"][0]
+        enc_num_frames += d["enc len max"][0] * d["batch size"][0]
+        dec_num_frames += d["out len longest sum"][0]
+        num_steps += 1
+    if data_len_frame_factor is None:
+        print(f"{prefix} timings ERROR, none found")
+        return
+    data_len_secs = data_len_num_frames / data_len_frame_factor
+    time_enc_secs = time_enc_ns * 1e-9
+    time_dec_secs = time_dec_ns * 1e-9
+    print(f"{prefix}: timings total:")
+    print(f"  num steps: {num_steps}")
+    print(f"  data len: {data_len_secs:.2f} secs")
+    print(f"  encoder num frames: {enc_num_frames}")
+    print(f"  decoder num frames: {dec_num_frames} (tokens)")
+    print(f"  Host/GPU: {', '.join(dev_ls_s) or '<unknown>'}")
+    print(f"  encoder time: {time_enc_secs:.2f} secs")
+    print(f"  decoder time: {time_dec_secs:.2f} secs")
+    print(f"  RTF: {(time_enc_secs + time_dec_secs) / data_len_secs:.4f}")
+
+
+if __name__ == "__main__":
+    try:
+        import better_exchook
+
+        better_exchook.install()
+    except ImportError:
+        better_exchook = None
+    main()
---
+++ users/zeyer/sis_tools/sis_common.py
@@ -0,0 +1,439 @@
+"""
+Copied from sis-setup-utils, adapted here.
+
+Many functions are already moved elsewhere, e.g. to :mod:`sis_path`,
+but this here is to ease the transition of other sis-setup-utils scripts.
+"""
+
+from __future__ import annotations
+from typing import Optional, Any, List, Tuple, TextIO
+from contextlib import contextmanager
+from collections.abc import Mapping
+import sys
+import os
+import re
+from dataclasses import dataclass
+from functools import reduce
+
+
+_my_dir = os.path.dirname(__file__)
+_base_recipe_dir = reduce(lambda p, _: os.path.dirname(p), range(4), _my_dir)
+_setup_base_dir = os.path.dirname(_base_recipe_dir)
+_checked_setup_base_dir = False
+
+
+def get_setup_base_dir() -> str:
+    """
+    :return: The setup base dir, where there is work and recipe.
+    """
+    global _checked_setup_base_dir
+    if not _checked_setup_base_dir:
+        assert os.path.exists(f"{_setup_base_dir}/work") and os.path.exists(f"{_setup_base_dir}/recipe")
+        _checked_setup_base_dir = True
+    return _setup_base_dir
+
+
+def get_work_dir() -> str:
+    from i6_experiments.users.zeyer.utils import sis_path
+
+    return sis_path.get_work_dir()
+
+
+def get_work_dir_prefix() -> str:
+    from i6_experiments.users.zeyer.utils import sis_path
+
+    return sis_path.get_work_dir_prefix()
+
+
+def get_work_dir_prefix2() -> str:
+    from i6_experiments.users.zeyer.utils import sis_path
+
+    return sis_path.get_work_dir_prefix2()
+
+
+def get_job_aliases(job: str) -> Optional[List[str]]:
+    """
+    :param job: without "work/" prefix
+    """
+    from i6_experiments.users.zeyer.utils import job_aliases_from_log
+
+    return job_aliases_from_log.get_job_aliases(job)
+
+
+@contextmanager
+def open_job_log(job: str, task: str = "run", index: int = 1) -> Tuple[Optional[TextIO], Optional[str]]:
+    from i6_experiments.users.zeyer.utils import job_log
+
+    with job_log.open_recent_job_log(job, task=task, index=index) as v:
+        yield v
+
+
+def get_inputs(job: str) -> list[str]:
+    work_dir_prefix = get_work_dir_prefix()
+    work_dir_prefix2 = get_work_dir_prefix2()
+    inputs = []
+    with open(work_dir_prefix + job + "/info") as f:
+        for line in f.read().splitlines():
+            key, value = line.split(": ", 1)
+            if key != "INPUT":
+                continue
+            if not (value.startswith(work_dir_prefix) or value.startswith(work_dir_prefix2)):
+                continue
+            inputs.append(get_job_from_work_output(value))
+    return inputs
+
+
+def get_inputs_per_key(job: str) -> dict[str, str]:
+    """
+    param job: e.g. JoinScoreResultsJob
+    """
+    work_dir_prefix = get_work_dir_prefix()
+    info_params = []
+    with open(work_dir_prefix + job + "/info") as f:
+        for line in f.read().splitlines():
+            if line.startswith("PARAMETER: "):
+                info_params.append(parse_info_parameter(line))
+    return collect_inputs_per_key(info_params)
+
+
+def get_job_from_work_output(filename: str, *, allow_none: bool = False) -> Optional[str]:
+    work_dir_prefix = get_work_dir_prefix()
+    work_dir_prefix2 = get_work_dir_prefix2()
+    if filename.startswith(work_dir_prefix):
+        filename = filename[len(work_dir_prefix) :]
+    elif filename.startswith(work_dir_prefix2):
+        filename = filename[len(work_dir_prefix2) :]
+    elif filename.startswith("work/") and os.path.exists(get_setup_base_dir() + "/" + filename):
+        filename = filename[len("work/") :]
+    else:
+        if allow_none:
+            return None
+        raise ValueError(f"invalid {filename=}")
+    s = 0
+    while True:
+        f = filename.find("/", s)
+        if f < 0:
+            if allow_none:
+                return None
+            raise ValueError(f"No job found from {filename=}")
+        if is_job_dir(filename[:f]):
+            return filename[:f]
+        s = f + 1
+
+
+def is_job_dir(job: str) -> bool:
+    """
+    :param job: without "work/" prefix
+    """
+    d = get_work_dir_prefix() + job
+    if not os.path.isdir(d):
+        return False
+    if not os.path.isfile(d + "/info"):
+        return False
+    if not os.path.isdir(d + "/output"):
+        return False
+    return True
+
+
+def get_job_from_arg(job: str) -> str:
+    """
+    :param job: job path, job name, job output
+    :return: job, such that is_job_dir(job) is True
+    """
+    work_dir_prefix = get_work_dir_prefix()
+    if job.startswith("work/"):
+        job = job[len("work/") :]
+    elif job.startswith(work_dir_prefix):
+        job = job[len(work_dir_prefix) :]
+    elif job.startswith("output/"):
+        job = get_job_from_work_output(os.path.realpath(job))
+    elif job.startswith("Hub5ScoreJob."):  # small shortcut, special case
+        job = "i6_core/recognition/scoring/" + job
+    elif job.startswith("ReturnnSearchJobV2."):  # shortcut
+        job = "i6_core/returnn/search/" + job
+    assert os.path.exists(work_dir_prefix + job), f"job '{work_dir_prefix}{job}' does not exist"
+    assert os.path.exists(work_dir_prefix + job + "/info")
+    assert os.path.exists(work_dir_prefix + job + "/output")
+    return job
+
+
+def _get_sisyphus_dir() -> str:
+    setup_base_dir = get_setup_base_dir()
+    potential_dirs = []
+    if os.path.islink(f"{setup_base_dir}/sis"):
+        potential_dirs.append(os.path.dirname(os.readlink(f"{setup_base_dir}/sis")))
+    potential_dirs.extend([f"{setup_base_dir}/tools/sisyphus", f"{setup_base_dir}/ext/sisyphus"])
+    for d in potential_dirs:
+        if (
+            os.path.exists(f"{d}/sisyphus")
+            and os.path.exists(f"{d}/sisyphus/__init__.py")
+            and os.path.exists(f"{d}/sis")
+        ):
+            return d
+    # Fallback: Try global import.
+    import sisyphus
+
+    return sisyphus.__file__
+
+
+def _setup_sisyphus():
+    """
+    Some setup for Sisyphus. This is currently not used anymore.
+    Setting SIS_GLOBAL_SETTINGS_FILE might still be useful.
+    I'm not sure about _dummy_recog_training_exp, where this was needed/useful.
+    """
+    sis_dir = _get_sisyphus_dir()
+    if sis_dir not in sys.path:
+        sys.path.append(sis_dir)
+
+    setup_base_dir = get_setup_base_dir()
+    sys.path.append(setup_base_dir + "/recipe")
+    os.environ["SIS_GLOBAL_SETTINGS_FILE"] = setup_base_dir + "/settings.py"
+
+    import sisyphus  # noqa
+
+    import i6_experiments.users.zeyer.recog
+
+    i6_experiments.users.zeyer.recog.recog_training_exp = _dummy_recog_training_exp
+
+
+def _dummy_recog_training_exp(prefix_name: str, task, model, recog_def, *args, **kwargs):
+    from i6_experiments.users.zeyer.model_interfaces import ModelWithCheckpoints
+
+    assert isinstance(model, ModelWithCheckpoints)
+    import sisyphus.toolkit as tk
+
+    # ignore the recog jobs, register some dep on the train job
+    tk.register_output(prefix_name + "/train-scores", model.scores_and_learning_rates)
+
+
+def parse_info_parameter(info_line: str) -> ParsedInfoParameter:
+    """
+    :param info_line: line from Sis job info file
+    """
+    s = info_line
+    assert s.startswith("PARAMETER: ")
+    s = s[len("PARAMETER: ") :]
+    name, s = s.split(": ", 1)
+    if not s:
+        return ParsedInfoParameter(name, None)
+    if s.startswith("/") and (os.path.exists(s) or " " not in s):  # heuristic
+        return ParsedInfoParameter(name, ParsedInfoParameterPath(s))
+    # match to sth like "<i6_experiments.users.zeyer.model_interfaces.ModelDefWithCfg object at 0x7f01c870c490>"
+    pattern = re.compile(r"<(\S+\.)?(\S+) object at 0x[0-9a-f]+>")
+    if "<Path" in s or "<Variable" in s or s[:1] in "[({" or s.isnumeric() or pattern.search(s):  # heuristic
+        s = re.sub(r"<Variable (\S+) (\S+)>", r"_Variable('\1', \2)", s)
+        s = re.sub(r"<Path (\S+)>", r"_Path('\1')", s)
+        s = pattern.sub("'\2'", s)
+        try:
+            content = eval(
+                s, {"_Variable": ParsedInfoParameterVariable, "_Path": ParsedInfoParameterPath}, _AutoLocals()
+            )
+        except Exception as exc:
+            raise ValueError("unhandled:\n" + s) from exc
+        return ParsedInfoParameter(name, content)
+    # unknown, random string, but does not really matter
+    return ParsedInfoParameter(name, s)
+
+
+@dataclass
+class ParsedInfoParameter:
+    """info parameter"""
+
+    name: str
+    content: Any
+
+
+@dataclass
+class ParsedInfoParameterVariable:
+    """
+    Variable
+
+    like "<Variable work/i6_core/recognition/scoring/ScliteJob.HcIwh2mW7AZ6/output/wer 2.6>"
+    """
+
+    path: str
+    value: Any
+
+
+@dataclass
+class ParsedInfoParameterPath:
+    """
+    Path
+
+    <Path /u/zeyer/.../ScliteJob.aLj6yUUIDD93/output/reports>
+    """
+
+    path: str
+
+
+def collect_inputs_per_key(info_params: list[ParsedInfoParameter]) -> dict[str, str]:
+    """
+    :param info_params: via :func:`parse_info_parameter`
+    :return: path -> job
+    """
+    import tree
+
+    d = {param.name: param.content for param in info_params}
+
+    visited_jobs = set()
+    jobs = {}  # path -> job
+
+    def _visit_func(path, obj):
+        if not isinstance(obj, (ParsedInfoParameterVariable, ParsedInfoParameterPath)):
+            return
+        job = get_job_from_work_output(obj.path, allow_none=True)
+        if not job:
+            return
+        if job in visited_jobs:
+            return
+        visited_jobs.add(job)
+        jobs[path] = job
+
+    tree.map_structure_with_path(_visit_func, d)
+
+    if not jobs:
+        return jobs
+
+    if len(jobs) > 1:
+        # remove common prefix/postfix
+        while True:
+            paths = list(jobs.keys())
+            if not paths[0]:
+                break
+            prefix = paths[0][0]
+            if all(prefix == path[0] for path in paths):
+                jobs = {path[1:]: job for path, job in jobs.items()}
+                continue
+            postfix = paths[0][-1]
+            if all(postfix == path[-1] for path in paths):
+                jobs = {path[:-1]: job for path, job in jobs.items()}
+                continue
+            break
+
+    jobs_ = {"/".join(path): job for path, job in jobs.items()}
+    assert len(jobs_) == len(jobs)  # unique joined paths
+    return jobs_
+
+
+class _AutoLocals(Mapping[str, Any]):
+    def __init__(self):
+        self._locals = {}
+
+    def __getitem__(self, item: str):
+        if item in self._locals:
+            return self._locals[item]
+        if item[:1].isupper():
+            value = type(item, (dict,), {})
+            self[item] = value
+            return value
+        raise KeyError(f"auto locals: var {item} not found")
+
+    def __setitem__(self, key: str, value):
+        self._locals[key] = value
+
+    def __contains__(self, item: str) -> bool:
+        try:
+            self.__getitem__(item)
+            return True
+        except KeyError:
+            return False
+
+    def __len__(self):
+        return len(self._locals)
+
+    def __iter__(self):
+        return iter(self._locals)
+
+
+def test_parse_info_parameter():
+    s = (
+        "PARAMETER: score_results:"
+        " {'dev-clean': ScoreResult(dataset_name='dev-clean',"
+        " main_measure_value=<Variable work/i6_core/recognition/scoring/ScliteJob.HcIwh2mW7AZ6/output/wer 2.6>,"
+        " report=<Path /u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+        "ScliteJob.HcIwh2mW7AZ6/output/reports>),"
+        " 'dev-other': ScoreResult(dataset_name='dev-other',"
+        " main_measure_value=<Variable work/i6_core/recognition/scoring/ScliteJob.aLj6yUUIDD93/output/wer 5.4>,"
+        " report=<Path /u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+        "ScliteJob.aLj6yUUIDD93/output/reports>),"
+        " 'test-clean': ScoreResult(dataset_name='test-clean',"
+        " main_measure_value=<Variable work/i6_core/recognition/scoring/ScliteJob.IJ1p9FdPgzgi/output/wer 2.81>,"
+        " report=<Path /u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+        "ScliteJob.IJ1p9FdPgzgi/output/reports>),"
+        " 'test-other': ScoreResult(dataset_name='test-other',"
+        " main_measure_value=<Variable work/i6_core/recognition/scoring/ScliteJob.OcYJiwXkWAUI/output/wer 6.5>,"
+        " report=<Path /u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+        "ScliteJob.OcYJiwXkWAUI/output/reports>)}"
+    )
+    param = parse_info_parameter(s)
+    print(param)
+    assert param.name == "score_results"
+    assert isinstance(param.content, dict)
+    assert set(param.content) == {"dev-clean", "dev-other", "test-clean", "test-other"}
+    for k, scores in param.content.items():
+        assert isinstance(scores, dict) and type(scores).__name__ == "ScoreResult"
+        assert set(scores) == {"dataset_name", "main_measure_value", "report"}
+        assert scores["dataset_name"] == k
+        measure = scores["main_measure_value"]
+        assert isinstance(measure, ParsedInfoParameterVariable)
+        assert measure.path.startswith("work/i6_core/") and measure.path.endswith("/output/wer")
+        assert measure.value == {"dev-clean": 2.6, "dev-other": 5.4, "test-clean": 2.81, "test-other": 6.5}[k]
+        report = scores["report"]
+        assert isinstance(report, ParsedInfoParameterPath)
+        assert report.path.startswith("/u/zeyer/setups/") and report.path.endswith("/output/reports")
+
+
+def test_collect_inputs_per_key():
+    param = ParsedInfoParameter(
+        name="score_results",
+        content={
+            "dev-clean": {
+                "dataset_name": "dev-clean",
+                "main_measure_value": ParsedInfoParameterVariable(
+                    path="work/i6_core/recognition/scoring/ScliteJob.HcIwh2mW7AZ6/output/wer", value=2.6
+                ),
+                "report": ParsedInfoParameterPath(
+                    path="/u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+                    "ScliteJob.HcIwh2mW7AZ6/output/reports"
+                ),
+            },
+            "dev-other": {
+                "dataset_name": "dev-other",
+                "main_measure_value": ParsedInfoParameterVariable(
+                    path="work/i6_core/recognition/scoring/ScliteJob.aLj6yUUIDD93/output/wer", value=5.4
+                ),
+                "report": ParsedInfoParameterPath(
+                    path="/u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+                    "ScliteJob.aLj6yUUIDD93/output/reports"
+                ),
+            },
+            "test-clean": {
+                "dataset_name": "test-clean",
+                "main_measure_value": ParsedInfoParameterVariable(
+                    path="work/i6_core/recognition/scoring/ScliteJob.IJ1p9FdPgzgi/output/wer", value=2.81
+                ),
+                "report": ParsedInfoParameterPath(
+                    path="/u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+                    "ScliteJob.IJ1p9FdPgzgi/output/reports"
+                ),
+            },
+            "test-other": {
+                "dataset_name": "test-other",
+                "main_measure_value": ParsedInfoParameterVariable(
+                    path="work/i6_core/recognition/scoring/ScliteJob.OcYJiwXkWAUI/output/wer", value=6.5
+                ),
+                "report": ParsedInfoParameterPath(
+                    path="/u/zeyer/setups/combined/2021-05-31/work/i6_core/recognition/scoring/"
+                    "ScliteJob.OcYJiwXkWAUI/output/reports"
+                ),
+            },
+        },
+    )
+
+    jobs = collect_inputs_per_key([param])
+    assert set(jobs) == {"dev-clean", "dev-other", "test-clean", "test-other"}
+    assert all("/ScliteJob." in job for job in jobs.values())
+
+
+# _setup_sisyphus()
