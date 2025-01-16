832161cfe3521f973f4e6475d214ec4d44d7a4bd
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2025-01-16 23:00:48 +0100

    fix vocab loading

--- users/zeyer/decoding/prior_rescoring.py
+++ users/zeyer/decoding/prior_rescoring.py
@@ -56,7 +56,7 @@ class SearchPriorRescoreJob(Job):
     Use prior to rescore some recog output.
     """
 
-    __sis_version__ = 1
+    __sis_version__ = 2
 
     def __init__(
         self, search_py_output: tk.Path, *, prior: tk.Path, prior_type: str, vocab: tk.Path, output_gzip: bool = True
@@ -86,7 +86,7 @@ class SearchPriorRescoreJob(Job):
         d = eval(util.uopen(self.search_py_output, "rt").read(), {"nan": float("nan"), "inf": float("inf")})
         assert isinstance(d, dict)  # seq_tag -> bpe string
 
-        vocab: List[str] = list(util.uopen(self.vocab, "rt"))
+        vocab: List[str] = util.uopen(self.vocab, "rt").read().splitlines()
         vocab_to_idx: Dict[str, int] = {word: i for (i, word) in enumerate(vocab)}
 
         prior = np.loadtxt(self.prior.get_path())
