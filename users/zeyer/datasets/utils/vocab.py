df6263eb62b2f27d380ddd83f0494c5447173131
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2025-01-17 17:44:41 +0100

    ExtractVocabSpecialLabelsJob

--- users/zeyer/datasets/utils/vocab.py
+++ users/zeyer/datasets/utils/vocab.py
@@ -55,6 +55,68 @@ class ExtractVocabLabelsJob(Job):
                 f.write(label + "\n")
 
 
+class ExtractVocabSpecialLabelsJob(Job):
+    """
+    Takes any RETURNN vocabulary, and extracts special labels from it,
+    stores it in a dict.
+    """
+
+    def __init__(self, vocab_opts: Dict[str, Any], *, returnn_root: Optional[tk.Path] = None):
+        """
+        :param vocab_opts:
+        :param returnn_root: path, optional, the RETURNN root dir.
+        """
+        self.vocab_opts = vocab_opts
+        self.returnn_root = returnn_root
+
+        self.out_vocab_special_labels_dict = self.output_path("vocab_special_labels.py")
+
+    def tasks(self):
+        yield Task("run", mini_task=True)
+
+    def run(self):
+        import sys
+        import os
+        import i6_experiments
+
+        recipe_dir = os.path.dirname(os.path.dirname(i6_experiments.__file__))
+        sys.path.insert(0, recipe_dir)
+
+        import i6_core.util as util
+
+        returnn_root = util.get_returnn_root(self.returnn_root)
+        sys.path.insert(1, returnn_root.get_path())
+
+        from returnn.datasets.util.vocabulary import Vocabulary
+
+        vocab = self.vocab_opts
+        vocab = util.instanciate_delayed(vocab)
+        print("RETURNN vocab opts:", vocab)
+        vocab = Vocabulary.create_vocab(**vocab)
+        print("Vocab:", vocab)
+        print("num labels:", vocab.num_labels)
+        assert vocab.num_labels == len(vocab.labels)
+        labels = vocab.labels
+
+        d = {"unknown_label": vocab.unknown_label}
+        if vocab.bos_label_id is not None:
+            d["bos_label"] = labels[vocab.bos_label_id]
+        if vocab.eos_label_id is not None:
+            d["eos_label"] = labels[vocab.eos_label_id]
+        if vocab.pad_label_id is not None:
+            d["pad_label"] = labels[vocab.pad_label_id]
+        if vocab.control_symbol_ids:
+            d["control_symbols"] = {k: labels[v] for k, v in vocab.control_symbol_ids.items()}
+        if vocab.user_defined_symbol_ids:
+            d["user_defined_symbols"] = {k: labels[v] for k, v in vocab.user_defined_symbol_ids.items()}
+
+        with util.uopen(self.out_vocab_special_labels_dict.get_path(), "wt") as f:
+            f.write("{\n")
+            for k, v in d.items():
+                f.write(f"  {k!r}: {v!r},\n")
+            f.write("}\n")
+
+
 class ExtendVocabLabelsByNewLabelJob(Job):
     """
     Takes vocab (line-based (maybe gzipped) list of labels),
