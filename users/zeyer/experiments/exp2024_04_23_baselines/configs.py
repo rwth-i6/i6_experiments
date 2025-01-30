48b927f01343b41d76b194f9d901f22a117c7489
Albert Zeyer <albzey@gmail.com>
2025-01-30 09:56:34 +0100

    _get_cfg_lrlin_oclr_by_bs_nep_v4 more options

--- users/zeyer/experiments/exp2024_04_23_baselines/configs.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/configs.py
@@ -157,19 +157,22 @@ def _get_cfg_lrlin_oclr_by_bs_nep_v3(
 def _get_cfg_lrlin_oclr_by_bs_nep_v4(
     n_ep: int,
     *,
+    base_lr: float = 1.0,
     peak_lr: float = 1e-3,
     low_lr: float = 1e-5,
     lowest_lr: float = 1e-6,
+    step_peak_fraction: float = 0.45,
+    step_finetune_fraction: float = 0.9,
 ) -> Dict[str, Any]:
     """
     :param n_ep: num epochs
     """
     return {
         "__num_epochs": n_ep,
-        "learning_rate": 1.0,
+        "learning_rate": base_lr,
         "dynamic_learning_rate": dyn_lr_piecewise_linear,
         "learning_rate_piecewise_by_epoch_continuous": True,
-        "learning_rate_piecewise_steps": [0.45 * n_ep, 0.9 * n_ep, n_ep],
+        "learning_rate_piecewise_steps": [step_peak_fraction * n_ep, step_finetune_fraction * n_ep, n_ep],
         "learning_rate_piecewise_values": [low_lr, peak_lr, low_lr, lowest_lr],
     }
 
