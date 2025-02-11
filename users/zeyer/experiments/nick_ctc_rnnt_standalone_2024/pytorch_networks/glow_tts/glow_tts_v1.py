c1a3d862b75303a8b9d0795cb9eb8f2ede30238f
Albert Zeyer <albzey@gmail.com>
2025-02-11 14:43:28 +0100

    black

--- users/zeyer/experiments/nick_ctc_rnnt_standalone_2024/pytorch_networks/glow_tts/glow_tts_v1.py
+++ users/zeyer/experiments/nick_ctc_rnnt_standalone_2024/pytorch_networks/glow_tts/glow_tts_v1.py
@@ -9,14 +9,16 @@ from typing import Any, Dict
 from . import modules
 from . import commons
 from . import attentions
+
 try:
     from .monotonic_align.path import maximum_path
 except:
     import subprocess
     import sys
+
     subprocess.call(
-        [sys.executable, 'setup.py', 'build_ext', '--inplace'],
-        cwd=os.path.join(os.path.realpath(os.path.dirname(__file__)), "monotonic_align")
+        [sys.executable, "setup.py", "build_ext", "--inplace"],
+        cwd=os.path.join(os.path.realpath(os.path.dirname(__file__)), "monotonic_align"),
     )
     from .monotonic_align.path import maximum_path
 
@@ -26,21 +28,23 @@ from ..tts_shared.encoder.transformer import TTSTransformerTextEncoderV1Config
 from ..tts_shared.encoder.duration_predictor import SimpleConvDurationPredictorV1Config
 from ..tts_shared.util import generate_path
 
+
 @dataclass
-class FlowDecoderConfig():
+class FlowDecoderConfig:
     """
-        Args:
-            target_channels: Number of feature- and latent space channels
-            hidden_channels: Number of hidden channels
-            kernel_size: Kernel Size for convolutions in coupling blocks
-            dilation_rate: Dilation Rate to define dilation in convolutions of coupling block
-            num_blocks: Number of coupling blocks
-            num_layers_per_block: Number of layers in CNN of the coupling blocks
-            dropout: Dropout probability for CNN in coupling blocks.
-            num_splits: Number of splits for the 1x1 convolution for flows in the decoder.
-            num_squeeze: Squeeze the feature and latent space by this factor.
-            use_sigmoid_scale: Boolean to define if log probs in coupling layers should be rescaled using sigmoid. Defaults to False.
+    Args:
+        target_channels: Number of feature- and latent space channels
+        hidden_channels: Number of hidden channels
+        kernel_size: Kernel Size for convolutions in coupling blocks
+        dilation_rate: Dilation Rate to define dilation in convolutions of coupling block
+        num_blocks: Number of coupling blocks
+        num_layers_per_block: Number of layers in CNN of the coupling blocks
+        dropout: Dropout probability for CNN in coupling blocks.
+        num_splits: Number of splits for the 1x1 convolution for flows in the decoder.
+        num_squeeze: Squeeze the feature and latent space by this factor.
+        use_sigmoid_scale: Boolean to define if log probs in coupling layers should be rescaled using sigmoid. Defaults to False.
     """
+
     target_channels: int
     hidden_channels: int
     kernel_size: int
@@ -51,7 +55,6 @@ class FlowDecoderConfig():
     num_squeeze: int
     dropout: float
     use_sigmoid_scale: bool
-
 
 
 class FlowDecoder(nn.Module):
@@ -73,10 +76,7 @@ class FlowDecoder(nn.Module):
         for b in range(cfg.num_blocks):
             self.flows.append(modules.ActNorm(channels=cfg.target_channels * cfg.num_squeeze))
             self.flows.append(
-                modules.InvConvNear(
-                    channels=cfg.target_channels * cfg.num_squeeze,
-                    n_split=cfg.num_splits
-                )
+                modules.InvConvNear(channels=cfg.target_channels * cfg.num_squeeze, n_split=cfg.num_splits)
             )
             self.flows.append(
                 attentions.CouplingBlock(
@@ -119,11 +119,12 @@ class FlowDecoder(nn.Module):
 @dataclass
 class Config:
     """
-        Args:
-            num_speakers: input size of the speaker embedding matrix
-            speaker_embedding_size: output size of the speaker embedding matrix
-            mean_only: if true, standard deviation for latent z estimation is fixed to 1
+    Args:
+        num_speakers: input size of the speaker embedding matrix
+        speaker_embedding_size: output size of the speaker embedding matrix
+        mean_only: if true, standard deviation for latent z estimation is fixed to 1
     """
+
     feature_extraction_config: DbMelFeatureExtractionConfig
     encoder_config: TTSTransformerTextEncoderV1Config
     duration_predictor_config: SimpleConvDurationPredictorV1Config
@@ -168,24 +169,17 @@ class Model(BaseTTSModelV1):
             speaker_embedding_size=config.speaker_embedding_size,
         )
 
-
         self.proj_m = nn.Conv1d(config.encoder_config.basic_dim, config.flow_decoder_config.target_channels, 1)
         if not config.mean_only:
             self.proj_s = nn.Conv1d(config.encoder_config.basic_dim, config.flow_decoder_config.target_channels, 1)
-    
-        self.decoder = FlowDecoder(
-            cfg=config.flow_decoder_config,
-            speaker_embedding_size=config.speaker_embedding_size
-        )
+
+        self.decoder = FlowDecoder(cfg=config.flow_decoder_config, speaker_embedding_size=config.speaker_embedding_size)
 
     def forward(
         self, x, x_lengths, raw_audio=None, raw_audio_lengths=None, g=None, gen=False, noise_scale=1.0, length_scale=1.0
     ):
         if not gen:
-            y, y_lengths = self.extract_features(
-                raw_audio=raw_audio,
-                raw_audio_lengths=raw_audio_lengths
-            )
+            y, y_lengths = self.extract_features(raw_audio=raw_audio, raw_audio_lengths=raw_audio_lengths)
             feature_lengths = y_lengths
         else:
             y, y_lengths = (None, None)
@@ -195,7 +189,7 @@ class Model(BaseTTSModelV1):
             labels_lengths=x_lengths,
             speaker_label=g,
         )
-        
+
         h_m = self.proj_m(h) * h_mask
         if not self.config.mean_only:
             h_logs = self.proj_s(h) * h_mask
@@ -222,7 +216,11 @@ class Model(BaseTTSModelV1):
 
             z = (z_m + torch.exp(z_logs) * torch.randn_like(z_m) * noise_scale) * z_mask
             y, logdet = self.decoder(z, z_mask, g=spk, reverse=True)
-            return (y, z_m, z_logs, logdet, z_mask, y_lengths), (h_m, h_logs, h_mask), (attn, log_durations, logw_, w_ceil)
+            return (
+                (y, z_m, z_logs, logdet, z_mask, y_lengths),
+                (h_m, h_logs, h_mask),
+                (attn, log_durations, logw_, w_ceil),
+            )
         else:
             z, logdet = self.decoder(y, z_mask, g=spk, reverse=False)
             with torch.no_grad():
@@ -244,7 +242,12 @@ class Model(BaseTTSModelV1):
             )  # [b, t', t], [b, t, d] -> [b, d, t']
 
             logw_ = torch.log(1e-8 + torch.sum(attn, -1)) * h_mask
-            return (z, z_m, z_logs, logdet, z_mask), (h_m, h_logs, h_mask), (y_lengths, feature_lengths), (attn, log_durations, logw_)
+            return (
+                (z, z_m, z_logs, logdet, z_mask),
+                (h_m, h_logs, h_mask),
+                (y_lengths, feature_lengths),
+                (attn, log_durations, logw_),
+            )
 
     def preprocess(self, y, y_lengths, y_max_length):
         if y_max_length is not None:
@@ -273,4 +276,3 @@ def train_step(*, model: Model, data, run_ctx, **kwargs):
 
     run_ctx.mark_as_loss(name="mle", loss=l_mle)
     run_ctx.mark_as_loss(name="dp", loss=l_dp)
-
