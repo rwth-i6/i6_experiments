1b434e3a9356d8ec67da249fe727e131a282be52
Albert Zeyer <albzey@gmail.com>
2025-03-12 21:34:38 +0100

    job aliases via job info file, small fix, keep compat

--- users/zeyer/utils/job_aliases_from_info.py
+++ users/zeyer/utils/job_aliases_from_info.py
@@ -11,6 +11,7 @@ def get_job_aliases(job: str) -> List[str]:
     Get job aliases from job info file
 
     :param job: without "work/" prefix
+    :return: list of aliases. those are paths all with "alias/" prefix
     """
     job_dir = get_job_base_dir(job)
     aliases = []
@@ -18,5 +19,7 @@ def get_job_aliases(job: str) -> List[str]:
     with open(f"{job_dir}/info") as f:
         for line in f:
             if line.startswith("ALIAS: "):
-                aliases.append(line[len("ALIAS: ") :].strip())
+                # Add "alias/" prefix to make it a valid path,
+                # and also to have it consistent to job_aliases_from_log.
+                aliases.append("alias/" + line[len("ALIAS: ") :].strip())
     return aliases
