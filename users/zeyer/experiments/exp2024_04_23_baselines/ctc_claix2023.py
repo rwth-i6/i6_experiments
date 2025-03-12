448c0f519d79df1051bb478283a26d6fa7302496
Albert Zeyer <albzey@gmail.com>
2025-03-12 11:51:40 +0100

    more

--- users/zeyer/experiments/exp2024_04_23_baselines/ctc_claix2023.py
+++ users/zeyer/experiments/exp2024_04_23_baselines/ctc_claix2023.py
@@ -24,7 +24,7 @@ from i6_experiments.users.zeyer.sis_tools.instanciate_delayed import use_instanc
 
 import returnn.frontend as rf
 from returnn.frontend.decoder.transformer import TransformerDecoder
-from returnn.frontend.encoder.conformer import ConformerEncoderLayer, ConformerPositionwiseFeedForward
+from returnn.frontend.encoder.conformer import ConformerEncoder, ConformerEncoderLayer, ConformerPositionwiseFeedForward
 
 
 def py():
@@ -70,6 +70,54 @@ def py():
         # avoid OOM
         # env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
     )
+
+    for num_layers, num_dims, batch_size in [(16, 512, 150_000)]:
+        ctc_train_exp(
+            f"L{num_layers}-D{num_dims}-spm10k-auxAED-b{batch_size // 1000}k",
+            config_96gb_bf16_accgrad1,
+            model_config={
+                "enc_build_dict": rf.build_dict(
+                    # ConformerEncoder(in_dim, enc_model_dim, **enc_opts)
+                    ConformerEncoder,
+                    enc_input_layer=rf.build_dict(
+                        ConformerConvSubsample,
+                        out_dims=[32, 64, 64],
+                        filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                        pool_sizes=[(1, 2)],
+                        strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                    ),
+                    num_layers=num_layers,
+                    out_dim=num_dims,
+                    encoder_layer=rf.build_dict(
+                        ConformerEncoderLayer,
+                        ff=rf.build_dict(
+                            ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                        ),
+                        num_heads=8,
+                    ),
+                ),
+                "feature_batch_norm": True,
+            },
+            config_updates={
+                **_get_cfg_lrlin_oclr_by_bs_nep_v3(batch_size, 100, batch_size_factor=_batch_size_factor),
+                "optimizer.weight_decay": 1e-2,
+                "__train_audio_preprocess": speed_pert_librosa_config,
+                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+                # purely used for training
+                "aux_attention_decoder": rf.build_dict(TransformerDecoder, num_layers=6),
+                "max_seq_length_default_target": None,
+                # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+                # out of 281241 seqs in train, we removed only 71 seqs.
+                # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+                "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+            },
+            post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+            vocab="spm10k",
+            train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+            dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+            # avoid OOM
+            # env_updates={"PYTORCH_CUDA_ALLOC_CONF": "backend:cudaMallocAsync,expandable_segments:True"},
+        )
 
     # Consistency regularization (CR) (crLoss).
     for opts, cr_ctc_variants in [
