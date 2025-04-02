a9f42dce384bc87c78a9e875cf4105a2b3f59c6b
Albert Zeyer <albzey@gmail.com>
2025-04-02 14:21:19 +0200

    yourtts demo more

--- users/zeyer/external_models/coqui_ai_tts.py
+++ users/zeyer/external_models/coqui_ai_tts.py
@@ -162,6 +162,13 @@ def _demo():
     rnd = random.Random(args.seed)
     torch.random.manual_seed(rnd.randint(0, 2**32 - 1))
 
+    try:
+        import lovely_tensors
+
+        lovely_tensors.monkey_patch()
+    except ImportError:
+        pass
+
     model_name = args.model_name
     model_dir = args.model_dir
     assert os.path.exists(model_dir), f"model dir does not exist: {model_dir}"
@@ -182,6 +189,7 @@ def _demo():
     from TTS.utils.manage import ModelManager
     from TTS.utils.synthesizer import synthesis
     from TTS.utils.audio.numpy_transforms import save_wav
+    from TTS.tts.models.vits import Vits
 
     def _disallowed_create_dir_and_download_model(*_args, **_kwargs):
         raise RuntimeError(
@@ -194,31 +202,38 @@ def _demo():
     ModelManager.create_dir_and_download_model = _disallowed_create_dir_and_download_model
 
     print("Loading TTS model...")
-    tts = TTS(model_name=model_name, progress_bar=False, gpu=dev.type == "cuda")
+    tts = TTS(model_name=model_name, progress_bar=False)
     tts.to(dev)
 
     # See tts.tts() func to how it generates audio.
     # This is a high-level wrapper, but we want to call it more directly to be able to use it in batch mode.
 
-    tts_model = tts.synthesizer.tts_model
+    tts_model: Vits = tts.synthesizer.tts_model  # typing Vits is just an example
     tts_config = tts.synthesizer.tts_config
-    use_cuda = tts.synthesizer.use_cuda
     sample_rate = tts.synthesizer.output_sample_rate
 
     print(f"{type(tts_model) = }")
+
     speakers = tts.speakers
     print(f"speakers: {speakers}")
-    speaker = rnd.choice(speakers) if speakers else None
+    speaker = None
+    speaker_id = None
+    speaker_embedding = None
+    if tts_model.speaker_manager:
+        speaker_id = rnd.randint(0, tts_model.speaker_manager.num_embeddings - 1)
+        print("random speaker id:", speaker_id)
+        emb = list(tts_model.speaker_manager.embeddings.values())[speaker_id]
+        speaker = emb["name"]
+        speaker_embedding = np.array(emb["embedding"])  # [emb_dim]
     print(f"speaker: {speaker!r}")
 
-    if hasattr(tts_model, "emb_g"):
-        print("emb_g:", tts_model.emb_g)  # speaker embedding
+    print("emb_g:", tts_model.emb_g if hasattr(tts_model, "emb_g") else None)  # speaker embedding
     print(f"{tts_config.use_d_vector_file = }")
     if tts_model.speaker_manager:
         print(f"{len(tts_model.speaker_manager.embeddings) = }")
-        print("embedding keys:", list(tts_model.speaker_manager.embeddings.keys()))
-        for emb in tts_model.speaker_manager.embeddings.values():
-            print(f"  name={emb['name']!r}, keys:{list(emb.keys())}, dim:{len(emb['embedding'])}")
+        # print("embedding keys:", list(tts_model.speaker_manager.embeddings.keys()))
+        # for emb in tts_model.speaker_manager.embeddings.values():
+        #     print(f"  name={emb['name']!r}, keys:{list(emb.keys())}, dim:{len(emb['embedding'])}")
 
     language = args.language
     if tts_model.language_manager:
@@ -234,14 +249,19 @@ def _demo():
 
     print(f"{hasattr(tts_model, "synthesize") = }")  # if False, then it will call the synthesis func
     assert not hasattr(tts_model, "synthesize")  # we assume that we can call the synthesis func
-    print(f"{use_cuda = }")
     print(f"{sample_rate = }")
 
     # could also take this from config test_sentences
-    txt = "It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent."
+    text = "It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent."
+
+    # This is maybe Vits specific:
+    print(f"{tts_model.length_scale = }")
+    tts_model.length_scale = 1.0  # overwrite (testing...)
+    print(f"{tts_model.inference_noise_scale = }")
+    print(f"{tts_model.inference_noise_scale_dp = }")
 
     # Test high-level API.
-    wav = tts.tts(txt, speaker=speaker, language=language)
+    wav = tts.tts(text, speaker=speaker, language=language)
     print("tts() output:", type(wav))
     # if tensor convert to numpy
     if torch.is_tensor(wav):
@@ -250,8 +270,25 @@ def _demo():
         wav = np.array(wav)
     assert isinstance(wav, np.ndarray)
     print("shape:", wav.shape)
-
     save_wav(wav=wav, path="demo.wav", sample_rate=sample_rate)
+
+    # Generate another one. How much randomness?
+    wav = tts.tts(text, speaker=speaker, language=language)
+    print("tts() output:", type(wav))
+    # if tensor convert to numpy
+    if torch.is_tensor(wav):
+        wav = wav.cpu().numpy()
+    if isinstance(wav, list):
+        wav = np.array(wav)
+    assert isinstance(wav, np.ndarray)
+    print("shape:", wav.shape)
+    save_wav(wav=wav, path="demo2.wav", sample_rate=sample_rate)
+
+    if tts_model.speaker_manager:
+        if tts_config.use_d_vector_file:
+            speaker_id = None  # the model expects speaker_embedding only, not speaker_id
+        else:
+            speaker_embedding = None  # speaker_id is used
 
     # outputs = synthesis(
     #     model=tts_model,
@@ -267,7 +304,45 @@ def _demo():
     # )
     # waveform = outputs["wav"]
 
-    ...  # TODO...
+    # See also TTS.utils.synthesizer.synthesis().
+
+    # convert text to sequence of token IDs
+    text_inputs = np.asarray(
+        tts_model.tokenizer.text_to_ids(text, language=language),
+        dtype=np.int32,
+    )  # [T_in]
+    text_inputs = torch.tensor(text_inputs, dtype=torch.int32, device=dev)[None]  # [B,T_in]
+    print(f"{text_inputs.shape = }")
+    text_inputs_lens = torch.tensor([text_inputs.shape[1]], device=dev)  # [B]
+    speaker_id = torch.tensor(speaker_id, device=dev)[None] if speaker_id is not None else None  # [B]
+    speaker_embedding = torch.tensor(speaker_embedding, device=dev, dtype=torch.float32) if speaker_embedding is not None else None
+    speaker_embedding = speaker_embedding[None] if speaker_embedding is not None else None  # [B, emb_dim]
+    language_id = torch.tensor(tts_model.language_manager.name_to_id[language], device=dev)[None] if language else None  # [B]
+
+    outputs = tts_model.inference(
+        text_inputs,
+        aux_input={
+            "x_lengths": text_inputs_lens,
+            "speaker_ids": speaker_id,
+            "d_vectors": speaker_embedding,
+            "language_ids": language_id,
+        },
+    )
+    print(outputs)
+
+    model_outputs = outputs["model_outputs"]  # for Vits: [B, 1, T_wav]
+    y_mask = outputs["y_mask"]  # before the final waveform_decoder
+    print(f"{model_outputs.shape = }")
+    print(f"{y_mask.shape = }")
+
+    # convert outputs to numpy. select first batch
+    model_outputs = model_outputs[0].cpu().squeeze().numpy()  # [T_wav]
+    assert model_outputs.ndim == 1
+    wav = model_outputs
+    save_wav(wav=wav, path="demo3.wav", sample_rate=sample_rate)
+
+    # if hasattr(tts_model, "waveform_decoder"):
+    #     print("waveform_decoder:", tts_model.waveform_decoder)
 
 
 if __name__ == "__main__":
