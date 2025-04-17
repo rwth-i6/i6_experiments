d1e93fc790cda7f9a963fc44010fb3263829e923
Albert Zeyer <albzey@gmail.com>
2025-04-17 21:35:42 +0200

    unwrap hdf fixes

--- users/zeyer/datasets/utils/unwrap_hdf.py
+++ users/zeyer/datasets/utils/unwrap_hdf.py
@@ -9,7 +9,7 @@ Here we provide :func:`unwrap_hdf_dataset` to recover the original data format.
 
 from typing import Any, Dict
 import functools
-from returnn.tensor import TensorDict, Tensor, batch_dim
+from returnn.tensor import TensorDict, Tensor, Dim, batch_dim
 
 
 def unwrap_hdf_dataset(hdf_dataset: Dict[str, Any], *, extern_data: Dict[str, Dict[str, Any]]) -> Dict[str, Any]:
@@ -22,11 +22,12 @@ def unwrap_hdf_dataset(hdf_dataset: Dict[str, Any], *, extern_data: Dict[str, Di
         which we intend to recover like that
     :return: dataset which provides data matching to ``extern_data``. we use :class:`PostprocessingDataset` for that.
     """
+    map_outputs = {k: _wrap_extern_data_value_to_pp_dataset_map_outputs(v) for k, v in extern_data.items()}
     return {
         "class": "PostprocessingDataset",
         "dataset": hdf_dataset,
-        "map_seq": functools.partial(_unwrap_hdf_extern_data, extern_data=extern_data),
-        "map_outputs": {k: _wrap_extern_data_value_to_pp_dataset_map_outputs(v) for k, v in extern_data.items()},
+        "map_seq": functools.partial(_unwrap_hdf_map_seq, map_outputs=map_outputs),
+        "map_outputs": map_outputs,
     }
 
 
@@ -45,21 +46,65 @@ def _wrap_extern_data_value_to_pp_dataset_map_outputs(data: Dict[str, Any]) -> D
     return out
 
 
-def _unwrap_hdf_extern_data(data: TensorDict, *, extern_data: Dict[str, Dict[str, Any]], **_other) -> TensorDict:
+def _unwrap_hdf_map_seq(data: TensorDict, *, map_outputs: Dict[str, Dict[str, Any]], **_other) -> TensorDict:
     # See returnn.datasets.hdf.SimpleHDFWriter._insert_h5_other for reference.
     # Also note that the handling for "data" is slightly different from for other keys.
     res = TensorDict()
-    for key, out_data_template in extern_data.items():
+
+    # Handle "data" first, as this is also serialized slightly differently,
+    # and it will store dynamic sizes in "sizes" when there are multiple.
+    if "data" in map_outputs:
+        key = "data"
+        out_data_template = map_outputs[key]
+        in_data: Tensor = data.data[key]
+        in_raw = in_data.raw_tensor
+        out_data = Tensor(key, **out_data_template)
+        assert batch_dim not in out_data.dims  # sanity check
+        if not out_data.dims:
+            # in_data should be of shape [1]
+            assert in_raw.shape == (1,)
+            in_raw = in_raw.squeeze(0)
+        else:
+            if in_raw.ndim == 2:  # assume [flat_seq_len,dim]
+                assert out_data.dims[-1].dimension == in_raw.shape[-1]
+                flattened_dims = out_data.dims[:-1]  # could also be empty
+            elif in_raw.ndim == 1:  # assume [flat_seq_len]
+                flattened_dims = out_data.dims
+            else:
+                raise ValueError(
+                    f"unexpected 'data' {in_data} in_raw shape {in_raw.shape}, expect to convert to {out_data_template}"
+                )
+            if len(flattened_dims) > 1:
+                # Expect "sizes" in data.
+                assert "sizes" in data.data, f"got {data.data}, 'sizes' is missing"
+                sizes = data.data["sizes"].raw_tensor
+                assert sizes.ndim == 1 and sizes.shape[0] == len(flattened_dims)
+                in_raw = in_raw.reshape(*sizes, *in_raw.shape[1:])
+            elif len(flattened_dims) == 1:
+                pass
+                # No need to reshape, should already be good.
+            else:  # no dims
+                assert in_raw.shape[0] == 1  # there was a dummy time dim
+                in_raw = in_raw.squeeze(0)
+        out_data.raw_tensor = in_raw
+        res.data[key] = out_data
+
+    for key, out_data_template in map_outputs.items():
+        if key == "data":
+            continue
         assert key in data.data
         in_data: Tensor = data.data[key]
         in_raw = in_data.raw_tensor
         out_data = Tensor(key, **out_data_template)
-        assert in_data.sparse == out_data.sparse
-        if len(in_data.dims) != len(out_data.dims):
-            assert len(in_data.dims) < len(out_data.dims)
-            # TODO...
+        assert batch_dim not in out_data.dims  # sanity check
+        if len(out_data.dims) == 0:
+            assert in_raw.shape == (1,)
+            in_raw = in_raw.squeeze(0)
+        else:
+            assert in_raw.ndim == len(out_data.dims)
         out_data.raw_tensor = in_raw
         res.data[key] = out_data
+
     # There might be other meta data in `data`, like "seq_tag", "complete_frac",
     # which we should just copy over.
     for key, in_data in data.data.items():
