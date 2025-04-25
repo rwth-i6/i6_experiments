b119b0e7f522dd1f2df5dc5fa25f11ccfa13ce0c
Dorian Koch <dorian.koch@ml.rwth-aachen.de>
2025-04-25 16:23:55 +0200

    Add job

--- users/dorian_koch/jobs/AggregateOutputsAsCsv.py
+++ users/dorian_koch/jobs/AggregateOutputsAsCsv.py
@@ -4,11 +4,10 @@ from i6_core.util import uopen
 
 
 class AggregateOutputsAsCsv(Job):
-
     def __init__(
         self,
         *,
-        inputs: List[Tuple[str, tk.AbstractPath]],
+        inputs: list[tuple[str, tk.AbstractPath]],
     ):
         self.inputs = inputs
 
@@ -35,3 +34,36 @@ class AggregateOutputsAsCsv(Job):
             out.write("key,value\n")
             for val, content in data:
                 out.write(f"{val},{content}\n")
+
+
+class WriteFinishedPathsAsCsv(Job):
+    def __init__(
+        self,
+        *,
+        inputs: list[tuple[str, tk.AbstractPath | str]],
+        seperator: str = ",",
+    ):
+        self.inputs = inputs
+        self.seperator = seperator
+        self.out_file = self.output_path("out.txt")
+
+    def tasks(self):
+        yield Task("run", mini_task=True)
+
+    def run(self):
+        data = []
+
+        for key, file in self.inputs:
+            val = None
+            if isinstance(file, tk.Variable):
+                val = str(file.get())
+            elif isinstance(file, tk.Path):
+                val = str(file)
+            else:
+                assert False
+            data.append((key, val.strip()))
+
+        with uopen(self.out_file, "wt") as out:
+            out.write(f"key{self.seperator}value\n")
+            for val, content in data:
+                out.write(f"{val}{self.seperator}{content}\n")
