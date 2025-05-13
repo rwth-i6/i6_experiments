35f395c716a04277e2c92c9d2fa118fe2fac5230
Albert Zeyer <albzey@gmail.com>
2025-05-13 15:21:57 +0200

    small fix

--- users/zeyer/experiments/exp2025_05_05_align.py
+++ users/zeyer/experiments/exp2025_05_05_align.py
@@ -994,7 +994,9 @@ class ExtractInGradsFromPhi4MultimodalInstructLongFormDumpChunkSegmentationJob(J
             print("  Words per chunks:", words_indices_start_end)
 
             assert len(words_indices_start_end) == len(chunk_start_end)
-            hdf_writer.insert_batch(np.array(words_covered)[None], seq_len=[len(words)], seq_tag=[f"seq-{seq_idx}"])
+            hdf_writer.insert_batch(
+                np.array(words_indices_start_end)[None], seq_len=[len(words)], seq_tag=[f"seq-{seq_idx}"]
+            )
 
         hdf_writer.close()
 
