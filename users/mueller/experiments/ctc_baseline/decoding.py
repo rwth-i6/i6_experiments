Fix train_lm during gradient computation

users/mueller/experiments/ctc_baseline/decoding.py
@@ -18,7 +18,7 @@ from i6_core.util import uopen
 from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.recog_ext.ctc_flashlight_neural_lm import _format_align_label_seq
 from i6_experiments.users.mueller.experiments.language_models.ffnn import FeedForwardLm
 from i6_experiments.users.mueller.experiments.ctc_baseline.model import Model, OUT_BLANK_LABEL
-from i6_experiments.users.mueller.experiments.ctc_baseline.sum_criterion import sum_loss_ffnn, get_lm_logits, sum_loss_ngram_rf, sum_loss_ngram
+from i6_experiments.users.mueller.experiments.ctc_baseline.sum_criterion import sum_loss_ffnn, get_lm_logits, sum_loss_ngram_rf, sum_loss_ngram, get_lm_logits
 from i6_experiments.users.mueller.experiments.ctc_baseline import recombination
 from i6_experiments.users.zeyer.nn_rf.torch_ctc_fixed_grad import ctc_loss_fixed_grad
 
@@ -99,6 +99,7 @@ def recog_flashlight_ngram(
         assert model.recog_language_model
         assert model.recog_language_model.vocab_dim == model.target_dim
         context_size = int(lm_name[len("ffnn"):])
+        assert model.recog_language_model.conv_filter_size_dim.dimension == context_size
         arpa_4gram_lm = FFNN_LM_flashlight(model.recog_language_model, model.recog_language_model.vocab_dim, context_size)
     
     use_lm = hyp_params.pop("use_lm", True)
@@ -317,6 +318,7 @@ def recog_flashlight_ffnn(
         assert model.recog_language_model
         assert model.recog_language_model.vocab_dim == model.target_dim
         lm: FeedForwardLm = model.recog_language_model
+    assert lm.conv_filter_size_dim.dimension == context_size
     # noinspection PyUnresolvedReferences
     lm_scale: float = hyp_params["lm_weight"]
 
@@ -709,6 +711,7 @@ def recog_ffnn(
             assert model.recog_language_model
             assert model.recog_language_model.vocab_dim == model.target_dim
             lm: FeedForwardLm = model.recog_language_model
+        assert lm.conv_filter_size_dim.dimension == context_size
         # noinspection PyUnresolvedReferences
         lm_scale: float = hyp_params["lm_weight"]
     
@@ -735,10 +738,14 @@ def recog_ffnn(
 
     if lm_name is not None:
         with torch.no_grad():
+            unk_mask = rf.sparse_to_dense(
+                model.target_dim.vocab.label_to_id("<unk>"), axis=model.target_dim, label_value=float("-inf"), other_value=0.0
+            )
             lm_state = lm.default_initial_state(batch_dims=batch_dims_)  # Batch, InBeam, ...
             lm_logits, lm_state = get_lm_logits(batch_dims, target, lm, context_dim, lm_out_dim, lm_state)
             lm_logits = rf.gather(lm_logits, axis=lm_out_dim, indices=rf.last_frame_position_of_dim(lm_out_dim))
             assert lm_logits.dims == (*batch_dims_, model.target_dim)
+            lm_logits = lm_logits + unk_mask
             lm_log_probs = rf.log_softmax(lm_logits, axis=model.target_dim)  # Batch, InBeam, Vocab
             lm_log_probs *= lm_scale
             if label_prior and prior is not None:
@@ -851,6 +858,7 @@ def recog_ffnn(
                     lm_logits_, lm_state_ = get_lm_logits([packed_new_label_dim], target_, lm, context_dim, lm_out_dim, lm_state_)
                     lm_logits_ = rf.gather(lm_logits_, axis=lm_out_dim, indices=rf.last_frame_position_of_dim(lm_out_dim))
                     assert lm_logits_.dims == (packed_new_label_dim, model.target_dim)
+                    lm_logits_ = lm_logits_ + unk_mask
                     lm_log_probs_ = rf.log_softmax(lm_logits_, axis=model.target_dim)  # Flat_Batch_Beam, Vocab
                     lm_log_probs_ *= lm_scale
                     if label_prior and prior is not None:
@@ -962,6 +970,7 @@ def recog_gradients(
     
     use_ffnn_lm = isinstance(lm_name, str) and lm_name.startswith("ffnn")
     if use_ffnn_lm:
+        assert train_lm
         context_size = int(lm_name[len("ffnn"):])
         if train_lm:
             assert model.train_language_model
@@ -971,6 +980,7 @@ def recog_gradients(
             assert model.recog_language_model
             assert model.recog_language_model.vocab_dim == model.target_dim
             lm: FeedForwardLm = model.recog_language_model
+        assert lm.conv_filter_size_dim.dimension == context_size
     else:
         assert arpa_lm is not None
         assert isinstance(lm_name, int)
@@ -1017,25 +1027,79 @@ def recog_gradients(
         label_log_prob_raw = label_log_prob.raw_tensor
         label_log_prob_raw.requires_grad = True
         with torch.set_grad_enabled(True):
-            loss = sum_loss_ffnn(
-                model=model,
-                log_probs=label_log_prob,
-                lm=lm,
-                context_size=context_size,
-                log_prior=prior,
-                input_lengths=enc_spatial_dim,
-                top_k=beam_size,
-                am_scale=am_scale,
-                lm_scale=lm_scale,
-                prior_scale=prior_weight,
-                horizontal_prior=not label_prior,
-                blank_prior=not label_prior,
-                device=dev_s,
-                use_recombination=use_recombination,
-                recomb_blank=recomb_blank,
-                recomb_after_topk=recomb_after_topk,
-                recomb_with_sum=recomb_with_sum,
-            )
+            if beam_size == 0:
+                with torch.no_grad():
+                    context_dim = rf.Dim(context_size, name="context")
+                    lm_out_dim = rf.Dim(context_size + 1, name="context+1")
+                    target = torch.arange(model.target_dim.dimension, device=dev)
+                    if context_size == 2:
+                        target1 = target.unsqueeze(1).expand(model.target_dim.dimension, model.target_dim.dimension)
+                        target2 = target.unsqueeze(0).expand(model.target_dim.dimension, model.target_dim.dimension)
+                        target = torch.stack([target1, target2], dim=-1)
+                        batch_dims = [Dim(model.target_dim.dimension, name="v1"), Dim(model.target_dim.dimension, name="v2")]
+                    elif context_size == 1:
+                        target = target.unsqueeze(1)
+                        batch_dims = [Dim(model.target_dim.dimension, name="v1")]
+                    else:
+                        raise NotImplementedError(f"Full-sum on context size {context_size} not implemented")
+                    target = rf.convert_to_tensor(target, dims=batch_dims + [context_dim], sparse_dim=model.target_dim)
+                    lm_state = lm.default_initial_state(batch_dims=[])
+                    lm_logits, lm_state = get_lm_logits(batch_dims, target, lm, context_dim, lm_out_dim, lm_state)
+                    lm_logits = rf.gather(lm_logits, axis=lm_out_dim, indices=rf.last_frame_position_of_dim(lm_out_dim))
+                    assert lm_logits.dims == (*batch_dims, model.target_dim)
+                    lm_log_probs = rf.log_softmax(lm_logits, axis=model.target_dim)  # Batch, InBeam, Vocab
+                    lm_log_probs = lm_log_probs.raw_tensor
+                
+                log_prob_raw = label_log_prob_raw.permute(1, 0, 2)
+                loss = sum_loss_ngram(
+                    log_probs=log_prob_raw,
+                    log_lm_probs=lm_log_probs,
+                    log_prior=prior.raw_tensor,
+                    input_lengths=enc_spatial_dim.dyn_size_ext.raw_tensor,
+                    top_k=beam_size,
+                    LM_order=lm_log_probs.ndim,
+                    am_scale=am_scale,
+                    lm_scale=lm_scale,
+                    prior_scale=prior_weight,
+                    horizontal_prior=not label_prior,
+                    blank_prior=not label_prior,
+                    blank_idx=model.blank_idx,
+                    eos_idx=model.eos_idx,
+                    device=dev_s,
+                )
+                # print("LOSS2", loss.tolist())
+                # 1: [-137.5575714111328, -63.205379486083984, -93.46190643310547, -119.84935760498047, -119.04203796386719, -94.6239013671875]
+                # 2: [-160.18882751464844, -84.68201446533203, -103.0686264038086, -131.49192810058594, -134.6106414794922, -103.7826919555664]
+                # 5: [-166.92628479003906, -88.21932983398438, -103.35894012451172, -131.9812774658203, -134.9491424560547, -103.97010803222656]
+                # 64:[-168.89434814453125, -88.38604736328125, -103.4526596069336, -132.01197814941406, -135.21649169921875, -103.97358703613281]
+                # 0: [-168.90281677246094, -88.3888931274414, -103.45686340332031, -132.01219177246094, -135.2233428955078, -103.97364807128906]
+            else:
+                loss = sum_loss_ffnn(
+                    model=model,
+                    log_probs=label_log_prob,
+                    lm=lm,
+                    context_size=context_size,
+                    log_prior=prior,
+                    input_lengths=enc_spatial_dim,
+                    top_k=beam_size,
+                    am_scale=am_scale,
+                    lm_scale=lm_scale,
+                    prior_scale=prior_weight,
+                    horizontal_prior=not label_prior,
+                    blank_prior=not label_prior,
+                    device=dev_s,
+                    use_recombination=use_recombination,
+                    recomb_blank=recomb_blank,
+                    recomb_after_topk=recomb_after_topk,
+                    recomb_with_sum=recomb_with_sum,
+                )
+                
+                # print(loss.tolist())
+                # 1: [-116.46820068359375, -51.48450469970703, -86.56108856201172, -111.2684097290039, -114.91572570800781, -87.6579360961914]
+                # 2: [-156.4869384765625, -79.76004791259766, -99.71067810058594, -128.70213317871094, -131.5652313232422, -103.05582427978516]
+                # 5: [-164.3080596923828, -87.10165405273438, -101.98390197753906, -131.62818908691406, -134.31692504882812, -103.86699676513672]
+                # 64:[-167.51242065429688, -88.04119873046875, -103.08740997314453, -132.03030395507812, -135.0449676513672, -103.97388458251953]
+                # 0: [-5.465193748474121, -6.856778144836426, -6.993954181671143, -7.083093166351318, -7.361469268798828, -7.381970405578613]
             
             loss.backward(torch.ones_like(loss, device=dev))
             gradients = -label_log_prob_raw.grad

users/mueller/experiments/ctc_baseline/sum_criterion.py
@@ -1412,7 +1412,7 @@ def sum_loss_ffnn_exact(
         prev_target_wb = target_wb
         prev_beam = beam_dim
 
-        seq_log_prob = seq_log_prob + log_probs_ta[t]  # Batch, InBeam, VocabWB
+        seq_log_prob += log_probs_ta[t]  # Batch, InBeam, VocabWB
 
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
@@ -1437,7 +1437,9 @@ def sum_loss_ffnn_exact(
                     0.0,
                 )  # Batch, InBeam, VocabWB
             
-        beam_dim = rf.Dim(int(model.wb_target_dim.get_dim_value()) ** (t + 1 if t < context_size else context_size), name=f"dec-step{t}-beam")
+        if t < context_size:
+            beam_dim = rf.Dim(int(model.wb_target_dim.get_dim_value()) ** (t + 1 if t < context_size else context_size), name=f"dec-step{t}-beam")
+            new_Log_probs = torch.zeros((batch_dim.get_dim_value(), beam_dim.get_dim_value()), dtype=torch.float32, device=device)
         if t <= context_size:
             target_wb_tmp = rf.range_over_dim(model.wb_target_dim)
             target_wb_tmp = rf.expand_dims(target_wb_tmp, dims=batch_dims + [prev_beam])
@@ -1454,51 +1456,27 @@ def sum_loss_ffnn_exact(
                 ),
                 prev_target,
             )  # Batch, Beam -> Vocab
-            target_raveled = torch.tensor(
-                np.array(
-                    [
-                        [
-                            [
-                                np.ravel_multi_index(target.raw_tensor[b, k, :, v].cpu().numpy(), (model.target_dim.dimension,) * context_size)
-                                for v in range(target.dims[-1].get_dim_value())
-                            ]
-                            for k in range(prev_beam.get_dim_value())
-                        ]
-                        for b in range(batch_dim.get_dim_value())
-                    ]
-                ),
-                device=device
-            )
-            print(target_raveled.shape)
+            target_raveled = target.raw_tensor[:, :, 0, :] * model.target_dim.dimension ** (context_size - 1)
+            for i in range(context_size - 1):
+                target_raveled += target.raw_tensor[:, :, i + 1, :] * model.target_dim.dimension ** (context_size - i - 2)
+            target_raveled = target_raveled.long()
             target_raveled = target_raveled.view(batch_dim.get_dim_value(), -1)
             
             if t < context_size:
                 target_wb, _ = rf.merge_dims(target_wb_tmp, dims=[prev_beam, model.wb_target_dim], out_dim=beam_dim)
-            print(target_wb)
-        
-            target, _ = rf.merge_dims(target, dims = [prev_beam, model.wb_target_dim], out_dim = beam_dim)
-            got_new_label, _ = rf.merge_dims(got_new_label, dims = [prev_beam, model.wb_target_dim], out_dim = beam_dim)
+            
+                target, _ = rf.merge_dims(target, dims = [prev_beam, model.wb_target_dim], out_dim = beam_dim)
+                got_new_label, _ = rf.merge_dims(got_new_label, dims = [prev_beam, model.wb_target_dim], out_dim = beam_dim)
 
-        print(target.raw_tensor.shape, seq_log_prob.raw_tensor.shape)
-        
         seq_log_prob = seq_log_prob.raw_tensor.view(batch_dim.get_dim_value(), -1)
         
         # do a scatter logsumexp here together with the new target labels
-        new_Log_probs = torch.zeros((batch_dim.get_dim_value(), beam_dim.get_dim_value()), dtype=torch.float32, device=device)
-        print(new_Log_probs.shape)
-        
         seq_log_prob = scatter_safe_logsumexp(new_Log_probs, -1, target_raveled, seq_log_prob, include_self=False)
         seq_log_prob = rf.convert_to_tensor(seq_log_prob, dims=batch_dims + [beam_dim], dtype="float32", device=device, name="seq_log_prob")
-        print(seq_log_prob)
         
-        print(got_new_label)
-        print(target)
-
         if use_lm and t < context_size:
-            print(lm_log_probs)
             lm_log_probs = rf.expand_dim(lm_log_probs, dim=model.wb_target_dim)
             lm_log_probs, _ = rf.merge_dims(lm_log_probs, dims = [prev_beam, model.wb_target_dim], out_dim = beam_dim)
-            print(lm_log_probs, lm_log_probs.raw_tensor.shape)
             
             with torch.no_grad():
                 got_new_label_cpu = rf.copy_to_device(got_new_label, "cpu")
@@ -1648,7 +1626,7 @@ def get_lm_logits(batch_dims: list[rf.Dim], target: rf.Tensor, lm: FeedForwardLm
     while not done:
         try:
             if splits > 1:
-                batch_size = batch_dims[0].dyn_size_ext.raw_tensor.item()
+                batch_size = batch_dims[0].get_dim_value()
                 n_seqs = int(np.ceil(batch_size / splits))
                 new_dims = []
                 for i in range(splits):
@@ -1666,7 +1644,7 @@ def get_lm_logits(batch_dims: list[rf.Dim], target: rf.Tensor, lm: FeedForwardLm
                         state=lm_state,
                     )
                     lm_logits_split.append((lm_logits_i, new_dims[i]))
-                lm_logits = rf.concat(lm_logits_split, out_dim=batch_dims[0])
+                lm_logits, _ = rf.concat(*lm_logits_split, out_dim=batch_dims[0])
             else:
                 lm_logits, lm_state = lm(
                     target,
@@ -1675,14 +1653,14 @@ def get_lm_logits(batch_dims: list[rf.Dim], target: rf.Tensor, lm: FeedForwardLm
                     state=lm_state,
                 )
             done = True
-        except Exception as exc:
-            print(f"OOM with {splits} splits")
-            diagnose_gpu.garbage_collect()
-            splits *= 2
-            if splits <= batch_dims[0].dyn_size_ext.raw_tensor.item():
-                continue
-            else:
-                raise
+        except RuntimeError as exc:
+            if "out of memory" in str(exc):
+                print(f"OOM with {splits} splits:", exc)
+                diagnose_gpu.garbage_collect()
+                splits *= 2
+                if splits <= batch_dims[0].get_dim_value():
+                    continue
+            raise
     return lm_logits, lm_state
 
 def dynamic_slice(tensor: torch.Tensor, indices_list: list[torch.Tensor]) -> torch.Tensor:
@@ -2109,8 +2087,8 @@ def test():
     
     batch_size = 12 # 14000 per GPU, 1250 stpes a 12 seqs (0.7 sec/step)
     vocab_size = 185
-    frames = 100
-    LM_order = 2
+    frames = 300
+    LM_order = 3
     
     torch.manual_seed(0)
     # torch.cuda.manual_seed_all(0)
@@ -2139,6 +2117,8 @@ def test():
         # am[0, 0, vocab_size - 1] = float(3)
         # am[2, 0, vocab_size - 1] = float(3)
         am.requires_grad = True
+        
+        am_o = am
         
         am = am.permute(1, 0, 2)
         # am = ag(am, "logits", "/u/marten.mueller/dev/ctc_baseline/recipe/i6_experiments/users/mueller/experiments/ctc_baseline", False, None, True, 0, [],  ["Logits"], frames)
@@ -2225,6 +2205,7 @@ def test():
         
         
     l.backward(torch.ones_like(l, device=device))
+    print(am_o.grad)
     e1 = time.time()
     # print(f"Sum loss took {time.strftime('%H:%M:%S', time.gmtime(e1-s1))}: {l}") # 5:00 mins
     

users/mueller/experiments/ctc_baseline/training.py
@@ -43,6 +43,8 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
     log_probs = model.log_probs_wb_from_logits(logits)
     
     norm_dim = targets_spatial_dim
+    print_original_ctc = False
+    original_targets = None
     
     if seq_tags is not None:
         seq = "train-other-500/7492-105653-0055/7492-105653-0055"
@@ -60,15 +62,23 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
         hyperparameters = config.typed_value("hyperparameters_decoder").copy()
         prior_file = config.typed_value("empirical_prior")
         assert hyperparameters and prior_file
+        # print(log_probs.raw_tensor[0, 0].tolist())
         if "decay" in hyperparameters and hyperparameters["decay"] < 1.0:
-            print(hyperparameters)
             curr_step = rf.get_run_ctx().step
             assert isinstance(curr_step, int)
             decay = hyperparameters.pop("decay")
             decay_limit = hyperparameters.pop("decay_limit", 0.0)
             start_weight = hyperparameters["lm_weight"]
-            hyperparameters["lm_weight"] = decay_limit + ((start_weight - decay_limit) * decay ** curr_step)
-            print("LM weight:", hyperparameters["lm_weight"])
+            hyperparameters["lm_weight"] = 0.2 + (0.2 * decay ** curr_step)
+            # hyperparameters["lm_weight"] = 0.3
+            am_weight = 1.0 - (0.9 * 0.99997 ** curr_step)
+            dec_log_probs = log_probs * am_weight
+            hyperparameters["prior_weight"] = 0.3 - (0.2999 * 0.99999 ** curr_step)
+            # hyperparameters["prior_weight"] = 0.0
+            if curr_step % 100 == 0:
+                print("LM weight:", hyperparameters["lm_weight"], "Prior weight:", hyperparameters["prior_weight"], "AM weight:", am_weight)
+        else:
+            dec_log_probs = log_probs
         
         assert seq_tags is not None
         if save_seqs:
@@ -77,15 +87,19 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
                 hdf_writer = config.typed_value("train_hdf_writer")
                 assert isinstance(hdf_writer, SimpleHDFWriter)
         with torch.no_grad():
-            batch_size = log_probs.raw_tensor.shape[0]
+            batch_size = dec_log_probs.raw_tensor.shape[0]
             batch_dims = data.remaining_dims(data_spatial_dim)
-            hyps, new_scores = recog_ffnn(model=model, label_log_prob=log_probs, enc_spatial_dim=enc_spatial_dim, hyperparameters=hyperparameters, batch_dims=batch_dims, prior_file=prior_file, train_lm=True)
+            hyps, new_scores = recog_ffnn(model=model, label_log_prob=dec_log_probs, enc_spatial_dim=enc_spatial_dim, hyperparameters=hyperparameters, batch_dims=batch_dims, prior_file=prior_file, train_lm=True)
             assert len(hyps) == batch_size
             assert len(hyps[0]) == 1
             hyps = [convert_to_output_hyps(model, hyps_batch[0]) for hyps_batch in hyps]
             if len(hyps[0]) < 2:
                 print("SHORT HYP:", hyps[0])
                 # print("REFERENCE:", targets.raw_tensor[0].tolist())
+            else:
+                if curr_step % 20 == 0:
+                    print("HYP:", hyps[0])
+                    print("REFERENCE:", targets.raw_tensor[0].tolist())
             lengths = [len(h) for h in hyps]
             max_length = max(lengths)
             new_targets_spatial_dim = torch.tensor(lengths, dtype=torch.int32, device=data.raw_tensor.device)
@@ -123,6 +137,13 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
                 #     seq_len={0: [1]},
                 #     seq_tag=seq_tags.raw_tensor.tolist(),
                 # )
+                norm_dim = targets_spatial_dim
+            else:
+                print_original_ctc = True
+                original_targets = (targets, targets_spatial_dim)
+                targets = new_targets
+                targets_spatial_dim = new_targets_spatial_dim
+                norm_dim = targets_spatial_dim + 1
             
         # targets_ls = []
         # lengths_ls = []
@@ -146,7 +167,6 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
         # targets_spatial_dim = rf.convert_to_tensor(targets_spatial_dim, dims=(batch_dim,))
         # targets_spatial_dim = Dim(targets_spatial_dim, name="out_spatial", dyn_size_ext=targets_spatial_dim)
         # targets = rf.convert_to_tensor(targets, dims=(batch_dim, targets_spatial_dim), sparse_dim=model.target_dim)
-        norm_dim = targets_spatial_dim
     
     if nbest > 1 and rf.get_run_ctx().train_flag:
         assert nbest_lengths is not None
@@ -337,6 +357,8 @@ def ctc_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, targets: 
     )
     
     _seq_len_error(log_probs, model, targets_spatial_dim, enc_spatial_dim)
+    if print_original_ctc:
+        _ctc_error(log_probs, original_targets[0], model, original_targets[1], enc_spatial_dim)
 
     assert not model.decoder
     if model.decoder:
@@ -480,6 +502,7 @@ def full_sum_train(*, model: Model, data: rf.Tensor, data_spatial_dim: Dim, seq_
         assert model.train_language_model.vocab_dim == model.target_dim
         lm: FeedForwardLm = model.train_language_model
         lm_order = int(lm_name[len("ffnn"):])
+        assert lm.conv_filter_size_dim.dimension == lm_order
 
     collected_outputs = {}
     logits, enc, enc_spatial_dim = model(data, in_spatial_dim=data_spatial_dim, collected_outputs=collected_outputs)
@@ -1053,6 +1076,8 @@ def _LM_score(
     assert model.train_language_model
     assert model.train_language_model.vocab_dim == model.target_dim
     lm: FeedForwardLm = model.train_language_model
+    lm_order = int(lm_name[len("ffnn"):])
+    assert lm.conv_filter_size_dim.dimension == lm_order
     
     targets_w_eos, (targets_w_eos_spatial_dim,) = rf.pad(
         targets, axes=[targets_spatial_dim], padding=[(0, 1)], value=model.eos_idx
@@ -1193,6 +1218,8 @@ def _rescore(
     assert model.train_language_model
     assert model.train_language_model.vocab_dim == model.target_dim
     lm: FeedForwardLm = model.train_language_model
+    lm_order = int(lm_name[len("ffnn"):])
+    assert lm.conv_filter_size_dim.dimension == lm_order
     # noinspection PyUnresolvedReferences
     lm_scale: float = hyp_params["lm_weight"]
     
@@ -1257,9 +1284,13 @@ def _seq_len_error(log_probs, model, targets_spatial_dim, enc_spatial_dim):
             num_argmax_non_blank += len(seq_collapsed)
 
         num_target_non_blank = targets_spatial_dim.dyn_size_ext.raw_tensor.sum()
+        if num_target_non_blank > 0:
+            fraction = num_argmax_non_blank / num_target_non_blank
+        else:
+            fraction = float(num_argmax_non_blank)
 
         greedy_non_blank_to_ground_truth_fraction = rf.convert_to_tensor(
-            num_argmax_non_blank / num_target_non_blank,
+            fraction,
         )
         greedy_non_blank_to_ground_truth_fraction.mark_as_loss("greedy_non_blank_to_ground_truth_fraction_error", as_error=True)
     

users/mueller/experiments/language_models/ffnn.py
@@ -15,8 +15,7 @@ import returnn.frontend as rf
 import returnn.torch.frontend as rtf
 
 from i6_experiments.users.zeyer.model_interfaces import ModelDefWithCfg, TrainDef, ModelDef
-
-from ..ctc_baseline.configs import (
+from i6_experiments.users.mueller.experiments.ctc_baseline.configs import (
     config_11gb_v6_f32_accgrad1_mgpu4_pavg100_wd1e_4,
 )
 from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.lm import _get_cfg_lrlin_oclr_by_bs_nep
@@ -115,9 +114,6 @@ class FFNN_LM_flashlight(CTCDecoderLM):
     
     
 # ---------------------------------------------------
-
-
-
     
 def get_ffnn_lm(vocab: Bpe, context_size: int, num_layers: int = 2, ff_hidden_dim: int = 2048, dropout: float = 0.0, embed_dropout: float = 0.0) -> ModelWithCheckpoint:
     from i6_experiments.users.mueller.train import train