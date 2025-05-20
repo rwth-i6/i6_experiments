03452d28a5c0752eeba7cabee8ce36d1c7de0e75
Albert Zeyer <albzey@gmail.com>
2025-05-20 10:26:50 +0200

    move OpenASRLeaderboardTextNormalizationJob

---
+++ users/zeyer/datasets/huggingface/open_asr_leaderboard.py
@@ -0,0 +1,80 @@
+import functools
+from sisyphus import Job, Task, tk
+from i6_core.util import uopen
+
+
+def text_dict_normalize_file(text: tk.Path) -> tk.Path:
+    """
+    :param text: in textdict format
+    :return: normalized text, in textdict format
+    """
+    from i6_core.returnn.search import SearchOutputRawReplaceJob
+
+    text = SearchOutputRawReplaceJob(text, replacement_list=[(" c o ", " co ")]).out_search_results
+    text = OpenASRLeaderboardTextNormalizationJob(
+        text, open_asr_leaderboard_repo_dir=get_open_asr_leaderboard_repo_dir()
+    ).out_text
+    return text
+
+
+@functools.cache
+def get_open_asr_leaderboard_repo_dir() -> tk.Path:
+    """
+    :return: path to the open_asr_leaderboard repo
+    """
+    from i6_core.tools.git import CloneGitRepositoryJob
+
+    open_asr_leaderboard_repo = CloneGitRepositoryJob(
+        "https://github.com/huggingface/open_asr_leaderboard.git",
+        # 2025-05-01 (includes parakeet-v2, phi4mi, ...)
+        commit="2472403cae1434752b7448b8f7cda560bd549e0f",
+    )
+    tk.register_output("open_asr_leaderboard_repo", open_asr_leaderboard_repo.out_repository)
+    return open_asr_leaderboard_repo.out_repository
+
+
+class OpenASRLeaderboardTextNormalizationJob(Job):
+    """
+    https://github.com/huggingface/open_asr_leaderboard/blob/main/normalizer/data_utils.py
+    """
+
+    __sis_version__ = 2
+
+    def __init__(self, text: tk.Path, *, open_asr_leaderboard_repo_dir: tk.Path):
+        """
+        :param text: e.g. via ExtractTextFromHuggingFaceEsbDatasetJob
+        """
+        super().__init__()
+        self.text = text
+        self.open_asr_leaderboard_repo_dir = open_asr_leaderboard_repo_dir
+
+        self.rqmt = {"time": 4, "cpu": 2, "mem": 10}
+
+        self.out_text = self.output_path("normalized.txt.py.gz")
+
+    def tasks(self):
+        yield Task("run", rqmt=self.rqmt)
+
+    def run(self):
+        import sys
+        import types
+
+        sys.path.insert(0, self.open_asr_leaderboard_repo_dir.get_path())
+
+        # normalizer.eval_utils does `import evaluate`, which is not needed by us,
+        # so do this hack to just ignore it, without needing the dependency
+        sys.modules["evaluate"] = types.ModuleType("<dummy_evaluate>")
+
+        from normalizer.data_utils import normalizer
+
+        with uopen(self.text.get_path(), "rt") as in_:
+            in_text = eval(in_.read())
+        assert isinstance(in_text, dict)
+
+        with uopen(self.out_text.get_path(), "wt") as out:
+            out.write("{\n")
+            for seq_tag, text in in_text.items():
+                assert isinstance(text, str)
+                norm_text = normalizer(text)
+                out.write(f"{seq_tag!r}: {norm_text!r},\n")
+            out.write("}\n")
--- users/zeyer/experiments/exp2025_05_05_align.py
+++ users/zeyer/experiments/exp2025_05_05_align.py
@@ -4,7 +4,6 @@ Continuation of :mod:`exp24_09_16_grad_align`.
 
 from __future__ import annotations
 
-import functools
 from typing import TYPE_CHECKING, Optional, Union, Any, Dict, List, Tuple
 from sisyphus import tk, Job, Task
 from i6_experiments.users.zeyer.external_models.huggingface import (
@@ -13,8 +12,6 @@ from i6_experiments.users.zeyer.external_models.huggingface import (
     get_content_dir_from_hub_cache_dir,
 )
 from i6_core.datasets.huggingface import DownloadAndPrepareHuggingFaceDatasetJob
-from i6_core.tools.git import CloneGitRepositoryJob
-from i6_core.util import uopen
 
 if TYPE_CHECKING:
     import torch
@@ -343,77 +340,6 @@ def py():
     # TODO test https://huggingface.co/nvidia/parakeet-tdt-0.6b-v2? other recent interesting models?
 
     # TODO AtP https://arxiv.org/pdf/2403.00745?
-
-
-def text_normalize_file(text: tk.Path) -> tk.Path:
-    from i6_core.returnn.search import SearchOutputRawReplaceJob
-
-    text = SearchOutputRawReplaceJob(text, replacement_list=[(" c o ", " co ")]).out_search_results
-    text = OpenASRLeaderboardTextNormalizationJob(
-        text, open_asr_leaderboard_repo_dir=get_open_asr_leaderboard_repo_dir()
-    ).out_text
-    return text
-
-
-@functools.cache
-def get_open_asr_leaderboard_repo_dir() -> tk.Path:
-    """
-    :return: path to the open_asr_leaderboard repo
-    """
-    open_asr_leaderboard_repo = CloneGitRepositoryJob(
-        "https://github.com/huggingface/open_asr_leaderboard.git",
-        # 2025-05-01 (includes parakeet-v2, phi4mi, ...)
-        commit="2472403cae1434752b7448b8f7cda560bd549e0f",
-    )
-    tk.register_output("open_asr_leaderboard_repo", open_asr_leaderboard_repo.out_repository)
-    return open_asr_leaderboard_repo.out_repository
-
-
-class OpenASRLeaderboardTextNormalizationJob(Job):
-    """
-    https://github.com/huggingface/open_asr_leaderboard/blob/main/normalizer/data_utils.py
-    """
-
-    __sis_version__ = 2
-
-    def __init__(self, text: tk.Path, *, open_asr_leaderboard_repo_dir: tk.Path):
-        """
-        :param text: e.g. via ExtractTextFromHuggingFaceEsbDatasetJob
-        """
-        super().__init__()
-        self.text = text
-        self.open_asr_leaderboard_repo_dir = open_asr_leaderboard_repo_dir
-
-        self.rqmt = {"time": 4, "cpu": 2, "mem": 10}
-
-        self.out_text = self.output_path("normalized.txt.py.gz")
-
-    def tasks(self):
-        yield Task("run", rqmt=self.rqmt)
-
-    def run(self):
-        import sys
-        import types
-
-        sys.path.insert(0, self.open_asr_leaderboard_repo_dir.get_path())
-
-        # normalizer.eval_utils does `import evaluate`, which is not needed by us,
-        # so do this hack to just ignore it, without needing the dependency
-        sys.modules["evaluate"] = types.ModuleType("<dummy_evaluate>")
-
-        from normalizer.data_utils import normalizer
-
-        with uopen(self.text.get_path(), "rt") as in_:
-            in_text = eval(in_.read())
-        assert isinstance(in_text, dict)
-
-        with uopen(self.out_text.get_path(), "wt") as out:
-            out.write("{\n")
-            for seq_tag, text in in_text.items():
-                assert isinstance(text, str)
-                norm_text = normalizer(text)
-                out.write(f"{seq_tag!r}: {norm_text!r},\n")
-            out.write("}\n")
 
 
 class GenAya(Job):
