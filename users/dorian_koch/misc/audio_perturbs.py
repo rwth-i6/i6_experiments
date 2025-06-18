1f87b966ba316a233985d10d518c09ef8b99417a
Dorian Koch <dorian.koch@ml.rwth-aachen.de>
2025-06-18 10:17:23 +0200

    fix mixup

--- users/dorian_koch/misc/audio_perturbs.py
+++ users/dorian_koch/misc/audio_perturbs.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from i6_experiments.users.zeyer.nn_rf.mixup import MixupOpts
 from returnn.frontend import Tensor, Dim
 
 import returnn.frontend as rf
@@ -131,3 +132,123 @@ def generalized_specaugment(
         _mask_branch,
         lambda: x,
     )
+
+
+class MixupWithBugsFixed(rf.Module):
+    """
+    see i6_experiments/users/zeyer/nn_rf/mixup.py
+
+    Changes:
+    - use appropriate trainflag
+    - fix scatter logic at the end
+    - (1-lambda)src + lambda * mixup instead of src + lambda * mixup
+    """
+
+    def __init__(self, *, feature_dim: Dim, opts: MixupOpts):
+        super().__init__()
+        self.feature_dim = feature_dim
+        self.opts = opts
+        self.buffer_size_dim = Dim(opts.buffer_size, name="buffer_size")
+        self.buffer = rf.Parameter([self.buffer_size_dim, feature_dim], auxiliary=True)
+        self.buffer_pos = rf.Parameter(
+            [], dtype="int32", sparse_dim=self.buffer_size_dim, initial=0, auxiliary=True, device="cpu"
+        )
+        self.buffer_filled = rf.Parameter([], dtype="bool", initial=False, auxiliary=True, device="cpu")
+
+    def __call__(self, src: Tensor, *, spatial_dim: Dim) -> Tensor:
+        if not rf.get_run_ctx().is_train_flag_enabled(func=MixupWithBugsFixed.__call__):
+            return src
+
+        assert spatial_dim in src.dims and self.feature_dim in src.dims
+
+        # Apply mixup before we add the new data to the buffer.
+        src_ = self._maybe_apply_mixup(src, spatial_dim=spatial_dim)
+
+        self._append_to_buffer(src, spatial_dim=spatial_dim)
+
+        return src_
+
+    def _append_to_buffer(self, src: Tensor, *, spatial_dim: Dim):
+        batch_dims = src.remaining_dims((spatial_dim, self.feature_dim))
+        opts = self.opts
+
+        # Fill buffer with new data:
+        src_flat, src_flat_dim = rf.pack_padded(src, dims=[*batch_dims, spatial_dim])
+        new_pos = rf.minimum(self.buffer_pos + src_flat_dim.get_size_tensor(), opts.buffer_size)
+        part_fill_len = new_pos - self.buffer_pos
+        src_flat_part, src_flat_part_dim = rf.slice(src_flat, axis=src_flat_dim, end=part_fill_len)
+        self.buffer.assign_key(
+            axis=self.buffer_size_dim,
+            key=slice(self.buffer_pos, new_pos),
+            key_dim=src_flat_part_dim,
+            value=src_flat_part,
+        )
+        if (self.buffer_pos + src_flat_dim.get_size_tensor() >= opts.buffer_size).raw_tensor:
+            self.buffer_filled.assign(True)
+            part_fill_len_ = rf.minimum(src_flat_dim.get_size_tensor() - part_fill_len, opts.buffer_size)
+            src_flat_part, src_flat_part_dim = rf.slice(
+                src_flat, axis=src_flat_dim, start=part_fill_len, end=part_fill_len + part_fill_len_
+            )
+            self.buffer.assign_key(
+                axis=self.buffer_size_dim, key=slice(0, part_fill_len_), key_dim=src_flat_part_dim, value=src_flat_part
+            )
+            new_pos = part_fill_len_
+        self.buffer_pos.assign(new_pos)
+
+    def _maybe_apply_mixup(self, src: Tensor, *, spatial_dim: Dim) -> Tensor:
+        batch_dims = src.remaining_dims((spatial_dim, self.feature_dim))
+        opts = self.opts
+
+        if (rf.random_uniform((), device="cpu") >= opts.apply_prob).raw_tensor:
+            return src
+
+        buffer_filled_size = rf.where(self.buffer_filled, opts.buffer_size, self.buffer_pos)
+        if (buffer_filled_size < spatial_dim.get_dim_value_tensor()).raw_tensor:
+            return src
+
+        # Apply Mixup. Collect all data we are going to add for each sequence.
+        num_mixup = rf.random_uniform(
+            batch_dims, minval=1, maxval=opts.max_num_mix + 1, dtype="int32", device="cpu"
+        )  # [B]
+        num_mixup_dim = Dim(num_mixup, name="num_mixup")
+
+        buffer_start = rf.random_uniform(
+            [*batch_dims, num_mixup_dim],
+            maxval=buffer_filled_size - spatial_dim.get_dim_value_tensor() + 1,
+            dtype="int32",
+            sparse_dim=self.buffer_size_dim,
+        )  # [B, N]
+        n_mask = rf.sequence_mask(num_mixup_dim)  # [B, N]
+        buffer_start_flat, num_mixup_flat_dim = rf.masked_select(
+            buffer_start, mask=n_mask, dims=[*batch_dims, num_mixup_dim]
+        )  # [B_N']
+
+        idx = rf.range_over_dim(spatial_dim)  # [T]
+        idx = rf.combine_bc(idx, "+", buffer_start_flat)  # [B_N', T]
+
+        mixup_values = rf.gather(self.buffer, indices=idx, axis=self.buffer_size_dim)  # [B_N', T, F]
+
+        # Scale the mixup values.
+        lambda_ = rf.random_uniform(batch_dims, minval=opts.lambda_min, maxval=opts.lambda_max, dtype=src.dtype)
+        mixup_scales = rf.random_uniform([*batch_dims, num_mixup_dim], minval=0.001, maxval=1.0, dtype=src.dtype)
+        mixup_scales /= rf.reduce_sum(mixup_scales, axis=num_mixup_dim)  # [B,N]
+        mixup_scales_flat, _ = rf.masked_select(
+            mixup_scales, mask=n_mask, dims=[*batch_dims, num_mixup_dim], out_dim=num_mixup_flat_dim
+        )  # [B_N']
+        mixup_values *= mixup_scales_flat  # [B_N', T, F]
+
+        mixup_value = rf.masked_scatter(
+            mixup_values, mask=n_mask, dims=[*batch_dims, num_mixup_dim], in_dim=num_mixup_flat_dim
+        )
+        mixup_value = rf.reduce_sum(mixup_value, axis=num_mixup_dim)
+        # idx_b = rf.range_over_merged_dims(batch_dims)  # [B] -> B
+        # idx_b, _ = rf.masked_select(
+        #    idx_b, mask=n_mask, dims=batch_dims + [num_mixup_dim], out_dim=num_mixup_flat_dim
+        # )  # [B_N'] -> B
+
+        # mixup_value = rf.scatter(
+        #    mixup_values, indices=idx_b, indices_dim=num_mixup_flat_dim, out_dim=batch_dims
+        # )  # [B,T,F]
+
+        src = (1 - lambda_) * src + lambda_ * rf.stop_gradient(mixup_value)
+        return src
