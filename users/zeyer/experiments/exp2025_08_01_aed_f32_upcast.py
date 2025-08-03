cd025396c8eb18d34972106ce09e27480e7d90ed
Albert Zeyer <albzey@gmail.com>
2025-08-03 13:03:40 +0200

    RMSNormF32

--- users/zeyer/experiments/exp2025_08_01_aed_f32_upcast.py
+++ users/zeyer/experiments/exp2025_08_01_aed_f32_upcast.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 
+from typing import Union, Sequence
 from i6_experiments.users.zeyer.utils.sis_setup import get_setup_prefix_for_module
 
 from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.aed import (
@@ -15,6 +16,7 @@ from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.configs impo
 
 from i6_experiments.users.zeyer.speed_pert.librosa_config import speed_pert_librosa_config
 
+from returnn.tensor import Tensor
 import returnn.frontend as rf
 from returnn.frontend.decoder.transformer import TransformerDecoder
 from returnn.frontend.encoder.conformer import (
@@ -212,7 +214,7 @@ def py():
         )
 
     aed_train_exp(
-        "EncL16-DecL6-D1024-DecPosEncAbs-spm10k-bpeSample001-baseLr0.5-b100k-accGrad1",
+        "EncL16-DecL6-D1024-DecPosEncAbs-spm10k-bpeSample001-baseLr0.5-b100k",
         config_96gb_bf16_accgrad1,
         prefix=prefix + "/aed/",
         model_config={
@@ -271,8 +273,99 @@ def py():
         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
     )
 
+    aed_train_exp(
+        "EncL16-DecL6-D1024-DecPosEncAbs-rmsF32-spm10k-bpeSample001-baseLr0.5-b100k",
+        config_96gb_bf16_accgrad1,
+        prefix=prefix + "/aed/",
+        model_config={
+            "enc_build_dict": rf.build_dict(
+                ConformerEncoder,
+                input_layer=rf.build_dict(
+                    ConformerConvSubsample,
+                    out_dims=[32, 64, 64],
+                    filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                    pool_sizes=[(1, 2)],
+                    strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                ),
+                num_layers=16,
+                out_dim=1024,
+                encoder_layer=rf.build_dict(
+                    ConformerEncoderLayer,
+                    ff=rf.build_dict(
+                        ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                    ),
+                    num_heads=8,
+                ),
+            ),
+            # Default AED decoder size: 6 layers, 512 dim
+            # The DLM decoder size: 16 layers, 1024 dim (but that is trained on all DLM data)
+            "dec_build_dict": rf.build_dict(
+                TransformerDecoder,
+                num_layers=6,
+                model_dim=1024,
+                norm=rf.build_dict(RMSNormF32),
+                ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                # When only trained on LS ASR data, keep the default dropout?
+                # dropout=0.0,
+                # att_dropout=0.0,
+            ),
+        },
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+            "batch_size": 100_000 * _batch_size_factor,
+            "optimizer.weight_decay": 1e-2,
+            "accum_grad_multiple_step": 1,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "max_seq_length_default_target": None,
+            # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+            # out of 281241 seqs in train, we removed only 71 seqs.
+            # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+            "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+        },
+        # config_deletes=["torch_amp"],
+        post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+        vocab="spm10k",
+        # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+        train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+
     # TODO go through https://github.com/rwth-i6/returnn/issues/1741, test each
     #  how?
     #  hook get_model?
     #  then go through model submodules, check for LayerNorm etc, hook them?
     #  also hook SelfAttention or dot_attention or so? or monkey patch them?
+
+
+class RMSNormF32(rf.Module):
+    """
+    `Root Mean Square Layer Normalization (RMSNorm) <https://arxiv.org/abs/1910.07467>`__.
+
+    Alternative to :class:`LayerNorm` that uses the root-mean-square of the input as the normalization factor.
+    I.e. the main difference to layer norm is: *No subtraction of mean.*
+
+    Note, the bias here is optional, *and disabled by default* (in line with most implementations of RMSNorm),
+    unlike our :class:`LayerNorm`, where the bias is enabled by default.
+    """
+
+    def __init__(self, in_dim: Union[rf.Dim, Sequence[rf.Dim]], *, eps: float = 1e-6, with_bias: bool = False):
+        super().__init__()
+        self.in_dim = in_dim
+        self.eps = eps
+        self.scale = rf.Parameter([self.in_dim] if isinstance(self.in_dim, rf.Dim) else self.in_dim)
+        self.scale.initial = 1.0
+        self.bias = None
+        if with_bias:
+            self.bias = rf.Parameter(self.scale.dims)
+            self.bias.initial = 0.0
+
+    def __call__(self, x: Tensor) -> Tensor:
+        variance = rf.reduce_mean(rf.square(rf.cast(x, "float32")), axis=self.in_dim)
+        norm_x = x * rf.rsqrt(variance + self.eps)
+        out = norm_x * self.scale
+        if self.bias is not None:
+            out += self.bias
+        return out
