597bfcf907738f6786ba3aebd5192f86491901b5
Albert Zeyer <albzey@gmail.com>
2025-08-08 01:24:43 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -511,10 +511,10 @@ def py():
 
     # DecPosEncAbs: TODO also try in encoder (EncPosEncAbs)...
 
-    # Try different decoder dropout. (Default is dropout 0.1.)
-    for dec_drop in [0.2]:
+    # Try different decoder dropout. (Default is dropout 0.1, att_dropout 0.1.)
+    for dec_drop, dec_att_drop in [(0.2, 0.2), (0.1, 0.2), (0.2, 0.1)]:
         aed_train_exp(
-            "EncL16-DecL6-D1024-DecPosEncAbs-DecDrop02-spm10k-bpeSample001-baseLr0.5-b100k",
+            f"EncL16-DecL6-D1024-DecPosEncAbs-DecDrop{dec_drop}-DecAttDrop{dec_att_drop}-spm10k-bpeSample001-baseLr0.5-b100k",
             config_96gb_bf16_accgrad1,
             prefix=prefix + "/aed/",
             model_config={
@@ -546,7 +546,67 @@ def py():
                     ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
                     layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
                     dropout=dec_drop,
-                    att_dropout=dec_drop,
+                    att_dropout=dec_att_drop,
+                ),
+            },
+            config_updates={
+                **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+                "batch_size": 100_000 * _batch_size_factor,
+                "optimizer.weight_decay": 1e-2,
+                "accum_grad_multiple_step": 1,
+                "__train_audio_preprocess": speed_pert_librosa_config,
+                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+                "max_seq_length_default_target": None,
+                # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+                # out of 281241 seqs in train, we removed only 71 seqs.
+                # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+                "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+            },
+            # config_deletes=["torch_amp"],
+            post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+            vocab="spm10k",
+            # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+            train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+            dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+            env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+        )
+
+    # Try different dec model dim.
+    for dec_model_dim in [512, 640, 768, 1024]:
+        aed_train_exp(
+            f"EncL16-DecL6-D1024-DecD{dec_model_dim}-DecPosEncAbs-spm10k-bpeSample001-baseLr0.5-b100k",
+            config_96gb_bf16_accgrad1,
+            prefix=prefix + "/aed/",
+            model_config={
+                "enc_build_dict": rf.build_dict(
+                    ConformerEncoder,
+                    input_layer=rf.build_dict(
+                        ConformerConvSubsample,
+                        out_dims=[32, 64, 64],
+                        filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                        pool_sizes=[(1, 2)],
+                        strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                    ),
+                    num_layers=16,
+                    out_dim=1024,
+                    encoder_layer=rf.build_dict(
+                        ConformerEncoderLayer,
+                        ff=rf.build_dict(
+                            ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                        ),
+                        num_heads=8,
+                    ),
+                ),
+                # Default AED decoder size: 6 layers, 512 dim
+                "dec_build_dict": rf.build_dict(
+                    TransformerDecoder,
+                    num_layers=6,
+                    model_dim=dec_model_dim,
+                    norm=rf.build_dict(rf.RMSNorm),
+                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                    layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                    dropout=0.1,
+                    att_dropout=0.1,
                 ),
             },
             config_updates={
