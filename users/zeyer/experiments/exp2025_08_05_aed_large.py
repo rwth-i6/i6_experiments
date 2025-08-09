81dc45281cc437595b64d318a8ea71c06b70f562
Albert Zeyer <albzey@gmail.com>
2025-08-10 01:10:57 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -4,6 +4,7 @@ New AED baseline tuning for larger AED model.
 
 from __future__ import annotations
 
+import functools
 from i6_experiments.users.zeyer.utils.sis_setup import get_setup_prefix_for_module
 
 from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.aed import (
@@ -862,6 +863,72 @@ def py():
             dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
             env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
         )
+
+    from i6_experiments.users.zeyer.nn_rf.layerdrop import SequentialLayerDrop
+
+    # Try stochastic depth in decoder.
+    for layer_drop in [0.1, 0.2]:
+        aed_train_exp(
+            f"EncL16-DecL6-D1024-DecPosEncAbs-DecLayerDrop{layer_drop}-spm10k-bpeSample001-baseLr0.5-b100k",
+            config_96gb_bf16_accgrad1,
+            prefix=prefix + "/aed/",
+            model_config={
+                "enc_build_dict": rf.build_dict(
+                    ConformerEncoder,
+                    input_layer=rf.build_dict(
+                        ConformerConvSubsample,
+                        out_dims=[32, 64, 64],
+                        filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                        pool_sizes=[(1, 2)],
+                        strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                    ),
+                    num_layers=16,
+                    out_dim=1024,
+                    encoder_layer=rf.build_dict(
+                        ConformerEncoderLayer,
+                        ff=rf.build_dict(
+                            ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                        ),
+                        num_heads=8,
+                    ),
+                ),
+                # Default AED decoder size: 6 layers, 512 dim
+                "dec_build_dict": rf.build_dict(
+                    TransformerDecoder,
+                    num_layers=6,
+                    model_dim=1024,
+                    norm=rf.build_dict(rf.RMSNorm),
+                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                    layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                    sequential=functools.partial(SequentialLayerDrop, layer_drop=layer_drop),
+                    # When only trained on LS ASR data, keep the default dropout?
+                    # dropout=0.0,
+                    # att_dropout=0.0,
+                ),
+            },
+            config_updates={
+                **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+                "batch_size": 100_000 * _batch_size_factor,
+                "optimizer.weight_decay": 1e-2,
+                "accum_grad_multiple_step": 1,
+                "__train_audio_preprocess": speed_pert_librosa_config,
+                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+                "max_seq_length_default_target": None,
+                # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+                # out of 281241 seqs in train, we removed only 71 seqs.
+                # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+                "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+            },
+            # config_deletes=["torch_amp"],
+            post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+            vocab="spm10k",
+            # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+            train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+            dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+            env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+        )
+
+    # TODO lstm decoder
 
     # TODO prior/ILM?
     # TODO recog, also with LM, maybe ILM
