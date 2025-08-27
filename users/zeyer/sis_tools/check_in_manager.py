3fe3989275fd4648ea7e7381cfd9c13e9d1a52ca
Albert Zeyer <albzey@gmail.com>
2025-08-27 11:22:28 +0200

    check_in_manager fix

--- users/zeyer/sis_tools/check_in_manager.py
+++ users/zeyer/sis_tools/check_in_manager.py
@@ -2,8 +2,8 @@
 Check whether we are in manager
 """
 
-import threading
-from sisyphus.manager import Manager
+import sys
+import sisyphus.manager
 
 
 def check_in_manager() -> bool:
@@ -16,9 +16,14 @@ def check_in_manager() -> bool:
     via some external scripts, or Sis console, or so.
     We really only return True here when we detect the manager.
 
-    The manager is detected currently by checking the threads.
+    The manager is detected currently by checking whether :func:`sisyphus.manager.manager`
+    is in the call stack.
     """
-    for thread in threading.enumerate():
-        if isinstance(thread, Manager):
+    manager_func = sisyphus.manager.manager
+    frame = sys._getframe()
+    assert frame
+    while frame:
+        if frame.f_code is manager_func.__code__:
             return True
+        frame = frame.f_back
     return False
