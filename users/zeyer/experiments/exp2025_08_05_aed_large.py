0b29d0be80ad35f0e6bda3023674812fe935d151
Albert Zeyer <albzey@gmail.com>
2025-08-29 10:38:02 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -1044,6 +1044,10 @@ def py():
     # 2. To see how much it changes by a second run (hopefully not much).
     # (Note, the "-s2" at the end is to have a different name, to not use the same alias.
     #  but for other experiments, we would not add the "-s2".)
+    # baseline (s1): {"dev-clean": 2.81, "dev-other": 4.72, "test-clean": 2.86, "test-other": 5.08}
+    #           s2:  {"dev-clean": 3.09, "dev-other": 4.97, "test-clean": 3.49, "test-other": 5.40}
+    # This is a very unfortunate result...
+    # It is also not exactly the same... e.g. behavior_version 21 -> 24, num_workers 25 -> 4
     aed_train_exp(
         "EncL16-DecL6-D1024-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k-s2",
         config_96gb_bf16_accgrad1,
@@ -1051,6 +1055,69 @@ def py():
         model_config={
             # More futureproof, but also required for some funcs / setups.
             "behavior_version": 24,
+            "__serialization_version": 2,
+            "enc_build_dict": rf.build_dict(
+                ConformerEncoder,
+                input_layer=rf.build_dict(
+                    ConformerConvSubsample,
+                    out_dims=[32, 64, 64],
+                    filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                    pool_sizes=[(1, 2)],
+                    strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                ),
+                num_layers=16,
+                out_dim=1024,
+                encoder_layer=rf.build_dict(
+                    ConformerEncoderLayer,
+                    ff=rf.build_dict(
+                        ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                    ),
+                    num_heads=8,
+                ),
+            ),
+            # Default AED decoder size: 6 layers, 512 dim
+            "dec_build_dict": rf.build_dict(
+                TransformerDecoder,
+                num_layers=6,
+                model_dim=1024,
+                norm=rf.build_dict(rf.RMSNorm),
+                ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                # When only trained on LS ASR data, keep the default dropout?
+                # dropout=0.0,
+                # att_dropout=0.0,
+            ),
+            "feature_batch_norm": True,
+        },
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+            "batch_size": 100_000 * _batch_size_factor,
+            "optimizer.weight_decay": 1e-2,
+            "accum_grad_multiple_step": 1,
+            "__train_audio_preprocess": speed_pert_librosa_config,
+            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "aux_loss_layers": [4, 10, 16],
+            "max_seq_length_default_target": None,
+            # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+            # out of 281241 seqs in train, we removed only 71 seqs.
+            # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+            "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+        },
+        post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+        vocab="spm10k",
+        # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+        train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+
+    # For comparison, behavior version 21 again.
+    aed_train_exp(
+        "EncL16-DecL6-D1024-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k-s2-bhv21",
+        config_96gb_bf16_accgrad1,
+        prefix=prefix + "/aed/",
+        model_config={
+            "behavior_version": 21,
             "__serialization_version": 2,
             "enc_build_dict": rf.build_dict(
                 ConformerEncoder,
