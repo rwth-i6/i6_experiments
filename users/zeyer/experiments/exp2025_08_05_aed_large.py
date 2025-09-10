fe9460c79d8b8818f77705bbf17ddb5568d5565d
Albert Zeyer <albzey@gmail.com>
2025-09-10 17:01:32 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -789,65 +789,73 @@ def py():
     # label smoothing
     # 0.1: {"dev-clean": 2.38, "dev-other": 4.87, "test-clean": 2.89, "test-other": 5.35}
     # 0.2: {"dev-clean": 3.43, "dev-other": 5.82, "test-clean": 3.55, "test-other": 6.06}
-    # for label_smoothing in [0.1, 0.2]:
-    #     aed_train_exp(
-    #         f"EncL16-DecL6-D1024-DecPosEncAbs-ls{label_smoothing}-spm10k-bpeSample001-baseLr0.5-b100k",
-    #         config_96gb_bf16_accgrad1,
-    #         prefix=prefix + "/aed/",
-    #         model_config={
-    #             "enc_build_dict": rf.build_dict(
-    #                 ConformerEncoder,
-    #                 input_layer=rf.build_dict(
-    #                     ConformerConvSubsample,
-    #                     out_dims=[32, 64, 64],
-    #                     filter_sizes=[(3, 3), (3, 3), (3, 3)],
-    #                     pool_sizes=[(1, 2)],
-    #                     strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
-    #                 ),
-    #                 num_layers=16,
-    #                 out_dim=1024,
-    #                 encoder_layer=rf.build_dict(
-    #                     ConformerEncoderLayer,
-    #                     ff=rf.build_dict(
-    #                         ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
-    #                     ),
-    #                     num_heads=8,
-    #                 ),
-    #             ),
-    #             # Default AED decoder size: 6 layers, 512 dim
-    #             "dec_build_dict": rf.build_dict(
-    #                 TransformerDecoder,
-    #                 num_layers=6,
-    #                 model_dim=1024,
-    #                 norm=rf.build_dict(rf.RMSNorm),
-    #                 ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
-    #                 layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
-    #                 # When only trained on LS ASR data, keep the default dropout?
-    #                 # dropout=0.0,
-    #                 # att_dropout=0.0,
-    #             ),
-    #         },
-    #         config_updates={
-    #             **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
-    #             "batch_size": 100_000 * _batch_size_factor,
-    #             "optimizer.weight_decay": 1e-2,
-    #             **({"label_smoothing": label_smoothing} if label_smoothing != 0.1 else {}),
-    #             "accum_grad_multiple_step": 1,
-    #             "__train_audio_preprocess": speed_pert_librosa_config,
-    #             "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-    #             "max_seq_length_default_target": None,
-    #             # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
-    #             # out of 281241 seqs in train, we removed only 71 seqs.
-    #             # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
-    #             "max_seq_length_default_input": 19.5 * _raw_sample_rate,
-    #         },
-    #         post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
-    #         vocab="spm10k",
-    #         # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
-    #         train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
-    #         dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
-    #         env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
-    #     )
+    # TODO put CTC results, comment then out again...
+    for label_smoothing in [0.1, 0.2]:
+        name = f"EncL16-DecL6-D1024-DecPosEncAbs-ls{label_smoothing}-spm10k-bpeSample001-baseLr0.5-b100k"
+        exp = aed_train_exp(
+            name,
+            config_96gb_bf16_accgrad1,
+            prefix=prefix + "/aed/",
+            model_config={
+                "enc_build_dict": rf.build_dict(
+                    ConformerEncoder,
+                    input_layer=rf.build_dict(
+                        ConformerConvSubsample,
+                        out_dims=[32, 64, 64],
+                        filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                        pool_sizes=[(1, 2)],
+                        strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+                    ),
+                    num_layers=16,
+                    out_dim=1024,
+                    encoder_layer=rf.build_dict(
+                        ConformerEncoderLayer,
+                        ff=rf.build_dict(
+                            ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                        ),
+                        num_heads=8,
+                    ),
+                ),
+                # Default AED decoder size: 6 layers, 512 dim
+                "dec_build_dict": rf.build_dict(
+                    TransformerDecoder,
+                    num_layers=6,
+                    model_dim=1024,
+                    norm=rf.build_dict(rf.RMSNorm),
+                    ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                    layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                    # When only trained on LS ASR data, keep the default dropout?
+                    # dropout=0.0,
+                    # att_dropout=0.0,
+                ),
+            },
+            config_updates={
+                **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+                "batch_size": 100_000 * _batch_size_factor,
+                "optimizer.weight_decay": 1e-2,
+                **({"label_smoothing": label_smoothing} if label_smoothing != 0.1 else {}),
+                "accum_grad_multiple_step": 1,
+                "__train_audio_preprocess": speed_pert_librosa_config,
+                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+                "max_seq_length_default_target": None,
+                # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+                # out of 281241 seqs in train, we removed only 71 seqs.
+                # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+                "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+            },
+            post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+            vocab="spm10k",
+            # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+            train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+            dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+            env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+        )
+        aed_ctc_timesync_recog_recomb_auto_scale(
+            prefix=prefix + "/aed/" + name + "/aed+ctc",
+            task=task_spm10k,
+            aed_ctc_model=exp.get_last_fixed_epoch(),
+            aux_ctc_layer=8,
+        )
 
     # wdblackNorm: add LayerNorm/RMSNorm to weight decay blacklist
     # no wdblackNorm: {"dev-clean": 2.38, "dev-other": 4.87, "test-clean": 2.89, "test-other": 5.35}
@@ -3047,6 +3055,9 @@ def py():
 
     # Standard Transformer decoder more label smoothing.
     # (Note: somewhat outdated baseline. still spmSample007, no featBN)
+    # (Also see other label smoothing experiments before,
+    #  EncL16-DecL6-D1024-DecPosEncAbs-ls{label_smoothing}-spm10k-bpeSample001-baseLr0.5-b100k)
+    # (ESPnet uses LS 0.1 usually...)
     # 0.0: {"dev-clean": 3.40, "dev-other": 5.40, "test-clean": 3.63, "test-other": 5.66}
     # 0.1: {"dev-clean": 3.40, "dev-other": 5.23, "test-clean": 3.58, "test-other": 5.44}
     # 0.2: {"dev-clean": 3.39, "dev-other": 5.18, "test-clean": 3.59, "test-other": 5.44}
