5ff4c5ad48896eae845105606a1ae50185973ddf
Albert Zeyer <albzey@gmail.com>
2025-09-10 14:39:45 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -3123,86 +3123,89 @@ def py():
     #     )
 
     # See i6_experiments/users/zeyer/experiments/exp2024_04_23_baselines/ctc.py for some earlier usage.
-    from returnn.frontend.encoder.e_branchformer import EBranchformerLayer
+    # from returnn.frontend.encoder.e_branchformer import EBranchformerLayer
 
     # EBranchformer.
     # The EBranchformer has more components which makes it bigger for the same num layers and dim,
     # thus we reduce the dim (896 instead of 1024) to make it comparable in total num params to the Conformer.
     # (But actually, you should look more at absolute speed, not so much at num params...)
+    # (But memory consumption with D1024-EBranchformer is still higher, and runs OOM with this batch size...)
     # D1024 (Conformer):  {"dev-clean": 2.81, "dev-other": 4.72, "test-clean": 2.86, "test-other": 5.08}
     # D896-EBranchformer: {"dev-clean": 2.74, "dev-other": 4.84, "test-clean": 2.87, "test-other": 5.22}
-    # TODO joint AED+CTC...
-    name = "EncL16-DecL6-D896-EBranchformer-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k"
-    exp = aed_train_exp(
-        name,
-        config_96gb_bf16_accgrad1,
-        prefix=prefix + "/aed/",
-        model_config={
-            "enc_build_dict": rf.build_dict(
-                ConformerEncoder,
-                input_layer=rf.build_dict(
-                    ConformerConvSubsample,
-                    out_dims=[32, 64, 64],
-                    filter_sizes=[(3, 3), (3, 3), (3, 3)],
-                    pool_sizes=[(1, 2)],
-                    strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
-                ),
-                num_layers=16,
-                out_dim=896,
-                encoder_layer=rf.build_dict(
-                    EBranchformerLayer,
-                    ff=rf.build_dict(
-                        rf.encoder.conformer.ConformerPositionwiseFeedForward,
-                        # Note: the ffdim in the original EBranchformer is only 1024, but here we use 2048,
-                        # as this is also what we use for Conformer.
-                        # (But this results in more parameters for the EBranchformer, due to more params in cgMLP.)
-                        activation=rf.build_dict(rf.relu_square),
-                        with_bias=False,
-                    ),
-                    num_heads=8,
-                ),
-            ),
-            # Default AED decoder size: 6 layers, 512 dim
-            "dec_build_dict": rf.build_dict(
-                TransformerDecoder,
-                num_layers=6,
-                model_dim=1024,
-                norm=rf.build_dict(rf.RMSNorm),
-                ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
-                layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
-                # When only trained on LS ASR data, keep the default dropout?
-                # dropout=0.0,
-                # att_dropout=0.0,
-            ),
-            "feature_batch_norm": True,
-        },
-        config_updates={
-            **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
-            "batch_size": 100_000 * _batch_size_factor,
-            "optimizer.weight_decay": 1e-2,
-            "accum_grad_multiple_step": 1,
-            "__train_audio_preprocess": speed_pert_librosa_config,
-            "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-            "aux_loss_layers": [4, 10, 16],
-            "max_seq_length_default_target": None,
-            # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
-            # out of 281241 seqs in train, we removed only 71 seqs.
-            # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
-            "max_seq_length_default_input": 19.5 * _raw_sample_rate,
-        },
-        post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
-        vocab="spm10k",
-        # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
-        train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
-        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
-        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
-    )
-    aed_ctc_timesync_recog_recomb_auto_scale(
-        prefix=prefix + "/aed/" + name + "/aed+ctc",
-        task=task_spm10k,
-        aed_ctc_model=exp.get_last_fixed_epoch(),
-        aux_ctc_layer=16,
-    )
+    # +CTC:
+    # D1024 (Conformer):  {"dev-clean": 1.86, "dev-other": 4.26, "test-clean": 2.10, "test-other": 4.50}
+    # D896-EBranchformer: {"dev-clean": 1.92, "dev-other": 4.31, "test-clean": 2.09, "test-other": 4.53}
+    # name = "EncL16-DecL6-D896-EBranchformer-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k"
+    # exp = aed_train_exp(
+    #     name,
+    #     config_96gb_bf16_accgrad1,
+    #     prefix=prefix + "/aed/",
+    #     model_config={
+    #         "enc_build_dict": rf.build_dict(
+    #             ConformerEncoder,
+    #             input_layer=rf.build_dict(
+    #                 ConformerConvSubsample,
+    #                 out_dims=[32, 64, 64],
+    #                 filter_sizes=[(3, 3), (3, 3), (3, 3)],
+    #                 pool_sizes=[(1, 2)],
+    #                 strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+    #             ),
+    #             num_layers=16,
+    #             out_dim=896,
+    #             encoder_layer=rf.build_dict(
+    #                 EBranchformerLayer,
+    #                 ff=rf.build_dict(
+    #                     rf.encoder.conformer.ConformerPositionwiseFeedForward,
+    #                     # Note: the ffdim in the original EBranchformer is only 1024, but here we use 2048,
+    #                     # as this is also what we use for Conformer.
+    #                     # (But this results in more parameters for the EBranchformer, due to more params in cgMLP.)
+    #                     activation=rf.build_dict(rf.relu_square),
+    #                     with_bias=False,
+    #                 ),
+    #                 num_heads=8,
+    #             ),
+    #         ),
+    #         # Default AED decoder size: 6 layers, 512 dim
+    #         "dec_build_dict": rf.build_dict(
+    #             TransformerDecoder,
+    #             num_layers=6,
+    #             model_dim=1024,
+    #             norm=rf.build_dict(rf.RMSNorm),
+    #             ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+    #             layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+    #             # When only trained on LS ASR data, keep the default dropout?
+    #             # dropout=0.0,
+    #             # att_dropout=0.0,
+    #         ),
+    #         "feature_batch_norm": True,
+    #     },
+    #     config_updates={
+    #         **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+    #         "batch_size": 100_000 * _batch_size_factor,
+    #         "optimizer.weight_decay": 1e-2,
+    #         "accum_grad_multiple_step": 1,
+    #         "__train_audio_preprocess": speed_pert_librosa_config,
+    #         "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+    #         "aux_loss_layers": [4, 10, 16],
+    #         "max_seq_length_default_target": None,
+    #         # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+    #         # out of 281241 seqs in train, we removed only 71 seqs.
+    #         # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+    #         "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+    #     },
+    #     post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+    #     vocab="spm10k",
+    #     # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+    #     train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+    #     dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+    #     env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    # )
+    # aed_ctc_timesync_recog_recomb_auto_scale(
+    #     prefix=prefix + "/aed/" + name + "/aed+ctc",
+    #     task=task_spm10k,
+    #     aed_ctc_model=exp.get_last_fixed_epoch(),
+    #     aux_ctc_layer=16,
+    # )
 
     from i6_experiments.users.zeyer.nn_rf.decoder.lstm import LstmDecoder
 
