59a39b256fc71b8f278213e9c27f97e8beb1bb33
Albert Zeyer <albzey@gmail.com>
2025-09-18 15:34:20 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -3100,11 +3100,89 @@ def py():
 
     # Same as above, but without auxDec3.
     # New baseline: bhv24 + s2 + fixMp + auxShared + auxNoBias + AudioPadRnd100
+    # New to prev: auxShared + auxNoBias + AudioPadRnd100
     # Prev baseline: EncL16-DecL6-D1024-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k-bhv24-s2-fixMp-h0
     # prev: {"dev-clean": 3.69, "dev-other": 5.36, "test-clean": 3.57, "test-other": 5.53}
     # +CTC: {"dev-clean": 1.92, "dev-other": 4.32, "test-clean": 2.07, "test-other": 4.49}
-    # new: TODO... +CTC...
-    name = "EncL16-DecL6-D1024-AudioPadRnd100-DecPosEncAbs-featBN-aux4_10_16-auxShared-auxNoBias-spm10k-bpeSample001-baseLr0.5-b100k"
+    # new: {"dev-clean": 3.83, "dev-other": 5.29, "test-clean": 4.14, "test-other": 5.88}
+    # +CTC: {"dev-clean": 1.93, "dev-other": 4.31, "test-clean": 2.08, "test-other": 4.66}
+    # Note: It looks like auxShared + auxNoBias (and maybe also fixMp) actually improve convergence / overfitting,
+    # while AudioPadRnd100 adds a bit of regularization, but not enough to compensate.
+    # name = "EncL16-DecL6-D1024-AudioPadRnd100-DecPosEncAbs-featBN-aux4_10_16-auxShared-auxNoBias-spm10k-bpeSample001-baseLr0.5-b100k"
+    # exp = aed_train_exp(
+    #     name,
+    #     config_96gb_bf16_accgrad1,
+    #     prefix=prefix + "/aed/",
+    #     model_config={
+    #         "behavior_version": 24,
+    #         "__serialization_version": 2,
+    #         "enc_build_dict": rf.build_dict(
+    #             ConformerEncoder,
+    #             input_layer=rf.build_dict(
+    #                 ConformerConvSubsample,
+    #                 out_dims=[32, 64, 64],
+    #                 filter_sizes=[(3, 3), (3, 3), (3, 3)],
+    #                 pool_sizes=[(1, 2)],
+    #                 strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
+    #             ),
+    #             num_layers=16,
+    #             out_dim=1024,
+    #             encoder_layer=rf.build_dict(
+    #                 ConformerEncoderLayer,
+    #                 ff=rf.build_dict(
+    #                     ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+    #                 ),
+    #                 num_heads=8,
+    #             ),
+    #         ),
+    #         # Default AED decoder size: 6 layers, 512 dim
+    #         "dec_build_dict": rf.build_dict(
+    #             TransformerDecoder,
+    #             num_layers=6,
+    #             model_dim=1024,
+    #             norm=rf.build_dict(rf.RMSNorm),
+    #             ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+    #             layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+    #             # When only trained on LS ASR data, keep the default dropout?
+    #             # dropout=0.0,
+    #             # att_dropout=0.0,
+    #         ),
+    #         "pad_audio": {"train": ((0, 100), (0, 100))},
+    #         "feature_batch_norm": True,
+    #         "aux_loss_layers": [4, 10, 16],
+    #         "enc_aux_logits_share_weights": True,
+    #         "enc_aux_logits_with_bias": False,
+    #     },
+    #     config_updates={
+    #         **_get_cfg_lrlin_oclr_by_bs_nep_v4(100, base_lr=0.5),
+    #         "batch_size": 100_000 * _batch_size_factor,
+    #         "optimizer.weight_decay": 1e-2,
+    #         "accum_grad_multiple_step": 1,
+    #         "__train_audio_preprocess": speed_pert_librosa_config,
+    #         "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+    #         "max_seq_length_default_target": None,
+    #         # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+    #         # out of 281241 seqs in train, we removed only 71 seqs.
+    #         # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+    #         "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+    #     },
+    #     post_config_updates={"log_grad_norm": True, "__multi_proc_dataset": {"num_workers": 25}},
+    #     vocab="spm10k",
+    #     # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+    #     train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+    #     dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+    #     env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    # )
+    # aed_ctc_timesync_recog_recomb_auto_scale(
+    #     prefix=prefix + "/aed/" + name + "/aed+ctc",
+    #     task=task_spm10k,
+    #     aed_ctc_model=exp.get_last_fixed_epoch(),
+    #     aux_ctc_layer=16,
+    # )
+
+    # Like above, but now use num workers 4 again (i.e. remove fixMp).
+    # This should add a bit more regularization...?
+    name = "EncL16-DecL6-D1024-AudioPadRnd100-DecPosEncAbs-featBN-aux4_10_16-auxShared-auxNoBias-spm10k-bpeSample001-baseLr0.5-b100k-nW4"
     exp = aed_train_exp(
         name,
         config_96gb_bf16_accgrad1,
@@ -3162,7 +3240,7 @@ def py():
             # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
             "max_seq_length_default_input": 19.5 * _raw_sample_rate,
         },
-        post_config_updates={"log_grad_norm": True, "__multi_proc_dataset": {"num_workers": 25}},
+        post_config_updates={"log_grad_norm": True},
         vocab="spm10k",
         # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
         train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
