31bd537c81ee6724e8a30873e9e8b347514f4f62
schmitt <robin.schmitt1@rwth-aachen.de>
2025-09-19 17:46:57 +0200

    update

---
+++ users/schmitt/experiments/exp2025_08_14_speech_llms/data/spm.py
@@ -0,0 +1,77 @@
+"""
+Dataset helpers for the SPM-based training
+"""
+from typing import Optional
+
+from sisyphus import tk
+
+from i6_core.g2p.convert import BlissLexiconToG2PLexiconJob
+from i6_core.lexicon.bpe import CreateBPELexiconJob
+
+from i6_experiments.common.datasets.librispeech import get_ogg_zip_dict, get_bliss_lexicon
+from i6_experiments.common.datasets.librispeech.vocab import get_subword_nmt_bpe_v2
+from i6_experiments.common.setups.returnn.datastreams.vocabulary import SentencePieceDatastream
+from i6_experiments.users.zeyer.datasets.librispeech import _get_spm_vocab
+from i6_experiments.users.zeyer.utils.dict_update import dict_update_deep
+
+from .common import DatasetSettings, TrainingDatasets, build_training_datasets
+from ..default_tools import RETURNN_ROOT, RETURNN_EXE, SUBWORD_NMT_REPO
+
+
+def get_spm_datastream(vocab_size: int) -> SentencePieceDatastream:
+    """
+    Returns the datastream for the spm labels
+
+    :param vocab_size: the size of the vocabulary
+    """
+    spm_model = _get_spm_vocab(dim=vocab_size)
+
+    spm_targets = SentencePieceDatastream(
+        available_for_inference=False,
+        spm_model=spm_model.model_file,
+        vocab_size=vocab_size,
+    )
+    return spm_targets
+
+
+def build_spm_training_datasets(
+    prefix: str,
+    librispeech_key: str,
+    vocab_size: int,
+    settings: DatasetSettings,
+    returnn_root: tk.Path = RETURNN_ROOT,
+    alpha: Optional[float] = None,
+) -> TrainingDatasets:
+    """
+    Builds the training datasets for the SPM-based training.
+    """
+    label_datastream = get_spm_datastream(
+        vocab_size=vocab_size,
+    )
+
+    ogg_zip_dict = get_ogg_zip_dict(prefix, returnn_root=returnn_root, returnn_python_exe=RETURNN_EXE)
+    train_ogg = ogg_zip_dict[librispeech_key]
+    dev_clean_ogg = ogg_zip_dict["dev-clean"]
+    dev_other_ogg = ogg_zip_dict["dev-other"]
+
+    training_datasets = build_training_datasets(
+        train_ogg=train_ogg,
+        dev_clean_ogg=dev_clean_ogg,
+        dev_other_ogg=dev_other_ogg,
+        settings=settings,
+        label_datastream=label_datastream,
+    )
+
+    # SentencePieceDatastream only covers limited options and always adds EOS, which we don't want
+    for dataset in [training_datasets.train, training_datasets.cv, training_datasets.devtrain]:
+        dataset.dataset.target_options.pop("add_eos", None)
+        if dataset == training_datasets.train:
+            dataset.dataset.target_options.update(
+                {
+                    "alpha": alpha,
+                    "enable_sampling": True,
+                }
+                if alpha is not None
+                else {},
+            )
+    return training_datasets
---
+++ users/schmitt/experiments/exp2025_08_14_speech_llms/serializer.py
@@ -0,0 +1,183 @@
+"""
+Contains the serializers for the ReturnnConfig epilog which write the model import and task functions import
+including serializing their parameters.
+"""
+from typing import Any, Dict, Optional
+
+from i6_core.util import instanciate_delayed
+from i6_core.text.label.sentencepiece.vocab import ExtractSentencePieceVocabJob
+
+from i6_experiments.common.setups.returnn_pytorch.serialization import Collection
+from i6_experiments.common.setups.serialization import ExternalImport, Import, PartialImport, NonhashedCode
+
+from . import PACKAGE
+from .default_tools import I6_MODELS_REPO_PATH, I6_NATIVE_OPS_REPO_PATH
+
+from sisyphus import tk
+
+
+def serialize_extern_data(extern_data: Dict[str, Any]):
+    from returnn.util.pprint import pformat
+
+    extern_data_str = f"extern_data = {pformat(extern_data)}\n"
+
+    return NonhashedCode(extern_data_str)
+
+
+def serialize_training(
+    network_module: str,
+    train_step_module: str,
+    net_args: Dict[str, Any],
+    train_args: Dict[str, Any],
+    extern_data: Dict[str, Any],
+    unhashed_net_args: Optional[Dict[str, Any]] = None,
+    include_native_ops=False,
+    debug: bool = False,
+) -> Collection:
+    """
+    Helper function to create the serialization collection
+
+    :param network_module: path to the pytorch config file containing Model
+    :param net_args: arguments for the model
+    :param unhashed_net_args: as above but not hashed
+    :param include_native_ops: include the i6_native_ops for e.g. Fast-Baum-Welch or Warp-RNNT
+    :param debug: run training in debug mode: linking from recipe instead of copy
+    :return: Collection object to be added to the ReturnnConfig epilog
+    """
+    package = PACKAGE
+
+    pytorch_model_import = PartialImport(
+        code_object_path=package + ".%s.Model" % network_module,
+        unhashed_package_root=PACKAGE,
+        hashed_arguments=net_args,
+        unhashed_arguments=unhashed_net_args or {},
+        import_as="get_model",
+    )
+    pytorch_train_step = PartialImport(
+        code_object_path=package + ".%s.train_step" % train_step_module,
+        unhashed_package_root=PACKAGE,
+        hashed_arguments=train_args,
+        unhashed_arguments={},
+    )
+    # i6_models = ExternalImport(import_path=I6_MODELS_REPO_PATH)
+
+    serializer_objects = [
+        # i6_models,
+        serialize_extern_data(instanciate_delayed(extern_data)),
+        pytorch_model_import,
+        pytorch_train_step,
+    ]
+
+    if include_native_ops:
+        i6_native_ops = ExternalImport(import_path=I6_NATIVE_OPS_REPO_PATH)
+        # has to be in the beginning
+        serializer_objects.insert(0, i6_native_ops)
+
+    serializer = Collection(
+        serializer_objects=serializer_objects,
+        make_local_package_copy=not debug,
+        packages={
+            package,
+        },
+    )
+    return serializer
+
+
+def serialize_forward(
+    network_module: str,
+    net_args: Dict[str, Any],
+    extern_data: Dict[str, Any],
+    vocab_opts: Dict[str, Any],
+    unhashed_net_args: Optional[Dict[str, Any]] = None,
+    forward_module: Optional[str] = None,
+    forward_step_name: str = "forward",
+    forward_init_args: Optional[Dict[str, Any]] = None,
+    unhashed_forward_init_args: Optional[Dict[str, Any]] = None,
+    include_native_ops=False,
+    debug: bool = False,
+):
+    """
+    Serialize for a forward job. Can be used e.g. for search or prior computation.
+
+    :param network_module: path to the pytorch config file containing Model
+    :param net_args: arguments for the model
+    :param unhashed_net_args: as above but not hashed
+    :param forward_module: optionally define a module file which contains the forward definition.
+        If not provided the network_module is used.
+    :param forward_step_name: path to the search decoder file containing forward_step and hooks
+    :param forward_init_args: additional arguments to pass to forward_init
+    :param unhashed_forward_init_args: additional non-hashed arguments to pass to forward_init
+    :param include_native_ops: include the i6_native_ops for e.g. Fast-Baum-Welch or Warp-RNNT
+    :param debug: run training in debug mode: linking from recipe instead of copy
+    :return:
+    """
+
+    package = PACKAGE  # + ".pytorch_networks"
+
+    pytorch_model_import = PartialImport(
+        code_object_path=package + ".%s.Model" % network_module,
+        unhashed_package_root=PACKAGE,
+        hashed_arguments=net_args,
+        unhashed_arguments=unhashed_net_args or {},
+        import_as="get_model",
+    )
+
+    # i6_models = ExternalImport(import_path=I6_MODELS_REPO_PATH)
+
+    serializer_objects = [
+        # i6_models,
+        serialize_extern_data(instanciate_delayed(extern_data)),
+        pytorch_model_import,
+    ]
+
+    if include_native_ops:
+        i6_native_ops = ExternalImport(import_path=I6_NATIVE_OPS_REPO_PATH)
+        # has to be in the beginning
+        serializer_objects.insert(0, i6_native_ops)
+
+    forward_module = forward_module or network_module
+
+    forward_step = PartialImport(
+        code_object_path=package + ".%s.%s_step" % (forward_module, forward_step_name),
+        unhashed_package_root=PACKAGE,
+        import_as="forward_step",
+        hashed_arguments={
+            "beam_size": 12,
+            "max_tokens_per_sec": 20,
+            "sample_rate": 16_000,
+        },
+        unhashed_arguments={},
+    )
+
+    assert vocab_opts["class"] == "SentencePieces"
+    spm_model_file = vocab_opts["model_file"]
+    vocab_file = ExtractSentencePieceVocabJob(model=spm_model_file).out_vocab
+    callback = PartialImport(
+        code_object_path=f"{package}.recognition.aed.callback.RecognitionToTextDictCallback",
+        import_as="forward_callback",
+        hashed_arguments={"vocab": vocab_file},
+        unhashed_arguments={},
+        unhashed_package_root=None,
+    )
+    # init_hook = PartialImport(
+    #     code_object_path=package + ".%s.%s_init_hook" % (forward_module, forward_step_name),
+    #     unhashed_package_root=PACKAGE,
+    #     hashed_arguments=forward_init_args or {},
+    #     unhashed_arguments=unhashed_forward_init_args or {},
+    #     import_as="forward_init_hook",
+    # )
+    # finish_hook = Import(
+    #     code_object_path=package + ".%s.%s_finish_hook" % (forward_module, forward_step_name),
+    #     unhashed_package_root=PACKAGE,
+    #     import_as="forward_finish_hook",
+    # )
+    serializer_objects.extend([forward_step, callback])
+
+    serializer = Collection(
+        serializer_objects=serializer_objects,
+        make_local_package_copy=not debug,
+        packages={
+            package,
+        },
+    )
+    return serializer
