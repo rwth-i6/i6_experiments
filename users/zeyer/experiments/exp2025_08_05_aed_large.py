579132f206de3178fbc2527df300217e9882e2c3
Albert Zeyer <albzey@gmail.com>
2025-09-23 00:06:52 +0200

    more

--- users/zeyer/experiments/exp2025_08_05_aed_large.py
+++ users/zeyer/experiments/exp2025_08_05_aed_large.py
@@ -3568,6 +3568,8 @@ def py():
     # About num params. From the E-Branchformer paper (https://arxiv.org/pdf/2210.00077):
     # Conformer enc: 114.9M params, E-Branchformer (L) enc: 116.0M params.
     # E-Branch (L): 17 layers, feat dimension d=512, heads d/64=8, hidden dim cgMLP 6d, FFN 2d (for macaron-style).
+    # Conformer (+CTC): {"dev-clean": 1.86, "dev-other": 4.26, "test-clean": 2.10, "test-other": 4.50}
+    # E-Branchf (+CTC): {"dev-clean": 1.93, "dev-other": 4.48, "test-clean": 2.09, "test-other": 4.66}
     name = "EncL16-DecL6-D1024-FFD2k-EBranchformer-DecPosEncAbs-featBN-aux4_10_16-spm10k-bpeSample001-baseLr0.5-b100k"
     exp = aed_train_exp(
         name,
