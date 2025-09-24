a9287fc2307d3f2f8e8b9ee482d113e9d8c97ab8
maxazv <azevedomaximilian@gmail.com>
2025-09-24 17:52:34 +0200

    added streamable versions of relu_mean_abs and shared_obs

--- users/azevedo/experiments/librispeech/ctc_rnnt_standalone_2025/pytorch_networks/ctc/search/beam_search.py
+++ users/azevedo/experiments/librispeech/ctc_rnnt_standalone_2025/pytorch_networks/ctc/search/beam_search.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Union, Tuple
-from torchaudio.models.decoder import CTCDecoderLM, CTCDecoderLMState
+# from torchaudio.models.decoder import CTCDecoderLM, CTCDecoderLMState
 
 from sisyphus import tk
 
@@ -12,34 +12,34 @@ from ...search._base_decoder import DecoderConfig, ExtraConfig, BaseDecoderModul
 from ...base_config import BaseConfig
 
 
-class CustomLM(CTCDecoderLM):
-    def __init__(self, language_model: torch.nn.Module):
-        CTCDecoderLM.__init__(self)
-        self.language_model = language_model
-        self.sil = -1
-        self.states = {}
-
-        self.language_model.eval()
-
-    def start(self, start_with_nothing: bool = False):
-        state = CTCDecoderLMState()
-        with torch.no_grad():
-            score = self.language_model(self.sil)
-
-        self.states[state] = score
-        return state
-
-    def score(self, state: CTCDecoderLMState, token_index: int):
-        outstate = state.child(token_index)
-        if outstate not in self.states:
-            score = self.language_model(token_index)
-            self.states[outstate] = score
-        score = self.states[outstate]
-
-        return outstate, score
-
-    def finish(self, state: CTCDecoderLMState):
-        return self.score(state, self.sil)
+# class CustomLM(CTCDecoderLM):
+#     def __init__(self, language_model: torch.nn.Module):
+#         CTCDecoderLM.__init__(self)
+#         self.language_model = language_model
+#         self.sil = -1
+#         self.states = {}
+#
+#         self.language_model.eval()
+#
+#     def start(self, start_with_nothing: bool = False):
+#         state = CTCDecoderLMState()
+#         with torch.no_grad():
+#             score = self.language_model(self.sil)
+#
+#         self.states[state] = score
+#         return state
+#
+#     def score(self, state: CTCDecoderLMState, token_index: int):
+#         outstate = state.child(token_index)
+#         if outstate not in self.states:
+#             score = self.language_model(token_index)
+#             self.states[outstate] = score
+#         score = self.states[outstate]
+#
+#         return outstate, score
+#
+#     def finish(self, state: CTCDecoderLMState):
+#         return self.score(state, self.sil)
 
 
 @dataclass(kw_only=True)
