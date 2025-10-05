f5cac99153c0659a3d977a519568c1747550bff5
marti.juanola <marti.juanola@ml.rwth-aachen.de>
2025-10-05 12:44:07 +0200

    [mj.demo1] creation, import changes and new v3 settings added.
    No major modifications from schmitt setup (exp2025_08_14_speech_llms).
    
    V2 works, V3 in process but seems promising.

---
+++ users/juanola/experiments/e25_09_08_demo1_sllm/recognition/aed/__init__.py
@@ -0,0 +1,2 @@
+from .beam_search import *
+from .forward_step import *
---
+++ users/juanola/lr_schedules/__init__.py
@@ -0,0 +1,9 @@
+"""
+LR schedules
+
+Most of them are intended to be used as ``dynamic_learning_rate`` in RETURNN,
+and they use the RETURNN global config logic to get other options,
+but otherwise, they are not specific to RETURNN.
+
+You also find some demo for plotting in some of the modules.
+"""
