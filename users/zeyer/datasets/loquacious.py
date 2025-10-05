d915d6a084e217ae5a54f1bcb5aaef10c75c1517
Albert Zeyer <albzey@gmail.com>
2025-10-05 16:48:30 +0200

    more

--- users/zeyer/datasets/loquacious.py
+++ users/zeyer/datasets/loquacious.py
@@ -1,20 +1,29 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Union, Any, Sequence, Dict
-from functools import partial
+from functools import partial, cache
 from sisyphus import tk, Path
-from i6_core.datasets.huggingface import TransformAndMapHuggingFaceDatasetJob
+from i6_core.datasets.huggingface import TransformAndMapHuggingFaceDatasetJob, ExtractTextFromHuggingFaceDatasetJob
+from i6_core.text.label.sentencepiece.train import TrainSentencePieceJob, SentencePieceType
+from i6_core.text.label.sentencepiece.vocab import ExtractSentencePieceVocabJob
 from i6_experiments.users.zeyer import tools_paths
+from .utils.spm import SentencePieceModel
 
 if TYPE_CHECKING:
     from datasets import DatasetDict
 
 
+_alias_prefix = "datasets/Loquacious/"
+
+
 def py():
     for name in ["small", "medium", "large"]:
         for q in [3, 4]:
-            tk.register_output(f"datasets/loquacious_hf_{name}_q{q}_ogg", get_loquacious_hf_ogg(name, quality=q))
+            get_loquacious_hf_ogg(name, quality=q)
+    get_train_corpus_text()
+    get_spm_vocab(dim=10_240)
 
 
+@cache
 def get_loquacious_hf_ogg(name: str = "large", *, quality: int = 3) -> Path:
     ffmpeg_binary = tools_paths.get_ffmpeg_binary()
 
@@ -26,7 +35,64 @@ def get_loquacious_hf_ogg(name: str = "large", *, quality: int = 3) -> Path:
         map_opts=_map_opts,
     )
     job.rqmt.update({"cpu": 32, "time": 24, "mem": 32})
+    job.add_alias(_alias_prefix + f"dataset_hf_{name}_q{quality}_ogg")
+    tk.register_output(f"{_alias_prefix}dataset_hf_{name}_q{quality}_ogg", job.out_dir)
     return job.out_dir
+
+
+@cache
+def get_train_corpus_text() -> Path:
+    job = ExtractTextFromHuggingFaceDatasetJob("speechbrain/LoquaciousSet", "large", split="train", column_name="text")
+    job.add_alias(f"{_alias_prefix}train_corpus.txt.extract")
+    tk.register_output(f"{_alias_prefix}train_corpus.txt.gz", job.out_text)
+    return job.out_text
+
+
+@cache
+def get_spm_vocab(
+    *, dim: Union[int, str], model_type: SentencePieceType = SentencePieceType.UNIGRAM
+) -> SentencePieceModel:
+    """
+    Get a SentencePiece model and vocab of given dimension trained on the Loquacious transcriptions.
+    """
+    dim_str = str(dim)
+    if isinstance(dim, str):
+        # Not sure if power-of-two or just multiple-of-64, but 10240 has more 2s in it (2048*5) than 10048.
+        dim = {"20k": 20_480, "10k": 10_240, "5k": 5_120, "4k": 4_096, "1k": 1_024, "512": 512, "128": 128, "64": 64}[
+            dim
+        ]
+    assert isinstance(dim, int) and dim >= 10
+
+    # https://github.com/google/sentencepiece/blob/master/doc/options.md
+    _spm_train_job = TrainSentencePieceJob(
+        training_text=get_train_corpus_text(),
+        vocab_size=dim,
+        model_type=model_type,
+        additional_options={
+            "split_digits": True,
+            "unk_id": 2,  # default is 0
+            "bos_id": 1,  # default is 1
+            "eos_id": 0,  # default is 2
+            "train_extremely_large_corpus": True,
+            "shuffle_input_sentence": True,
+            "input_sentence_size": 10_000_000,
+        },
+    )
+    _spm_train_job.rqmt.update({"time": 12, "mem": 126})  # needs much more mem, maybe little longer
+    _spm_train_job.add_alias(f"{_alias_prefix}vocab/spm_{model_type.value}_{dim_str}_train")
+    tk.register_output(f"{_alias_prefix}vocab/spm_{model_type.value}_{dim_str}_train.model", _spm_train_job.out_model)
+    tk.register_output(
+        f"{_alias_prefix}vocab/spm_{model_type.value}_{dim_str}_train.vocab",
+        ExtractSentencePieceVocabJob(_spm_train_job.out_model).out_vocab,
+    )
+    spm = SentencePieceModel(
+        dim=dim,
+        model_file=_spm_train_job.out_model,
+        unknown_label="<unk>",
+        bos_idx=1,
+        eos_idx=0,
+    )
+    return spm
 
 
 def _transform_rename_columns(ds: DatasetDict) -> DatasetDict:
