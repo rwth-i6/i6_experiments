4a1d2945f472540c93a9f796bcb834618037e989
Albert Zeyer <albzey@gmail.com>
2025-10-10 16:12:59 +0200

    more

--- users/zeyer/datasets/openwebtext2.py
+++ users/zeyer/datasets/openwebtext2.py
@@ -6,7 +6,7 @@ https://huggingface.co/datasets/Geralt-Targaryen/openwebtext2
 """
 
 
-def _load():
+def _load_cleaned_en():
     from datasets import load_dataset
 
     ds = load_dataset("Geralt-Targaryen/openwebtext2", split="train")
