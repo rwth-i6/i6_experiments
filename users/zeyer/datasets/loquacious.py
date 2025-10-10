0a4169a64adf8d885e32a2af4de25d8a28ae9929
Albert Zeyer <albzey@gmail.com>
2025-10-10 22:58:07 +0200

    small cleanup

--- users/zeyer/datasets/loquacious.py
+++ users/zeyer/datasets/loquacious.py
@@ -566,7 +566,13 @@ class TextNormaliser:
 
     def english_specific_preprocess(self, sentence, upper_case=True, symbols_limit=4):
         """
-        Preprocess English text. This function relies on different tools to convert numerals and special symbols. This also removes various punctuation and treats it as word boundaries. It normalises and retains various apostrophes (’‘´) between letters, but not other ones, which are probably quotation marks. It capitalises all text. This function may error out if new characters show up in the given sentence.
+        Preprocess English text.
+        This function relies on different tools to convert numerals and special symbols.
+        This also removes various punctuation and treats it as word boundaries.
+        It normalises and retains various apostrophes (’‘´) between letters,
+        but not other ones, which are probably quotation marks.
+        It capitalises all text.
+        This function may error out if new characters show up in the given sentence.
 
         Parameters
         ----------
@@ -575,7 +581,8 @@ class TextNormaliser:
         upper_case : bool
             Whether to upper case (if True) or lower case (if False) the string.
         symbols_limit : int
-            If a sentence contains more than symbols_limit, it will not be normalised and skipped. This is because in most case, the pronunciation will not be certain enough.
+            If a sentence contains more than symbols_limit, it will not be normalised and skipped.
+            This is because in most case, the pronunciation will not be certain enough.
         Returns
         -------
         str
@@ -612,7 +619,7 @@ class TextNormaliser:
             # Symbols that could be pronounced in various ways.
             "]"
         )
-        if not re.search(stop_characters, sentence) is None:
+        if re.search(stop_characters, sentence) is not None:
             return None
 
         # encoding goes brrrrr
@@ -698,7 +705,18 @@ class TextNormaliser:
         removes these char in the text.
 
         """
-        unwanted_char = "\u0159\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+        unwanted_char = (
+            "\u0159\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n"
+            "\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19"
+            "\x1a\x1b\x1c\x1d\x1e\x1f\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f"
+            "\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f"
+            "\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf"
+            "\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf"
+            "\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf"
+            "\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf"
+            "\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef"
+            "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
+        )
         text = "".join([(" " if n in unwanted_char else n) for n in text if n not in unwanted_char])
         return text
 
@@ -717,7 +735,6 @@ class TextNormaliser:
         -------
         int
             The total count
-
 
         """
         cpt = 0
