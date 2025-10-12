bfce4b5278e851b1d13787ca37a06305fd8ad65d
Albert Zeyer <albzey@gmail.com>
2025-10-12 17:18:18 +0200

    yodas

---
+++ users/zeyer/datasets/yodas.py
@@ -0,0 +1,16 @@
+"""
+Yodas
+
+https://huggingface.co/datasets/espnet/yodas
+https://huggingface.co/datasets/espnet/yodas/tree/refs%2Fconvert%2Fparquet/en000/partial-train
+
+https://huggingface.co/datasets/espnet/yodas_owsmv4
+https://huggingface.co/datasets/espnet/yodas-granary
+
+https://huggingface.co/datasets/espnet/long-yodas-segmented
+
+https://huggingface.co/datasets/espnet/yodas2
+
+The Loquacious post-processing / filtering / text-normalization on Yodas might be of interest:
+https://github.com/speechbrain/speechbrain/pull/2802/files
+"""
