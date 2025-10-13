ce635d24193b36559e64de41148387dc87cc55e9
Albert Zeyer <albzey@gmail.com>
2025-10-13 10:04:18 +0200

    _clean_alternative_file_cache_dirs

--- users/zeyer/returnn/global_startup_callback.py
+++ users/zeyer/returnn/global_startup_callback.py
@@ -6,13 +6,77 @@ This should be optional and usually not needed if everything is set up correctly
 """
 
 from __future__ import annotations
+
 from typing import Optional, Any, Dict, List
+import os
+import shutil
+import sys
 import textwrap
+import traceback
 from i6_experiments.common.setups import serialization
 
 
 def global_startup_callback(*_args, **_kwargs):
     print("RETURNN global startup callback.")
+    # noinspection PyBroadException
+    try:
+        _clean_alternative_file_cache_dirs()
+
+    except Exception:
+        # ignore all errors, it's not critical
+        print("Warning: global_startup_callback failed:", file=sys.stderr)
+        traceback.print_exc()
+        sys.stderr.flush()
+
+
+def _clean_alternative_file_cache_dirs():
+    """
+    In some older setups, we left TMPDIR unchanged,
+    and the RWTH IPC Slurm setup creates a new temp dir for each job,
+    in the style /w0/tmp/slurm_az668407.61594861.
+
+    Go through all those temp dirs, check if they contain a RETURNN file cache,
+    and if so, potentially clean them up.
+    """
+    from returnn.util.basic import get_login_username, human_bytes_size
+    from returnn.util import file_cache
+
+    if not os.path.exists("/w0/tmp"):  # RWTH IPC
+        return
+
+    dirs = os.listdir("/w0/tmp")
+    username = get_login_username()
+
+    disk_usage = shutil.disk_usage("/w0/tmp")
+    print(
+        f"/w0/tmp disk usage: total {human_bytes_size(disk_usage.total)},"
+        f" used {human_bytes_size(disk_usage.used)},"
+        f" free {human_bytes_size(disk_usage.free)}"
+    )
+
+    freed = 0
+    for d in dirs:
+        if not d.startswith(f"slurm_{username}."):
+            continue
+        tmp_dir = "/w0/tmp/" + d
+        cache_directory = f"{tmp_dir}/{username}/returnn/file_cache"
+        if not os.path.exists(cache_directory):
+            continue
+
+        print(f"Found file cache directory: {cache_directory}")
+        cache = file_cache.FileCache(cache_directory=cache_directory, cleanup_files_always_older_than_days=0.1)
+        res = cache.cleanup()
+        print(res)
+        freed += res.freed
+
+    print(f"Total freed space: {human_bytes_size(freed)}")
+    if freed:
+        disk_usage = shutil.disk_usage("/w0/tmp")
+        print(
+            f"/w0/tmp disk usage after cleanup: total {human_bytes_size(disk_usage.total)},"
+            f" used {human_bytes_size(disk_usage.used)},"
+            f" free {human_bytes_size(disk_usage.free)}"
+        )
 
 
 def serialize_global_startup_callback() -> serialization.NonhashedCode:
