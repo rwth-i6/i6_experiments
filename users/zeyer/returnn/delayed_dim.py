fe7c092a0c526627b449a8f29c1fb3fbe7c4e135
Albert Zeyer <albzey@gmail.com>
2025-10-20 13:37:36 +0200

    cleanup

--- users/zeyer/returnn/delayed_dim.py
+++ users/zeyer/returnn/delayed_dim.py
@@ -4,7 +4,6 @@ e.g. because it depends on a vocab that we don't want to load in the Sis manager
 """
 
 from __future__ import annotations
-from typing import Any, Dict
 from returnn.tensor import Dim
 import functools
 
