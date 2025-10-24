a0f77b9c97461112e419eba2d1552d29297a6795
Albert Zeyer <albzey@gmail.com>
2025-10-24 10:22:26 +0200

    RasrCompatibleLogMelFeatureExtractionV1 wrapper, different api, more RF compat

--- users/zeyer/nn_rf/i6_models/primitives/feature_extraction.py
+++ users/zeyer/nn_rf/i6_models/primitives/feature_extraction.py
@@ -3,13 +3,13 @@ Wraps i6_models feature extraction
 """
 
 from __future__ import annotations
-from typing import Tuple
+from typing import Optional, Tuple
 from returnn.tensor import Tensor, Dim
 import returnn.frontend as rf
 
 
 class RasrCompatibleLogMelFeatureExtractionV1(rf.Module):
-    def __init__(self, **config):
+    def __init__(self, out_dim: Optional[Dim] = None, **config):
         super().__init__()
 
         from i6_models.primitives import feature_extraction as i6_mod
@@ -17,12 +17,13 @@ class RasrCompatibleLogMelFeatureExtractionV1(rf.Module):
         self._config = i6_mod.RasrCompatibleLogMelFeatureExtractionV1Config(**config)
         self._torch_mod = i6_mod.RasrCompatibleLogMelFeatureExtractionV1(self._config)
 
-    def __call__(
-        self, raw_audio: Tensor, *, in_spatial_dim: Dim, out_dim: Dim, sampling_rate: int
-    ) -> Tuple[Tensor, Dim]:
-        assert self._config.sample_rate == sampling_rate
-        assert self._config.num_filters == out_dim.dimension
+        if not out_dim:
+            out_dim = Dim(self._config.num_filters, name="feature")
+        else:
+            assert out_dim.dimension == self._config.num_filters
+        self.out_dim = out_dim
 
+    def __call__(self, raw_audio: Tensor, *, in_spatial_dim: Dim) -> Tuple[Tensor, Dim]:
         if raw_audio.feature_dim and raw_audio.feature_dim.dimension == 1:
             raw_audio = rf.squeeze(raw_audio, axis=raw_audio.feature_dim)
 
@@ -33,5 +34,5 @@ class RasrCompatibleLogMelFeatureExtractionV1(rf.Module):
             raw_audio.raw_tensor, in_spatial_dim.get_size_tensor(device="cpu").copy_compatible_to_dims_raw(batch_dims)
         )
         out_spatial_dim = Dim(rf.convert_to_tensor(out_length_raw, dims=batch_dims), name="time")
-        out = rf.convert_to_tensor(out_raw, dims=batch_dims + [out_spatial_dim, out_dim])
+        out = rf.convert_to_tensor(out_raw, dims=batch_dims + [out_spatial_dim, self.out_dim])
         return out, out_spatial_dim
