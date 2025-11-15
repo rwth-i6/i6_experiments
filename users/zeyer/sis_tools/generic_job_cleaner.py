c56bddde62473f0d6cc994c867d7cb8ea00c6923
Albert Zeyer <albzey@gmail.com>
2025-11-15 18:55:48 +0100

    nicer

--- users/zeyer/sis_tools/generic_job_cleaner.py
+++ users/zeyer/sis_tools/generic_job_cleaner.py
@@ -40,7 +40,7 @@ def clean(*, work_dir: str, dry_run: bool = False, stop_after_n_jobs: int = -1) 
     for root, dirs, files in os.walk(work_dir):
         if time.monotonic() - prev_report_time > 10:
             prev_report_time = time.monotonic()
-            print(f"Jobs found so far: {count_all_jobs}, cleaned: {count_cleaned_jobs}")
+            print(f"Jobs found so far: {count_all_jobs}, cleaned: {count_cleaned_jobs}, current dir: {root!r}")
 
         job_dir_names = set()
         for dir_name in dirs:
