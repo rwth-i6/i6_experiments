d38967e78fee46391e24ac192c9e3e613f364cdb
Albert Zeyer <albzey@gmail.com>
2025-11-16 16:56:45 +0100

    better

--- users/zeyer/sis_tools/generic_job_cleaner.py
+++ users/zeyer/sis_tools/generic_job_cleaner.py
@@ -86,6 +86,9 @@ def _is_job_dir(job_dir: str) -> bool:
     """
     :param job_dir: absolute or relative path to job directory
     """
+    base_name = os.path.basename(job_dir)
+    if "." not in base_name:
+        return False
     if not os.path.isfile(job_dir + "/info"):
         return False
     if not os.path.isdir(job_dir + "/output"):
