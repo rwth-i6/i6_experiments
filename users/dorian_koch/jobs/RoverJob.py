0f477883b9bc8eb3f7190cde4cd389ce40627447
Dorian Koch <dorian.koch@ml.rwth-aachen.de>
2025-11-21 15:19:10 +0100

    rover

--- users/dorian_koch/jobs/RoverJob.py
+++ users/dorian_koch/jobs/RoverJob.py
@@ -3,11 +3,14 @@ import shutil
 import subprocess as sp
 import tempfile
 import collections
+import math
 import re
 from typing import List, Optional, Dict, Tuple
 
-from sisyphus import *
-from i6_core.lib.corpus import *
+from sisyphus import tk, Job, Task, gs
+from sisyphus.delayed_ops import DelayedBase
+
+# from i6_core.lib.corpus import *
 from i6_core.util import uopen
 import i6_core.util as util
 
@@ -17,40 +20,47 @@ class RoverJob(Job):
     Wroom wroom!
     """
 
-    __sis_hash_exclude__ = {"sctk_binary_path": None, "precision_ndigit": 1}
+    __sis_hash_exclude__ = {"sctk_binary_path": None}
 
     def __init__(
         self,
-        hyps: List[tk.Path],
+        hyps: list[tk.Path],
         *,
         sort_files: bool = False,
-        method: str = "meth1",
-        additional_args: Optional[List[str]] = None,
+        method: str = "avgconf",
+        alpha: float | DelayedBase = 1.0,
+        null_confidence: float | DelayedBase = 0.0,
+        additional_args: Optional[list[str]] = None,
         sctk_binary_path: Optional[tk.Path] = None,
-        precision_ndigit: Optional[int] = 1,
     ):
         """
         :param hyps: hypothesis ctm text files
         :param sort_files: sort ctm and stm before scoring
         :param additional_args: additional command line arguments passed to the Rover binary call
         :param sctk_binary_path: set an explicit binary path.
+        :param method: rover combination method, e.g. "avgconf"
+        :param alpha: Alpha is the tradeoff between using word occurrence counts and confidence scores.
+            Alpha = 1 means only word occurence counts are used
+        :param null_confidence: Set confidence score associated with NULL transition arcs
 
+        See SCTK/doc/rover.htm
         """
         self.set_vis_name("Rover")
 
-        assert len(hyps) > 2  # otherwise doesnt make sense
+        assert len(hyps) > 1  # otherwise doesnt make sense
         self.hyps = hyps
         self.method = method
         self.sort_files = sort_files
         self.additional_args = additional_args
         self.sctk_binary_path = sctk_binary_path
-        self.precision_ndigit = precision_ndigit
+        self.alpha = alpha
+        self.null_confidence = null_confidence
 
         self.out = self.output_path("outfile")
-        self.rqmt = {"cpu": 1, "mem": 24, "time": 1}
+        self.rqmt = {"cpu": 1, "mem": 12, "time": 1}
 
     def tasks(self):
-        yield Task("run", resume="run", rqmt=self.rqmt)
+        yield Task("run", resume="run", rqmt=self.rqmt, mini_task=True)
 
     def run(self):
         # print env
@@ -80,7 +90,24 @@ class RoverJob(Job):
             rover_path = os.path.join(gs.SCTK_PATH, "rover") if hasattr(gs, "SCTK_PATH") else "rover"
         ctm_files = sorted_files if self.sort_files else [hyp.get_path() for hyp in self.hyps]
 
-        args = [rover_path, "-o", self.out.get_path(), "-m", self.method]
+        alpha = self.alpha
+        if isinstance(alpha, DelayedBase):
+            alpha = alpha.get()
+        null_confidence = self.null_confidence
+        if isinstance(null_confidence, DelayedBase):
+            null_confidence = null_confidence.get()
+
+        args = [
+            rover_path,
+            "-o",
+            self.out.get_path(),
+            "-m",
+            self.method,
+            "-a",
+            str(alpha),
+            "-c",
+            str(null_confidence),
+        ]
         for ctm_file in ctm_files:
             args += ["-h", ctm_file, "ctm"]
 
@@ -92,7 +119,7 @@ class RoverJob(Job):
     @classmethod
     def hash(cls, parsed_args):
         d = dict(**parsed_args)
-        d["__version"] = 2
+        d["__version"] = 3
         return super().hash(d)
 
 
@@ -103,13 +130,21 @@ class SearchToRoverCtms(Job):
 
     __sis_hash_exclude__ = {"output_gzip": False}
 
-    def __init__(self, search_py_output: tk.Path, num_ctms: int, *, top_k: Optional[int] = None):
+    def __init__(
+        self,
+        search_py_output: tk.Path,
+        num_ctms: int,
+        *,
+        convert_log_probs_to_probs: bool = False,
+        length_norm_words: bool = False,
+    ):
         """
         :param search_py_output: a search output file from RETURNN in python format (n-best)
         """
         self.search_py_output = search_py_output
         self.out_ctms = [self.output_path(f"rover_hyp_{i}.ctm") for i in range(num_ctms)]
-        self.top_k = top_k
+        self.convert_log_probs_to_probs = convert_log_probs_to_probs
+        self.length_norm_words = length_norm_words
 
     def tasks(self):
         """task"""
@@ -132,8 +167,8 @@ class SearchToRoverCtms(Job):
 
                 # sort entry, highest score first
                 entry = sorted(entry, key=lambda x: x[0], reverse=True)
-                if self.top_k is not None:
-                    entry = entry[: self.top_k]
+                if len(entry) > len(self.out_ctms):
+                    entry = entry[: len(self.out_ctms)]
 
                 seg_start = 0.0
                 seg_end = 1000.0
@@ -144,17 +179,22 @@ class SearchToRoverCtms(Job):
                     time_step_per_word = (seg_end - seg_start) / max(len(words), 1)
                     avg_dur = time_step_per_word * 0.9
                     count = 0
+
+                    real_score = score
+                    if self.length_norm_words:
+                        real_score = score / max(len(words), 1)
+                    if self.convert_log_probs_to_probs:
+                        real_score = math.exp(real_score)
+
                     for i in range(len(words)):
-                        out.write(f"{seq_tag} 1 {seg_start + time_step_per_word * i} {avg_dur} {words[i]} {score}\n")
+                        out.write(
+                            f"{seq_tag} 1 {seg_start + time_step_per_word * i} {avg_dur} {words[i]} {real_score}\n"
+                        )
                         count += 1
                     if count == 0:
-                        # sclite cannot handle empty sequences, and would stop with an error like:
-                        #   hyp file '4515-11057-0054' and ref file '4515-11057-0053' not synchronized
-                        #   sclite: Alignment failed.  Exiting
-                        # So we make sure it is never empty.
-                        # For the WER, it should not matter, assuming the reference sequence is non-empty,
-                        # you will anyway get a WER of 100% for this sequence.
-                        out.write(f"{seq_tag} 1 {seg_start} {avg_dur} <empty-sequence> {score}\n")
+                        # some logic to prevent empty sequences
+                        # this is just copied from sclitejob
+                        out.write(f"{seq_tag} 1 {seg_start} {avg_dur} <empty-sequence> {real_score}\n")
         finally:
             for fh in file_handles:
                 fh.close()
