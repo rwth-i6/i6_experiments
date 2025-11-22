eaf26f00c05e40aa63c8471edd1b825bfd3ca041
Albert Zeyer <albzey@gmail.com>
2025-11-22 12:03:16 +0100

    more

--- users/zeyer/experiments/exp2025_11_22_lm_scaling_laws_loquacious.py
+++ users/zeyer/experiments/exp2025_11_22_lm_scaling_laws_loquacious.py
@@ -1,17 +1,32 @@
+from __future__ import annotations
+from typing import Dict, Tuple
+
+from i6_core.returnn import ReturnnTrainingJob
 from sisyphus import tk
 from i6_experiments.users.zeyer.utils.dict_update import dict_update_deep
-from i6_experiments.users.zeyer.model_interfaces import ModelDefWithCfg
+from i6_experiments.users.zeyer.model_interfaces import ModelDefWithCfg, ModelWithCheckpoint
 from i6_experiments.users.zeyer.utils.sis_setup import get_setup_prefix_for_module
 
 from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.configs import (
     config_96gb_bf16_accgrad1,
     _get_cfg_lrlin_oclr_by_bs_nep_v3,
+    _get_cfg_lrlin_oclr_by_bs_nep_v4,
+    _batch_size_factor,
 )
 
-from i6_experiments.users.zeyer.datasets.loquacious import get_loquacious_text_only_dataset_v2
+from i6_experiments.users.zeyer.datasets.loquacious import (
+    get_loquacious_task_raw_v2,
+    get_loquacious_text_only_dataset_v2,
+)
 
 import returnn.frontend as rf
 from returnn.frontend.decoder.transformer import TransformerDecoder
+from returnn.frontend.encoder.conformer import (
+    ConformerEncoder,
+    ConformerEncoderLayer,
+    ConformerConvSubsample,
+    ConformerPositionwiseFeedForward,
+)
 
 
 __setup_root_prefix__ = "exp2025_11_22_lm_scaling_laws_loquacious"
@@ -23,12 +38,13 @@ def py():
         recog_ext_labelwise_with_lm,
     )
 
-    train_base_asr_models()
-    train_lms()
+    prefix = get_setup_prefix_for_module(__name__)
+
+    ctc_model_name, ctc_model = get_ctc_model()
+    lms = train_lms()
 
     # Scaling laws for std LMs (num params, num epochs, total training time)
 
-    from i6_experiments.users.zeyer.train_v4 import train_models_by_prefix as train_v4_models
     from i6_experiments.users.zeyer.returnn.model_num_params_from_config import GetNumParamsFromReturnnConfigJob
     from i6_experiments.users.zeyer.returnn.total_runtime_from_training import GetTotalRuntimeFromReturnnTrainingJob
     from i6_experiments.users.zeyer.plots.scaling_laws import ScalingLawPlotJob
@@ -37,25 +53,25 @@ def py():
     params_points = {}
     time_points = {}
 
-    ctc_model_name = "L16-D1024-spm10k-auxAED-b100k"
-
     params_points_ = params_points["CTC+LM"] = []
     time_points_ = time_points["CTC+LM"] = []
 
-    for lm_name, exp in train_v4_models.items():
+    for lm_name, lm in lms.items():
         if not lm_name.startswith("lm/"):
             continue
 
-        num_params = GetNumParamsFromReturnnConfigJob(exp.get_training_job().returnn_config).out_num_params
-        tk.register_output(f"{lm_name}/num_params.txt", num_params)
+        train_job: ReturnnTrainingJob = lm.checkpoint.path.creator
+        num_params = GetNumParamsFromReturnnConfigJob(train_job.out_returnn_config_file).out_num_params
+        tk.register_output(f"{prefix}/{lm_name}/num_params.txt", num_params)
 
-        train_time_secs = GetTotalRuntimeFromReturnnTrainingJob(exp.scores_and_learning_rates).out_train_time_secs
-        tk.register_output(f"{lm_name}/total_train_time_secs.txt", train_time_secs)
+        train_time_secs = GetTotalRuntimeFromReturnnTrainingJob(train_job.out_learning_rates).out_train_time_secs
+        tk.register_output(f"{prefix}/{lm_name}/total_train_time_secs.txt", train_time_secs)
 
         res = recog_ext_with_lm(
             ctc_model_name=ctc_model_name,
-            lm_name=lm_name[len("lm/") :],
-            lm=exp.get_last_fixed_epoch(),
+            ctc_model=ctc_model,
+            lm_name=lm_name,
+            lm=lm,
             ctc_soft_collapse_threshold=0.9,
         )
         res_wer = res.get_main_measure_value_as_variable()
@@ -64,78 +80,114 @@ def py():
             time_points_.append((train_time_secs / 60 / 60, res_wer))
 
     tk.register_output(
-        "lm/scaling_plot.pdf",
+        f"{prefix}/lm_scaling_plot.pdf",
         ScalingLawPlotJob(
             x_label="Train time [h]", y_label="WER [%]", points=time_points, filter_outliers=True
         ).out_plot_pdf,
     )
 
 
-def train_base_asr_models():
-    from returnn.frontend.encoder.conformer import (
-        ConformerEncoder,
-        ConformerEncoderLayer,
-        ConformerConvSubsample,
-        ConformerPositionwiseFeedForward,
-    )
-    from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.ctc import (
-        train_exp as ctc_train_exp,
+def get_ctc_model(*, subset: str = "large", total_k_hours: int = 250) -> Tuple[str, ModelWithCheckpoint]:
+    from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.aed import (
+        train_exp as aed_train_exp,
         _raw_sample_rate,
-        _batch_size_factor,
-        speed_pert_librosa_config,
+    )
+    from i6_experiments.users.zeyer.experiments.exp2024_04_23_baselines.recog_ext.aed_ctc import (
+        aed_ctc_timesync_recog_recomb_auto_scale,
     )
 
-    for num_layers, num_dims, batch_size in [(16, 1024, 100_000)]:
-        # Baseline (without using TTS).
-        # Warning: this keeps aux_loss_layers=[4, 8], not sure if this is optimal...
-        ctc_train_exp(
-            f"L{num_layers}-D{num_dims}-spm10k-auxAED-b{batch_size // 1000}k",
-            config_96gb_bf16_accgrad1,
-            model_config={
-                "enc_build_dict": rf.build_dict(
-                    # ConformerEncoder(in_dim, enc_model_dim, **enc_opts)
-                    ConformerEncoder,
-                    input_layer=rf.build_dict(
-                        ConformerConvSubsample,
-                        out_dims=[32, 64, 64],
-                        filter_sizes=[(3, 3), (3, 3), (3, 3)],
-                        pool_sizes=[(1, 2)],
-                        strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
-                    ),
-                    num_layers=num_layers,
-                    out_dim=num_dims,
-                    encoder_layer=rf.build_dict(
-                        ConformerEncoderLayer,
-                        ff=rf.build_dict(
-                            ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
-                        ),
-                        num_heads=8,
-                    ),
+    prefix = get_setup_prefix_for_module(__name__)
+    task_spm10k = get_loquacious_task_raw_v2(vocab="spm10k")
+    # i6_experiments.users.zeyer.experiments.exp2025_10_04_loquacious.py
+    train_epoch_split_per_subset = {"clean": 13, "small": 1, "medium": 2, "large": 25}
+    hours_per_subset = {"clean": 13_000, "small": 250, "medium": 2_500, "large": 25_000}
+    train_epoch_split = train_epoch_split_per_subset[subset]
+    num_full_ep = total_k_hours * 1_000 / hours_per_subset[subset]
+    n_ep = round(num_full_ep * train_epoch_split)
+    name = f"base-v2-{subset}-nFullEp{num_full_ep:.1f}-nEp{n_ep}-totalHours{total_k_hours}k"
+    exp = aed_train_exp(
+        name,
+        config_96gb_bf16_accgrad1,
+        prefix=prefix + "/aed/",
+        task=get_loquacious_task_raw_v2(vocab="spm10k", subset_name=subset, train_epoch_split=train_epoch_split),
+        model_config={
+            "behavior_version": 24,
+            "__serialization_version": 2,
+            "enc_build_dict": rf.build_dict(
+                ConformerEncoder,
+                input_layer=rf.build_dict(
+                    ConformerConvSubsample,
+                    out_dims=[32, 64, 64],
+                    filter_sizes=[(3, 3), (3, 3), (3, 3)],
+                    pool_sizes=[(1, 2)],
+                    strides=[(1, 1), (3, 1), (2, 1)],  # downsampling 6
                 ),
-                "feature_batch_norm": True,
-            },
-            config_updates={
-                **_get_cfg_lrlin_oclr_by_bs_nep_v3(batch_size, 100, batch_size_factor=_batch_size_factor),
-                "optimizer.weight_decay": 1e-2,
-                "__train_audio_preprocess": speed_pert_librosa_config,
-                "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
-                # purely used for training
-                "aux_attention_decoder": rf.build_dict(TransformerDecoder, num_layers=6),
-                "max_seq_length_default_target": None,
-                # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
-                # out of 281241 seqs in train, we removed only 71 seqs.
-                # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
-                "max_seq_length_default_input": 19.5 * _raw_sample_rate,
-            },
-            post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
-            vocab="spm10k",
-            train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
-            dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
-            env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
-        )
+                num_layers=16,
+                out_dim=1024,
+                encoder_layer=rf.build_dict(
+                    ConformerEncoderLayer,
+                    ff=rf.build_dict(
+                        ConformerPositionwiseFeedForward, activation=rf.build_dict(rf.relu_square), with_bias=False
+                    ),
+                    num_heads=8,
+                ),
+            ),
+            # Default AED decoder size: 6 layers, 512 dim
+            "dec_build_dict": rf.build_dict(
+                TransformerDecoder,
+                num_layers=6,
+                model_dim=1024,
+                norm=rf.build_dict(rf.RMSNorm),
+                ff=rf.build_dict(rf.decoder.transformer.FeedForwardGated),
+                layer_opts=dict(self_att=rf.build_dict(rf.RotaryPosCausalSelfAttention, with_bias=False)),
+                # When only trained on LS ASR data, keep the default dropout?
+                # dropout=0.0,
+                # att_dropout=0.0,
+            ),
+            "feature_batch_norm": True,
+        },
+        config_updates={
+            **_get_cfg_lrlin_oclr_by_bs_nep_v4(n_ep, base_lr=0.5),
+            "batch_size": 100_000 * _batch_size_factor,
+            "optimizer.weight_decay": 1e-2,
+            "accum_grad_multiple_step": 1,
+            # "__train_audio_preprocess": speed_pert_librosa_config,
+            # "speed_pert_discrete_values": [0.7, 0.8, 0.9, 1.0, 1.1],
+            "aux_loss_layers": [4, 10, 16],
+            "dec_aux_loss_layers": [3],
+            "max_seq_length_default_target": None,
+            # Note on max seq len stats: Before, when we used max_seq_length_default_target=75 with bpe10k,
+            # out of 281241 seqs in train, we removed only 71 seqs.
+            # With max seq len 19.5 secs on the audio, we also remove exactly 71 seqs.
+            "max_seq_length_default_input": 19.5 * _raw_sample_rate,
+        },
+        post_config_updates={"log_grad_norm": True, "__multi_proc_dataset_opts": {"num_workers": 25}},
+        vocab="spm10k",
+        # train_vocab_opts={"other_opts": {"enable_sampling": True, "alpha": 0.7}},
+        train_vocab_opts={"other_opts": {"class": "SamplingBytePairEncoding", "breadth_prob": 0.01}},
+        dataset_train_opts={"train_epoch_split": 1, "train_epoch_wise_filter": None},
+        env_updates={"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"},
+    )
+    model = exp.get_last_fixed_epoch()
+    aed_ctc_timesync_recog_recomb_auto_scale(
+        prefix=f"{prefix}/aed/{name}/aed+ctc",
+        task=task_spm10k,
+        aed_ctc_model=model,
+        aux_ctc_layer=16,
+    )
+    model_def = model.definition
+    extra_config = {"aux_loss_layers": [16]}
+    if isinstance(model_def, ModelDefWithCfg):
+        cfg = model_def.config
+        cfg = dict_update_deep(cfg, extra_config)
+        model_def = ModelDefWithCfg(model_def.model_def, cfg)
+    else:
+        model_def = ModelDefWithCfg(model_def, extra_config)
+    model = ModelWithCheckpoint(definition=model_def, checkpoint=model.checkpoint)
+    return name, model
 
 
-def train_lms():
+def train_lms() -> Dict[str, ModelWithCheckpoint]:
     from i6_experiments.users.zeyer.train_v4 import train
     from i6_experiments.users.zeyer.experiments.exp2025_10_04_loquacious import (
         lm_model_def,
@@ -145,7 +197,7 @@ def train_lms():
 
     prefix = get_setup_prefix_for_module(__name__)
 
-    base_lm_exps()
+    lms = base_lm_exps().copy()
 
     # Try various configurations.
     # The goal is to have, for e.g. a given number of training time, or given amount of parameters, etc,
@@ -222,7 +274,7 @@ def train_lms():
         # n32-d1024-noAbsPos-rmsNorm-ffGated-rope-noBias-drop0-b400_20k-nEp100 by default
         # reorder and set defaults
         opts = {"n": opts.pop("n", 32), "d": opts.pop("d", 1024), **opts}
-        name = f"{prefix}/lm/trafo-v2-{_name_for_dict(opts)}-spm10k"
+        name = f"trafo-v2-{_name_for_dict(opts)}-spm10k"
         n_l = opts.pop("n")
         dim = opts.pop("d")
         n_ep = opts.pop("nEp", 50)
@@ -231,8 +283,8 @@ def train_lms():
         drop = opts.pop("drop", 0.0)
         att_drop = opts.pop("adrop", drop)
         assert not opts
-        train(
-            name,
+        exp = train(
+            f"{prefix}/lm/{name}",
             config=dict_update_deep(
                 config_96gb_bf16_accgrad1,
                 {
@@ -268,6 +320,9 @@ def train_lms():
             ),
             train_def=lm_train_def,
         )
+        lms[name] = exp.get_last_fixed_epoch()
+
+    return lms
 
 
 def _name_for_dict(cfg: dict) -> str:
