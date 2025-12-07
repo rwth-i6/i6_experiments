8634843011e88f2268870a7c36f544cca63ce9e3
Dorian Koch <dorian.koch@ml.rwth-aachen.de>
2025-12-07 17:15:32 +0100

    fix

--- users/dorian_koch/datasets/ConcatDataPostproc.py
+++ users/dorian_koch/datasets/ConcatDataPostproc.py
@@ -13,7 +13,10 @@ from returnn.log import log
 from decimal import Decimal
 from itertools import islice
 from typing import Callable
-from collections.abc import Iterator
+from collections.abc import Iterator, Sequence
+import torch
+from returnn.torch.frontend._backend import TorchBackend
+
 
 class ConcatDataPostproc(Callable[[Iterator[TensorDict]], Iterator[TensorDict]]):
     """
@@ -22,60 +25,76 @@ class ConcatDataPostproc(Callable[[Iterator[TensorDict]], Iterator[TensorDict]])
 
     preserves_num_seqs = False
 
-    def __init__(self, concat_dims: dict[str, rf.Dim], *, concat_prob: float = 0.1, length_key: str = "data"):
+    def __init__(self, concat_keys: Sequence[str], *, concat_prob: float = 0.1, length_key: str = "data"):
         """
-        :param concat_dims: dict of dimensions along which to concatenate. If not given, that tensor will not be concatenated and will be taken from the second sequence.
+        :param concat_keys: data keys which should be concatenated
         :param concat_prob: probability of concatenating two sequences.
         :param length_key: data key to determine the segment length from for ordering.
         """
         self.length_key = length_key
         self.concat_prob = concat_prob
-        self.concat_dims = concat_dims
+        self.concat_keys = concat_keys
 
     def __call__(self, iterator: Iterator[TensorDict], **kwargs) -> Iterator[TensorDict]:
         """:return: generator"""
         iterator = iter(iterator)
-        
 
         is_first = True
-        while True:
+        try:
+            while True:
+                # get one
+                n = next(iterator)
+                if is_first:
+                    # print every entry once
+                    is_first = False
+                    print("ConcatDataPostproc: first data entry (debug):")
+                    for name, tensor in n.data.items():
+                        print(f"ConcatDataPostproc: tensor '{name}': {tensor}")
 
-            # get one
-            n = next(iterator)
-            if is_first:
-                # print every entry once
-                is_first = False
-                print("ConcatDataPostproc: first data entry (debug):")
-                for name, tensor in n.data.items():
-                    print(f"ConcatDataPostproc: tensor '{name}': {tensor}")
+                if random.random() > self.concat_prob:
+                    yield n
+                    continue
 
-            if random.random() > self.concat_prob:
-                yield n
-                continue
+                # get another one
+                m = next(iterator, None)
+                if m is None:  # done
+                    yield n
+                    break
 
-            # get another one
-            m = next(iterator, None)
-            if m is None: # done
-                yield n
-                break
- 
-            # we copy everything from m, because that tensors' complete_frac and other info is more relevant
-            x = {}
-            for name, tensor in m.data.items():
-                if name in self.concat_dims:
-                    dim = self.concat_dims[name]
-                    assert dim in n.data[name].dims, f"concat dim {dim} not in tensor {name} dims {n.data[name].dims}"
-                    # TODO maybe a seperator token?
-                    concatenated, _ = rf.concat((n.data[name], dim), (tensor, dim))
-                    x[name] = concatenated
-                else:
-                    x[name] = tensor
+                # we copy everything from m, because that tensors' complete_frac and other info is more relevant
+                x = {}
+                for name, tensor in m.data.items():
+                    if name in self.concat_keys:
+                        assert len(n.data[name].dims_set) == 1
+                        assert len(tensor.dims_set) == 1
+                        # TODO maybe a seperator token?
 
-            yield TensorDict(x)
+                        # rf concat doesnt work because the dims are whacky
+                        # just use torch code
+                        # concatenated, _ = rf.concat((n.data[name], n.data[name].dims[0]), (tensor, tensor.dims[0]))
+                        a: numpy.ndarray = n.data[name].raw_tensor
+                        b: numpy.ndarray = tensor.raw_tensor
+                        assert isinstance(a, numpy.ndarray)
+                        assert isinstance(b, numpy.ndarray)
+                        assert a.ndim == 1
+                        assert b.ndim == 1
 
-    def _get_seq_len(self, tdict: TensorDict) -> int:
-        """
-        :return: segment length of the segment in `tdict` as measured by `self.length_key` for comparison.
-        """
-        return tdict.data[self.length_key].raw_tensor.shape[0]
+                        # apparently these are numpy arrays???
+                        concat = numpy.concatenate([a, b], axis=0)
+                        # concat = torch.cat([a, b], dim=0)
 
+                        # rf.convert_to_tensor crashes for some reason...
+                        x[name] = rf.Tensor(
+                            f"{tensor.name or 'b'}_concat",
+                            dims=tensor.dims,
+                            dtype=tensor.dtype,
+                            sparse_dim=tensor.sparse_dim,
+                            feature_dim=tensor.feature_dim,
+                            raw_tensor=concat,
+                        )
+                    else:
+                        x[name] = tensor
+
+                yield TensorDict(x)
+        except StopIteration:
+            return  # postprocessing dataset doesnt like StopIteration being raised?
