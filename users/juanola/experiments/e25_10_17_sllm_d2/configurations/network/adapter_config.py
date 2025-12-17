rwth-i6/i6_experiments

commit d052939fec40c40aa5f5f1034a67bb8e0f2c08f1

[mj.SLLM] Adds adapter configurations, and setup for LinearAdapter
Changes hashes and model paths!

Not tested! waiting for work/i6_core/returnn/training/ReturnnTrainingJob.Tpdd1nj0rvvV

users/juanola/experiments/e25_10_17_sllm_d2/configurations/network/adapter_config.py
@@ -1,0 +1,41 @@
+from dataclasses import dataclass
+
+from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.protocols.has_name_protocol import \
+    HasNameProtocol
+
+
+@dataclass(frozen=True)
+class AdapterConfig(HasNameProtocol):
+    """
+    Encoder configuration base dataclass.
+
+    Can contain default values.
+    """
+    adapter_class_path: str
+
+    # add parameters if needed
+
+    @property
+    def name(self) -> str:
+        return self._get_model_name()
+
+    def _get_model_name(self) -> str:
+        return self.adapter_class_path.split(".")[-1]
+
+
+"""
+Specific configurations set below.
+"""
+
+
+def linear_adapter_with_downsampling() -> AdapterConfig:
+    return AdapterConfig(
+        adapter_class_path="i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.networks.adapters.linear_adapter_with_concat_downsampling.LinearAdapterWithConcatDownsampling",
+    )
+
+def linear_adapter() -> AdapterConfig:
+    return AdapterConfig(
+        adapter_class_path="i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.networks.adapters.linear_adapter.LinearAdapter.",
+    )
+
+# For inheritance use: dataclasses.replace(OriginalClass, elements_to_modify)

users/juanola/experiments/e25_10_17_sllm_d2/networks/conformer_qwen_v1.py
@@ -27,10 +27,11 @@ from i6_models.primitives.feature_extraction import (
     RasrCompatibleLogMelFeatureExtractionV1Config,
 )
 from i6_models.primitives.specaugment import specaugment_v1_by_length
+from .adapters.linear_adapter import LinearAdapter
+from .adapters.linear_adapter_with_concat_downsampling import LinearAdapterWithConcatDownsampling
 
 from .interfaces.aed_ctc_model_protocol import AedCtcModelProtocol
 from .interfaces.base_encoder_decoder_model import BaseEncoderDecoderModel
-from .linear_adapter_with_concat_downsampling import LinearAdapterWithConcatDownsampling
 from .qwen2_decoder_state import Qwen2DecoderState
 
 
@@ -101,6 +102,9 @@ class Model(nn.Module, AedCtcModelProtocol,
             bos_idx: int,
             eos_idx: int,
             blank_idx: Optional[int] = None,
+
+            # ADAPTER
+            adapter_class_path: str,
 
             # RF DEFAULTS
             dropout: float = 0.1,
@@ -254,10 +258,13 @@ class Model(nn.Module, AedCtcModelProtocol,
             self.decoder_embed_func = nn.Embedding(vocab_size, qwen2_config.hidden_size)
 
             # Adapter
-            self.encoder_decoder_adapter = LinearAdapterWithConcatDownsampling(
+            adapter_class = eval(adapter_class_path)
+            self.encoder_decoder_adapter = adapter_class(
                 in_dim=encoder_dim,
                 out_dim=qwen2_config.hidden_size,
+                # TODO: more parameters could be passed through a dict or so!
             )
+
 
         if verbose:
             print(" ***** MODEL PARAMETERS *****")

users/juanola/experiments/e25_10_17_sllm_d2/configurations/experiment_config.py
@@ -6,7 +6,7 @@ from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.d
     label_baseline
 from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.network.network_config import \
     NetworkConfig, \
-    network_baseline, network_SLLM_dropout, network_SLLM_tuned_dropout, network_SLLM_small_decoder
+    network_baseline, network_SLLM_dropout, network_SLLM_tuned_dropout, network_SLLM_small_decoder, network_linear_adapter
 from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.pipeline.prior_config import \
     PriorConfig, prior_v1
 from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.pipeline.search_config import \
@@ -69,5 +69,8 @@ def exp_v4() -> ExperimentConfig:
         prior=prior_v1(),
         search=search_baseline(),
     )
+
+def exp_v5() -> ExperimentConfig:
+    return replace(exp_v4(), network=network_linear_adapter())
 
 # For inheritance use: dataclasses.replace(OriginalClass, elements_to_modify)

users/juanola/experiments/e25_10_17_sllm_d2/configurations/experiment_version.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from .experiment_config import exp_baseline, exp_v2, ExperimentConfig, exp_v3, exp_v4
+from .experiment_config import exp_baseline, exp_v2, ExperimentConfig, exp_v3, exp_v4, exp_v5
 
 
 class ExperimentVersion(Enum):
@@ -8,6 +8,7 @@ class ExperimentVersion(Enum):
     V2_DROPOUT = "SLLM_dropout"
     V3_TUNED = "SLLM_tuned_dropout"
     V4_SMALL_DECODER = "SLLM_small_decoder"
+    V5_LINEAR_ADAPTER = "SLLM_linear_adapter"
     # Expand here
 
 
@@ -16,6 +17,7 @@ _EXPERIMENT_BUILDERS = {
     ExperimentVersion.V2_DROPOUT: exp_v2,
     ExperimentVersion.V3_TUNED: exp_v3,
     ExperimentVersion.V4_SMALL_DECODER: exp_v4,
+    ExperimentVersion.V5_LINEAR_ADAPTER: exp_v5,
     # Expand here
 }
 

users/juanola/experiments/e25_10_17_sllm_d2/configurations/network/network_config.py
@@ -1,10 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 
+from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.network.adapter_config import (
+    AdapterConfig,
+    linear_adapter_with_downsampling,
+    linear_adapter,
+)
 from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.network.decoder_config import (
     DecoderConfig,
     decoder_baseline,
     decoder_dropout,
-    decoder_dropout_tuned, small_decoder,
+    decoder_dropout_tuned,
+    small_decoder,
 )
 from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.network.encoder_config import (
     EncoderConfig,
@@ -14,7 +20,9 @@ from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.n
     FeatureExtractionConfig,
     feature_extraction_baseline,
 )
-from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.protocols.has_name_protocol import HasNameProtocol
+from i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.configurations.protocols.has_name_protocol import (
+    HasNameProtocol,
+)
 
 
 @dataclass(frozen=True)
@@ -27,11 +35,12 @@ class NetworkConfig(HasNameProtocol):
 
     feature_extraction: FeatureExtractionConfig
     encoder: EncoderConfig
+    adapter: AdapterConfig
     decoder: DecoderConfig
 
     @property
     def name(self) -> str:
-        return f"{self.encoder.name}-{self.decoder.name}"
+        return f"{self.encoder.name}-{self.adapter.name}-{self.decoder.name}"
 
 
 """
@@ -43,6 +52,7 @@ def network_baseline() -> NetworkConfig:
     return NetworkConfig(
         feature_extraction=feature_extraction_baseline(),
         encoder=encoder_baseline(),
+        adapter=linear_adapter_with_downsampling(),
         decoder=decoder_baseline(),
     )
 
@@ -51,6 +61,7 @@ def network_SLLM_dropout() -> NetworkConfig:
     return NetworkConfig(
         feature_extraction=feature_extraction_baseline(),
         encoder=encoder_baseline(),
+        adapter=linear_adapter_with_downsampling(),
         decoder=decoder_dropout(),
     )
 
@@ -59,15 +70,22 @@ def network_SLLM_tuned_dropout() -> NetworkConfig:
     return NetworkConfig(
         feature_extraction=feature_extraction_baseline(),
         encoder=encoder_baseline(),
+        adapter=linear_adapter_with_downsampling(),
         decoder=decoder_dropout_tuned(),
     )
+
 
 def network_SLLM_small_decoder() -> NetworkConfig:
     return NetworkConfig(
         feature_extraction=feature_extraction_baseline(),
         encoder=encoder_baseline(),
+        adapter=linear_adapter_with_downsampling(),
         decoder=small_decoder(),
     )
+
+
+def network_linear_adapter() -> NetworkConfig:
+    return replace(network_SLLM_small_decoder(), adapter=linear_adapter())
 
 
 # For inheritance use: dataclasses.replace(OriginalClass, elements_to_modify)

users/juanola/experiments/e25_10_17_sllm_d2/entrypoints.py
@@ -18,6 +18,7 @@ def main():
     e3_d()
     e3_td()
     e3v4()
+    e3v5()
 
     # DEBUGS
     e3d()
@@ -50,6 +51,10 @@ def e3_td():
 
 def e3v4():
     ex3.sllm_ep([ExperimentVersion.V4_SMALL_DECODER], itc_training=True)
+
+
+def e3v5():
+    ex3.sllm_ep([ExperimentVersion.V5_LINEAR_ADAPTER], itc_training=True)
 
 
 """

users/juanola/experiments/e25_10_17_sllm_d2/experiments.py
@@ -105,7 +105,8 @@ def sllm_ep(
             epochs_to_evaluate = [partition_epochs]
         else:
             run_best_4 = run_best = run_test = True
-            epochs_to_evaluate = default_returnn_keep_epochs(partition_epochs)
+            specific_epochs = set({})
+            epochs_to_evaluate = default_returnn_keep_epochs(partition_epochs) | specific_epochs
 
         # Tune-Eval
         results: Dict[Any, Any] = create_tune_and_evaluate_jobs(
@@ -164,12 +165,11 @@ def get_network_args_and_alias(config: ExperimentConfig) -> dict[str, Any]:
     label_config = asdict(config.labels)
     fe_config = asdict(config.network.feature_extraction)
     encoder_config = asdict(config.network.encoder)
+    adapter_config = asdict(config.network.adapter)
     qwen2_decoder_config_job = Qwen2DecoderConfigJobV2(config.network.decoder, config.labels, target_filename=f"config-{config.network.decoder.name}-for-i6-spm.json")
-    decoder_config ={"config_path": qwen2_decoder_config_job.out_file}
+    decoder_config = {"config_path": qwen2_decoder_config_job.out_file}
 
-
-    network_args = label_config | fe_config | encoder_config | decoder_config
-
+    network_args = label_config | fe_config | encoder_config | adapter_config | decoder_config
     return network_args
 
 

users/juanola/experiments/e25_10_17_sllm_d2/networks/adapters/linear_adapter.py
@@ -1,0 +1,24 @@
+import torch
+from torch import nn, Tensor
+
+
+class LinearAdapter(nn.Module):
+    def __init__(
+        self,
+        in_dim: int,
+        out_dim: int,
+        *,
+        with_bias: bool = True,
+    ):
+        super().__init__()
+
+        self.speech_to_llm_proj = torch.nn.Linear(in_dim, out_dim, bias=with_bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+
+        :param x: input of shape [B, T, F]
+        :return:
+        """
+        x = self.speech_to_llm_proj(x)
+        return x

commit eab714f17ec9b657b3e8e66c27ac2bc9f4a279af

[mj.SLLM] Fix of adapter loading! Was broken in d052939fec40c40aa5f5f1034a67bb8e0f2c08f1

users/juanola/experiments/e25_10_17_sllm_d2/configurations/network/adapter_config.py
@@ -41,7 +41,7 @@ def linear_adapter_with_downsampling() -> AdapterConfig:
 
 def linear_adapter() -> AdapterConfig:
     return AdapterConfig(
-        adapter_class_path="i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.networks.adapters.linear_adapter.LinearAdapter.",
+        adapter_class_path="i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.networks.adapters.linear_adapter.LinearAdapter",
     )
 
 # For inheritance use: dataclasses.replace(OriginalClass, elements_to_modify)

users/juanola/experiments/e25_10_17_sllm_d2/networks/conformer_qwen_v1.py
@@ -1,5 +1,6 @@
 __all__ = ["Model"]
 
+import importlib
 from typing import Dict, List, Literal, Optional, Sequence, Tuple, TypedDict, Union, Any
 
 import tree
@@ -27,8 +28,8 @@ from i6_models.primitives.feature_extraction import (
     RasrCompatibleLogMelFeatureExtractionV1Config,
 )
 from i6_models.primitives.specaugment import specaugment_v1_by_length
-from .adapters.linear_adapter import LinearAdapter
-from .adapters.linear_adapter_with_concat_downsampling import LinearAdapterWithConcatDownsampling
+from .adapters.linear_adapter import LinearAdapter # NEEDED!
+from .adapters.linear_adapter_with_concat_downsampling import LinearAdapterWithConcatDownsampling # NEEDED!
 
 from .interfaces.aed_ctc_model_protocol import AedCtcModelProtocol
 from .interfaces.base_encoder_decoder_model import BaseEncoderDecoderModel
@@ -43,6 +44,11 @@ from .qwen_decoder.modeling_qwen2_with_dropout import Qwen2ForCausalLM
 def _relu_sq(x):
     """Squared ReLU."""
     return nn.functional.relu(x) ** 2.0
+
+def load_class_from_path(class_path: str): # TODO: this should be improved maybe just storing an enum in the adapters package
+    module_path, class_name = class_path.rsplit(".", 1)
+    module = importlib.import_module(module_path)
+    return getattr(module, class_name)
 
 
 class _SpecAugArgs(TypedDict):
@@ -104,7 +110,7 @@ class Model(nn.Module, AedCtcModelProtocol,
             blank_idx: Optional[int] = None,
 
             # ADAPTER
-            adapter_class_path: str,
+            adapter_class_path: str = "i6_experiments.users.juanola.experiments.e25_10_17_sllm_d2.networks.adapters.linear_adapter_with_concat_downsampling.LinearAdapterWithConcatDownsampling",
 
             # RF DEFAULTS
             dropout: float = 0.1,
@@ -258,7 +264,7 @@ class Model(nn.Module, AedCtcModelProtocol,
             self.decoder_embed_func = nn.Embedding(vocab_size, qwen2_config.hidden_size)
 
             # Adapter
-            adapter_class = eval(adapter_class_path)
+            adapter_class = load_class_from_path(adapter_class_path)
             self.encoder_decoder_adapter = adapter_class(
                 in_dim=encoder_dim,
                 out_dim=qwen2_config.hidden_size,