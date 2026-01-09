9a12a7c0785c01829cc16ab2e20074b02a65eed1
Albert Zeyer <albzey@gmail.com>
2026-01-09 02:21:11 +0100

    optimize

--- users/zeyer/nn_rf/encoder/chunked_conformer_v2.py
+++ users/zeyer/nn_rf/encoder/chunked_conformer_v2.py
@@ -417,16 +417,6 @@ class ChunkedRelPosSelfAttention(rf.RelPosSelfAttention):
         hist_dim = Dim(None, name=f"{axis.description}:kv")
         k, _ = rf.replace_dim(k, in_dim=axis, out_dim=hist_dim)
         v, _ = rf.replace_dim(v, in_dim=axis, out_dim=hist_dim)
-        if chunking:
-            v, hist_dim_ = _mem_chunks(
-                v,
-                spatial_dim=hist_dim,
-                chunked_time_dim=chunking.chunked_time_dim,
-                mem_size=chunking.chunk_history,
-                end_chunk_size_dim=chunking.end_chunk_size_dim,
-            )
-        else:
-            hist_dim_ = hist_dim
         q_with_bias_u = (q + self.pos_bias_u) if self.pos_bias_u is not None else q  # (batch, head, time1, d_k)
         q_with_bias_v = (q + self.pos_bias_v) if self.pos_bias_v is not None else q  # (batch, head, time1, d_k)
 
@@ -437,8 +427,10 @@ class ChunkedRelPosSelfAttention(rf.RelPosSelfAttention):
                 if chunking.end_chunk_size_dim.is_static()
                 else chunking.end_chunk_size_dim.get_size_tensor(device=source.device)
             )
+            hist_dim_ = chunking.chunk_history * chunking.end_chunk_size_dim + hist_dim
         else:
             query_offset = None
+            hist_dim_ = hist_dim
 
         if self.learned_pos_emb is not None:
             pos_emb, pos_emb_spatial_dim = self.learned_pos_emb(
@@ -485,9 +477,38 @@ class ChunkedRelPosSelfAttention(rf.RelPosSelfAttention):
         scores *= self.key_dim_per_head.dimension**-0.5
         att_weights = rf.softmax(scores, axis=hist_dim_)
         att_weights = rf.dropout(att_weights, self.att_dropout, axis=self.att_dropout_broadcast and hist_dim_)
-        # Masking not needed because softmax should already have masked,
-        # so we have 0.0 att weights for padded frames.
-        att = rf.matmul(att_weights, v, reduce=hist_dim_, use_mask=False)
+
+        if chunking:
+            # Undo the _mem_chunks for att_weights to save memory
+            # and to be able to split the history dimension.
+            att_weights_parts = rf.split(
+                att_weights,
+                axis=hist_dim_,
+                out_dims=chunking.chunk_history * [chunking.end_chunk_size_dim] + [hist_dim],
+            )
+            att_weights_parts = [
+                rf.pad(
+                    part,
+                    axes=[chunking.end_chunk_size_dim],
+                    padding=[(0, hist_dim - chunking.end_chunk_size_dim)],
+                    value=0.0,
+                    out_dims=[hist_dim],
+                )[0]
+                for part in att_weights_parts[:-1]
+            ] + [att_weights_parts[-1]]
+            att_weights, history_parts_dim = rf.stack(att_weights_parts)
+            # (batch, head, time1, time2', history_parts)
+
+            # Masking not needed because softmax should already have masked,
+            # so we have 0.0 att weights for padded frames.
+            att = rf.matmul(att_weights, v, reduce=(hist_dim, history_parts_dim), use_mask=False)
+            # (batch, head, time1, feat)
+
+        else:
+            # Masking not needed because softmax should already have masked,
+            # so we have 0.0 att weights for padded frames.
+            att = rf.matmul(att_weights, v, reduce=hist_dim, use_mask=False)
+
         output, _ = rf.merge_dims(att, dims=(self.num_heads, self.value_dim_per_head), out_dim=self.value_dim_total)
         if self.proj:
             output = self.proj(output)
