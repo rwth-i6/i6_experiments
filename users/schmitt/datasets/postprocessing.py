83d09704c1e8d6a90e4210e8549377fb13a3a838
schmitt <robin.schmitt1@rwth-aachen.de>
2026-01-22 11:59:06 +0100

    add PostprocessingDataset wrapper

---
+++ users/schmitt/datasets/postprocessing.py
@@ -0,0 +1,44 @@
+from typing import List, Optional, Dict, Any, Union, Callable
+
+from sisyphus import Path, tk
+
+from i6_experiments.common.setups.returnn.datasets.base import ControlDataset, Dataset
+
+
+class PostprocessingDataset(Dataset):
+    def __init__(
+            self,
+            *,
+            dataset: Dataset,
+            map_seq_stream: Dict,
+            buf_size: int,
+            num_workers: int,
+    ):
+        super().__init__(
+            additional_options=None
+        )
+        self.dataset = dataset
+        self.map_seq_stream = map_seq_stream
+        self.buf_size = buf_size
+        self.num_workers = num_workers
+
+    def as_returnn_opts(self) -> Dict[str, Any]:
+        """
+        See `Dataset` definition
+        """
+
+        d = {
+            "class": "PostprocessingDataset",
+            "dataset": self.dataset.as_returnn_opts(),
+            "map_seq_stream": self.map_seq_stream,
+            "buf_size": self.buf_size,
+            "num_workers": self.num_workers,
+        }
+
+        sd = super().as_returnn_opts()
+        assert all([k not in sd.keys() for k in d.keys()]), (
+            "conflicting keys in %s and %s" % (str(list(sd.keys())), str(list(d.keys()))),
+        )
+        d.update(sd)
+
+        return d
