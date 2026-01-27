724e3d518009f0c497f62707ab818bae0d60d0ef
Albert Zeyer <albzey@gmail.com>
2026-01-27 10:13:49 +0100

    update

--- users/zeyer/experiments/exp2025_11_11_ctc_lm_search.py
+++ users/zeyer/experiments/exp2025_11_11_ctc_lm_search.py
@@ -56,6 +56,9 @@ def py():
         aux_ctc_layer=aux_ctc_layer,
     )
 
+    from .exp2024_04_23_baselines.recog_ext.ctc import model_recog_with_recomb
+    from .exp2024_04_23_baselines.recog_ext.ctc_v2 import model_recog_with_recomb_v2
+
     lm_name, lm = get_lm(prefix=prefix, vocab=vocab)
     ctc_recog_recomb_labelwise_prior_auto_scale(
         prefix=f"{prefix}/aed/{name}/ctc+lm-v2/{lm_name}",
@@ -64,10 +67,8 @@ def py():
         extra_config={"aux_loss_layers": [aux_ctc_layer]},
         lm=lm,
         prior_dataset=get_loquacious_train_subset_dataset_v2(vocab=vocab),
+        recog_def=model_recog_with_recomb,
     )
-
-    from .exp2024_04_23_baselines.recog_ext.ctc import model_recog_with_recomb
-    from .exp2024_04_23_baselines.recog_ext.ctc_v2 import model_recog_with_recomb_v2
 
     ctc_recog_recomb_labelwise_prior_auto_scale(
         prefix=f"{prefix}/aed/{name}/ctc+lm-v3/{lm_name}",
