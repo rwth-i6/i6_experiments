62c45fb06f284e7edb525d1132bbea42a3b61927
Albert Zeyer <albzey@gmail.com>
2026-02-02 18:58:23 +0100

    more

--- users/zeyer/external_models/qwen2.py
+++ users/zeyer/external_models/qwen2.py
@@ -4,27 +4,38 @@ Qwen2
 
 import functools
 from sisyphus import Path
+from i6_core.returnn.training import PtCheckpoint
 from i6_experiments.common.utils.fake_job import make_fake_job
+from i6_experiments.users.zeyer.model_interfaces import ModelWithCheckpoint, ModelDefWithCfg
 
 
-def get_lm():
+def get_lm() -> ModelWithCheckpoint:
     # /hpcwork/p0023999/hq237549/sisyphus-work-dirs/2026-01-20--llm/work/i6_core/returnn/training/ReturnnTrainingJob.MIU24HbRi60L/output/returnn.config
 
     get_model = functools.partial(
         _qwen2_get_model,
         **{
-            "hf_hub_cache_dir": "/rwthfs/rz/cluster/home/hq237549/experiments/2026-01-20--llm/work/i6_experiments/users/schmitt/external_models/huggingface/DownloadHuggingFaceRepoJob.r7AjtV7muFpk/output/hub_cache",
+            # "/home/hq237549/experiments/2026-01-20--llm/work/i6_experiments/users/schmitt/external_models/huggingface/DownloadHuggingFaceRepoJob.r7AjtV7muFpk/output/hub_cache",
+            "hf_hub_cache_dir": Path(
+                "hub_cache",
+                creator=make_fake_job(
+                    module="i6_experiments.users.schmitt.external_models.huggingface",
+                    name="DownloadHuggingFaceRepoJob",
+                    sis_hash="r7AjtV7muFpk",
+                ),
+            ),
             "freeze_params": False,
             "lora_opts": None,
             "freeze_embedding_layer": True,
         },
     )
+    config = {}
 
     from speech_llm.prefix_lm.model.custom_missing_load_funcs.qwen import qwen_load_tied_embedding_matrices
 
-    preload_from_files = {
+    config["preload_from_files"] = {
         "qwen": {
-            # /rwthfs/rz/cluster/home/hq237549/experiments/2026-01-20--llm/work/i6_core/tools/download/DownloadJob.6SV1LOlUtQMG/output/qwen2-0_5b_model.safetensors
+            # /home/hq237549/experiments/2026-01-20--llm/work/i6_core/tools/download/DownloadJob.6SV1LOlUtQMG/output/qwen2-0_5b_model.safetensors
             "filename": Path(
                 "qwen2-0_5b_model.safetensors",
                 creator=make_fake_job(module="i6_core.tools.download", name="DownloadJob", sis_hash="6SV1LOlUtQMG"),
@@ -36,6 +47,37 @@ def get_lm():
             "ignore_missing": True,
         }
     }
+
+    vocab = {
+        "dim": 151646,
+        "sparse": True,
+        "vocab": {
+            "class": "HuggingFaceTokenizer",
+            # "/home/hq237549/experiments/2026-01-20--llm/work/i6_experiments/users/schmitt/external_models/huggingface/DownloadHuggingFaceRepoJobV2.PUGzhO2dOEpK/output/content",
+            "huggingface_repo_dir": Path(
+                "content",
+                creator=make_fake_job(
+                    module="i6_experiments.users.schmitt.external_models.huggingface",
+                    name="DownloadHuggingFaceRepoJobV2",
+                    sis_hash="PUGzhO2dOEpK",
+                ),
+            ),
+        },
+    }
+
+    # /hpcwork/p0023999/hq237549/sisyphus-work-dirs/2026-01-20--llm/work/i6_core/returnn/training/GetBestPtCheckpointJob.biueEBxdJI4u/output/checkpoint.pt
+    # from /hpcwork/p0023999/hq237549/sisyphus-work-dirs/2026-01-20--llm/work/i6_core/returnn/training/ReturnnTrainingJob.4mQ2qrd6hR3a
+    checkpoint = Path(
+        "checkpoint.pt",
+        creator=make_fake_job(
+            module="i6_core.returnn.training",
+            name="GetBestPtCheckpointJob",
+            sis_hash="biueEBxdJI4u",
+        ),
+    )
+
+    model_with_cfg = ModelDefWithCfg(model_def=get_model, config=config)
+    return ModelWithCheckpoint(definition=model_with_cfg, checkpoint=PtCheckpoint(checkpoint))
 
 
 def _qwen2_get_model(**kwargs):
