c01897123849565d36efd4fbf67420687b2d6eb2
Albert Zeyer <albzey@gmail.com>
2026-02-02 15:17:21 +0100

    qwen (wip)

---
+++ users/zeyer/external_models/qwen2.py
@@ -0,0 +1,40 @@
+"""
+Qwen2
+"""
+
+from sisyphus import Path
+from i6_experiments.common.utils.fake_job import make_fake_job
+
+
+def get_lm():
+    # /hpcwork/p0023999/hq237549/sisyphus-work-dirs/2026-01-20--llm/work/i6_core/returnn/training/ReturnnTrainingJob.MIU24HbRi60L/output/returnn.config
+
+    get_model = __import__("functools").partial(
+        __import__(
+            "speech_llm.prefix_lm.model.definitions.decoders.qwen",
+            fromlist=["Qwen2DecoderV3"],
+        ).Qwen2DecoderV3,
+        **{
+            "hf_hub_cache_dir": "/rwthfs/rz/cluster/home/hq237549/experiments/2026-01-20--llm/work/i6_experiments/users/schmitt/external_models/huggingface/DownloadHuggingFaceRepoJob.r7AjtV7muFpk/output/hub_cache",
+            "freeze_params": False,
+            "lora_opts": None,
+            "freeze_embedding_layer": True,
+        },
+    )
+
+    from speech_llm.prefix_lm.model.custom_missing_load_funcs.qwen import qwen_load_tied_embedding_matrices
+
+    preload_from_files = {
+        "qwen": {
+            # /rwthfs/rz/cluster/home/hq237549/experiments/2026-01-20--llm/work/i6_core/tools/download/DownloadJob.6SV1LOlUtQMG/output/qwen2-0_5b_model.safetensors
+            "filename": Path(
+                "qwen2-0_5b_model.safetensors",
+                creator=make_fake_job(module="i6_core.tools.download", name="DownloadJob", sis_hash="6SV1LOlUtQMG"),
+            ),
+            "init_for_train": True,
+            "checkpoint_key": None,
+            "prefix": "model.",
+            "custom_missing_load_func": qwen_load_tied_embedding_matrices,
+            "ignore_missing": True,
+        }
+    }
