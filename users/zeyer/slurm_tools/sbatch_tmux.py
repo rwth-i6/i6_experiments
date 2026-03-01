70c164447ecdc85dbb8503c3595128c866659851
Albert Zeyer <albzey@gmail.com>
2026-03-01 22:01:40 +0100

    tmux bin arg

--- users/zeyer/slurm_tools/sbatch_tmux.py
+++ users/zeyer/slurm_tools/sbatch_tmux.py
@@ -35,6 +35,7 @@ def main():
         usage="%(prog)s [options] [sbatch arguments...]",
     )
     arg_parser.add_argument("--start-tmux", action="store_true", help=argparse.SUPPRESS)
+    arg_parser.add_argument("--tmux-binary", type=str, help="path to the tmux binary")
     arg_parser.add_argument("--group", type=str, help="chgrp for the tmux socket")
     args, sbatch_args = arg_parser.parse_known_args()
 
@@ -47,7 +48,11 @@ def main():
         *sbatch_args,
         "--signal=B:USR1@5",
         "--wrap=exec %s"
-        % " ".join([sys.executable, __file__, "--start-tmux"] + (["--group", args.group] if args.group else [])),
+        % " ".join(
+            [sys.executable, __file__, "--start-tmux"]
+            + (["--group", args.group] if args.group else [])
+            + (["--tmux-binary", args.tmux_binary] if args.tmux_binary else [])
+        ),
     )
 
 
@@ -55,14 +60,15 @@ def _run_tmux(args):
     signal.signal(signal.SIGUSR1, _signal_handler)
     signal.signal(signal.SIGTERM, _signal_handler)
 
+    tmux_binary = args.tmux_binary if args.tmux_binary else "tmux"
     job_id = os.environ.get("SLURM_JOB_ID", "unknown_slurm_job_id")
     hostname = socket.gethostname()
     socket_filename = f"tmux.sbatch.job_{job_id}.host_{hostname}.pid_{os.getpid()}.socket"
 
     while True:
-        run("tmux", "-S", socket_filename, "new-session", "-d")
+        run(tmux_binary, "-S", socket_filename, "new-session", "-d")
         print(f"Started tmux session with socket: {socket_filename}")
-        print(f"To attach to the tmux session, run: ssh {hostname} -t tmux -S {socket_filename} attach")
+        print(f"To attach to the tmux session, run: ssh {hostname} -t {tmux_binary} -S {socket_filename} attach")
         print("Waiting for tmux session to end...")
         sys.stdout.flush()
 
@@ -72,20 +78,20 @@ def _run_tmux(args):
             run("chmod", "g+rw", socket_filename)
 
         # sanity check that the tmux session is running
-        sp.check_call(["tmux", "-S", socket_filename, "has-session"])
+        sp.check_call([tmux_binary, "-S", socket_filename, "has-session"])
 
         # wait until the tmux session ends, either by the user or by receiving a signal
         try:
             while True:
                 try:
-                    sp.check_call(["tmux", "-S", socket_filename, "has-session"])
+                    sp.check_call([tmux_binary, "-S", socket_filename, "has-session"])
                 except sp.CalledProcessError:
                     print("Tmux session has ended.")
                     break
                 time.sleep(1)
 
         except BaseException:
-            run("tmux", "-S", socket_filename, "kill-session")
+            run(tmux_binary, "-S", socket_filename, "kill-session")
             raise
 
         finally:
