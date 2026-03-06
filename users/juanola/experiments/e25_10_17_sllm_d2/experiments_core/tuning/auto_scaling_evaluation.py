a9c3e02aac5f0bc87dcf633bcea05d926eb93c63
marti.juanola <marti.juanola@ml.rwth-aachen.de>
2026-03-06 11:52:09 +0100

    [mj.SLLM] autoscaling works for CTC + SLLM

--- users/juanola/experiments/e25_10_17_sllm_d2/experiments_core/tuning/auto_scaling_evaluation.py
+++ users/juanola/experiments/e25_10_17_sllm_d2/experiments_core/tuning/auto_scaling_evaluation.py
@@ -1,4 +1,5 @@
 import copy
+from enum import Enum
 from typing import Dict, Any
 
 from i6_core.returnn import ReturnnForwardJobV2
@@ -24,22 +25,43 @@ default_returnn = {
     "returnn_root": RETURNN_ROOT,
 }
 
-def ctc_label_sync_eval_auto_scale(
-        asr_model: ASRModel,
-        search_config: SearchConfig,
-        train_data: TrainingDatasets,
-        tune_datasets: Dict[str, Any],
-        evaluation_name: str,
 
-        label_datastream_key="labels",
-        ogg_zip_dataset_target_key="classes",
-        lowercase_ref: bool = False,
+class Scales(Enum):
+    CTC = "ctc"
+    LLM = "llm"
+    SLLM = "sllm"
+    PRIOR = "prior"
+
+def ctc_label_sync_eval_auto_scale(
+    asr_model: ASRModel,
+    search_config: SearchConfig,
+    train_data: TrainingDatasets,
+    tune_datasets: Dict[str, Any],
+    evaluation_name: str,
+    label_datastream_key="labels",
+    ogg_zip_dataset_target_key="classes",
+    lowercase_ref: bool = False,
+
 ) -> Dict[str, tk.Variable]:
     """
     Robins code (SLLM repo)
-
-    # TODO: for now using CTC & SLLM
     """
+
+    use_ctc = search_config.ctc_scales is not None and len(search_config.ctc_scales) == 1 and search_config.ctc_scales[0] == 1.0
+    use_llm = search_config.ctc_scales is not None and len(search_config.lm_scales) == 1 and search_config.lm_scales[0] == 1.0
+    use_sllm = search_config.ctc_scales is not None and len(search_config.sllm_scales) == 1 and search_config.sllm_scales[0] == 1.0
+    use_prior = search_config.ctc_scales is not None and len(search_config.prior_scales) == 1 and search_config.prior_scales[0] == 1.0
+
+    print(f"Using: ", end="")
+    if use_ctc:
+        print("CTC ", end="")
+    if use_llm:
+        print("LLM ", end="")
+    if use_sllm:
+        print("SLLM ", end="")
+    if use_prior:
+        print("PRIOR ", end="")
+    print("scales!")
 
     # DATASET FOR TUNNING
     assert len(tune_datasets.items()) == 1, "Only one dataset is supported for now!"
@@ -50,7 +72,9 @@ def ctc_label_sync_eval_auto_scale(
     tune_dataset_dict["dataset"]["targets"] = train_data.datastreams[label_datastream_key].as_returnn_targets_opts()
 
     # GET CHECKPOINT & PATH
-    recog_path = f"{evaluation_name}/auto_scaling_test1/{tune_dataset_name}" # TODO: add some params of what is being tuned!
+    recog_path = (
+        f"{evaluation_name}/auto_scaling_test1/{tune_dataset_name}"  # TODO: add some params of what is being tuned!
+    )
 
     # TEXT REFERENCES
     ref = ReturnnDatasetToTextDictJob(
@@ -60,13 +84,10 @@ def ctc_label_sync_eval_auto_scale(
     ).out_txt
     if lowercase_ref:
         ref = PipelineJob(
-            ref,
-            pipeline=[r"""perl -pi -e "s/(:\\s*)(['\"])(.*?)\\2/\$1 . \$2 . lc(\$3) . \$2/e" """]
+            ref, pipeline=[r"""perl -pi -e "s/(:\\s*)(['\"])(.*?)\\2/\$1 . \$2 . lc(\$3) . \$2/e" """]
         ).out
 
-    # Net args # todo!
-
-
+    # Net args # todo! use model with extra networks
 
     # BASE RECOG CONFIG
     beam_size = search_config.beam_search.beam_sizes[0]
@@ -78,136 +99,155 @@ def ctc_label_sync_eval_auto_scale(
         "batch_size": 2_000 * search_config.batch_size_factor,
         "max_seqs": search_config.max_seqs,
     }
-    forward_params = { # fix params from here
+    forward_params = {  # fix params from here
         "beam_size": beam_size,
         "max_tokens_per_sec": 20,
         "sample_rate": 16000,
     }
 
-    # CTC N-BEST RECOGNITION
-    returnn_ctc_n_best_config = get_forward_config_v2(
-        network_import_path=asr_model.network_import_path,
-        base_config=forward_config,
-        net_args=asr_model.net_args,
-        decoder_args=forward_params,
-        forward_module="recognition",
-        forward_method="forward_step_v2", # TODO: check if this works!
-        callback_name="RecognitionToTextDictCallbackV2",
-        label_datastream=train_data.datastreams[label_datastream_key],
-        callback_opts={"include_beam": True, "merge_labels": False,},
-        debug=search_config.debug_returnn_param,
-    )
-    ctc_n_best = forward_single(
-        f"{recog_path}/ctc-n-best",
-        returnn_config=returnn_ctc_n_best_config,
-        checkpoint=(asr_model.checkpoint),
-        dataset_dict=tune_dataset_dict,
-        **default_returnn,
-        rqmt={},
-    )
-    ctc_n_best_merged = SearchOutputRawReplaceJob(
-        ctc_n_best, replacement_list=[(" ", ""), ("Ġ", " ")]
-    ).out_search_results
+    # ACCUMULATED SCORES
+    scores = {}
 
+    if use_ctc:
+        # CTC N-BEST RECOGNITION
+        returnn_ctc_n_best_config = get_forward_config_v2(
+            network_import_path=asr_model.network_import_path,
+            base_config=forward_config,
+            net_args=asr_model.net_args,
+            decoder_args=forward_params,
+            forward_module="recognition",
+            forward_method="forward_step_v2",
+            callback_name="RecognitionToTextDictCallbackV2",
+            label_datastream=train_data.datastreams[label_datastream_key],
+            callback_opts={
+                "include_beam": True,
+                "merge_labels": False,
+            },
+            debug=search_config.debug_returnn_param,
+        )
+        ctc_n_best_original = forward_single(
+            f"{recog_path}/ctc-n-best",
+            returnn_config=returnn_ctc_n_best_config,
+            checkpoint=(asr_model.checkpoint),
+            dataset_dict=tune_dataset_dict,
+            **default_returnn,
+            rqmt={},
+        )
+        ctc_n_best = SearchOutputRawReplaceJob(
+            ctc_n_best_original, replacement_list=[(" ", ""), ("▁", " ")]
+        ).out_search_results
+        scores[Scales.CTC.value] = ctc_n_best
+    else:
+        assert False, "NEEDS CTC"
 
-    # LM RESCORING
-    vocab_file = ExtractVocabLabelsJob(train_data.datastreams[label_datastream_key].as_returnn_targets_opts()).out_vocab
-    rescore_data = {
-        "class": "TextDictDataset",
-        "filename":ctc_n_best,
-        "vocab": {
-            "class": "Vocabulary",
-            "vocab_file": vocab_file,
-            "unknown_label": None,
+    if use_sllm:
+        # SLLM RESCORING
+        vocab_file = ExtractVocabLabelsJob(train_data.datastreams[label_datastream_key].as_returnn_targets_opts()).out_vocab
+        rescore_data = {
+            "class": "TextDictDataset",
+            "filename": ctc_n_best_original,
+            "vocab": {
+                "class": "Vocabulary",
+                "vocab_file": vocab_file,
+                "unknown_label": None,
+            },
         }
-    }
-    rescore_data = { # TODO: add prior and SLLM to be tuned here?
-        "class": "MetaDataset",
-        "datasets": {
-            "orig_data": tune_dataset_dict["dataset"],
-            "hyps": rescore_data
-        },
-        "data_map": {
-            "audio": ("orig_data", "data"), # TODO?? data was audio first...
-            "hyps_flat": ("hyps", "data_flat"),
-            "hyps_seq_lens": ("hyps", "data_seq_lens"),
-        },
-        "seq_order_control_dataset": "hyps",
-    }
-    returnn_rescoring_config = get_forward_config_v2(
-        network_import_path=asr_model.network_import_path,
-        base_config=forward_config_low_batch_size, # because of large beam dim, we need to lower the batch size
-        net_args=asr_model.net_args,
-        decoder_args=forward_params,
-        forward_module="rescoring",
-        forward_method="forward_step_v1",
-        callback_name="RecognitionToTextDictCallbackV2",
-        label_datastream=train_data.datastreams[label_datastream_key],
-        callback_opts={"include_beam": True},
-        extern_data={ # TODO: check out
-            DATA_PARAM_NAME: {"shape": (None,)},
-            "hyps_flat": {
-                "shape": [None],
-                "dtype": "int32",
-                "vocab": rescore_data["datasets"]["hyps"]["vocab"],
+        rescore_data = {  # TODO: add prior and SLLM to be tuned here?
+            "class": "MetaDataset",
+            "datasets": {"orig_data": tune_dataset_dict["dataset"], "hyps": rescore_data},
+            "data_map": {
+                "audio": ("orig_data", "data"),
+                "hyps_flat": ("hyps", "data_flat"),
+                "hyps_seq_lens": ("hyps", "data_seq_lens"),
             },
-            "hyps_seq_lens": {
-                "shape": [beam_size],
-                "dtype": "int32"
+            "seq_order_control_dataset": "hyps",
+        }
+        returnn_rescoring_config = get_forward_config_v2(
+            network_import_path=asr_model.network_import_path,
+            base_config=forward_config_low_batch_size,  # because of large beam dim, we need to lower the batch size
+            net_args=asr_model.net_args,
+            decoder_args=forward_params, # TODO: add param with which module to use for decoding (SLLM or ext LLM)
+            forward_module="rescoring",
+            forward_method="forward_step_v1",
+            callback_name="RecognitionToTextDictCallbackV2",
+            label_datastream=train_data.datastreams[label_datastream_key],
+            callback_opts={"include_beam": True},
+            extern_data={
+                "audio": {"dim": 1},
+                "hyps_flat": {
+                    "shape": [None],
+                    "dtype": "int32",
+                    "vocab": rescore_data["datasets"]["hyps"]["vocab"],
+                },
+                "hyps_seq_lens": {"shape": [beam_size], "dtype": "int32"},
             },
-        },
-        debug=search_config.debug_returnn_param,
-    )
-    sllm_rescore_results = forward_single(  # TODO: change to LM? and proper network...
-        f"{recog_path}/sllm_rescoring",
-        returnn_config=returnn_rescoring_config,
-        checkpoint=(asr_model.checkpoint),
-        dataset_dict=rescore_data,
-        **default_returnn,
-        rqmt={},
-    )
+            debug=search_config.debug_returnn_param,
+        )
+        sllm_rescore_results = forward_single(  # TODO: change to LM? and proper network...
+            f"{recog_path}/sllm_rescoring",
+            returnn_config=returnn_rescoring_config,
+            checkpoint=(asr_model.checkpoint),
+            dataset_dict=rescore_data,
+            **default_returnn,
+            rqmt={},
+        )
+        sllm_rescore_results = SearchOutputRawReplaceJob(
+            sllm_rescore_results, replacement_list=[(" ", ""), ("▁", " ")]
+        ).out_search_results
+        scores[Scales.SLLM.value] = sllm_rescore_results
 
-    # TODO: add prior here !!
+    if use_llm:
+        pass
+
+    if use_prior:
+        pass
+
+        # TODO: add prior here !!
+        # Broadcast prior for batch and beam.. ?
+        # Use trivial forward job rescoring/../broadcast_prior
+
 
     # AUTO SCALING
     opt_scales_job = ScaleTuningJob(
-        scores={"ctc": ctc_n_best_merged, "slm": sllm_rescore_results},
+        scores=scores,
         ref=ref,
-        fixed_scales={"ctc": 1.0},
+        fixed_scales={Scales.CTC.value: 1.0},
         evaluation="edit_distance",
     )
     opt_scales_job.rqmt["engine"] = "short"  # should be fine
     tk.register_output(f"{recog_path}/opt-real-scales", opt_scales_job.out_real_scales)
     tk.register_output(f"{recog_path}/opt-rel-scales", opt_scales_job.out_scales)
-    combined_scores = SearchCombineScoresJob([
-            (opt_scales_job.out_real_scale_per_name["ctc"], ctc_n_best_merged),
-            (opt_scales_job.out_real_scale_per_name["slm"], sllm_rescore_results),
-        ]).out_search_results
+    combined_scores = SearchCombineScoresJob(
+        [
+            (opt_scales_job.out_real_scale_per_name[Scales.CTC.value], scores[Scales.CTC.value]),
+            (opt_scales_job.out_real_scale_per_name[Scales.SLLM.value], scores[Scales.SLLM.value]),
+        ]
+    ).out_search_results
     best_rescore = SearchTakeBestJob(combined_scores).out_best_search_results
-
 
     # GET SCORES
     tune_score_result = generic_sclite_score_recog_out(
         dataset=tune_dataset_dict,
         recog_output=best_rescore,
-        corpus_name="dev", # TODO?
+        corpus_name="dev",  # TODO?
         vocab=train_data.datastreams[label_datastream_key].as_returnn_targets_opts(),
         data_key=ogg_zip_dataset_target_key,
         use_lowercase=lowercase_ref,
-        #apply_text_norm=False,
+        # apply_text_norm=False,
     )
     tk.register_output(f"{recog_path}/tune_rescore/wer", tune_score_result.main_measure_value)
     tk.register_output(f"{recog_path}/tune_rescore/report", tune_score_result.report)
 
     return opt_scales_job.out_real_scale_per_name
 
+
 def generic_sclite_score_recog_out(
-        dataset: Dict[str, Any],
-        recog_output: tk.Path,
-        vocab: Dict[str, Any],
-        data_key: str,
-        corpus_name: str,
-        use_lowercase: bool = False,
+    dataset: Dict[str, Any],
+    recog_output: tk.Path,
+    vocab: Dict[str, Any],
+    data_key: str,
+    corpus_name: str,
+    use_lowercase: bool = False,
 ) -> ScoreResult:
     ref = RecogOutput(
         output=ReturnnDatasetToTextDictJob(
@@ -220,8 +260,7 @@ def generic_sclite_score_recog_out(
     if use_lowercase:
         ref = RecogOutput(
             output=PipelineJob(
-                ref.output,
-                pipeline=[r"""perl -pi -e "s/(:\\s*)(['\"])(.*?)\\2/\$1 . \$2 . lc(\$3) . \$2/e" """]
+                ref.output, pipeline=[r"""perl -pi -e "s/(:\\s*)(['\"])(.*?)\\2/\$1 . \$2 . lc(\$3) . \$2/e" """]
             ).out
         )
 
@@ -231,13 +270,13 @@ def generic_sclite_score_recog_out(
 
 
 def forward_single(
-        prefix_name: str,
-        returnn_config: ReturnnConfig,
-        checkpoint: tk.Path,
-        dataset_dict: Dict,
-        returnn_exe: tk.Path,
-        returnn_root: tk.Path,
-        rqmt: Dict,
+    prefix_name: str,
+    returnn_config: ReturnnConfig,
+    checkpoint: tk.Path,
+    dataset_dict: Dict,
+    returnn_exe: tk.Path,
+    returnn_root: tk.Path,
+    rqmt: Dict,
 ):
     """
     Run search for a specific test dataset
--- users/juanola/experiments/e25_10_17_sllm_d2/rescoring/forward_step.py
+++ users/juanola/experiments/e25_10_17_sllm_d2/rescoring/forward_step.py
@@ -19,43 +19,36 @@ def forward_step_v1(
 ):
     """from SLLM repo"""
 
+    # DATA (audio)
     from returnn.config import get_global_config
-
     config = get_global_config(return_empty_if_none=True)
-
     data_key = config.value("default_data_key", "audio")
     data_ = extern_data[data_key]
-    if data_.feature_dim and data_.feature_dim.dimension == 1:
-        data_ = rf.squeeze(data_, axis=data_.feature_dim)
+    #if data_.feature_dim and data_.feature_dim.dimension == 1:
+    #    data_ = rf.squeeze(data_, axis=data_.feature_dim)
     data = data_.raw_tensor  # (B, T, F)
-    data_lens = (
-        extern_data[data_key].dims[1].dyn_size_ext.raw_tensor.to(device=data.device)
-    )
+    data_lens = (extern_data[data_key].dims[1].dyn_size_ext.raw_tensor.to(device=data.device))
 
+    # HYPS (ctc-hyps)
     targets_flat = extern_data["hyps_flat"]
     targets_flat_time_dim = targets_flat.dims[1]
     targets_seq_lens = extern_data["hyps_seq_lens"]  # [B, beam]
     targets_beam_dim = targets_seq_lens.dims[1]
-    targets_spatial_dim = Dim(
-        rf.copy_to_device(targets_seq_lens, "cpu"), name="targets_spatial"
-    )
+    targets_spatial_dim = Dim(rf.copy_to_device(targets_seq_lens, "cpu"), name="targets_spatial")
     targets = rf.pad_packed(
         targets_flat,
         in_dim=targets_flat_time_dim,
         dims=[targets_beam_dim, targets_spatial_dim],
     )
-    targets = targets.copy_transpose(
-        [batch_dim, targets_beam_dim, targets_spatial_dim]
-    )  # [B, beam, T]
+    targets = targets.copy_transpose([batch_dim, targets_beam_dim, targets_spatial_dim])  # [B, beam, T]
 
     beam_size = targets_beam_dim.dimension
     batch_size = targets.raw_tensor.shape[0]
     target_lens = targets_seq_lens.raw_tensor.view(-1).to(data.device)  # [B * beam]
     targets = targets.raw_tensor.reshape(batch_size * beam_size, -1)  # [B * beam, T]
 
-    adapter_output, aux_log_probs, adapter_output_lens, _, encoder_output_lens = (
-        model.forward(data, data_lens) # TODO: !!!
-    )
+    # ENCODER FORWARD
+    adapter_output, _, adapter_output_lens, _ = (model.forward(data, data_lens))
     adapter_output = adapter_output.repeat_interleave(
         beam_size, dim=0
     )  # [B * beam, T, D]
@@ -63,35 +56,44 @@ def forward_step_v1(
         beam_size, dim=0
     )  # [B * beam]
 
-    input_labels = F.pad(targets, (1, 0), "constant", value=model.bos_idx)
+    # DECODER FORWARD
+    input_labels = F.pad(targets, (1, 0), "constant", value=model.bos_idx) # Add BOS
     input_labels_len = target_lens + 1
-    logits = model.decode_seq( # TODO: !!!
+    logits = model.decode_seq(
         x=input_labels,
         x_lens=input_labels_len.to(device=input_labels.device),
-        audio_features=adapter_output,
-        audio_features_lens=adapter_output_lens,
+        encoder_output=adapter_output,
+        encoder_output_lens=adapter_output_lens,
     )  # [B * beam, T+1, vocab]
-    if model.dec_out_blank_logits is not None: # TODO: !!!
-        # decode_seq returns a tuple of (logits, aux_log_probs)
-        logits, _ = logits
+    # if model.dec_out_blank_logits is not None: # what is this?
+    #     # decode_seq returns a tuple of (logits, aux_log_probs) # if needed, get from encoder forward
+    #     logits, _ = logits
 
     log_probs = F.log_softmax(logits, dim=-1)
-    # this is wrong because of missing EOS -> see v2 below
+
+    targets_w_eos = F.pad(targets, (0, 1), "constant", value=0)  # [B * beam, T+1]
+    targets_w_eos = torch.scatter(
+        targets_w_eos,
+        dim=-1,
+        index=target_lens.unsqueeze(-1).long(),
+        src=torch.full_like(target_lens.unsqueeze(-1), model.eos_idx),
+    )
+    target_lens_w_eos = target_lens + 1  # [B * beam]
+
     seq_log_prob = torch.gather(
-        log_probs, dim=-1, index=targets.unsqueeze(-1).long()
-    ).squeeze(
-        -1
-    )  # [B * beam, T]
+        log_probs, dim=-1, index=targets_w_eos.unsqueeze(-1).long()
+    ).squeeze(-1)  # [B * beam, T]
     seq_log_prob = torch.where(
         torch.arange(seq_log_prob.size(1), device=seq_log_prob.device).unsqueeze(0)
-        < target_lens.unsqueeze(1),
+        < target_lens_w_eos.unsqueeze(1),
         seq_log_prob,
         torch.zeros_like(seq_log_prob),
-        )
+    )
     seq_log_prob = torch.sum(seq_log_prob, dim=-1).view(
         batch_size, beam_size
     )  # [B, beam]
 
+    # DIMS
     beam_dim = Dim(beam_size, name="beam")
     vocab_dim = Dim(model.num_labels, name="vocab")
     lens_data = rf.convert_to_tensor(
@@ -99,6 +101,7 @@ def forward_step_v1(
     )
     lens_dim = Dim(lens_data, name="seq_len")
 
+    # OUTPUTS (tokens & scores)
     ctx = rf.get_run_ctx()
     targets = targets.view(batch_size, beam_size, -1)  # [B, beam, T]
     seq_targets_rf = rf.convert_to_tensor(
