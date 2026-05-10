d43cab0d883c7a5ef0b0af6f28d833a9b47fd16a
Albert Zeyer <albzey@gmail.com>
2026-05-10 14:35:20 +0200

    more

--- users/zeyer/experiments/exp2025_10_21_chunked_ctc.py
+++ users/zeyer/experiments/exp2025_10_21_chunked_ctc.py
@@ -316,6 +316,28 @@ def py():
         },
     )
 
+    # Dyn-v2: Try to make it faster.
+    train(
+        f"chunked-L{left_n * center_size}-C{center_size}-R{right_size}-v2.3-dynV2-rope-ctembed",
+        {
+            "model.enc_build_dict": rf.build_dict(
+                ChunkedConformerEncoderV2,
+                encoder_layer=rf.build_dict(ChunkedConformerEncoderLayerV2, self_att=ChunkedRotaryPosSelfAttentionV2),
+                chunk_size=center_size,
+                chunk_history_size=left_n * center_size,
+                chunk_lookahead_size=right_size,
+                chunk_size_train_pool=[center_size, center_size * 2, center_size * 4, center_size * 8] + [None] * 4,
+                chunk_history_size_train_pool=[left_n * center_size, left_n * center_size // 2, 0],
+                chunk_lookahead_size_train_pool=[right_size, right_size // 2, 0],
+                use_chunk_type_embedding=True,
+                version=3,
+            ),
+            "train.batch_size": bs * configs._batch_size_factor,
+            "train.max_seqs": max_seqs,
+            "lm_recog_extra.__serialization_version_stats": 2,
+        },
+    )
+
     # Overlapping chunks (chunk_num_overlaps=2)
     train(
         f"chunked-L{left_n * center_size}-C{center_size}-R{right_size}-v2.3-overlap",
