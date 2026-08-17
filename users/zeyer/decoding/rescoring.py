82c59012ed2ef71468930cbfa617b24d30a5f66f
Albert Zeyer <albzey@gmail.com>
2026-08-17 13:28:36 +0200

    model_checkpoint simplify (same sis hash), more generic (any type)

--- users/zeyer/decoding/rescoring.py
+++ users/zeyer/decoding/rescoring.py
@@ -137,7 +137,7 @@ def rescore(
             forward_post_config and forward_post_config.pop("__env_updates", None)
         )
     forward_job = ReturnnForwardJobV2(
-        model_checkpoint=model.checkpoint.path if model.checkpoint is not None else None,
+        model_checkpoint=model.checkpoint,
         returnn_config=_returnn_rescore_config(
             recog_output=recog_output,
             vocab_opts=vocab_opts,
